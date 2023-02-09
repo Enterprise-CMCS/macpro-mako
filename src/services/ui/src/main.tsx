e04d2f4a6eacdae88d2d5d4a5f0002c5d50a8792
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-02-09 15:14:16 -0500

    add react-location

--- src/services/ui/src/main.tsx
+++ src/services/ui/src/main.tsx
@@ -1,9 +1,16 @@
+import { Outlet, ReactLocation, Router } from "@tanstack/react-location";
+import { ReactLocationDevtools } from "@tanstack/react-location-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+const location = new ReactLocation();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router location={location} routes={[{ path: "/", element: <App /> }]}>
+      <ReactLocationDevtools initialIsOpen={false} />
+      <Outlet />
+    </Router>
   </React.StrictMode>
 );
