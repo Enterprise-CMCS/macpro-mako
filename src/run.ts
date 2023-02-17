440630a194b657ff19f3fb179f4d37a8946e64d4
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2023-02-17 14:43:11 -0500

    feat(silence-runner): Added a silenced parameter to runner and used it in base-update (#89)
    * Add a silenced parameter to runner and use it on adding remote to base in base-update functionality
    
    * improve cloud climate results by reducing code duplication
    
    * cleaning up per code climate's feedback
    
    * correct Boolean to boolean

--- src/run.ts
+++ src/run.ts
@@ -198,7 +198,7 @@ yargs(process.argv.slice(2))
   )
   .command(
     "base-update",
-    "this will upgrade your code to the latest version of the base template",
+    "this will update your code to the latest version of the base template",
     {},
     async () => {
       const addRemoteCommand = [
@@ -210,16 +210,20 @@ yargs(process.argv.slice(2))
       ];
 
       await runner.run_command_and_output(
-        "Upgrade from Base | adding remote",
+        "Update from Base | adding remote",
         addRemoteCommand,
         ".",
-        true
+        true,
+        {
+          stderr: true,
+          close: true,
+        }
       );
 
       const fetchBaseCommand = ["git", "fetch", "base"];
 
       await runner.run_command_and_output(
-        "Upgrade from Base | fetching base template",
+        "Update from Base | fetching base template",
         fetchBaseCommand,
         "."
       );
@@ -227,7 +231,7 @@ yargs(process.argv.slice(2))
       const mergeCommand = ["git", "merge", "base/production", "--no-ff"];
 
       await runner.run_command_and_output(
-        "Upgrade from Base | merging code from base template",
+        "Update from Base | merging code from base template",
         mergeCommand,
         ".",
         true
