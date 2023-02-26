5193f891dc9b38c0c1f0cbe600ea85cc5b6b02cb
Benjamin Paige <benjamin.paige@gmail.com>
2023-02-26 12:41:44 -0700

    Add libs file and tests

--- src/services/dashboard/handlers/templatizeCloudWatchDashboard.ts
+++ src/services/dashboard/handlers/templatizeCloudWatchDashboard.ts
@@ -7,13 +7,12 @@ import type {
 
 export const replaceStringValues = (
   string: string,
-  replacables: { [key: string]: string }
+  replacables: Record<string, string>
 ) => {
-  let result = string;
-
-  for (const [key, value] of Object.entries(replacables)) {
-    result = result.replaceAll(key, value);
-  }
+  const result = string.replace(
+    new RegExp(Object.keys(replacables).join("|"), "gi"),
+    (matched) => replacables[matched]
+  );
   return result;
 };
 
@@ -22,27 +21,24 @@ export const handler = async (
   _context: Context,
   _callback: APIGatewayProxyCallback
 ) => {
-  //environment variables passed to lambda from serverless.yml
-  const service = process.env.service!;
-  const accountId = process.env.accountId!;
-  const stage = process.env.stage!;
-  const region = process.env.region!;
-
-  const client = new CloudWatch({});
-  const dashboard = await client.getDashboard({
-    DashboardName: `${stage}-dashboard`,
-  });
-
-  const replacables = {
-    [accountId]: "${aws:accountId}",
-    [stage]: "${sls:stage}",
-    [region]: "${env:REGION_A}",
-    [service]: "${self:service}",
-  };
-
-  let templateJson = dashboard.DashboardBody!;
-
-  const results = replaceStringValues(templateJson, replacables);
-
-  return results;
+  try {
+    // Environment variables passed to lambda from serverless.yml
+    const { service, accountId, stage, region } = process.env;
+    const client = new CloudWatch({});
+    const dashboard = await client.getDashboard({
+      DashboardName: `${stage}-dashboard`,
+    });
+    const replacables = {
+      [accountId!]: "${aws:accountId}",
+      [stage!]: "${sls:stage}",
+      [region!]: "${env:REGION_A}",
+      [service!]: "${self:service}",
+    };
+    const templateJson = dashboard.DashboardBody!;
+    const results = replaceStringValues(templateJson, replacables);
+    return results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
--- src/services/dashboard/handlers/tests/templatizeCloudWatchDashboard.test.ts
+++ src/services/dashboard/handlers/tests/templatizeCloudWatchDashboard.test.ts
@@ -1,5 +1,20 @@
-import { replaceStringValues } from "../templatizeCloudWatchDashboard";
-import { it, describe, expect } from "vitest";
+import { it, describe, expect, vi, beforeEach } from "vitest";
+import { CloudWatch } from "@aws-sdk/client-cloudwatch";
+import type {
+  APIGatewayEvent,
+  APIGatewayProxyCallback,
+  Context,
+} from "aws-lambda";
+import { handler, replaceStringValues } from "../templatizeCloudWatchDashboard";
+
+// Mock the CloudWatch client
+vi.mock("@aws-sdk/client-cloudwatch", () => ({
+  CloudWatch: vi.fn().mockImplementation(() => ({
+    getDashboard: vi.fn().mockResolvedValue({
+      DashboardBody: "test-dashboard-body",
+    }),
+  })),
+}));
 
 describe("replaceStringValues", () => {
   it("replaces string with correct values", () => {
@@ -7,5 +22,36 @@ describe("replaceStringValues", () => {
     const replacables = { hello: "world", jim: "wally" };
     const response = replaceStringValues(string, replacables);
     expect(response).toBe("world wally");
+  });
+
+  it("should handle empty string", () => {
+    const string = "";
+    const replacables = { name: "Alice" };
+    expect(replaceStringValues(string, replacables)).toBe("");
+  });
+});
+
+describe("handler", () => {
+  const mockEvent: APIGatewayEvent = {} as APIGatewayEvent;
+  const mockContext: Context = {} as Context;
+  const mockCallback: APIGatewayProxyCallback = {} as APIGatewayProxyCallback;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+    process.env.service = "test-service";
+    process.env.accountId = "test-account-id";
+    process.env.stage = "test-stage";
+    process.env.region = "test-region";
+  });
+
+  it("should return the replaced dashboard body", async () => {
+    const result = await handler(mockEvent, mockContext, mockCallback);
+    expect(result).toBe("test-dashboard-body");
+    expect(CloudWatch).toHaveBeenCalledTimes(1);
+    expect(CloudWatch).toHaveBeenCalledWith({});
+    // expect(CloudWatch.getDashboard).toHaveBeenCalledTimes(1);
+    // expect(CloudWatch.prototype.getDashboard).toHaveBeenCalledWith({
+    //   DashboardName: "test-stage-dashboard",
+    // });
   });
 });
