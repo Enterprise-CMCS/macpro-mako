f13db42fb87b72dd4032a0b91c54a1dd2d5e9255
Ishmeal Baawuah <ibaawuah@ip-10-232-40-5.ec2.internal>
2023-02-28 16:35:08 -0500

    running pre-commit

--- src/services/database/handlers/seeddatabase.ts
+++ src/services/database/handlers/seeddatabase.ts
@@ -1,19 +1,35 @@
-const AWS = require("@aws-sdk/client-dynamodb");
-const response = require("cfn-response");
-const docClient = new AWS.DynamoDB.DocumentClient();
-exports.handler = function (event, context) {
-  console.log(JSON.stringify(event, null, 2));
-  var params = {
-    TableName: event.ResourceProperties.DynamoTableName,
-    Item: {
-      id: "abc123",
-    },
-  };
-  docClient.put(params, function (err, data) {
-    if (err) {
-      response.send(event, context, "FAILED", {});
-    } else {
-      response.send(event, context, "SUCCESS", {});
-    }
-  });
+// const AWS = require("@aws-sdk/client-dynamodb");
+// const response = require("cfn-response");
+// const docClient = new AWS.DynamoDB.DocumentClient();
+// exports.handler = function (event, context) {
+//   console.log(JSON.stringify(event, null, 2));
+//   var params = {
+//     TableName: event.ResourceProperties.DynamoTableName,
+//     Item: {
+//       id: "abc123",
+//     },
+//   };
+//   docClient.put(params, function (err, data) {
+//     if (err) {
+//       response.send(event, context, "FAILED", {});
+//     } else {
+//       response.send(event, context, "SUCCESS", {});
+//     }
+//   });
+// };
+
+import { send, SUCCESS, FAILED } from "cfn-response-async";
+
+exports.handler = async function (event, context) {
+  console.log("Request:", JSON.stringify(event, undefined, 2));
+  const responseData = {};
+  let responseStatus: any = SUCCESS;
+  try {
+    console.log("hello world");
+  } catch (error) {
+    console.error(error);
+    responseStatus = FAILED;
+  } finally {
+    await send(event, context, responseStatus, responseData, "static");
+  }
 };
