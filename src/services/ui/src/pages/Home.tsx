e3aa6243767541211fc4776ea97d16cc915672c6
Adewale Sangobiyi <asangobiyi@ip-10-232-53-72.ec2.internal>
2023-02-28 11:10:03 -0500

    feat(npm package): add npm package and header comp to project

--- src/services/ui/src/pages/Home.tsx
+++ src/services/ui/src/pages/Home.tsx
@@ -1,3 +1,105 @@
+import { ActionsMenu, Button, Header, Link, Logo, Footer } from "@enterprise-cmcs/macpro-ux-lib";
+
 export const Home = () => {
-  return <>Home Page</>;
+  return <>
+  <Header
+  headerLogo={<Link href="" title="Project Title"><Logo altText="Project Logo" source="static/media/cms_logo.765085cf.png"/></Link>}
+  navData={[
+    {
+      buttonText: 'Current Section',
+      columns: [
+        [
+          {
+            href: '',
+            target: '_blank',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          }
+        ],
+        [
+          {
+            href: '',
+            text: 'Navigational Link'
+          }
+        ],
+        [
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          }
+        ],
+        [
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          }
+        ]
+      ],
+      current: '[Circular]'
+    },
+    {
+      buttonText: 'Section',
+      columns: [
+        [
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          },
+          {
+            href: '',
+            text: 'Navigational Link'
+          }
+        ]
+      ]
+    }
+  ]}
+  secondaryComponent={<ActionsMenu links={[{href: '', iconName: 'person', onClick: function noRefCheck(){}, target: '_blank', text: 'Manage Profile'}, {href: '', iconName: 'people', onClick: function noRefCheck(){}, target: '_blank', text: 'Request Role Change'}, {href: '', iconName: 'logout', onClick: function noRefCheck(){}, target: '_blank', text: 'Log Out'}]} name="My Account"/>}
+/>
+<Footer
+  emailAddress="storybook-test@cms.hhs.gov"
+/>
+  </>
 };
