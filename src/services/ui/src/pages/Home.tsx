b0a4b0bb6633347f65d394c72d73e84019e24cfb
Adewale Sangobiyi <asangobiyi@adewale-mac.mynetworksettings.com>
2023-03-01 17:10:47 -0500

    feat(npm package): add npm package and header comp to project

--- src/services/ui/src/pages/Home.tsx
+++ src/services/ui/src/pages/Home.tsx
@@ -1,113 +1,144 @@
-import { 
+import {
   ActionsMenu,
   Button,
   Header,
   Link,
   Logo,
   Footer,
-  UsaBanner
+  UsaBanner,
 } from "@enterprise-cmcs/macpro-ux-lib";
 import cmsLogo from "@enterprise-cmcs/macpro-ux-lib/build/assets/img/logos/cms_logo.svg";
 
 
 export const Home = () => {
-  return <>
-  <Header
-  headerLogo={<Link href="" title="Project Title"><Logo altText="Project Logo" source={cmsLogo}/></Link>}
-  navData={[
-    {
-      buttonText: 'Current Section',
-      columns: [
-        [
-          {
-            href: '',
-            text: 'Navigational Link'
-          },
-          {
-            href: '',
-            text: 'Navigational Link'
-          },
-          {
-            href: '',
-            text: 'Navigational Link'
-          },
-          {
-            href: '',
-            text: 'Navigational Link'
-          }
+  return (
+    <>
+    <Header
+      headerLogo={
+      <Link href="" title="Project Title">
+        <Logo altText="Project Logo" source={cmsLogo}/>
+        </Link>
+      }
+      navData={[
+        {
+          buttonText: 'Current Section',
+          columns: [
+            [
+              {
+                href: "",
+                text: 'Navigational Link',
+              },
+              {
+                href: '',
+                text: 'Navigational Link',
+              },
+              {
+                href: '',
+                text: 'Navigational Link',
+              },
+              {
+                href: '',
+                text: 'Navigational Link',
+              }
+            ],
+            [
+              {
+                href: '',
+                text: 'Navigational Link',
+              }
+            ],
+            [
+              {
+                href: '',
+                text: 'Navigational Link',
+              },
+              {
+                href: '',
+                text: 'Navigational Link',
+              },
+              {
+                href: '',
+                text: 'Navigational Link',
+              }
+            ],
+            [
+              {
+                href: '',
+                text: 'Navigational Link',
+              },
+              {
+                href: '',
+                text: 'Navigational Link',
+              },
+           ],
         ],
-        [
-          {
-            href: '',
-            text: 'Navigational Link'
-          }
-        ],
-        [
-          {
-            href: '',
-            text: 'Navigational Link'
-          },
-          {
-            href: '',
-            text: 'Navigational Link'
-          },
-          {
-            href: '',
-            text: 'Navigational Link'
-          }
-        ],
-        [
-          {
-            href: '',
-            text: 'Navigational Link'
-          },
-          {
-            href: '',
-            text: 'Navigational Link'
-          }
-        ]
-      ],
     },
     {
       buttonText: 'Section',
       columns: [
         [
           {
-            href: '',
-            text: 'Navigational Link'
+            href: "",
+            text: 'Navigational Link',
           },
           {
-            href: '',
-            text: 'Navigational Link'
+            href: "",
+            text: 'Navigational Link',
           },
           {
-            href: '',
-            text: 'Navigational Link'
+            href: "",
+            text: 'Navigational Link',
           },
           {
-            href: '',
-            text: 'Navigational Link'
+            href: "",
+            text: 'Navigational Link',
           },
           {
-            href: '',
-            text: 'Navigational Link'
+            href: "",
+            text: 'Navigational Link',
           },
           {
-            href: '',
-            text: 'Navigational Link'
+            href: "",
+            text: 'Navigational Link',
           },
           {
-            href: '',
-            text: 'Navigational Link'
+            href: "",
+            text: 'Navigational Link',
           }
         ]
       ]
     }
   ]}
-  secondaryComponent={<ActionsMenu links={[{href: '', iconName: 'person', onClick: function noRefCheck(){}, target: '_blank', text: 'Manage Profile'}, {href: '', iconName: 'people', onClick: function noRefCheck(){}, target: '_blank', text: 'Request Role Change'}, {href: '', iconName: 'logout', onClick: function noRefCheck(){}, target: '_blank', text: 'Log Out'}]} name="My Account"/>}
-/>
-<Footer
-  emailAddress="storybook-test@cms.hhs.gov"
-/>
-  </>
+          secondaryComponent={
+           <ActionsMenu
+           links={[
+             {
+               href: "", 
+               iconName: "person",
+               onClick: function noRefCheck(){}, 
+               target: "_blank", 
+               text: "Manage Profile",
+             }, 
+             {
+               href: "",
+               iconName: "people",
+               onClick: function noRefCheck(){},
+               target: "_blank",
+               text: 'Request Role Change',
+             }, 
+             {
+               href: "",
+               iconName: "logout", 
+               onClick: function noRefCheck(){}, 
+               target: "_blank",
+               text: "Log Out"
+             },
+           ]} 
+           name="My Account"
+         />
+       }
+     />
+     <Footer emailAddress="storybook-test@cms.hhs.gov" />
+   </>
+  );
 };
