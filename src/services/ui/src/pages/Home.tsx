304b3a802ccdc28dea7b8faaf8e4f1cb7215dd8e
Adewale Sangobiyi <asangobiyi@adewale-mac.mynetworksettings.com>
2023-03-02 08:11:35 -0500

    feat(npm package): add npm package and header comp to project

--- src/services/ui/src/pages/Home.tsx
+++ src/services/ui/src/pages/Home.tsx
@@ -11,132 +11,132 @@ import cmsLogo from "@enterprise-cmcs/macpro-ux-lib/build/assets/img/logos/cms_l
 export const Home = () => {
   return (
     <>
-    <Header
-      headerLogo={
-        <Link href="" title="Project Title">
-          <Logo altText="Project Logo" source={cmsLogo} />
-        </Link>
-      }
-      navData={[
-        {
-          buttonText: "Current Section",
-          columns: [
-            [
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
+      <Header
+        headerLogo={
+          <Link href="" title="Project Title">
+            <Logo altText="Project Logo" source={cmsLogo} />
+          </Link>
+        }
+        navData={[
+          {
+            buttonText: "Current Section",
+            columns: [
+              [
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+              ],
+              [
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+              ],
+              [
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+              ],
+              [
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+              ],
             ],
-            [
-              {
-                href: "",
-                text: "Navigational Link",
-              },
+          },
+          {
+            buttonText: "Section",
+            columns: [
+              [
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+                {
+                  href: "",
+                  text: "Navigational Link",
+                },
+              ],
             ],
-            [
+          },
+        ]}
+        secondaryComponent={
+          <ActionsMenu
+            links={[
               {
                 href: "",
-                text: "Navigational Link",
+                iconName: "person",
+                onClick: function noRefCheck() {},
+                target: "_blank",
+                text: "Manage Profile",
               },
               {
                 href: "",
-                text: "Navigational Link",
+                iconName: "people",
+                onClick: function noRefCheck() {},
+                target: "_blank",
+                text: "Request Role Change",
               },
               {
                 href: "",
-                text: "Navigational Link",
+                iconName: "logout",
+                onClick: function noRefCheck() {},
+                target: "_blank",
+                text: "Log Out",
               },
-            ],
-            [
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-            ],
-          ],
-        },
-        {
-          buttonText: "Section",
-          columns: [
-            [
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-              {
-                href: "",
-                text: "Navigational Link",
-              },
-            ],
-          ],
-        },
-      ]}
-      secondaryComponent={
-        <ActionsMenu
-          links={[
-            {
-              href: "",
-              iconName: "person",
-              onClick: function noRefCheck() {},
-              target: "_blank",
-              text: "Manage Profile",
-            },
-            {
-              href: "",
-              iconName: "people",
-              onClick: function noRefCheck() {},
-              target: "_blank",
-              text: "Request Role Change",
-            },
-            {
-              href: "",
-              iconName: "logout",
-              onClick: function noRefCheck() {},
-              target: "_blank",
-              text: "Log Out",
-            },
-          ]}
-          name="My Account"
-        />
-      }
-    />
-  <Footer emailAddress="storybook-test@cms.hhs.gov" />
-</>
-);
+            ]}
+            name="My Account"
+          />
+        }
+      />
+      <Footer emailAddress="storybook-test@cms.hhs.gov" />
+    </>
+  );
 };
