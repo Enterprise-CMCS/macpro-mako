caacc889eda8e96d64b9cf83e4659c023047492a
Ishmeal Baawuah <ibaawuah@Ishmeals-MBP.fios-router.home>
2023-03-16 10:17:13 -0400

    forcing the custom init function to run on each push

--- src/services/database/handlers/seeddatabase.ts
+++ src/services/database/handlers/seeddatabase.ts
@@ -1,16 +1,23 @@
 import { send, SUCCESS, FAILED } from "cfn-response-async";
 import { putItem } from "../../../libs";
-import * as items from "../items.json";
+import axios from "axios";
 
 exports.handler = async function (event: any, context: any) {
   console.log("Request:", JSON.stringify(event, undefined, 2));
   const responseData = {};
   let responseStatus: any = SUCCESS;
   try {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const response = await axios.get(url);
+    const items = response.data.map((item: any) => ({
+      PK: item.id,
+      SK: item.title,
+      body: item.body,
+    }));
     for await (const item of items) {
-      putItem({
+      await putItem({
         tableName: event.ResourceProperties.DynamoTableName,
-        item: { PK: item.recordId, SK: item.state },
+        item,
       });
     }
   } catch (error) {
