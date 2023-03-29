30ae6846449c9da1cd8304e6fabd888d41fabadc
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-03-29 13:26:50 -0400

    add a few crud operations

--- src/services/api/handlers/createPost.ts
+++ src/services/api/handlers/createPost.ts
@@ -1,11 +1,17 @@
 import type { APIGatewayEvent } from "aws-lambda";
 import { response } from "../libs/handler";
+import { post, postSchema } from "../models/Post";
+import { PostService } from "../services/postService";
 
 export const createPost = async (event: APIGatewayEvent) => {
   try {
+    const validPost = postSchema.parse(event.body);
+
+    const newPost = await new PostService(post).createPost(validPost);
+
     return response({
       statusCode: 201,
-      body: { post: {} },
+      body: newPost,
     });
   } catch (error) {
     return response({
