836b1e18138faf0e91381e79d0afe64d0bb4aa8f
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-04-10 10:01:07 -0400

    Revert "set things back"
    This reverts commit 85e4b9c43d27621d2295e831936908d91cadf9e9.

--- src/services/ui/next.config.js
+++ src/services/ui/next.config.js
@@ -1,6 +1,13 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {};
+const nextConfig = {
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/posts": { page: "/posts" },
+    };
+  },
+};
 
 module.exports = nextConfig;
