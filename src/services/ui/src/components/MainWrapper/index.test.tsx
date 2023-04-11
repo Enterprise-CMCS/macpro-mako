0e1b57f8fb5e1e15de1948c33d9b657ba16d73f2
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-04-11 14:06:52 -0400

    get test working in ui service

--- src/services/ui/src/components/MainWrapper/index.test.tsx
+++ src/services/ui/src/components/MainWrapper/index.test.tsx
@@ -1,22 +1,25 @@
-/**
- * @vitest-environment jsdom
- */
-
-import { TestWrapper } from "../../lib/TestWrapper";
 import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import MainWrapper from ".";
 
 describe("Header test", () => {
   test("should show main wrapper text", () => {
-    render(
-      <TestWrapper>
-        <MainWrapper>
-          <div>Test Children Content</div>
-        </MainWrapper>
-      </TestWrapper>
+    const router = createMemoryRouter(
+      [
+        {
+          path: "/",
+          element: <MainWrapper />,
+          children: [{ path: "test", element: <h3>testing</h3> }],
+        },
+      ],
+      {
+        initialEntries: ["/", "/test"],
+        initialIndex: 1,
+      }
     );
+    render(<RouterProvider router={router} />);
 
-    expect(screen.getByText(/Test Children Content/i)).toBeDefined();
+    expect(screen.getByText(/test/i)).toBeDefined();
   });
 });
