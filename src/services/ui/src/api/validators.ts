9dfd365171f9e626f33df2ade32c598c83ab615d
Benjamin Paige <benjamin.paige@gmail.com>
2023-04-21 15:33:59 -0600

    Add resolver to issue form

--- src/services/ui/src/api/validators.ts
+++ src/services/ui/src/api/validators.ts
@@ -13,9 +13,27 @@ const createPostSchema = z.object({
   post: z.string(),
 });
 
+export const issueSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
+  type: z
+    .string()
+    .refine((val) => ["look", "functionality", "other"].includes(val), {
+      message: 'Type must be one of "look", "functionality", or "other"',
+    }),
+  priority: z
+    .string()
+    .refine((val) => ["low", "medium", "high"].includes(val), {
+      message: 'Priority must be one of "low", "medium", or "high"',
+    }),
+  resolved: z.boolean().default(false),
+});
+
 export type Post = z.infer<typeof postSchema>;
 export type CreatePost = z.infer<typeof createPostSchema>;
+export type IssueSchema = z.infer<typeof issueSchema>;
 
 export const validatePost = postSchema.parse;
 export const validatePosts = z.array(postSchema).parse;
 export const validateCreatePost = createPostSchema.parse;
+export const validateIssue = issueSchema.parse;
--- src/services/ui/src/pages/issues.tsx
+++ src/services/ui/src/pages/issues.tsx
@@ -1,112 +1,120 @@
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as UI from "@enterprise-cmcs/macpro-ux-lib";
+import { IssueSchema, issueSchema } from "../api/validators";
 
 export function Issues() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IssueSchema>({ resolver: zodResolver(issueSchema) });
 
-  const onSubmit = (data) => {
+  const onSubmit = (data: IssueSchema) => {
     console.log(data);
   };
+
+  console.log({ errors });
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="max-w-screen-lg mt-4 mx-auto p-8"
+      className="max-w-screen-lg mx-auto px-8"
     >
-      <div className="mb-4">
-        <label htmlFor="title" className="block text-gray-700 font-bold mb-2">
-          Title
-        </label>
-        <input
-          {...register("title", { required: true })}
-          id="title"
-          type="text"
-          className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-            errors.title ? "border-red-500" : ""
-          }`}
-        />
-        {errors.title && (
-          <p className="text-red-500 text-xs mt-1">Title is required</p>
-        )}
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="description"
-          className="block text-gray-700 font-bold mb-2"
-        >
-          Description
-        </label>
-        <textarea
-          {...register("description", { required: true })}
-          id="description"
-          className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-            errors.description ? "border-red-500" : ""
-          }`}
-        />
-        {errors.description && (
-          <p className="text-red-500 text-xs mt-1">Description is required</p>
-        )}
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="priority"
-          className="block text-gray-700 font-bold mb-2"
-        >
-          Priority
-        </label>
-        <select
-          {...register("priority")}
-          id="priority"
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        >
-          <option value="" disabled selected>
-            -- select --
-          </option>
-          <option value="high">High</option>
-          <option value="medium">Medium</option>
-          <option value="low">Low</option>
-        </select>
-      </div>
-      <div className="mb-4">
-        <label htmlFor="type" className="block text-gray-700 font-bold mb-2">
-          Type
-        </label>
-        <select
-          {...register("type")}
-          id="type"
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        >
-          <option value="" disabled selected>
-            -- select --
-          </option>
-          <option value="look">Look</option>
-          <option value="functionality">Functionality</option>
-          <option value="other">Other</option>
-        </select>
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="resolved"
-          className="block text-gray-700 font-bold mb-2"
-        >
+      <div className="max-w-sm">
+        <UI.Typography as="h1" size="lg">
+          Add Issue
+        </UI.Typography>
+        <div className="mb-4">
+          <label htmlFor="title">
+            <UI.Typography as="p" size="sm" className="mb-2">
+              Title
+            </UI.Typography>
+          </label>
           <input
-            {...register("resolved")}
-            id="resolved"
-            type="checkbox"
-            className="mr-2 leading-tight"
+            {...register("title")}
+            id="title"
+            type="text"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.title ? "border-red-500" : ""
+            }`}
           />
-          <span className="text-sm">Resolved</span>
-        </label>
+          {errors.title && (
+            <p className="text-red-500 text-xs mt-1">{errors.title.message}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="description">
+            <UI.Typography as="p" size="sm" className="mb-2">
+              Description
+            </UI.Typography>
+          </label>
+          <textarea
+            {...register("description")}
+            id="description"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.description ? "border-red-500" : ""
+            }`}
+          />
+          {errors.description && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.description.message}
+            </p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="priority">
+            <UI.Typography as="p" size="sm" className="mb-2">
+              Priority
+            </UI.Typography>
+          </label>
+          <select
+            {...register("priority")}
+            id="priority"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.priority ? "border-red-500" : ""
+            }`}
+          >
+            <option value="" disabled selected>
+              -- select --
+            </option>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+          {errors.priority && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.priority.message}
+            </p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="type">
+            <UI.Typography as="p" size="sm" className="mb-2">
+              Type
+            </UI.Typography>
+          </label>
+          <select
+            {...register("type")}
+            defaultValue=""
+            id="type"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.type ? "border-red-500" : ""
+            }`}
+          >
+            <option value="" disabled>
+              -- select --
+            </option>
+            <option value="look">Look</option>
+            <option value="functionality">Functionality</option>
+            <option value="other">Other</option>
+          </select>
+          {errors.type && (
+            <p className="text-red-500 text-xs mt-1">{errors.type.message}</p>
+          )}
+        </div>
+        <UI.Button buttonText="Submit" type="submit" className="float-right" />
       </div>
-      <button
-        type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        Submit
-      </button>
     </form>
   );
 }
