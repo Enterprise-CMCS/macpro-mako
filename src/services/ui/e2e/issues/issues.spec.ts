11cb0f1e3c17b59d0514e54842d9f94596cf737a
Benjamin Paige <benjamin.paige@gmail.com>
2023-05-23 15:07:05 -0600

    add crud issue testing

--- src/services/ui/e2e/issues/issues.spec.ts
+++ src/services/ui/e2e/issues/issues.spec.ts
@@ -17,7 +17,7 @@ test("create issue should require description", async ({ page }) => {
   await expect(page.getByText("Description is required")).toBeVisible();
 });
 
-test("should be able to create an issue", async ({ page }) => {
+test("should be able to create and delete an issue", async ({ page }) => {
   const testDesc = uuidv4();
   goToIssuesPage(page);
 
@@ -29,4 +29,16 @@ test("should be able to create an issue", async ({ page }) => {
 
   await expect(page).toHaveURL(/.*issues/);
   await expect(page.getByRole("cell", { name: testDesc })).toBeVisible();
+
+  // this next bit is tricky because we need to find the row containing the text we just entered and then find the delete button in the same row.
+  // but we can do it using xpath selectors
+
+  // Select the "Delete" button using a single XPath selector
+  const buttonSelector = `//td[text()='${testDesc}']/following-sibling::td/button[@aria-label='Delete button']`;
+
+  // Click the "Delete" button
+  await page.click(buttonSelector);
+
+  // the row should be deleted
+  await expect(page.getByRole("cell", { name: testDesc })).not.toBeVisible();
 });
