dcd6f77def4722d750fe65b49c5603686d052f35
Benjamin Paige <benjamin.paige@gmail.com>
2023-06-07 09:44:03 -0600

    uncomment browsers

--- src/services/ui/playwright.config.ts
+++ src/services/ui/playwright.config.ts
@@ -25,20 +25,20 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
+  // Note: we can test on multiple browsers and resolutions defined here
   projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
 
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 });
