4c9e9b3a5c4f6dbfecca24ccaa6fc11ab7db3747
Mike Dial <michael.dial.4@gmail.com>
2023-06-12 12:02:47 -0400

    Functional login and out

--- src/services/ui/src/components/MainWrapper/index.tsx
+++ src/services/ui/src/components/MainWrapper/index.tsx
@@ -1,12 +1,31 @@
 import * as UI from "@enterprise-cmcs/macpro-ux-lib";
 import cmsLogo from "@enterprise-cmcs/macpro-ux-lib/build/assets/img/logos/cms_logo.svg";
 import { Outlet, useLoaderData } from "react-router-dom";
-
+import { Auth } from 'aws-amplify';
 
 export default function MainWrapper() {
-  const albums = useLoaderData();
-  console.log(albums);
-  console.log("SULLY");
+  const albums = useLoaderData() as any;
+
+  async function handleLogin(event: any) {
+    event.preventDefault();
+    try {
+      const authConfig = Auth.configure();
+      const { domain, redirectSignIn, responseType } = authConfig.oauth as any;
+      const clientId = authConfig.userPoolWebClientId;
+      const url = `https://${domain}/oauth2/authorize?redirect_uri=${redirectSignIn}&response_type=${responseType}&client_id=${clientId}`;
+      window.location.assign(url);
+    } catch (e) {
+      throw (e);
+    }
+  }
+
+  async function handleLogout(event: any) {
+    event.preventDefault();
+    await Auth.signOut();
+
+    // history.push("/");
+  }
+
   return (
     <div
       style={{
@@ -47,29 +66,47 @@ export default function MainWrapper() {
           },
         ]}
         secondaryComponent={
-          <UI.ActionsMenu
-            links={[
-              {
-                href: "",
-                iconName: "person",
-                target: "_blank",
-                text: "Manage Profile",
-              },
-              {
-                href: "",
-                iconName: "people",
-                target: "_blank",
-                text: "Request Role Change",
-              },
-              {
-                href: "",
-                iconName: "logout",
-                target: "_blank",
-                text: "Log Out",
-              },
-            ]}
-            name="My Account"
-          />
+          <>
+            {albums.spiderman ?
+              <UI.ActionsMenu
+                links={[
+                  {
+                    href: "",
+                    iconName: "person",
+                    target: "_blank",
+                    text: "Manage Profile",
+                  },
+                  {
+                    href: "",
+                    iconName: "people",
+                    target: "_blank",
+                    text: "Request Role Change",
+                  },
+                  {
+                    href: "",
+                    iconName: "logout",
+                    ///@ts-ignore
+                    onClick: (e) => handleLogout(e),
+                    text: "Log Out",
+                  },
+                ]}
+                name="My Account"
+              /> : <UI.ActionsMenu
+                links={[
+                  {
+                    href: "",
+                    iconName: "login",
+                    ///@ts-ignore
+                    onClick: (e) => handleLogin(e),
+                    text: "Login with IDM",
+                  },
+                ]}
+                name="Login"
+              />
+
+            }
+
+          </>
         }
       />
       <main className="padding-x-5" style={{ flex: 1 }}>
