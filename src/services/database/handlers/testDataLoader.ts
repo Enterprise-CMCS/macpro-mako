c0c94209dd4a1a86d966667bc2a91562774e7118
Mike Dial <michael.dial.4@gmail.com>
2023-06-12 20:06:21 -0400

    linting

--- src/services/database/handlers/testDataLoader.ts
+++ src/services/database/handlers/testDataLoader.ts
@@ -1,16 +1,16 @@
 import * as dynamodb from "../../../libs/dynamodb-lib";
-import { generateMock } from '@anatine/zod-mock';
+import { generateMock } from "@anatine/zod-mock";
 import { issueSchema } from "shared-types";
 exports.handler = async () => {
   try {
     if (!process.env.tableName) {
       throw "ERROR:  env variable 'tableName' should exist";
     }
-    let tableName = process.env.tableName;
+    const tableName = process.env.tableName;
     console.log("Putting test data...");
 
     for (let i = 0; i < 5; i++) {
-      let mockData = generateMock(issueSchema, {
+      const mockData = generateMock(issueSchema, {
         seed: 100 + i,
       });
       await dynamodb.putItem({
