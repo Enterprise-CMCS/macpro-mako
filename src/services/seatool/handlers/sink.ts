82bd1bb8c5ad7c5d66d473c35338efbaad411ce5
Mike Dial <michael.dial.4@gmail.com>
2023-06-14 09:34:51 -0400

    go back to latest

--- src/services/seatool/handlers/sink.ts
+++ src/services/seatool/handlers/sink.ts
@@ -2,9 +2,4 @@ import { Handler } from 'aws-lambda';
 
 export const handler: Handler = async (event) => {
   console.log(JSON.stringify(event, null, 2));
-  // for (const [topic, eventArray] of Object.entries(event.records)) {
-  //   console.log(`Topic ${topic}:`);
-  //   console.log(eventArray);
-  //   console.log('end');
-  // }
 };
