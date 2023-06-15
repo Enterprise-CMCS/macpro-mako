5b4ecc11b2c84b8ab6479d4268f22f32ac9b4bf8
Benjamin Paige <benjamin.paige@gmail.com>
2023-06-15 12:53:04 -0600

    update seatool-sink funciton

--- src/services/seatool/handlers/sink.ts
+++ src/services/seatool/handlers/sink.ts
@@ -1,5 +1,32 @@
-import { Handler } from 'aws-lambda';
+import { Handler } from "aws-lambda";
+import { putItem } from "../../../libs";
+import { decode } from "base-64";
 
 export const handler: Handler = async (event) => {
-  console.log(JSON.stringify(event, null, 2));
+  const records: { id: string; value: any }[] = [];
+  for (const key in event.records) {
+    event.records[key].forEach(
+      ({ key, value }: { key: string; value: string }) => {
+        // TODO: if value is null, delete instead of put
+        records.push({
+          id: JSON.parse(decode(key)),
+          ...JSON.parse(decode(value)),
+        });
+      }
+    );
+  }
+  try {
+    if (!process.env.tableName) {
+      throw "process.env.tableName cannot be undefined";
+    }
+
+    for await (const item of records) {
+      putItem({
+        tableName: process.env.tableName,
+        item,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
