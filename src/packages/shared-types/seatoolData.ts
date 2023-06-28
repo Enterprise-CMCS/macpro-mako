097d04a2ec457d0af28780498b79b3ecacc190ed
Benjamin Paige <benjamin.paige@gmail.com>
2023-06-27 20:57:02 -0600

    update types

--- src/packages/shared-types/seatoolData.ts
+++ src/packages/shared-types/seatoolData.ts
@@ -1,6 +1,6 @@
-export interface SeatoolData {
+export type SeatoolData = {
   ID: string;
   SUBMISSION_DATE: number;
   PLAN_TYPE: string; // should be enum
   STATE_CODE: string; // should be enum
-}
+};
--- src/services/ui/src/api/useGetSeatool.ts
+++ src/services/ui/src/api/useGetSeatool.ts
@@ -1,14 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import { API } from "aws-amplify";
 import { SeatoolData, ReactQueryApiError } from "shared-types";
 
-export const getSeaToolData = async (stateCode: string) => {
+export const getSeaToolData = async (
+  stateCode: string
+): Promise<SeatoolData[]> => {
   const seaToolData = await API.get("issues", `/seatool/${stateCode}`, {});
 
   return seaToolData;
 };
 
-export const useGetSeatool = (stateCode: string, options?: any) =>
+export const useGetSeatool = (
+  stateCode: string,
+  options?: UseQueryOptions<SeatoolData[], ReactQueryApiError>
+) =>
   useQuery<SeatoolData[], ReactQueryApiError>(["seatool", stateCode], {
     queryFn: () => getSeaToolData(stateCode),
     ...options,
