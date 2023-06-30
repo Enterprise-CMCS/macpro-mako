c00c99d3fd504ded3ac676239e18a5ee8a436f74
Mike Dial <michael.dial.4@gmail.com>
2023-06-30 15:32:24 -0400

    getting consumer group ids

--- src/services/seatool/handlers/rebuildTable.ts
+++ src/services/seatool/handlers/rebuildTable.ts
@@ -1,15 +1,48 @@
 import { Handler } from "aws-lambda";
 // import { deleteItem } from "../../../libs";
-
-export const handler: Handler = async (event) => {
-  console.log(JSON.stringify(event, null, 2));
+// import { Kafka, ConfigResourceTypes } from "kafkajs";
+import { LambdaClient, ListEventSourceMappingsCommand } from "@aws-sdk/client-lambda"
+export const handler: Handler = async () => {
   try {
-    console.log("hey ma");
     if (!process.env.tableName) {
       throw "process.env.tableName cannot be undefined";
     }
+
+    if (!process.env.functions) {
+      throw "process.env.functions cannot be undefined";
+    }
+
+    let functions = process.env.functions.split(',');
+    let consumerGroupIds = [];
+    for (const functionName of functions){
+      consumerGroupIds.push(...(await getConsumerGroupIdsForFunction(functionName)));
+    }
+    console.log(consumerGroupIds);
+
+
+    // const kafka = new Kafka({
+    //   clientId: "createTopics",
+    //   brokers: brokers,
+    //   ssl: true,
+    // });
+    // var admin = kafka.admin();
+
+
   } catch (error) {
     console.error(error);
     throw (error);
   }
 };
+
+async function getConsumerGroupIdsForFunction(functionName: string) {
+  const lambdaClient = new LambdaClient({});
+  const response = await lambdaClient.send((new ListEventSourceMappingsCommand({ FunctionName: functionName })));
+  let consumerGroupIds = []
+  for(const eventSourceMapping of response.EventSourceMappings || []) {
+    if(eventSourceMapping.SelfManagedKafkaEventSourceConfig){
+      consumerGroupIds.push(eventSourceMapping.SelfManagedKafkaEventSourceConfig.ConsumerGroupId);
+    }
+  }
+  return consumerGroupIds
+
+}
