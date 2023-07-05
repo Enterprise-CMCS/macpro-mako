c0c77c5897baa4658a8694b9d89c322d81d63813
Mike Dial <michael.dial.4@gmail.com>
2023-07-05 08:50:12 -0400

    bleh

--- src/services/seatool/handlers/rebuildTable.ts
+++ src/services/seatool/handlers/rebuildTable.ts
@@ -1,68 +1,90 @@
 import { Handler } from "aws-lambda";
 // import { deleteItem } from "../../../libs";
-import { Kafka, ConfigResourceTypes } from "kafkajs";
-import { LambdaClient, ListEventSourceMappingsCommand, UpdateEventSourceMappingCommand } from "@aws-sdk/client-lambda"
-export const handler: Handler = async () => {
-  try {
-    if (!process.env.tableName) {
-      throw "process.env.tableName cannot be undefined";
-    }
+import { Kafka } from "kafkajs";
+import { LambdaClient, ListEventSourceMappingsCommand, UpdateEventSourceMappingCommand } from "@aws-sdk/client-lambda";
 
+export const toggleTriggers: Handler = async () => {
+  try {
     if (!process.env.functions) {
       throw "process.env.functions cannot be undefined";
     }
-
-    let functions = process.env.functions.split(',');
-    let triggerInfo = new Array();
-    
-    // First, disable all triggers, and collect the consumerGroupIds
-    for (const functionName of functions){
-      console.log(`Disabling all Kafka triggers for function:  ${functionName}`);
-      triggerInfo.push(...(await toggleTriggers(functionName, false)));
+    const enabled = (process.env.enabled === "true");
+    for (const functionName of process.env.functions.split(',')){
+      const lambdaClient = new LambdaClient({});
+      const response = await lambdaClient.send((new ListEventSourceMappingsCommand({ FunctionName: functionName })));
+      for(const eventSourceMapping of response.EventSourceMappings || []) {
+        if(eventSourceMapping.SelfManagedKafkaEventSourceConfig){
+          console.log(`Disabling all Kafka triggers for function:  ${functionName}`);
+          await lambdaClient.send((new UpdateEventSourceMappingCommand({
+            UUID: eventSourceMapping.UUID,
+            Enabled: enabled,
+          })));
+        }
+      }
     }
-    console.log(`Found consumer group IDs:  ${triggerInfo.map(a => a.groupId)}`);
-    
-    // Second, wait until the consumer group ids are inactive
+  } catch(error) {
+    console.error(error);
+    throw (error);
+  }
+}
+
+export const getConsumerGroupStatus: Handler = async () => {
+  try {
+    if (!process.env.functions) {
+      throw "process.env.functions cannot be undefined";
+    }
+    let triggerInfo = new Array();
+    for (const functionName of process.env.functions.split(',')){
+      console.log(`Getting consumer groups for function:  ${functionName}`);
+      triggerInfo.push((await getConsumerGroupInfo(functionName)));
+    }
     const kafka = new Kafka({
       clientId: "consumerGroupResetter",
       brokers: process.env.brokerString?.split(',') || [],
       ssl: true,
     });
     var admin = kafka.admin();
-    while(true) {
-      let info = await admin.describeGroups(triggerInfo.map(a => a.groupId));
-      let statuses = info.groups.map(a => a.state.toString());
-      console.log(statuses);
-      if(!statuses.includes('Stable')){
-        console.log("All consumer groups are inactive");
-        break;
-      }
-      await new Promise(r => setTimeout(r, 10000));
-    }
+    let info = await admin.describeGroups(triggerInfo.map(a => a.groupId));
+    let statuses = info.groups.map(a => a.state.toString());
+    console.log(statuses);
+    await admin.disconnect();
+    return statuses;
+  } catch(error) {
+    console.error(error);
+    throw (error);
+  }
+}
 
-    // Third, reset the consumer group to the earliest offset
+export const resetConsumerGroups: Handler = async () => {
+  try {
+    if (!process.env.functions) {
+      throw "process.env.functions cannot be undefined";
+    }
+    let triggerInfo = new Array();
+    for (const functionName of process.env.functions.split(',')){
+      console.log(`Getting consumer groups for function:  ${functionName}`);
+      triggerInfo.push((await getConsumerGroupInfo(functionName)));
+    }
+    const kafka = new Kafka({
+      clientId: "consumerGroupResetter",
+      brokers: process.env.brokerString?.split(',') || [],
+      ssl: true,
+    });
+    var admin = kafka.admin();
     for (const trigger of triggerInfo){
       for (const topic of trigger.topics) {
         console.log(`Resetting group ${trigger.groupId} for topic ${topic}`);
         await admin.resetOffsets({ groupId: trigger.groupId, topic, earliest: true });
       }
     }
-
     await admin.disconnect();
-
-    // Fourth, enable all triggers
-    for (const functionName of functions){
-      console.log(`Enabling all Kafka triggers for function:  ${functionName}`);
-      await toggleTriggers(functionName, true);
-    }
-
-  } catch (error) {
+  } catch(error) {
     console.error(error);
     throw (error);
   }
-};
+}
 
-async function toggleTriggers(functionName: string, enabled: boolean) {
+async function getConsumerGroupInfo(functionName: string){
   const lambdaClient = new LambdaClient({});
   const response = await lambdaClient.send((new ListEventSourceMappingsCommand({ FunctionName: functionName })));
   let triggerInfo = []
@@ -72,10 +94,6 @@ async function toggleTriggers(functionName: string, enabled: boolean) {
         groupId: eventSourceMapping.SelfManagedKafkaEventSourceConfig.ConsumerGroupId,
         topics: eventSourceMapping.Topics
       });
-      await lambdaClient.send((new UpdateEventSourceMappingCommand({
-        UUID: eventSourceMapping.UUID,
-        Enabled: enabled
-      })));
     }
   }
   return triggerInfo;
