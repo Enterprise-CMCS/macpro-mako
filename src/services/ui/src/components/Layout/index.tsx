0eddffa85f14916ee4fb8c0dd9fe42147a50f6b4
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-07-17 14:48:12 -0400

    add mobile navigation

--- src/services/ui/src/components/Layout/index.tsx
+++ src/services/ui/src/components/Layout/index.tsx
@@ -2,8 +2,30 @@ import { Link, NavLink, NavLinkProps, Outlet } from "react-router-dom";
 import * as UI from "@enterprise-cmcs/macpro-ux-lib";
 import oneMacLogo from "@/assets/onemac_logo.svg";
 import { useMediaQuery } from "@/hooks";
-import { Bars3Icon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+
+const getLinks = (isAuthenticated: boolean) => {
+  if (isAuthenticated) {
+    return [
+      {
+        name: "Home",
+        link: "/",
+      },
+      {
+        name: "Dashboard",
+        link: "/dashboard",
+      },
+    ];
+  } else {
+    return [
+      {
+        name: "Home",
+        link: "/",
+      },
+    ];
+  }
+};
 
 export const Layout = () => {
   const isDesktop = useMediaQuery("(min-width: 768px)");
@@ -54,30 +76,48 @@ const ResponsiveNav = ({ isDesktop }: ResponsiveNavProps) => {
   if (isDesktop)
     return (
       <>
-        <NavLink to="/" className={setClassBasedOnNav}>
-          Home
-        </NavLink>
-        <NavLink to="/dashboard" className={setClassBasedOnNav}>
-          Dashboard
-        </NavLink>
+        {getLinks(true).map((link) => (
+          <NavLink
+            to={link.link}
+            key={link.name}
+            className={setClassBasedOnNav}
+          >
+            {link.name}
+          </NavLink>
+        ))}
         <div className="flex-1"></div>
         <button className="text-white hover:text-white/70">Sign In</button>
       </>
     );
 
-  if (!isDesktop) {
-    return (
-      <>
-        <div className="flex-1"></div>
-        <p>{isOpen && "its open"}</p>
-        <button
-          onClick={() => {
-            setIsOpen((prev) => !prev);
-          }}
-        >
-          <Bars3Icon className="w-6 h-6 min-w-[24px]" />
-        </button>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="flex-1"></div>
+      {isOpen && (
+        <div className="w-full fixed top-[100px] left-0">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-2 gap-4 rounded-lg bg-accent">
+            {getLinks(true).map((link) => (
+              <li key={link.link}>
+                <Link
+                  onClick={() => setIsOpen(false)}
+                  className='"block py-2 pl-3 pr-4 text-white rounded'
+                  to={link.link}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button
+        onClick={() => {
+          setIsOpen((prev) => !prev);
+        }}
+      >
+        {!isOpen && <Bars3Icon className="w-6 h-6 min-w-[24px]" />}
+        {isOpen && <XMarkIcon className="w-6 h-6 min-w-[24px]" />}
+      </button>
+    </>
+  );
 };
