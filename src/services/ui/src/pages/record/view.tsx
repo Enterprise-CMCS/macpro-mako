989b7355e1ea19f49d34efbb61e61bae7fff04bb
Benjamin Paige <benjamin.paige@gmail.com>
2023-07-24 12:52:35 -0600

    Splash (#64)
    * Add link to record page
    
    * add useGetRecord
    
    * update query

--- src/services/ui/src/pages/record/view.tsx
+++ src/services/ui/src/pages/record/view.tsx
@@ -1,15 +1,25 @@
+import { useGetRecord } from "@/api";
 import { useQuery } from "../../hooks";
+import { LoadingSpinner } from "@/components";
 
 export const ViewRecord = () => {
   const query = useQuery();
+  const id = query.get("id") as string;
+  const region = query.get("region") as string;
+  const { data, isLoading, error } = useGetRecord(id, region);
 
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+  if (error) {
+    return <p>error</p>;
+  }
   return (
-    <>
+    <div className="max-w-screen-lg mx-auto px-4 lg:px-8">
       <br />
-      <p>This is a route to view the details of record: </p>
-      <br />
-      <h2>Type: {query.get("type")}</h2>
-      <h2>ID: {query.get("id")}</h2>
-    </>
+      <h2>Region: {region}</h2>
+      <h2>ID: {id}</h2>
+      {JSON.stringify(data)}
+    </div>
   );
 };
