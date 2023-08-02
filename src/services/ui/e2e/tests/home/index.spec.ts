5cd75860231c45cf738125824b899d6dfad86c1f
Mike Dial <michael.dial.4@gmail.com>
2023-08-02 08:05:02 -0400

    fix(name):  Correct name as it appears in browser tabs

--- src/services/ui/e2e/tests/home/index.spec.ts
+++ src/services/ui/e2e/tests/home/index.spec.ts
@@ -5,7 +5,7 @@ test("has title", async ({ page }) => {
   await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/CMS OM Template/);
+  await expect(page).toHaveTitle(/CMS MAKO/);
 });
 
 test("get issues link", async ({ page }) => {
