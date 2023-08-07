aad9e5f14b137c4510777cf7bb7c7d5e8c3a90bb
Mike Dial <michael.dial.4@gmail.com>
2023-08-07 03:39:42 -0400

    update item auth

--- src/services/api/handlers/item.ts
+++ src/services/api/handlers/item.ts
@@ -1,7 +1,7 @@
 import { response } from "../libs/handler";
 import { APIGatewayEvent } from "aws-lambda";
 import * as os from "../../../libs/opensearch-lib";
-import { isAuthorized } from "../libs/auth/user";
+import { getStateFilter } from "../libs/auth/user";
 
 if (!process.env.osDomain) {
   throw "ERROR:  osDomain env variable is required,";
@@ -11,19 +11,24 @@ export const getItemData = async (event: APIGatewayEvent) => {
   try {
     const body = JSON.parse(event.body);
 
-    const query = {
+    let query: any = {};
+    query = {
       query: {
         bool: {
           must: [
             {
-              match: {
-                _id: body.id,
+              ids: {
+                values: [body.id],
               },
             },
           ],
         },
       },
     };
+    const stateFilter = await getStateFilter(event);
+    if (stateFilter) {
+      query.query.bool.must.push(stateFilter);
+    }
 
     const results = await os.search(process.env.osDomain, "main", query);
 
@@ -32,19 +37,10 @@ export const getItemData = async (event: APIGatewayEvent) => {
         statusCode: 404,
         body: { message: "No record found for the given id" },
       });
-    }
-
-    const stateCode = results.hits[0]._source.state;
-
-    if (isAuthorized(event, stateCode)) {
+    } else {
       return response<unknown>({
         statusCode: 200,
         body: results.hits[0],
-      });
-    } else {
-      return response({
-        statusCode: 403,
-        body: { message: "User is not authorized to access this resource" },
       });
     }
   } catch (error) {
