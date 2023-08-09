1def0891da7481d393167f483d7198dbcce85652
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-08-09 10:57:48 -0400

    ship it

--- src/packages/shared-types/onemac.ts
+++ src/packages/shared-types/onemac.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
+import s3parse from "s3-url-parser";
 
 export const onemacSchema = z.object({
   additionalInformation: z.string().optional(),
   componentType: z.string(),
+  submitterName: z.string(),
+  submitterEmail: z.string(),
   attachments: z
     .array(
       z.object({
@@ -13,12 +16,46 @@ export const onemacSchema = z.object({
         url: z.string().url(),
       })
     )
-    .optional(),
+    .nullish(),
 });
+
+// eventData.attachments = record.attachments || null;
+//       if (record.attachments && Array.isArray(record.attachments)) {
+//         eventData.attachments = record.attachments.map((attachment) => {
+//           const uploadDate = parseInt(attachment.s3Key.split("/")[0]);
+//           delete attachment.s3Key; // Once we have the date, this value is useless to us.  It's not the actual key
+//           const { bucket, key } = s3ParseUrl(attachment.url);
+//           return {
+//             ...attachment,
+//             uploadDate,
+//             bucket,
+//             key,
+//           };
+//         });
+//       }
 
 export const transformOnemac = (id: string) => {
   return onemacSchema.transform((data) => ({
-    filename: data.attachments?.[0].filename,
+    attachments:
+      data.attachments?.map((attachment) => {
+        const uploadDate = parseInt(attachment.s3Key.split("/")[0]);
+        const { bucket, key } = s3parse(attachment.url) as {
+          bucket: string;
+          key: string;
+        };
+
+        return {
+          ...attachment,
+          uploadDate,
+          bucket,
+          key,
+        };
+      }) ?? [],
+    additionalInformation: data.additionalInformation,
+    submitterName: data.submitterName,
+    submitterEmail: data.submitterEmail,
+    submissionOrigin: "Onemac",
+    id,
   }));
 };
 
--- src/services/data/handlers/sink.ts
+++ src/services/data/handlers/sink.ts
@@ -2,10 +2,10 @@ import { Handler } from "aws-lambda";
 import { decode } from "base-64";
 import * as os from "./../../../libs/opensearch-lib";
 import { SeaToolTransform, transformSeatoolData } from "shared-types/seatool";
+import { OneMacTransform, transformOnemac } from "shared-types/onemac";
 if (!process.env.osDomain) {
   throw "ERROR:  process.env.osDomain is required,";
 }
-import s3ParseUrl from "s3-url-parser";
 import { ZodError } from "zod";
 const osDomain: string = process.env.osDomain;
 const index = "main";
@@ -58,57 +58,32 @@ export const seatool: Handler = async (event) => {
 };
 
 export const onemac: Handler = async (event) => {
-  const records: Record<string, unknown>[] = [];
-  for (const key in event.records) {
-    event.records[key].forEach(
-      ({ key, value }: { key: string; value: string }) => {
-        const id: string = decode(key);
-        const eventData: Record<any, any> = {};
-        if (!value) {
-          // handle delete somehow
-        } else {
-          const record = { ...JSON.parse(decode(value)) };
+  const oneMacRecords: OneMacTransform[] = [];
 
-          if (record.sk !== "Package") {
-            console.log("Not a package type - ignoring");
-            return;
-          }
-          eventData.attachments = record.attachments || null;
-          if (record.attachments && Array.isArray(record.attachments)) {
-            eventData.attachments = record.attachments.map((attachment) => {
-              const uploadDate = parseInt(attachment.s3Key.split("/")[0]);
-              delete attachment.s3Key; // Once we have the date, this value is useless to us.  It's not the actual key
-              const { bucket, key } = s3ParseUrl(attachment.url);
-              return {
-                ...attachment,
-                uploadDate,
-                bucket,
-                key,
-              };
-            });
-          }
-          eventData.additionalInformation =
-            record.additionalInformation || null;
-          eventData.submitterName = record.submitterName || null;
-          eventData.submitterEmail = record.submitterEmail || null;
-          eventData.submissionOrigin = "OneMAC";
-          if (Object.keys(eventData).length) {
-            records.push({
-              key: id,
-              value: eventData,
-            });
-          }
-        }
+  for (const recordKey of Object.keys(event.records)) {
+    for (const onemacRecord of event.records[recordKey] as {
+      key: string;
+      value: string;
+    }[]) {
+      const { key, value } = onemacRecord;
+      const id: string = JSON.parse(decode(key));
+      const record = { id, ...JSON.parse(decode(value)) };
+
+      if (value && record && record.sk === "Package") {
+        const transformedRecord = transformOnemac(id).parse(record);
+
+        oneMacRecords.push(transformedRecord);
       }
-    );
+    }
   }
+
   try {
-    for (const item of records) {
+    for (const item of oneMacRecords) {
       await os.updateData(osDomain, {
         index,
-        id: item.key,
+        id: item.id,
         body: {
-          doc: item.value,
+          doc: item,
           doc_as_upsert: true,
         },
       });
@@ -117,3 +92,64 @@ export const onemac: Handler = async (event) => {
     console.error(error);
   }
 };
+
+// export const onemac: Handler = async (event) => {
+//   const records: Record<string, unknown>[] = [];
+//   for (const key in event.records) {
+//     event.records[key].forEach(
+//       ({ key, value }: { key: string; value: string }) => {
+//         const id: string = decode(key);
+//         const eventData: Record<any, any> = {};
+//         if (!value) {
+//           // handle delete somehow
+//         } else {
+//           const record = { ...JSON.parse(decode(value)) };
+
+//           if (record.sk !== "Package") {
+//             console.log("Not a package type - ignoring");
+//             return;
+//           }
+//           eventData.attachments = record.attachments || null;
+//           if (record.attachments && Array.isArray(record.attachments)) {
+//             eventData.attachments = record.attachments.map((attachment) => {
+//               const uploadDate = parseInt(attachment.s3Key.split("/")[0]);
+//               delete attachment.s3Key; // Once we have the date, this value is useless to us.  It's not the actual key
+//               const { bucket, key } = s3ParseUrl(attachment.url);
+//               return {
+//                 ...attachment,
+//                 uploadDate,
+//                 bucket,
+//                 key,
+//               };
+//             });
+//           }
+//           eventData.additionalInformation =
+//             record.additionalInformation || null;
+//           eventData.submitterName = record.submitterName || null;
+//           eventData.submitterEmail = record.submitterEmail || null;
+//           eventData.submissionOrigin = "OneMAC";
+//           if (Object.keys(eventData).length) {
+//             records.push({
+//               key: id,
+//               value: eventData,
+//             });
+//           }
+//         }
+//       }
+//     );
+//   }
+//   try {
+//     for (const item of records) {
+//       await os.updateData(osDomain, {
+//         index,
+//         id: item.key,
+//         body: {
+//           doc: item.value,
+//           doc_as_upsert: true,
+//         },
+//       });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
