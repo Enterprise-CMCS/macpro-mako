00ad9be798fc10557c699dd270072ac2df45d967
Adewale Sangobiyi <asangobiyi@adewale-mac.mynetworksettings.com>
2023-08-10 09:50:59 -0400

    feat(Add Faq Page): fix faq page styling

--- src/services/ui/src/main.tsx
+++ src/services/ui/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 // order is important on these import statements
 import "@enterprise-cmcs/macpro-ux-lib/build/assets/css/index.css"; // this one first
-import "./index.css"; // this one second
+import "./index.scss"; // this one second
 // end of order being important
 import { queryClient, router } from "./router";
 import { QueryClientProvider } from "@tanstack/react-query";
