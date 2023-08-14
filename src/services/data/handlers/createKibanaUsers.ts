911b2ef4ed3db2035a47dfe39285c439ea9ea337
James Dinh <jdinh@collabralink.com>
2023-08-14 08:10:59 -0700

    move bootstrap logic into data opensearch layer

--- src/services/data/handlers/createKibanaUsers.ts
+++ src/services/data/handlers/createKibanaUsers.ts
@@ -1,26 +1,26 @@
 import * as cognitolib from "../../../libs/cognito-lib";
-const users = require("../libs/users.json");
+import * as users from "../libs/users.json";
 const kibanaUserPoolId = process.env.kibanaUserPoolId;
 
 exports.handler = async function myHandler() {
   console.log("KIBANA USER POOL ID: ");
   console.log(kibanaUserPoolId);
 
-  for (var i = 0; i < users.length; i++) {
+  for (let i = 0; i < users.length; i++) {
     console.log(users[i]);
-    var poolData = {
+    const poolData = {
       UserPoolId: kibanaUserPoolId,
       Username: users[i].username,
       UserAttributes: users[i].attributes,
       MessageAction: "SUPPRESS",
     };
-    var passwordData = {
+    const passwordData = {
       Password: process.env.bootstrapUsersPassword,
       UserPoolId: kibanaUserPoolId,
       Username: users[i].username,
       Permanent: true,
     };
-    var attributeData = {
+    const attributeData = {
       Username: users[i].username,
       UserPoolId: kibanaUserPoolId,
       UserAttributes: users[i].attributes,
