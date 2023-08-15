d1f3d685dc32f6d14549cf08a3950bf3055a6c8d
Adewale Sangobiyi <asangobiyi@ip-10-232-38-185.ec2.internal>
2023-08-15 14:16:08 -0400

    feat(Add Faq Page): update page rotarion

--- src/services/ui/src/pages/faq/index.tsx
+++ src/services/ui/src/pages/faq/index.tsx
@@ -32,73 +32,82 @@ export const FAQ = () => {
   };
 */
 
-  const FAQAccordion = () => {
-    const [activeAccordion, setActiveAccordion] = useState(null || "");
-    const [iconOrientation, setIconOrientation] = useState("M9 5 5 1 1 5");
-  
-    const handleAccordionToggle = (sectionIndex:any, qandaIndex:any) => {
-      if (activeAccordion === `${sectionIndex}-${qandaIndex}`) {
-        setActiveAccordion("");
-        setIconOrientation("M9 5 5 1 1 5");
-      } else {
-        setActiveAccordion(`${sectionIndex}-${qandaIndex}`);
-        setIconOrientation("M9 1 5 5 1 1");
-      }
-    };
-  
-    return (
-      <div id="accordion-collapse" data-accordion="collapse">
-        {oneMACFAQContent.map((section, sectionIndex) => (
-          <div key={sectionIndex}>
-            <h2 className="topic-title">{section.sectionTitle}</h2>
-            {section.qanda.map((qandaItem, qandaIndex) => (
-              <div key={qandaIndex}>
-                <h2 id={`accordion-collapse-heading-${sectionIndex}-${qandaIndex}`}>
-                  
-                  <button
-                    type="button"
-                    className="flex items-center justify-between w-full p-5 font-medium text-left accordion-button-new "
-                    onClick={() => handleAccordionToggle(sectionIndex, qandaIndex)}
-                    aria-expanded={activeAccordion === `${sectionIndex}-${qandaIndex}`}
-                    aria-controls={`accordion-collapse-body-${sectionIndex}-${qandaIndex}`}
-                  >
-                    <span>{qandaItem.question}</span>
-                    <svg
-                      data-accordion-icon
-                      className={`w-3 h-3 rotate-180 shrink-0`}
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 10 6"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d={iconOrientation || "M9 5 5 1 1 5"} 
-                      />
-                    </svg>
-                  </button>
-                </h2>
-                <div
-                  id={`accordion-collapse-body-${sectionIndex}-${qandaIndex}`}
-                  className={`${
-                    activeAccordion === `${sectionIndex}-${qandaIndex}` ? '' : 'hidden'
-                  }`}
-                  aria-labelledby={`accordion-collapse-heading-${sectionIndex}-${qandaIndex}`}
+const FAQAccordion = () => {
+  const [activeAccordion, setActiveAccordion] = useState(null || "");
+  const initIconRotations:any = []
+  const [iconRotations, setIconRotations] = useState(initIconRotations);
+
+  const handleAccordionToggle = (sectionIndex:any, qandaIndex:any) => {
+    const newActiveAccordion:any = `${sectionIndex}-${qandaIndex}`;
+
+    //setActiveAccordion(newActiveAccordion);
+
+    const newIconRotations:any = [...iconRotations];    
+    if ( activeAccordion === newActiveAccordion) {
+      newIconRotations[newActiveAccordion] = "M9 5 5 1 1 5"
+      setActiveAccordion("");
+    }else{
+      newIconRotations[newActiveAccordion] = "M9 1 5 5 1 1"
+      setActiveAccordion(newActiveAccordion);
+    }
+    //newIconRotations[newActiveAccordion] = newIconRotations[newActiveAccordion] === "M9 1 5 5 1 1" ? "M9 5 5 1 1 5" : "M9 1 5 5 1 1";
+    setIconRotations(newIconRotations);
+  };
+
+  return (
+    <div id="accordion-collapse" data-accordion="collapse">
+      {oneMACFAQContent.map((section:any , sectionIndex: number) => (
+        <div key={sectionIndex}>
+          <h2 className="topic-title">{section.sectionTitle}</h2>
+          {section.qanda.map((qandaItem:any, qandaIndex:any) => (
+            <div key={qandaIndex}>
+              <h2 id={`accordion-collapse-heading-${sectionIndex}-${qandaIndex}`}>
+                <button
+                  type="button"
+                  className="flex items-center justify-between w-full p-5 font-medium text-left accordion-button-new "
+                  onClick={() => handleAccordionToggle(sectionIndex, qandaIndex)}
+                  aria-expanded={activeAccordion === `${sectionIndex}-${qandaIndex}`}
+                  aria-controls={`accordion-collapse-body-${sectionIndex}-${qandaIndex}`}
                 >
-                  <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-                    {qandaItem.answerJSX}
-                  </div>
+                  <span>{qandaItem.question}</span>
+                  <svg
+                    data-accordion-icon
+                    className={`w-3 h-3 rotate-180 shrink-0`}
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d={iconRotations[`${sectionIndex}-${qandaIndex}`] || "M9 5 5 1 1 5"}
+
+                    />
+                  </svg>
+                </button>
+              </h2>
+              <div
+                id={`accordion-collapse-body-${sectionIndex}-${qandaIndex}`}
+                className={`${
+                  activeAccordion === `${sectionIndex}-${qandaIndex}` ? '' : 'hidden'
+                }`}
+                aria-labelledby={`accordion-collapse-heading-${sectionIndex}-${qandaIndex}`}
+              >
+                <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+                  {qandaItem.answerJSX}
                 </div>
               </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  };
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 
 
   const infoDetails = [
