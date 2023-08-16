a92e9cc709d7ae0c2f2a5e5a0e9dc30213987e8f
Mike Dial <michael.dial.4@gmail.com>
2023-08-16 15:03:20 -0400

    remove the plan type id to name mapping in favor of using plan_types from top level

--- src/packages/shared-types/seatool.ts
+++ src/packages/shared-types/seatool.ts
@@ -2,23 +2,6 @@ import { z } from "zod";
 
 type AuthorityType = "SPA" | "WAIVER" | "MEDICAID" | "CHIP";
 
-const planTypeLookup = (val: number | null): null | string => {
-  if (!val) return null;
-
-  const lookup: Record<number, string> = {
-    121: "1115",
-    122: "1915b_waivers",
-    123: "1915c_waivers",
-    124: "CHIP_SPA",
-    125: "Medicaid_SPA",
-    126: "1115_Indep_Plus",
-    127: "1915c_Indep_Plus",
-    130: "UPL",
-  };
-
-  return lookup[val];
-};
-
 const authorityLookup = (val: number | null): null | string => {
   if (!val) return null;
 
@@ -125,7 +108,7 @@ export const transformSeatoolData = (id: string) => {
     authority: authorityLookup(data.STATE_PLAN.PLAN_TYPE),
     changedDate: data.STATE_PLAN.CHANGED_DATE,
     leadAnalyst: getLeadAnalyst(data),
-    planType: planTypeLookup(data.STATE_PLAN.PLAN_TYPE),
+    planType: data.PLAN_TYPES[0].PLAN_TYPE_NAME,
     planTypeId: data.STATE_PLAN.PLAN_TYPE,
     proposedDate: data.STATE_PLAN.PROPOSED_DATE,
     raiReceivedDate: getReceivedDate(data),
