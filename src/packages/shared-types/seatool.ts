0065ae8ef262780e24f87a10f3d611e12ce958ae
Benjamin Paige <benjamin.paige@gmail.com>
2023-08-24 11:49:40 -0600

    update date types and flattern data

--- src/packages/shared-types/seatool.ts
+++ src/packages/shared-types/seatool.ts
@@ -24,15 +24,26 @@ function getLeadAnalyst(eventData: SeaToolSink) {
     const leadAnalyst = eventData.LEAD_ANALYST.find(
       (analyst) => analyst.OFFICER_ID === eventData.STATE_PLAN.LEAD_ANALYST_ID
     );
-    console.log("the lead analsyt is: ", leadAnalyst);
 
-    if (leadAnalyst) return leadAnalyst; // {FIRST_NAME: string, LAST_NAME: string}
+    if (leadAnalyst) {
+      const leadAnalystOfficerId = leadAnalyst.OFFICER_ID;
+      const leadAnalystName = `${leadAnalyst.FIRST_NAME} ${leadAnalyst.LAST_NAME}`;
+      return {
+        leadAnalystOfficerId,
+        leadAnalystName,
+      };
+    }
   }
-  return null;
+  return {
+    leadAnalystOfficerId: null,
+    leadAnalystName: null,
+  };
 }
 
-const getReceivedDate = (data: SeaToolSink) => {
-  return (
+const getRaiReceivedDate = (data: SeaToolSink) => {
+  let raiReceivedDate = null;
+  let raiRequestedDate = null;
+  const raiDate =
     data.RAI?.sort((a, b) => {
       if (a.RAI_REQUESTED_DATE === null && b.RAI_REQUESTED_DATE === null) {
         return 0; // Both dates are null, so they're considered equal
@@ -44,8 +55,18 @@ const getReceivedDate = (data: SeaToolSink) => {
         return -1; // b comes after a because its date is null
       }
       return a.RAI_REQUESTED_DATE - b.RAI_REQUESTED_DATE; // Normal comparison
-    })[data.RAI.length - 1] ?? null
-  );
+    })[data.RAI.length - 1] ?? null;
+
+  if (raiDate && raiDate.RAI_RECEIVED_DATE) {
+    raiReceivedDate = new Date(raiDate.RAI_RECEIVED_DATE).toISOString();
+  }
+  if (raiDate && raiDate.RAI_REQUESTED_DATE) {
+    raiRequestedDate = new Date(raiDate.RAI_REQUESTED_DATE).toISOString();
+  }
+  return {
+    raiReceivedDate,
+    raiRequestedDate,
+  };
 };
 
 export const seatoolSchema = z.object({
@@ -111,22 +132,28 @@ export const seatoolSchema = z.object({
 });
 
 export const transformSeatoolData = (id: string) => {
-  return seatoolSchema.transform((data) => ({
-    id,
-    actionType: data.ACTIONTYPES?.[0].ACTION_NAME,
-    actionTypeId: data.ACTIONTYPES?.[0].ACTION_ID,
-    approvedEffectiveDate: data.STATE_PLAN.APPROVED_EFFECTIVE_DATE,
-    authority: authorityLookup(data.STATE_PLAN.PLAN_TYPE),
-    changedDate: data.STATE_PLAN.CHANGED_DATE,
-    leadAnalyst: getLeadAnalyst(data),
-    planType: data.PLAN_TYPES?.[0].PLAN_TYPE_NAME,
-    planTypeId: data.STATE_PLAN.PLAN_TYPE,
-    proposedDate: data.STATE_PLAN.PROPOSED_DATE,
-    raiReceivedDate: getReceivedDate(data),
-    state: data.STATES?.[0].STATE_CODE,
-    status: data.SPW_STATUS?.[0].SPW_STATUS_DESC,
-    submissionDate: data.STATE_PLAN.SUBMISSION_DATE,
-  }));
+  return seatoolSchema.transform((data) => {
+    const { leadAnalystName, leadAnalystOfficerId } = getLeadAnalyst(data);
+    const { raiReceivedDate, raiRequestedDate } = getRaiReceivedDate(data);
+    return {
+      id,
+      actionType: data.ACTIONTYPES?.[0].ACTION_NAME,
+      actionTypeId: data.ACTIONTYPES?.[0].ACTION_ID,
+      approvedEffectiveDate: data.STATE_PLAN.APPROVED_EFFECTIVE_DATE,
+      authority: authorityLookup(data.STATE_PLAN.PLAN_TYPE),
+      changedDate: data.STATE_PLAN.CHANGED_DATE,
+      leadAnalystOfficerId,
+      leadAnalystName,
+      planType: data.PLAN_TYPES?.[0].PLAN_TYPE_NAME,
+      planTypeId: data.STATE_PLAN.PLAN_TYPE,
+      proposedDate: data.STATE_PLAN.PROPOSED_DATE,
+      raiReceivedDate,
+      raiRequestedDate,
+      state: data.STATES?.[0].STATE_CODE,
+      status: data.SPW_STATUS?.[0].SPW_STATUS_DESC,
+      submissionDate: data.STATE_PLAN.SUBMISSION_DATE,
+    };
+  });
 };
 
 export type SeaToolTransform = z.infer<ReturnType<typeof transformSeatoolData>>;
