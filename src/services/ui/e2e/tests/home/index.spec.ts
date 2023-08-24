9ad2efe4604db2a655790978f2d682bfdb14e88f
Adewale Sangobiyi <asangobiyi@adewale-mac.mynetworksettings.com>
2023-08-24 08:35:51 -0400

    fix(update test): update test to include login and faq page

--- src/services/ui/e2e/tests/home/index.spec.ts
+++ src/services/ui/e2e/tests/home/index.spec.ts
@@ -1,21 +1,37 @@
 import { test, expect } from "@playwright/test";
+import { chromium } from "@playwright/test";
 import * as $ from "@/selectors";
 
+const user = {
+  userName: "george@example.com",
+  password: "bigTUNA1!"
+};
+
 test("has title", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("https://mako-dev.cms.gov/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/CMS MAKO/);
+  await expect(page).toHaveTitle(/.*CMS MAKO/);
 });
 
-test("get issues link", async ({ page }) => {
-  const navSelectors = new $.NavSelectors(page);
-  await page.goto("/");
+test("has faq Page", async ({ page }) => {
+  await page.goto("https://mako-dev.cms.gov/");
+  await page.getByRole("link", { name: "FAQ" }).click();
+});
 
-  // Click the issues link.
-  await navSelectors.issuesDropDown.click();
-  await navSelectors.allIssuesLink.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*issues/);
+test("log in test", async ({ page }) => {
+  //test.setTimeout(10000);
+  const loggedInIndicator = await page.getByRole("button", { name: "submit" });
+  if (loggedInIndicator) {
+    await page.goto("https://mako-dev.cms.gov/");
+    await page.getByRole("button", { name: "Sign In" }).click();
+    await page.getByRole("textbox", { name: "name@host.com" }).click();
+    await page.getByRole("textbox", { name: "Password" }).click();
+    await page.getByRole("textbox", { name: "Password" }).click();
+    await page.getByRole("button", { name: "submit" }).click();
+    console.log("User successfully logged in!");
+  }
+  else {
+    console.log("Login failed.");
+  }
 });
