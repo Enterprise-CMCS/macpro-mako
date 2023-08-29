5167049a64f42b8c3e2020c069ba3e2f49a14c2d
Adewale Sangobiyi <asangobiyi@adewale-mac.mynetworksettings.com>
2023-08-29 17:03:40 -0400

    fix(update test): update test to include login and faq page added playwright ui test

--- src/services/ui/playwright.config.ts
+++ src/services/ui/playwright.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(path.resolve(__dirname, ".env.local"));
 dotenv.config({ path: path.resolve(__dirname, ".env.local") });
 /**
  * See https://playwright.dev/docs/test-configuration.
