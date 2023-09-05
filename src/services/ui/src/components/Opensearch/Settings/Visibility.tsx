6c9ad490b7220a673fc1f5cf2737694303310a3b
Ishmeal Baawuah <ibaawuah@Ishmeals-MBP.fios-router.home>
2023-09-05 14:10:20 -0400

    adding aria-label to resolve 'Buttons do not have an accessible name'

--- src/services/ui/src/components/Opensearch/Settings/Visibility.tsx
+++ src/services/ui/src/components/Opensearch/Settings/Visibility.tsx
@@ -13,7 +13,7 @@ export const VisibilityPopover = <T extends Item>(props: Props<T>) => {
   return (
     <UI.Popover>
       <UI.PopoverTrigger>
-        <Icon name="visibility" />
+        <Icon name="visibility" aria-label="Visibility" />
       </UI.PopoverTrigger>
       <UI.PopoverContent className="bg-white">
         <div className="flex flex-col gap-2">
@@ -57,6 +57,7 @@ export const VisibilityMenu = <T extends Item>(props: Props<T>) => {
           key={`vis-${IT.field}`}
           onClick={() => props.onItemClick(IT.field)}
           {...IT}
+          aria-label={IT.label}
         />
       ))}
     </div>
