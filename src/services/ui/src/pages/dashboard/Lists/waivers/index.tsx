ba7d612adc63ee0f09af2f6fab1f34320ab7b14b
Benjamin Paige <benjamin.paige@gmail.com>
2023-09-06 08:53:05 -0600

    fix deploy issue and add loading to lists

--- src/services/ui/src/pages/dashboard/Lists/waivers/index.tsx
+++ src/services/ui/src/pages/dashboard/Lists/waivers/index.tsx
@@ -22,7 +22,7 @@ export const WaiversList = () => {
   return (
     <section className="tw-flex tw-flex-col tw-h-[calc(100vh-250px)]">
       <OsFiltering />
-      {/* {context.isLoading && <LoadingSpinner />} */}
+      {context.isLoading && <LoadingSpinner />}
       <OsTable columns={columns} />
       <Pagination
         pageNumber={params.state.pagination.number}
