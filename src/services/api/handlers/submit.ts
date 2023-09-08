7a04449bf4e98fd598bb237687f4d84ddbe8b251
Mike Dial <michael.dial.4@gmail.com>
2023-09-08 13:32:25 -0400

    successfully connecting

--- src/services/api/handlers/submit.ts
+++ src/services/api/handlers/submit.ts
@@ -1,47 +1,33 @@
 import { response } from "../libs/handler";
 import { APIGatewayEvent } from "aws-lambda";
-// import { getStateFilter } from "../libs/auth/user";
-// import { OsHit, OsMainSourceItem } from "shared-types";
+import * as sql from "mssql";
 
 export const submit = async (event: APIGatewayEvent) => {
   try {
     const body = JSON.parse(event.body);
     console.log(body);
 
+    const user = process.env.dbUser;
+    const password = process.env.dbPassword;
+    const server = process.env.dbIp;
+    const port = parseInt(process.env.dbPort);
+    const config = {
+      user: user,
+      password: password,
+      server: server,
+      port: port,
+      database: "SEA",
+    };
+
+    const pool = await sql.connect(config);
+    const result = await sql.query`SELECT * FROM sys.tables;`;
+    console.log(result);
+    await pool.close();
+
     return response({
       statusCode: 200,
       body: { message: "totally" },
     });
-
-    // const result = (await os.getItem(
-    //   process.env.osDomain,
-    //   "main",
-    //   body.id
-    // )) as OsHit<OsMainSourceItem> & { found: boolean };
-
-    // if (
-    //   stateFilter &&
-    //   !stateFilter.terms.state.includes(
-    //     result._source.state.toLocaleLowerCase()
-    //   )
-    // ) {
-    //   return response({
-    //     statusCode: 401,
-    //     body: { message: "Not authorized to view this resource" },
-    //   });
-    // }
-
-    // if (!result.found) {
-    //   return response({
-    //     statusCode: 404,
-    //     body: { message: "No record found for the given id" },
-    //   });
-    // } else {
-    //   return response<unknown>({
-    //     statusCode: 200,
-    //     body: result,
-    //   });
-    // }
   } catch (error) {
     console.error({ error });
     return response({
