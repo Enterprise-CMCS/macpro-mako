1279a7ee991f221274ba48ca1602d0eb43c6005f
James Dinh <jdinh@collabralink.com>
2023-09-11 12:30:01 -0700

    comment

--- src/services/ui/e2e/tests/home/index.spec.ts
+++ src/services/ui/e2e/tests/home/index.spec.ts
@@ -5,12 +5,12 @@ import { testUsers } from "e2e/utils/users";
 console.log("processenv", process.env);
 const secretId = `${process.env.project}/default/bootstrapUsersPassword`;
 
-const password = await Libs.getSecretsValue(
-  process.env.region as string,
-  secretId
-);
+// const password = await Libs.getSecretsValue(
+//   process.env.region as string,
+//   secretId
+// );
 
-console.log("PASSWORD TEST:", password);
+// console.log("PASSWORD TEST:", password);
 
 test("has title", async ({ page }) => {
   await page.goto("/");
