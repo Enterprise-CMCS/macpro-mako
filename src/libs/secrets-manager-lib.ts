3a41f356c59ada6096ad47a24361180bcc6ae650
James Dinh <jdinh@collabralink.com>
2023-09-12 12:13:09 -0700

    Update secrets-manager-lib.ts

--- src/libs/secrets-manager-lib.ts
+++ src/libs/secrets-manager-lib.ts
@@ -9,11 +9,18 @@ export const getSecretsValue = async (region: string, secretId: string) => {
   const input = { SecretId: secretId };
   const command = new GetSecretValueCommand(input);
 
+  console.log("client", client);
+  console.log("input", input);
+  console.log("command", command);
   try {
+    console.log("try");
     const response = await client.send(command);
+    console.log("response", response);
     const result = JSON.parse(response.SecretString ?? "");
+    console.log("result", result);
     return result;
   } catch (e) {
+    console.log("error2", e);
     console.log("ERROR getting secrets value", JSON.stringify(e, null, 2));
   }
 };
