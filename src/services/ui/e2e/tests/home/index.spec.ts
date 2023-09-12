e7e171fd286ffa388af28b1929dbe6ea7d80afb2
James Dinh <jdinh@collabralink.com>
2023-09-12 10:30:17 -0700

    Update index.spec.ts

--- src/services/ui/e2e/tests/home/index.spec.ts
+++ src/services/ui/e2e/tests/home/index.spec.ts
@@ -1,23 +1,14 @@
-import * as Libs from "../../../../../libs/secrets-manager-lib";
 import { test, expect } from "@playwright/test";
 import { testUsers } from "e2e/utils/users";
-console.log("test1");
-console.log("processenv", process.env);
-const secretId = `${process.env?.project}/default/bootstrapUsersPassword`;
 
-const password = await Libs.getSecretsValue(
-  process.env.region as string,
-  secretId
-);
-
-console.log("PASSWORD TEST:", password);
+const password = process.env.BOOTSTRAP_USERS_PW as string;
 
 test("has title", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle(/CMS MAKO/);
 });
 
-test("see frequently asked questions header when in faq page", async ({
+test("see frequesntly asked questions header when in faq page", async ({
   page,
 }) => {
   await page.goto("/");
@@ -29,13 +20,13 @@ test("see frequently asked questions header when in faq page", async ({
   expect(foundFaqHeading).toBeTruthy();
 });
 
-test("see dashboard link when log in", async ({ page }) => {
+test("see dahsboard link when log in", async ({ page }) => {
   await page.goto("/");
   await page.getByRole("button", { name: "Sign In" }).click();
   await page
     .getByRole("textbox", { name: "name@host.com" })
     .type(testUsers.state);
-  await page.getByRole("textbox", { name: "Password" }).type("password");
+  await page.getByRole("textbox", { name: "Password" }).type(password);
   await page.getByRole("button", { name: "submit" }).click();
   await page.getByRole("link", { name: "Dashboard" }).click();
 
@@ -49,11 +40,11 @@ test("failed incorrect login username", async ({ page }) => {
   await page.goto("/");
   await page.getByRole("button", { name: "Sign In" }).click();
   await page.getByRole("textbox", { name: "name@host.com" }).type(".");
-  await page.getByRole("textbox", { name: "Password" }).type("test");
+  await page.getByRole("textbox", { name: "Password" }).type(password);
   await page.getByRole("button", { name: "submit" }).click();
   await page.getByRole("paragraph").isVisible();
   const invalidInputTest = await page.$(
-    "p:has-text('The username or password you entered is invalid')"
+    'p:has-text("The username or password you entered is invalid")'
   );
   expect(invalidInputTest).toBeTruthy();
 });
