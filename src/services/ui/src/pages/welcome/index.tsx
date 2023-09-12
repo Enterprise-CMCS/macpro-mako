efd1bec41efdce8ad6721f5fc647ede88dabf749
Gavin St. Ours <gst-ours@fearless.tech>
2023-09-12 09:00:11 -0400

    Replace placeholder text with text from OneMAC

--- src/services/ui/src/pages/welcome/index.tsx
+++ src/services/ui/src/pages/welcome/index.tsx
@@ -24,8 +24,8 @@ export const Welcome = () => {
         <div className="max-w-screen-xl flex flex-col sm:flex-row sm:items-center gap-4 mx-auto p-4 lg:px-8">
           <img src={OneMacLogo} alt="One Mac Logo" className="p-4" />
           <p className="text-center text-white/90 font-light text-xl font-sans">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur,
-            voluptatem amet! Nihil numquam adipisci tempora explicabo.
+            Welcome to the official submission system for paper-based state plan
+            amendments (SPAs) and section 1915 waivers.
           </p>
         </div>
       </div>
@@ -39,37 +39,62 @@ export const Welcome = () => {
               <C.HowItWorks>
                 <C.Step
                   icon={<AcademicCapIcon className="min-w-[32px] w-8 h-8" />}
-                  title="Possimus a, odio."
-                  content="Lorem ipsum dolor sit amet."
+                  title="Login with IDM"
+                  content="Login with your IDM username and password to access your SPA and Waiver dashboard."
                 />
                 <C.Step
                   icon={<AcademicCapIcon className="min-w-[32px] w-8 h-8" />}
-                  title="Possimus a, odio."
-                  content="Lorem ipsum dolor sit amet."
+                  title="Attach your documents"
+                  content="Select a submission type and attach required documents relevant to your SPA and/or Waiver submission."
                 />
                 <C.Step
                   icon={<AcademicCapIcon className="min-w-[32px] w-8 h-8" />}
-                  title="Possimus a, odio."
-                  content="Lorem ipsum dolor sit amet."
+                  title="Receive an email confirmation"
+                  content="After you submit, you will receive an email confirmation that your submission was successful, marking the start of the 90-day review process."
                 />
               </C.HowItWorks>
               <div className="flex-grow">
-                <h4 className="font-bold text-xl mb-4">Lorem, ipsum.</h4>
+                <h4 className="font-bold text-xl mb-4">
+                  Submission Types include:
+                </h4>
                 <ul className="flex flex-col gap-4">
                   <li>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Architecto facilis laboriosam placeat molestias animi
-                      atque sint modi accusantium maiores. Aliquam sequi ad
-                      nobis nesciunt dignissimos natus dolorum quo illum vel?
+                      Amendments to your Medicaid and CHIP State Plans (not
+                      submitted through MACPro, MMDL or WMS).
                     </p>
                   </li>
                   <li>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Architecto facilis laboriosam placeat molestias animi
-                      atque sint modi accusantium maiores. Aliquam sequi ad
-                      nobis nesciunt dignissimos natus dolorum quo illum vel?
+                      Official state responses to formal requests for additional
+                      information (RAIs) for SPAs (not submitted through
+                      MACPro).
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Section 1915(b) waiver submissions (those not submitted
+                      through WMS).
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Section 1915(c) Appendix K amendments (which cannot be
+                      submitted through WMS).
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Official state responses to formal requests for additional
+                      information (RAIs) for Section 1915(b) waiver actions (in
+                      addition to submitting waiver changes in WMS, if
+                      applicable).
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      State requests for Temporary Extensions for section
+                      1915(b) and 1915(c) waivers.
                     </p>
                   </li>
                 </ul>
@@ -82,37 +107,50 @@ export const Welcome = () => {
               <C.HowItWorks>
                 <C.Step
                   icon={<AcademicCapIcon className="min-w-[32px] w-8 h-8" />}
-                  title="Possimus a, odio."
-                  content="Lorem ipsum dolor sit amet."
+                  title="Receive an email for submission notification"
+                  content="After a state adds a submission to OneMAC, you will receive an email notification that a submission was made requiring your review and the submission is on the clock."
                 />
                 <C.Step
                   icon={<AcademicCapIcon className="min-w-[32px] w-8 h-8" />}
-                  title="Possimus a, odio."
-                  content="Lorem ipsum dolor sit amet."
+                  title="Login with EUA"
+                  content="Login with your EUA username and password to access the SPA and Waiver dashboard."
                 />
                 <C.Step
                   icon={<AcademicCapIcon className="min-w-[32px] w-8 h-8" />}
-                  title="Possimus a, odio."
-                  content="Lorem ipsum dolor sit amet."
+                  title="Review your assigned submission"
+                  content="Search the submission ID from the email and click on the submission to view and review details and attachments."
                 />
               </C.HowItWorks>
               <div>
-                <h4 className="font-bold text-xl mb-4">Lorem, ipsum.</h4>
+                <h4 className="font-bold text-xl mb-4">
+                  Submission Types include:
+                </h4>
                 <ul className="flex flex-col gap-4">
                   <li>
+                    <p>Amendments to your Medicaid and CHIP State Plans.</p>
+                  </li>
+                  <li>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Architecto facilis laboriosam placeat molestias animi
-                      atque sint modi accusantium maiores. Aliquam sequi ad
-                      nobis nesciunt dignissimos natus dolorum quo illum vel?
+                      Official state responses to formal requests for additional
+                      information (RAIs) for SPAs.
+                    </p>
+                  </li>
+                  <li>
+                    <p>Section 1915(b) waiver submissions.</p>
+                  </li>
+                  <li>
+                    <p>Section 1915(c) Appendix K amendments.</p>
+                  </li>
+                  <li>
+                    <p>
+                      Official state responses to formal requests for additional
+                      information (RAIs) for Section 1915(b) waiver actions.
                     </p>
                   </li>
                   <li>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Architecto facilis laboriosam placeat molestias animi
-                      atque sint modi accusantium maiores. Aliquam sequi ad
-                      nobis nesciunt dignissimos natus dolorum quo illum vel?
+                      State requests for Temporary Extensions for section
+                      1915(b) and 1915(c) waivers.
                     </p>
                   </li>
                 </ul>
