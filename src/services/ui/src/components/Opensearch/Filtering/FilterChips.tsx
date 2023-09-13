2a1e613c18b37b89274ad68417c7669a5f723fa2
Daniel Belcher <daniel.belcher.cs@gmail.com>
2023-09-12 22:36:46 -0700

    render chip list

--- src/services/ui/src/components/Opensearch/Filtering/FilterChips.tsx
+++ src/services/ui/src/components/Opensearch/Filtering/FilterChips.tsx
@@ -1,23 +1,61 @@
-import { type FC } from "react";
+import { Fragment, type FC } from "react";
 
 import { useOsQuery } from "@/components/Opensearch/useOpensearch";
 import { Chip } from "@/components/Chip";
+import { OsFilterable, OsRangeValue } from "shared-types";
+
+interface RenderProps {
+  filter: OsFilterable;
+  index: number;
+}
+
+const renderMultipleValues: FC<RenderProps> = ({ filter, index }) => {
+  if (filter.value.constructor !== Array) return <></>;
+
+  return (
+    <Fragment key={`fragment-${filter.field}`}>
+      {filter.value.map((val, valIndex) => {
+        return (
+          <Chip key={`${index}-${filter.field}-${val}-${valIndex}`}>
+            {`${filter?.label + ": " ?? ""}${val}`}
+          </Chip>
+        );
+      })}
+    </Fragment>
+  );
+};
+
+const renderDateValue: FC<RenderProps> = ({ filter, index }) => {
+  const range = filter.value as OsRangeValue;
+  return (
+    <Chip key={`${index}-${filter.field}`}>
+      {`${filter?.label + ": " ?? ""}${new Date(
+        range?.gte ?? ""
+      ).toLocaleDateString()} - ${new Date(
+        range?.lte ?? ""
+      ).toLocaleDateString()}`}
+    </Chip>
+  );
+};
 
 export const FilterChips: FC = () => {
   const {
     state: { filters },
   } = useOsQuery();
+  const multipleFilters =
+    filters.length > 1 ||
+    (Array.isArray(filters[0].value) && filters[0].value.length > 1);
 
   return (
-    <div className="justify-start items-center py-2 flex">
+    <div className="justify-start items-center py-2 flex flex-wrap gap-y-2 gap-x-2">
       {filters.map((filter, index) => {
-        return (
-          <Chip className="mr-2" key={`${index}-${filter.field}`}>
-            {"test" + index}
-          </Chip>
-        );
+        const compRender =
+          filter.component === "dateRange"
+            ? renderDateValue
+            : renderMultipleValues;
+        return compRender({ filter, index });
       })}
-      {filters.length > 1 && <Chip variant={"function"}>Clear All</Chip>}
+      {multipleFilters && <Chip variant={"function"}>Clear All</Chip>}
     </div>
   );
 };
