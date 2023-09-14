0a6b235a7d74b14f57c98fac118beb0a8d7a1b1e
James Dinh <jdinh@collabralink.com>
2023-09-14 13:17:09 -0700

    Update playwright.config.ts

--- src/services/ui/playwright.config.ts
+++ src/services/ui/playwright.config.ts
@@ -42,9 +42,9 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
   ],
 });
