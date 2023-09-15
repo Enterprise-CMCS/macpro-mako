17271069a26b9249bee411cdd07dff506d409e9f
James Dinh <jdinh@collabralink.com>
2023-09-15 08:12:52 -0700

    add packages

--- src/services/ui/playwright.config.ts
+++ src/services/ui/playwright.config.ts
@@ -33,17 +33,18 @@ export default defineConfig({
   /* Configure projects for major browsers */
   // Note: we can test on multiple browsers and resolutions defined here
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chrome"] },
-    // },
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 });
