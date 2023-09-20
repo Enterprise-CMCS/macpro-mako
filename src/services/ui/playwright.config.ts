29e8ebee3c598ae7d1558c448264b6fad2ae0529
James Dinh <jdinh@collabralink.com>
2023-09-20 06:50:42 -0700

    test

--- src/services/ui/playwright.config.ts
+++ src/services/ui/playwright.config.ts
@@ -37,14 +37,14 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
 
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 });
