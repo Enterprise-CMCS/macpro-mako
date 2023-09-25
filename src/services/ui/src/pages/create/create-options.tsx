4bb0b2132811df13785ee0b58a41d8094c1915d1
Kevin Haube <khaube@fearless.tech>
2023-09-25 15:55:13 -0400

    Add OptionFieldset

--- src/services/ui/src/pages/create/create-options.tsx
+++ src/services/ui/src/pages/create/create-options.tsx
@@ -1,5 +1,48 @@
+import React, { PropsWithChildren, ReactNode } from "react";
+import { Link } from "react-router-dom";
+import {Card} from "@mui/material";
 
+export const SimplePageContainer = ({ children }: PropsWithChildren) => (
+    <div className="max-w-screen-xl mx-auto px-4 lg:px-8">
+        {children}
+    </div>
+);
+
+export const SimplePageTitle = ({ title }: { title: string }) => (
+    <div className="flex items-center justify-between my-4">
+        <h1 className="text-xl">{title}</h1>
+    </div>
+);
+
+export type MACCardListProps = PropsWithChildren<{
+    legend: string;
+    additionalContainerClassName?: string;
+}>;
+/** A styled MACCard for nesting {@link MACFieldsetCardOption} with a fieldset and
+ * legend */
+export const OptionFieldset = ({
+    children,
+    legend,
+    additionalContainerClassName,
+}: MACCardListProps) => {
+    return (
+        <section className={`mac-fieldset-wrapper ${additionalContainerClassName}`}>
+            <fieldset>
+                <legend className="mac-fieldset-legend">{legend}</legend>
+                <Card>
+                    {children}
+                </Card>
+            </fieldset>
+        </section>
+    );
+};
 
 export const NewSubmissionOptions = () => {
-    return (<>Test</>);
+    return (
+        <SimplePageContainer>
+            <SimplePageTitle title={"New Submission"}/>
+            <OptionFieldset legend={"Test"}>
+            </OptionFieldset>
+        </SimplePageContainer>
+    );
 };
