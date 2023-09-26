2fad3b8cae82e9446bd36a5913be2650f83251d7
Kevin Haube <khaube@fearless.tech>
2023-09-26 12:11:35 -0400

    Options as array of objects

--- src/services/ui/src/pages/create/create-options.tsx
+++ src/services/ui/src/pages/create/create-options.tsx
@@ -1,5 +1,5 @@
 import React, {PropsWithChildren} from "react";
-import {OptionCard, OptionFieldset} from "@/components/Cards/OptionCard";
+import {MACFieldsetOption, OptionCard, OptionFieldset} from "@/components/Cards/OptionCard";
 import {ROUTES} from "@/router";
 
 export const SimplePageContainer = ({ children }: PropsWithChildren) => (
@@ -14,22 +14,31 @@ export const SimplePageTitle = ({ title }: { title: string }) => (
     </div>
 );
 
+const OPTIONS: Omit<MACFieldsetOption, "altBg">[] = [
+    {
+        title: "State Plan Amendment (SPA)",
+        description: <p>Submit a Medicaid or CHIP State Plan Amendment</p>,
+        linkTo: ROUTES.HOME
+    },
+    {
+        title: "Waiver Action",
+        description: <p>Submit Waivers, Amendments, Renewals, and Temporary Extensions</p>,
+        linkTo: ROUTES.HOME
+    },
+];
+
 export const NewSubmissionOptions = () => {
     return (
         <SimplePageContainer>
             <SimplePageTitle title={"New Submission"}/>
             <OptionFieldset legend={"Select a Submission Type"}>
-                <OptionCard
-                    title="State Plan Amendment (SPA)"
-                    description={<p>Submit a Medicaid or CHIP State Plan Amendment</p>}
-                    linkTo={ROUTES.HOME}
-                />
-                <OptionCard
-                    title="Waiver Action"
-                    description={<p>Submit Waivers, Amendments, Renewals, and Temporary Extensions</p>}
-                    linkTo={ROUTES.HOME}
-                    altBg
-                />
+                {OPTIONS.map((opt, idx) =>
+                    <OptionCard
+                        key={idx}
+                        {...opt}
+                        altBg={idx % 2 === 1} // Even number option cards show altBg
+                    />
+                )}
             </OptionFieldset>
         </SimplePageContainer>
     );
