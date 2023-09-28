2afadf3de89013b2b1db5c1e8cf3f42468f6860e
James Dinh <jdinh@collabralink.com>
2023-09-28 13:10:44 -0700

    outlining logic

---
+++ src/services/api/handlers/layers/forms.ts
@@ -0,0 +1,14 @@
+import { response } from "../../libs/handler";
+import { APIGatewayEvent } from "aws-lambda";
+import { getStateFilter } from "../../libs/auth/user";
+import * as os from "../../../../libs/opensearch-lib";
+if (!process.env.osDomain) {
+  throw "ERROR:  osDomain env variable is required,";
+}
+
+// Handler function to search index
+export const formsLayer = async (event: APIGatewayEvent) => {
+  return console.log("layer test");
+};
+
+export const handler = formsLayer;
