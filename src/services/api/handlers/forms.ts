ae928df8b3e134f68905eaa2bb7ed0f7a57f6261
James Dinh <jdinh@collabralink.com>
2023-09-29 10:13:09 -0700

    change dir

--- src/services/api/handlers/forms.ts
+++ src/services/api/handlers/forms.ts
@@ -1,47 +1,14 @@
-import { response } from "../libs/handler";
+import { response } from "../../libs/handler";
 import { APIGatewayEvent } from "aws-lambda";
-import { getStateFilter } from "../libs/auth/user";
-import * as os from "./../../../libs/opensearch-lib";
+import { getStateFilter } from "../../libs/auth/user";
+import * as os from "../../../../libs/opensearch-lib";
 if (!process.env.osDomain) {
   throw "ERROR:  osDomain env variable is required,";
 }
 
 // Handler function to search index
-export const getSearchData = async (event: APIGatewayEvent) => {
-  try {
-    let query: any = {};
-    if (event.body) {
-      query = JSON.parse(event.body);
-    }
-
-    query.query = query?.query || {};
-    query.query.bool = query.query?.bool || {};
-    query.query.bool.must = query.query.bool?.must || [];
-
-    const stateFilter = await getStateFilter(event);
-    if (stateFilter) {
-      query.query.bool.must.push(stateFilter);
-    }
-
-    query.from = query.from || 0;
-    query.size = query.size || 100;
-
-    console.log("Sending query, built as follow:");
-    console.log(JSON.stringify(query, null, 2));
-
-    const results = await os.search(process.env.osDomain, "main", query);
-
-    return response<unknown>({
-      statusCode: 200,
-      body: results,
-    });
-  } catch (error) {
-    console.error({ error });
-    return response({
-      statusCode: 500,
-      body: { message: "Internal server error" },
-    });
-  }
+export const formsLayer = async (event: APIGatewayEvent) => {
+  return console.log("layer test");
 };
 
-export const handler = getSearchData;
+export const handler = formsLayer;
