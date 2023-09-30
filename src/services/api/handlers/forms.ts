635c845c3c58a2d6f0a6d2f7aaa03413d02f62b9
James Dinh <jdinh@collabralink.com>
2023-09-30 10:40:24 -0700

    test

--- src/services/api/handlers/forms.ts
+++ src/services/api/handlers/forms.ts
@@ -1,7 +1,7 @@
-import { response } from "../../libs/handler";
+import { response } from "../libs/handler";
 import { APIGatewayEvent } from "aws-lambda";
-import { getStateFilter } from "../../libs/auth/user";
-import * as os from "../../../../libs/opensearch-lib";
+import { getStateFilter } from "../libs/auth/user";
+import * as os from "../../../libs/opensearch-lib";
 if (!process.env.osDomain) {
   throw "ERROR:  osDomain env variable is required,";
 }
