883627d2b8c9bd9702967d239a93798b6d5d016a
Kevin Haube <khaube@fearless.tech>
2023-10-03 15:11:53 -0400

    Prettier code

--- src/services/ui/src/pages/create/index.ts
+++ src/services/ui/src/pages/create/index.ts
@@ -1,16 +1,16 @@
 export { Create } from "./create-form";
 export {
-    NewSubmissionInitialOptions,
-    SPASubmissionOptions,
-    MedicaidSPASubmissionOptions,
-    ChipSPASubmissionOptions,
-    WaiverSubmissionOptions,
-    BWaiverSubmissionOptions,
-    B4WaiverSubmissionOptions,
-    BCapWaiverSubmissionOptions
+  NewSubmissionInitialOptions,
+  SPASubmissionOptions,
+  MedicaidSPASubmissionOptions,
+  ChipSPASubmissionOptions,
+  WaiverSubmissionOptions,
+  BWaiverSubmissionOptions,
+  B4WaiverSubmissionOptions,
+  BCapWaiverSubmissionOptions,
 } from "./create-options";
 export {
-    MedicaidABPLandingPage,
-    MedicaidEligibilityLandingPage,
-    CHIPEligibilityLandingPage
+  MedicaidABPLandingPage,
+  MedicaidEligibilityLandingPage,
+  CHIPEligibilityLandingPage,
 } from "./external-landing/ExternalAppLandingPage";
