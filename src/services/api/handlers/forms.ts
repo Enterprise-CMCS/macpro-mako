4c4aa1ad233533e54d4e2e09bc72e1ba4ad9919f
James Dinh <jdinh@collabralink.com>
2023-10-04 09:33:11 -0700

    Update forms.ts

--- src/services/api/handlers/forms.ts
+++ src/services/api/handlers/forms.ts
@@ -9,7 +9,6 @@ export const forms = async (event) => {
     console.log(filePath);
     const cwd = process.cwd();
     console.log("CWD", cwd);
-    const jsonData2 = require("/opt/forms/form_v1.json");
     const jsonData3 = require("/opt/FormsLambdaLayer/form_v1.json");
 
     console.log(jsonData, jsonData2, jsonData3);
