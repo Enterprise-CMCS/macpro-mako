e40f03db7d4cb1d28e2f8ebc982e4f10458eff31
Kevin Haube <khaube@fearless.tech>
2023-10-05 11:48:36 -0400

    Implement iterative section rendering

--- src/services/ui/src/pages/submission-flow/config/forms.tsx
+++ src/services/ui/src/pages/submission-flow/config/forms.tsx
@@ -2,14 +2,24 @@ import { FormPageConfig } from "@/pages/submission-flow/renderers/FormPage";
 
 export const MEDICAID_SPA_FORM: FormPageConfig = {
   pageTitle: "Submit New Medicaid SPA",
-  descriptionHeading: "Medicaid SPA Details",
-  descriptionElement: (
-    <p>
-      Once you submit this form, a confirmation email is sent to you and to CMS.
-      CMS will use this content to review your package, and you will not be able
-      to edit this form. If CMS needs any additional information, they will
-      follow up by email.{" "}
-      <b>If you leave this page, you will lose your progress on this form.</b>
-    </p>
-  ),
+  description: {
+    heading: "Medicaid SPA Details",
+    instructions: (
+      <p>
+        Once you submit this form, a confirmation email is sent to you and to
+        CMS. CMS will use this content to review your package, and you will not
+        be able to edit this form. If CMS needs any additional information, they
+        will follow up by email.{" "}
+        <b>If you leave this page, you will lose your progress on this form.</b>
+      </p>
+    ),
+  },
+  fields: [
+    {
+      id: "spa-id",
+      heading: "SPA ID",
+      instructions: <p></p>,
+      content: <></>,
+    },
+  ],
 };
--- src/services/ui/src/pages/submission-flow/renderers/FormPage.tsx
+++ src/services/ui/src/pages/submission-flow/renderers/FormPage.tsx
@@ -3,27 +3,38 @@ import { SimplePageTitle } from "@/pages/submission-flow/renderers/OptionsPage";
 import { MEDICAID_SPA_FORM } from "@/pages/submission-flow/config/forms";
 import { ReactElement } from "react";
 
+type FormSection = {
+  id: string;
+  heading: string;
+  instructions: ReactElement;
+  content: ReactElement;
+};
+type FormDescription = Pick<FormSection, "heading" | "instructions">;
 export interface FormPageConfig {
   pageTitle: string;
-  descriptionHeading: string;
-  descriptionElement: ReactElement;
+  description: FormDescription;
+  fields: FormSection[];
 }
-const FormPage = ({
-  pageTitle,
-  descriptionHeading,
-  descriptionElement,
-}: FormPageConfig) => {
+const FormPage = ({ pageTitle, description, fields }: FormPageConfig) => {
   return (
     <SimplePageContainer>
       <SimplePageTitle title={pageTitle} />
       <section id="description">
-        <h2>{descriptionHeading}</h2>
+        <h2>{description.heading}</h2>
         <p>
           <span className="text-red-500">*</span> indicates required field
         </p>
-        {descriptionElement}
+        {description.instructions}
       </section>
-      <section id="spa-id"></section>
+      <form>
+        {fields.map((section, idx) => (
+          <section key={`${idx}-${section.id}`} id={section.id}>
+            <h3>{section.heading}</h3>
+            {section.instructions}
+            {section.content}
+          </section>
+        ))}
+      </form>
     </SimplePageContainer>
   );
 };
