e23d34efd33e7cf93dcd02e4a920a37a9a202228
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-10-05 12:49:29 -0400

    add unit tests

--- src/services/ui/src/components/BreadCrumb/BreadCrumb.tsx
+++ src/services/ui/src/components/BreadCrumb/BreadCrumb.tsx
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
 
 type BreadCrumbProps = {
   to: string;
+  active?: boolean;
 };
 
 export const BreadCrumb = ({
   to,
+  active = false,
   children,
 }: React.PropsWithChildren<BreadCrumbProps>) => {
-  return <Link to={to}>{children}</Link>;
+  const activeClass = "underline";
+  return (
+    <Link
+      className={twMerge(clsx(active && activeClass), "hover:underline")}
+      to={to}
+    >
+      {children}
+    </Link>
+  );
 };
 
 export const BreadCrumbBar = ({ children }: React.PropsWithChildren) => {
