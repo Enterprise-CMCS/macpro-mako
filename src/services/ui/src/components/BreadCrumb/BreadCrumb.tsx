36b1a7f2eaec6797febd8eb30b74f018880cf520
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-10-05 14:55:54 -0400

    add initial breadcrumbs with light styles

--- src/services/ui/src/components/BreadCrumb/BreadCrumb.tsx
+++ src/services/ui/src/components/BreadCrumb/BreadCrumb.tsx
@@ -1,28 +1,44 @@
 import { Link } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 import clsx from "clsx";
+import { type ReactNode } from "react";
+import { ChevronRight } from "lucide-react";
 
 type BreadCrumbProps = {
   to: string;
   active?: boolean;
+  showSeperator?: boolean;
+  seperator?: ReactNode;
 };
 
 export const BreadCrumb = ({
   to,
+  seperator = <BreadCrumbSeperator />,
+  showSeperator = true,
   active = false,
   children,
 }: React.PropsWithChildren<BreadCrumbProps>) => {
   const activeClass = "underline";
+
   return (
-    <Link
-      className={twMerge(clsx(active && activeClass), "hover:underline")}
-      to={to}
-    >
-      {children}
-    </Link>
+    <li className="flex items-center">
+      <div>{showSeperator && seperator}</div>
+      <Link
+        className={twMerge(clsx(active && activeClass), "hover:underline")}
+        to={to}
+      >
+        <div>{children}</div>
+      </Link>
+    </li>
   );
 };
 
+export const BreadCrumbSeperator = () => <ChevronRight className="w-5 h-5" />;
+
 export const BreadCrumbBar = ({ children }: React.PropsWithChildren) => {
-  return <div className="flex flex-col lg:flex-row">{children}</div>;
+  return (
+    <nav>
+      <ul className="flex flex-col gap-1 lg:flex-row">{children}</ul>
+    </nav>
+  );
 };
