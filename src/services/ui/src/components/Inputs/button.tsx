4ab22957c92bf026954870a4772768d2230dc2cf
Gavin St. Ours <gst-ours@fearless.tech>
2023-10-11 09:34:54 -0400

    Restore hover state for outline variation

--- src/services/ui/src/components/Inputs/button.tsx
+++ src/services/ui/src/components/Inputs/button.tsx
@@ -13,7 +13,8 @@ const buttonVariants = cva(
           "bg-primary text-slate-50 hover:bg-accent hover:text-accent-foreground",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline: "border-2 border-primary text-primary font-bold",
+        outline:
+          "border-2 border-primary text-primary font-bold hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
