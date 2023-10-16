b1d4650b5efebbcbb8af6acbef7450c757fff223
Gavin St. Ours <gst-ours@fearless.tech>
2023-10-16 13:31:29 -0400

    Update radio group spacing to match design

--- src/services/ui/src/components/Inputs/radio-group.tsx
+++ src/services/ui/src/components/Inputs/radio-group.tsx
@@ -11,7 +11,7 @@ const RadioGroup = React.forwardRef<
 >(({ className, ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Root
-      className={cn("grid gap-2", className)}
+      className={cn("grid gap-5", className)}
       {...props}
       ref={ref}
     />
@@ -35,7 +35,7 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-6 w-6 rounded-full border-2 border-primary text-primary focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 -mt-1 mb-2",
+        "aspect-square h-6 w-6 rounded-full border-2 border-primary text-primary focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 -mt-1",
         className
       )}
       {...props}
