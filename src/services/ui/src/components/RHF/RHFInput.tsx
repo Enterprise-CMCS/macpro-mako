e3fe97ed7abb2876e25b68267cd7208b4427b691
Gavin St. Ours <gst-ours@fearless.tech>
2023-10-16 09:27:38 -0400

    Update styling for "New row" button to match Figma

--- src/services/ui/src/components/RHF/RHFInput.tsx
+++ src/services/ui/src/components/RHF/RHFInput.tsx
@@ -434,8 +434,16 @@ export const FieldArray = <TFields extends FieldValues>(
       })}
       <div className="flex items-center gap-3 mt-2">
         <div className="flex-1 h-1 border-b-[2px]" />
-        <Button type="button" size="sm" onClick={onAppend}>
-          + New row
+        <Button type="button" size="sm" onClick={onAppend} variant="outline">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5 mr-2"
+          >
+            <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+          </svg>
+          New row
         </Button>
         <div className="flex-1 h-1 border-b-[2px]" />
       </div>
