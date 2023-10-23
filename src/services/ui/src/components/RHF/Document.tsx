e1c25de453153d9d91b9bdc48f263798112380f6
Gavin St. Ours <gst-ours@fearless.tech>
2023-10-23 14:20:39 -0400

    Add border and top gradient to form

--- src/services/ui/src/components/RHF/Document.tsx
+++ src/services/ui/src/components/RHF/Document.tsx
@@ -9,19 +9,22 @@ export const RHFDocument = <TFieldValues extends FieldValues>(props: {
   control: Control<TFieldValues>;
 }) => {
   return (
-    <div className="py-4">
-      <div className="mb-5">
-        <FormLabel className="font-bold text-4xl px-8 font-serif">
-          {props.document.header}
-        </FormLabel>
+    <div>
+      <div className="h-[5px] bg-gradient-to-r from-primary from-50% to-[#02bfe7] to-[66%] rounded-t"></div>
+      <div className="py-4 px-8 border-2 border-t-0 mt-0">
+        <div className="my-5">
+          <FormLabel className="font-bold text-4xl px-8 font-serif">
+            {props.document.header}
+          </FormLabel>
+        </div>
+        {props.document.sections.map((SEC, index) => (
+          <RHFSection
+            key={`rhf-section-${index}-${SEC.title}`}
+            control={props.control}
+            section={SEC}
+          />
+        ))}
       </div>
-      {props.document.sections.map((SEC, index) => (
-        <RHFSection
-          key={`rhf-section-${index}-${SEC.title}`}
-          control={props.control}
-          section={SEC}
-        />
-      ))}
     </div>
   );
 };
