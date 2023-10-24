d6c44c8502cf9cb7364c91e3516b3c349e1ba2db
Mike Dial <michael.dial.4@gmail.com>
2023-10-24 11:50:53 -0400

    uploads serfvice and presigned url generation

---
+++ src/services/api/handlers/getUploadUrl.ts
@@ -0,0 +1,52 @@
+import { response } from "../libs/handler";
+import { APIGatewayEvent } from "aws-lambda";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { v4 as uuidv4 } from "uuid";
+
+checkEnvVariables(["attachmentsBucketName", "attachmentsBucketRegion"]);
+
+const s3 = new S3Client({
+  region: process.env.attachmentsBucketRegion,
+});
+
+export const handler = async (event: APIGatewayEvent) => {
+  try {
+    const body = JSON.parse(event.body);
+    const bucket = process.env.attachmentsBucketName;
+    const key = uuidv4();
+    const url = await getSignedUrl(
+      s3,
+      new PutObjectCommand({
+        Bucket: bucket,
+        Key: key,
+      }),
+      {
+        expiresIn: 60,
+      }
+    );
+
+    return response<unknown>({
+      statusCode: 200,
+      body: { url, bucket, key },
+    });
+  } catch (error) {
+    console.error({ error });
+    return response({
+      statusCode: 500,
+      body: { message: "Internal server error" },
+    });
+  }
+};
+
+function checkEnvVariables(requiredEnvVariables) {
+  const missingVariables = requiredEnvVariables.filter(
+    (envVar) => !process.env[envVar]
+  );
+
+  if (missingVariables.length > 0) {
+    throw new Error(
+      `Missing required environment variables: ${missingVariables.join(", ")}`
+    );
+  }
+}
