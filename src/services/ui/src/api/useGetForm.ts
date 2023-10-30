53ca0ec50a9bd581ccbb3da2cc956eaa4a64de3e
Gavin St. Ours <gst-ours@fearless.tech>
2023-10-30 10:36:56 -0400

    Export useGetForm

--- src/services/ui/src/api/useGetForm.ts
+++ src/services/ui/src/api/useGetForm.ts
@@ -2,20 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import { API } from "aws-amplify";
 import { OsHit, OsMainSourceItem, ReactQueryApiError } from "shared-types";
 
-export const getForm = async (
-  formId: string,
-  formVersion?: number
-): Promise<OsHit<OsMainSourceItem>> => {
-  const form = await API.post("os", "/forms", {
-    body: { formId, formVersion },
-  });
+export const getForm = async (id: string): Promise<OsHit<OsMainSourceItem>> => {
+  const form = await API.post("os", "/forms", { body: { id } });
+
+  console.info("form", form);
 
   return form;
 };
 
-export const useGetForm = (id: string, formVersion?: number) => {
+export const useGetForm = (id: string) => {
   return useQuery<OsHit<OsMainSourceItem>, ReactQueryApiError>(
     ["formID", id],
-    () => getForm(id, formVersion)
+    () => getForm(id)
   );
 };
