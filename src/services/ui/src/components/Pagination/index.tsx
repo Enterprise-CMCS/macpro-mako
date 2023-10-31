6dbb9f606ce01f3bebe6702054ef250b8b4e1890
James Dinh <jdinh@collabralink.com>
2023-10-31 12:54:58 -0700

    Update index.tsx

--- src/services/ui/src/components/Pagination/index.tsx
+++ src/services/ui/src/components/Pagination/index.tsx
@@ -99,11 +99,11 @@ export const Pagination: FC<Props> = (props) => {
                   className={cn(
                     "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0",
                     {
-                      "bg-blue-400": isActive,
+                      "bg-blue-700": isActive,
                       "focus-visible:outline-indigo-600": isActive,
                       "text-white": isActive,
                       "hover:text-black": isActive,
-                      "hover:bg-blue-400": isActive,
+                      "hover:bg-blue-700": isActive,
                     }
                   )}
                 >
