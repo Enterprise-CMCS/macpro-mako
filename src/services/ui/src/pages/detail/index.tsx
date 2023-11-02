bbfade0b51edd72c8054124c1db9f139a2dd2a32
Kevin Haube <khaube@fearless.tech>
2023-11-02 14:09:54 -0400

    Fix box placement

--- src/services/ui/src/pages/detail/index.tsx
+++ src/services/ui/src/pages/detail/index.tsx
@@ -10,7 +10,7 @@ import {
   SubmissionInfo,
 } from "@/components";
 import { useGetUser } from "@/api/useGetUser";
-import { ItemResult } from "shared-types";
+import { CognitoUserAttributes, ItemResult } from "shared-types";
 import { useQuery } from "@/hooks";
 import { useGetItem } from "@/api";
 import { BreadCrumbs } from "@/components/BreadCrumb";
@@ -18,31 +18,49 @@ import { BREAD_CRUMB_CONFIG_PACKAGE_DETAILS } from "@/components/BreadCrumb/brea
 import { mapActionLabel } from "@/utils";
 import { Link } from "react-router-dom";
 import { useGetPackageActions } from "@/api/useGetPackageActions";
+import { PropsWithChildren } from "react";
 
+const DetailCardWrapper = ({
+  title,
+  children,
+}: PropsWithChildren<{
+  title: string;
+}>) => (
+  <CardWithTopBorder className="max-w-[18rem]">
+    <div className="p-4">
+      <h2>{title}</h2>
+      {children}
+    </div>
+  </CardWithTopBorder>
+);
+const StatusCard = ({ isCms, data }: { isCms: boolean; data: ItemResult }) => (
+  <DetailCardWrapper title={"Status"}>
+    <div>
+      <h2 className="text-xl font-semibold">
+        {isCms ? data._source.cmsStatus : data._source.stateStatus}
+      </h2>
+    </div>
+  </DetailCardWrapper>
+);
 const PackageActionsCard = ({ id }: { id: string }) => {
   const { data, error } = useGetPackageActions(id);
   if (!data?.actions || error) return <LoadingSpinner />;
   return (
-    <section id="package-actions" className="block md:flex mb-8 gap-8">
-      <CardWithTopBorder>
-        <div className="p-4">
-          <p className="text-gray-600 font-semibold mb-2">Actions</p>
-          <div>
-            <ul>
-              {data.actions.map((action, idx) => (
-                <Link
-                  className="text-sky-500 underline"
-                  to={`/action/${action}`}
-                  key={`${idx}-${action}`}
-                >
-                  <li>{mapActionLabel(action)}</li>
-                </Link>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </CardWithTopBorder>
-    </section>
+    <DetailCardWrapper title={"Actions"}>
+      <div>
+        <ul>
+          {data.actions.map((action, idx) => (
+            <Link
+              className="text-sky-500 underline"
+              to={`/action/${action}`}
+              key={`${idx}-${action}`}
+            >
+              <li>{mapActionLabel(action)}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </DetailCardWrapper>
   );
 };
 
@@ -71,23 +89,13 @@ export const DetailsContent = ({ data }: { data?: ItemResult }) => {
         ))}
       </aside>
       <div className="flex-1">
-        <div className="flex gap-8">
-          <section id="package-overview" className="block md:flex mb-8 gap-8">
-            <CardWithTopBorder>
-              <div className="p-4">
-                <p className="text-gray-600 font-semibold mb-2">Status</p>
-                <div>
-                  <h2 className="text-xl font-semibold mb-2">
-                    {user?.isCms
-                      ? data._source.cmsStatus
-                      : data._source.stateStatus}
-                  </h2>
-                </div>
-              </div>
-            </CardWithTopBorder>
-          </section>
+        <section
+          id="package-overview"
+          className="sm:flex lg:grid lg:grid-cols-2 gap-4 my-6"
+        >
+          <StatusCard isCms={user?.isCms || false} data={data} />
           <PackageActionsCard id={data._id} />
-        </div>
+        </section>
         <DetailsSection id="package-details" title="Package Details">
           <ChipSpaPackageDetails {...data?._source} />
         </DetailsSection>
