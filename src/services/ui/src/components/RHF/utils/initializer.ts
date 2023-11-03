8553ad3b9345a3a6b861360b3b8b7d9d1d75571b
Paul Kim <pkim@gswell.com>
2023-11-03 09:11:22 -0600

    feat(webform): doc validator

--- src/services/ui/src/components/RHF/utils/initializer.ts
+++ src/services/ui/src/components/RHF/utils/initializer.ts
@@ -2,27 +2,33 @@ import * as T from "@/components/RHF/types";
 
 type GL = Record<string, any>;
 
-export const formGroupReducer = (ACC: GL, FORM: T.FormGroup) => {
-  FORM.slots.reduce(slotReducer, ACC);
+export const formGroupInitializer = (ACC: GL, FORM: T.FormGroup) => {
+  FORM.slots.reduce(slotInitializer, ACC);
   return ACC;
 };
 
-export const slotReducer = (ACC: GL, SLOT: T.RHFSlotProps): GL => {
+export const slotInitializer = (ACC: GL, SLOT: T.RHFSlotProps): GL => {
   const optionReducer = (OPT: T.RHFOption) => {
-    if (OPT.form) OPT.form.reduce(formGroupReducer, ACC);
-    if (OPT.slots) OPT.slots.reduce(slotReducer, ACC);
+    if (OPT.form) OPT.form.reduce(formGroupInitializer, ACC);
+    if (OPT.slots) OPT.slots.reduce(slotInitializer, ACC);
     return ACC;
   };
 
-  const fieldReducer = (ACC1: GL, SLOT: T.RHFSlotProps): GL => {
+  const fieldInitializer = (ACC1: GL, SLOT: T.RHFSlotProps): GL => {
     if (SLOT.rhf === "FieldArray") {
-      return { ...ACC1, [SLOT.name]: [SLOT.fields?.reduce(fieldReducer, {})] };
+      return {
+        ...ACC1,
+        [SLOT.name]: [SLOT.fields?.reduce(fieldInitializer, {})],
+      };
     }
     if (SLOT.rhf === "FieldGroup") {
-      return { ...ACC1, [SLOT.name]: [SLOT.fields?.reduce(fieldReducer, {})] };
+      return {
+        ...ACC1,
+        [SLOT.name]: [SLOT.fields?.reduce(fieldInitializer, {})],
+      };
     }
 
-    return { ...ACC1, ...slotReducer(ACC1, SLOT) };
+    return { ...ACC1, ...slotInitializer(ACC1, SLOT) };
   };
 
   if (SLOT.rhf === "Input") ACC[SLOT.name] = "";
@@ -52,16 +58,16 @@ export const slotReducer = (ACC: GL, SLOT: T.RHFSlotProps): GL => {
   }
 
   if (SLOT.rhf === "FieldArray")
-    ACC[SLOT.name] = [SLOT.fields?.reduce(fieldReducer, {})];
+    ACC[SLOT.name] = [SLOT.fields?.reduce(fieldInitializer, {})];
   if (SLOT.rhf === "FieldGroup")
-    ACC[SLOT.name] = [SLOT.fields?.reduce(fieldReducer, {})];
+    ACC[SLOT.name] = [SLOT.fields?.reduce(fieldInitializer, {})];
 
   return ACC;
 };
 
 export const documentInitializer = (document: T.Document) => {
   return document.sections.reduce((ACC, SEC) => {
-    SEC.form.reduce(formGroupReducer, ACC);
+    SEC.form.reduce(formGroupInitializer, ACC);
     return ACC;
   }, {} as any);
 };
--- src/services/ui/src/pages/form/index.tsx
+++ src/services/ui/src/pages/form/index.tsx
@@ -3,7 +3,7 @@ import { Button, Form } from "@/components/Inputs";
 
 import { RHFDocument } from "@/components/RHF";
 import { ABP1 } from "./proto";
-import { documentInitializer } from "@/components/RHF";
+import { documentInitializer, documentValidator } from "@/components/RHF/utils";
 
 export function ExampleForm() {
   const defaultValues = documentInitializer(ABP1);
@@ -14,7 +14,70 @@ export function ExampleForm() {
 
   const onSubmit = form.handleSubmit(
     (data) => {
-      console.log({ data });
+      const validate = documentValidator(ABP1);
+      const isValid = validate({
+        alt_benefit_plan_population_name:
+          "agadgasdfg2f2fdsfascvcvaeqfwf22fasdfasdfsd",
+        is_enrollment_available: "no",
+        income_target: "income_target_below",
+        federal_poverty_level_percentage: "",
+        ssi_federal_benefit_percentage: "",
+        other_percentage: "",
+        other_describe: "",
+        income_definition_percentage: "other",
+        is_incremental_amount: false,
+        doller_incremental_amount: "",
+        income_definition_region_statewide_group: [
+          {
+            income_definition_region_statewide_arr: [
+              {
+                household_size: "",
+                standard: "",
+              },
+            ],
+            is_incremental_amount: false,
+            doller_incremental_amount: "",
+          },
+        ],
+        income_definition_specific: "other_standard",
+        income_definition: "income_definition_specific",
+        other_description: "asdfasdf",
+        health_conditions: ["physical_disability", "brain_injury", "hiv_aids"],
+        other_targeting_criteria_description: "",
+        target_criteria: [
+          "income_standard",
+          "health",
+          "other_targeting_criteria",
+        ],
+        is_geographic_area: "no",
+        specify_counties: "",
+        specify_regions: "",
+        specify_cities_towns: "",
+        specify_other: "",
+        geographic_variation: "other",
+        additional_information: "",
+        eligibility_groups: [
+          {
+            eligibility_group: "parents_caretaker_relatives",
+            mandatory_voluntary: "voluntary",
+          },
+        ],
+        income_definition_specific_statewide_group_other: [
+          {
+            name_of_group: "",
+            group_description: "",
+            is_incremental_amount: false,
+            doller_incremental_amount: "",
+            income_definition_specific_statewide_arr: [
+              {
+                household_size: "",
+                standard: "",
+              },
+            ],
+          },
+        ],
+      });
+      console.log({ isValid });
     },
     (err) => {
       console.log({ err });
