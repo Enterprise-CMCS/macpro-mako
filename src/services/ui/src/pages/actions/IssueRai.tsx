9943c1763ba931ac87965a395e052510550780bc
Mike Dial <michael.dial.4@gmail.com>
2023-11-07 06:02:30 -0500

    base

--- src/services/ui/src/pages/actions/IssueRai.tsx
+++ src/services/ui/src/pages/actions/IssueRai.tsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import * as I from "@/components/Inputs";
+import { API } from "aws-amplify";
 
 export const IssueRai = () => {
   const { id, type } = useParams<{
@@ -6,10 +8,31 @@ export const IssueRai = () => {
     type: string;
   }>();
 
+  async function handleClick() {
+    API.post("os", "/action", {
+      body: {
+        id: id,
+        action: "issueRai",
+      },
+    });
+  }
+
   return (
     <div>
       <span>ID: {id}</span>
+      <br />
       <span>Type: {type}</span>
+      <p>
+        <br />
+        <I.Button
+          type="button"
+          variant="outline"
+          onClick={() => handleClick()}
+          className="px-12"
+        >
+          Issue RAI
+        </I.Button>
+      </p>
     </div>
   );
 };
