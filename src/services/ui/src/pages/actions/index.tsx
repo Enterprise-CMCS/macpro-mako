76081ac99638c0eb767c05ff8eef4f820a0383b7
Mike Dial <michael.dial.4@gmail.com>
2023-11-07 05:40:31 -0500

    basic routing setup

--- src/services/ui/src/pages/actions/index.tsx
+++ src/services/ui/src/pages/actions/index.tsx
@@ -1,6 +1,7 @@
 import { Navigate, useParams } from "react-router-dom";
 import { ROUTES } from "@/routes";
 import { EnableRaiResponseWithdraw } from "@/pages/actions/EnableRaiResponseWithdraw";
+import { IssueRai } from "@/pages/actions/IssueRai";
 import { Action } from "shared-types";
 
 export const ActionFormIndex = () => {
@@ -8,6 +9,8 @@ export const ActionFormIndex = () => {
   switch (type) {
     case Action.ENABLE_RAI_WITHDRAW:
       return <EnableRaiResponseWithdraw />;
+    case Action.ISSUE_RAI:
+      return <IssueRai />;
     default:
       // TODO: Better error communication instead of navigate?
       //  "Hey, this action doesn't exist. Click to go back to the Dashboard."
