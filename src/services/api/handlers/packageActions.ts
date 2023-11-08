cc18f29df9c4d6b04e04a0296a0dc8c9070ff1ff
Mike Dial <michael.dial.4@gmail.com>
2023-11-08 13:26:49 -0500

    move to using a transaction with rollback etc

--- src/services/api/handlers/packageActions.ts
+++ src/services/api/handlers/packageActions.ts
@@ -28,27 +28,37 @@ export async function issueRai({
 }) {
   console.log("CMS issuing a new RAI");
   const pool = await sql.connect(config);
+  const transaction = new sql.Transaction(pool);
+  try {
+    await transaction.begin();
+    // Issue RAI
+    const query1 = `
+      Insert into SEA.dbo.RAI (ID_Number, RAI_Requested_Date)
+        values ('${id}'
+        ,dateadd(s, convert(int, left(${timestamp}, 10)), cast('19700101' as datetime)))
+    `;
+    const result1 = await transaction.request().query(query1);
+    console.log(result1);
 
-  // Create new RAI
-  const query1 = `
-    Insert into SEA.dbo.RAI (ID_Number, RAI_Requested_Date)
-      values ('${id}'
-      ,dateadd(s, convert(int, left(${timestamp}, 10)), cast('19700101' as datetime)))
-  `;
-  const result1 = await sql.query(query1);
-  console.log(result1);
+    // Update Status
+    const query2 = `
+      UPDATE SEA.dbo.State_Plan
+        SET SPW_Status_ID = (Select SPW_Status_ID from SEA.dbo.SPW_Status where SPW_Status_DESC = '${SEATOOL_STATUS.PENDING_RAI}')
+        WHERE ID_Number = '${id}'
+    `;
+    const result2 = await transaction.request().query(query2);
+    console.log(result2);
 
-  // Update Status
-  const query2 = `
-    UPDATE SEA.dbo.State_Plan
-      SET SPW_Status_ID = (Select SPW_Status_ID from SEA.dbo.SPW_Status where SPW_Status_DESC = '${SEATOOL_STATUS.PENDING_RAI}')
-      WHERE ID_Number = '${id}'
-  `;
-  const result2 = await sql.query(query2);
-  console.log(result2);
-
-  // Close pool
-  await pool.close();
+    // Commit transaction
+    await transaction.commit();
+  } catch (err) {
+    // Rollback and log
+    await transaction.rollback();
+    console.error("Error executing one or both queries:", err);
+  } finally {
+    // Close pool
+    await pool.close();
+  }
 }
 
 export async function withdrawRai(id, timestamp) {
