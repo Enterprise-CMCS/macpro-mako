053441fdae06e6cd9fb0baa7997b9b796651b01b
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2023-11-08 09:06:13 -0500

    remove action-type from schema (not needed)

--- src/packages/shared-types/action-types/withdraw-record.ts
+++ src/packages/shared-types/action-types/withdraw-record.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
-import { Action } from "../actions";
 
 export const withdrawRecordSchema = z.object({
-  actionType: z.nativeEnum(Action),
+  raiWithdrawEnabled: z.boolean(),
 });
