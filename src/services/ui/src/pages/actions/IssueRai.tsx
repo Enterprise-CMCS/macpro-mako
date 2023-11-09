fc11fbcd10edc71a22a9a43a75bff3bc552076e5
Mike Dial <michael.dial.4@gmail.com>
2023-11-09 18:17:36 -0500

    yut

--- src/services/ui/src/pages/actions/IssueRai.tsx
+++ src/services/ui/src/pages/actions/IssueRai.tsx
@@ -15,31 +15,100 @@ import {
 } from "@/components";
 import { FAQ_TARGET, ROUTES } from "@/routes";
 import { Link, useNavigate } from "react-router-dom";
-import { RaiSchema } from "shared-types";
+import { RaiTransform } from "shared-types";
 import { useGetUser } from "@/api/useGetUser";
 
 const formSchema = z.object({
   additionalInformation: z.string().max(4000).optional(),
+  attachments: z.object({
+    raiSupportingDocs: z
+      .array(z.instanceof(File))
+      .refine((value) => value.length > 0, {
+        message: "Required",
+      }),
+  }),
 });
 export type IssueRaiFormSchema = z.infer<typeof formSchema>;
+type UploadKeys = keyof IssueRaiFormSchema["attachments"];
+export type PreSignedURL = {
+  url: string;
+  key: string;
+  bucket: string;
+};
+
+const attachmentList = [
+  {
+    name: "raiSupportingDocs",
+    label: "RAI Supplemental Documentation",
+    required: true,
+  },
+] as const;
 
 export const IssueRai = () => {
   const { id, type } = useParams<{
     id: string;
     type: string;
   }>();
-  const form = useForm<IssueRaiFormSchema>({
-    resolver: zodResolver(formSchema),
-  });
   const [successModalIsOpen, setSuccessModalIsOpen] = useState(false);
   const [errorModalIsOpen, setErrorModalIsOpen] = useState(false);
   const [cancelModalIsOpen, setCancelModalIsOpen] = useState(false);
+  const form = useForm<IssueRaiFormSchema>({
+    resolver: zodResolver(formSchema),
+  });
   const { data: user } = useGetUser();
   const handleSubmit: SubmitHandler<IssueRaiFormSchema> = async (data) => {
+    // Set the timestamp that will uniquely identify this RAI
     const timestamp = Math.floor(new Date().getTime() / 1000) * 1000; // Truncating to match seatool
-    const dataToSubmit: RaiSchema = {
+
+    const uploadKeys = Object.keys(data.attachments) as UploadKeys[];
+    const uploadedFiles: any[] = [];
+    const fileMetaData: NonNullable<
+      RaiTransform["rais"][number]["state"]["attachments"]
+    > = [];
+
+    const presignedUrls: Promise<PreSignedURL>[] = uploadKeys
+      .filter((key) => data.attachments[key] !== undefined)
+      .map(() =>
+        API.post("os", "/getUploadUrl", {
+          body: {},
+        })
+      );
+    const loadPresignedUrls = await Promise.all(presignedUrls);
+
+    uploadKeys
+      .filter((key) => data.attachments[key] !== undefined)
+      .forEach((uploadKey, index) => {
+        const attachmenListObject = attachmentList?.find(
+          (item) => item.name === uploadKey
+        );
+        const title = attachmenListObject ? attachmenListObject.label : "Other";
+        const fileGroup = data.attachments[uploadKey] as File[];
+
+        // upload all files in this group and track there name
+        for (const file of fileGroup) {
+          uploadedFiles.push(
+            fetch(loadPresignedUrls[index].url, {
+              body: file,
+              method: "PUT",
+            })
+          );
+
+          fileMetaData.push({
+            key: loadPresignedUrls[index].key,
+            filename: file.name,
+            title: title,
+            bucket: loadPresignedUrls[index].bucket,
+            uploadDate: Date.now(),
+          });
+        }
+      });
+
+    await Promise.all(uploadedFiles);
+
+    const dataToSubmit = {
       id: id!,
       additionalInformation: data?.additionalInformation ?? null,
+      attachments: fileMetaData,
       requestedDate: timestamp,
       submitterEmail: user?.user?.email ?? "N/A",
       submitterName:
@@ -53,7 +122,7 @@ export const IssueRai = () => {
         body: dataToSubmit,
       });
       console.log(actionResponse);
-      // setSuccessModalIsOpen(true);
+      setSuccessModalIsOpen(true);
       console.log("END OF TRY");
     } catch (err) {
       console.log(err);
@@ -84,6 +153,71 @@ export const IssueRai = () => {
               </strong>
             </p>
           </section>
+          {/*-------------------------------------------------------- */}
+          <section>
+            <h3 className="text-2xl font-bold font-sans">Attachments</h3>
+            <p>
+              Maximum file size of 80 MB per attachment.{" "}
+              <strong>
+                You can add multiple files per attachment type, except for the
+                CMS Form 179.
+              </strong>{" "}
+              Read the description for each of the attachment types on the{" "}
+              {
+                <Link
+                  to="/faq/#medicaid-spa-attachments"
+                  target={FAQ_TARGET}
+                  rel="noopener noreferrer"
+                  className="text-blue-700 hover:underline"
+                >
+                  FAQ Page
+                </Link>
+              }
+              .
+            </p>
+            <br />
+            <p>
+              We accept the following file formats:{" "}
+              <strong className="bold">.docx, .jpg, .png, .pdf, .xlsx,</strong>{" "}
+              and a few others. See the full list on the{" "}
+              {
+                <Link
+                  to="/faq/#acceptable-file-formats"
+                  target={FAQ_TARGET}
+                  rel="noopener noreferrer"
+                  className="text-blue-700 hover:underline"
+                >
+                  FAQ Page
+                </Link>
+              }
+              .
+            </p>
+            <br />
+            <p>
+              <I.RequiredIndicator />
+              At least one attachment is required.
+            </p>
+          </section>
+          {attachmentList.map(({ name, label, required }) => (
+            <I.FormField
+              key={name}
+              control={form.control}
+              name={`attachments.${name}`}
+              render={({ field }) => (
+                <I.FormItem>
+                  <I.FormLabel>
+                    {label}
+                    {required ? <I.RequiredIndicator /> : ""}
+                  </I.FormLabel>
+                  <I.Upload
+                    files={field?.value ?? []}
+                    setFiles={field.onChange}
+                  />
+                  <I.FormMessage />
+                </I.FormItem>
+              )}
+            />
+          ))}
           {/*-------------------------------------------------------- */}
           <I.FormField
             control={form.control}
