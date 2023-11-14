2bac638348692c4cce54437b5eab9366c23fd021
Mike Dial <michael.dial.4@gmail.com>
2023-11-14 14:56:48 -0500

    working with reindex

---
+++ src/services/data/handlers/index.ts
@@ -0,0 +1,50 @@
+import { Handler } from "aws-lambda";
+import { send, SUCCESS, FAILED } from "cfn-response-async";
+type ResponseStatus = typeof SUCCESS | typeof FAILED;
+import * as os from "./../../../libs/opensearch-lib";
+
+export const customResourceWrapperHandler: Handler = async (event, context) => {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  const responseData = {};
+  let responseStatus: ResponseStatus = SUCCESS;
+  try {
+    if (event.RequestType == "Create" || event.RequestType == "Update") {
+      await manageIndex();
+    }
+  } catch (error) {
+    console.log(error);
+    responseStatus = FAILED;
+  } finally {
+    console.log("finally");
+    await send(event, context, responseStatus, responseData);
+  }
+};
+
+export const handler: Handler = async (event) => {
+  await manageIndex();
+};
+
+async function manageIndex() {
+  try {
+    const createIndexReponse = await os.createIndexIfNotExists(
+      process.env.osDomain,
+      "main"
+    );
+    console.log(createIndexReponse);
+
+    const updateFieldMappingResponse = await os.updateFieldMapping(
+      process.env.osDomain,
+      "main",
+      {
+        rais: {
+          type: "object",
+          enabled: false,
+        },
+      }
+    );
+    console.log(updateFieldMappingResponse);
+  } catch (error) {
+    console.log(error);
+    throw "ERROR:  Error occured during index management.";
+  }
+}
