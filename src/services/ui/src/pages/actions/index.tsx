c61ebbd669e710d0c88acd21984ea2caccdbe591
Adewale Sangobiyi <asangobiyi@adewale-mac.mynetworksettings.com>
2023-11-15 14:58:28 -0500

    feat(merge master): merge master

--- src/services/ui/src/pages/actions/index.tsx
+++ src/services/ui/src/pages/actions/index.tsx
@@ -1,29 +1,20 @@
 import { Navigate, useParams } from "react-router-dom";
 import { ROUTES } from "@/routes";
-<<<<<<< HEAD
-import { EnableRaiResponseWithdraw } from "@/pages/actions/EnableRaiResponseWithdraw";
-import {WithdrawPackage}from '@/pages/actions/WithdrawPackage';
-=======
 import { ToggleRaiResponseWithdraw } from "@/pages/actions/ToggleRaiResponseWithdraw";
 import { IssueRai } from "@/pages/actions/IssueRai";
->>>>>>> master
+import { WithdrawPackage } from "@/pages/actions/WithdrawPackage";
 import { Action } from "shared-types";
-
 
 export const ActionFormIndex = () => {
   const { type } = useParams<{ type: Action }>();
   switch (type) {
-    case Action.ENABLE_RAI_WITHDRAW:
-<<<<<<< HEAD
-      return <EnableRaiResponseWithdraw />;
     case Action.WITHDRAW_PACKGAGES:
       return <WithdrawPackage />;
-=======
+    case Action.ENABLE_RAI_WITHDRAW:
     case Action.DISABLE_RAI_WITHDRAW:
       return <ToggleRaiResponseWithdraw />;
     case Action.ISSUE_RAI:
       return <IssueRai />;
->>>>>>> master
     default:
       return <Navigate to={ROUTES.HOME} />;
   }
