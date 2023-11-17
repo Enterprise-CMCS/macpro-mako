647ba6614d5ff148f7c51f40b5bb6343acbecfa9
Mike Dial <michael.dial.4@gmail.com>
2023-11-17 14:34:42 -0500

    put a sentence back

--- src/services/ui/src/pages/actions/RespondToRai.tsx
+++ src/services/ui/src/pages/actions/RespondToRai.tsx
@@ -187,8 +187,9 @@ export const RespondToRai = () => {
           <section>
             <h3 className="text-2xl font-bold font-sans">Attachments</h3>
             <p>
-              Maximum file size of 80 MB per attachment. Read the description
-              for each of the attachment types on the{" "}
+              Maximum file size of 80 MB per attachment.{" "}
+              <strong>You can add multiple files per attachment type.</strong>{" "}
+              Read the description for each of the attachment types on the{" "}
               {
                 <Link
                   to="/faq/#medicaid-spa-attachments"
