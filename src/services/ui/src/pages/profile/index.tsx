2c517f5dcf585ff0ac9d89a63694aaa61a8b0732
Gavin St. Ours <gst-ours@fearless.tech>
2023-11-22 14:21:21 -0500

    Add alert banner

--- src/services/ui/src/pages/profile/index.tsx
+++ src/services/ui/src/pages/profile/index.tsx
@@ -1,5 +1,6 @@
 import { useGetUser } from "@/api/useGetUser";
-import { CardWithTopBorder } from "@/components";
+import { Alert, SubNavHeader } from "@/components";
+import { Button } from "@/components/Inputs";
 import { UserRoles, UserRolesString } from "shared-types";
 
 // Returns comma-separated string of user role descriptions:
@@ -36,7 +37,35 @@ export const Profile = () => {
 
   return (
     <>
-      <div className="flex flex-row justify-center gap-8 max-w-screen-xl mx-auto p-4 py-8">
+      <SubNavHeader>
+        <h1 className="text-xl font-medium">My Profile</h1>
+      </SubNavHeader>
+
+      <section className="block max-w-screen-xl m-auto px-4 lg:px-8 py-8 gap-10">
+        <Alert className="mb-6 bg-sky-100 flex flex-row">
+          <div className="py-1 mr-2 flex-none w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+
+          <div className="py-2 flex-1">
+            All changes to your access or profile must be made in IDM.
+          </div>
+          <a href="https://idm.cms.gov/" target="_blank" rel="noreferrer">
+            <Button variant="outline">Go to IDM</Button>
+          </a>
+        </Alert>
+
         <div className="basis-1/2 flex flex-col gap-6">
           <h1 className="text-2xl font-bold">My Information</h1>
 
@@ -65,6 +94,7 @@ export const Profile = () => {
             </p>
           </div>
         </div>
+      </section>
     </>
   );
 };
