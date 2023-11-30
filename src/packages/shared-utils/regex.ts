1778eb759a765a5da013be670b33d06ad3a5e70b
Benjamin Paige <benjamin.paige@gmail.com>
2023-11-30 11:43:57 -0700

    Prettier

--- src/packages/shared-utils/regex.ts
+++ src/packages/shared-utils/regex.ts
@@ -1,37 +1,44 @@
 export const convertRegexToString = (obj: any) => {
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            const value = obj[key];
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const value = obj[key];
 
-            if (value instanceof RegExp) {
-            // Convert RegExp to string
-            const str = value.toString();
-            // save it in this weird array thing
-            obj[key] = /\/(.*)\/(.*)/.exec(str)
-            } else if (typeof value === 'object' && value !== null) {
-            // Recursively process nested objects
-            obj[key] = convertRegexToString(value);
-            }
-        }
+      if (value instanceof RegExp) {
+        // Convert RegExp to string
+        const str = value.toString();
+        // save it in this weird array thing
+        obj[key] = /\/(.*)\/(.*)/.exec(str);
+      } else if (typeof value === "object" && value !== null) {
+        // Recursively process nested objects
+        obj[key] = convertRegexToString(value);
+      }
     }
+  }
 
-    return obj;
-}
+  return obj;
+};
 
 export const reInsertRegex = (obj: any) => {
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        if (typeof obj[key] === 'object' && obj[key] !== null) {
-          // If the current property is an object, recursively call the function
-          obj[key] = reInsertRegex(obj[key]);
-  
-          // Check if the current object has a property "pattern" with a "value" key
-          if (obj[key].hasOwnProperty('pattern') && typeof obj[key].pattern === 'object' && obj[key].pattern.hasOwnProperty('value')) {
-            // if its a pattern.value replace the value's value with a regex from the weird array thing
-            obj[key].pattern.value = new RegExp(obj[key].pattern.value[1], obj[key].pattern.value[2])
-          }
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (typeof obj[key] === "object" && obj[key] !== null) {
+        // If the current property is an object, recursively call the function
+        obj[key] = reInsertRegex(obj[key]);
+
+        // Check if the current object has a property "pattern" with a "value" key
+        if (
+          obj[key].hasOwnProperty("pattern") &&
+          typeof obj[key].pattern === "object" &&
+          obj[key].pattern.hasOwnProperty("value")
+        ) {
+          // if its a pattern.value replace the value's value with a regex from the weird array thing
+          obj[key].pattern.value = new RegExp(
+            obj[key].pattern.value[1],
+            obj[key].pattern.value[2]
+          );
         }
       }
     }
-    return obj;
   }
+  return obj;
+};
