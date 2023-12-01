e4e3a110e8f9b5ca9bd3732a0e2e185e44e62a81
Benjamin Paige <benjamin.paige@gmail.com>
2023-12-01 12:23:29 -0700

    adjust a thing

--- src/services/ui/src/components/Layout/index.tsx
+++ src/services/ui/src/components/Layout/index.tsx
@@ -20,7 +20,7 @@ import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import config from "@/config";
 
 const getLinks = (isAuthenticated: boolean, role?: boolean) => {
-  const isProd = window && window.location.hostname.includes("mako.cms.gov");
+  const isProd = window && window.location.hostname === "mako.cms.gov";
   return [
     {
       name: "Home",
@@ -241,7 +241,7 @@ const ResponsiveNav = ({ isDesktop }: ResponsiveNavProps) => {
             <>
               {data.user ? (
                 // When the user is signed in
-                <UserDropdownMenu/>
+                <UserDropdownMenu />
               ) : (
                 // When the user is not signed in
                 <>
