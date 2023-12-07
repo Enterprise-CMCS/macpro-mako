2de13b425402fd2cd63f6fba943a22a7b67b75d4
Mike Dial <michael.dial.4@gmail.com>
2023-12-07 13:45:17 -0500

    Remove now unused file

--- src/packages/shared-types/action-types/withdraw-record.ts
+++ src/packages/shared-types/action-types/withdraw-record.ts
@@ -1,46 +1,7 @@
 import { z } from "zod";
-import { onemacAttachmentSchema } from "../onemac";
-
-export const withdrawRaiSchema = z.object({
-  id: z.string(),
-  submitterName: z.string(),
-  submitterEmail: z.string(),
-  attachments: z.array(onemacAttachmentSchema.nullable()).nullish(),
-  withdrawDate: z.number(),
-  additionalInformation: z.string(),
-});
-
-export const raiActionSchema = z.object({
-  id: z.string(),
-  submitterName: z.string(),
-  submitterEmail: z.string(),
-  rais: z.record(z.string(), z.object({ response: z.null() })),
-  withdrawRaiMetaData: z.object({
-    additionalInformation: z.string().nullish(),
-    attachments: z.array(onemacAttachmentSchema.nullable()).nullish(),
-  }),
-});
-
-export const raiTransform = (activeKey: number) =>
-  withdrawRaiSchema.transform((data) => ({
-    id: data.id,
-    submitterName: data.submitterName,
-    submitterEmail: data.submitterEmail,
-    rais: {
-      [activeKey]: {
-        response: null,
-      },
-    },
-    withdrawRaiMetaData: {
-      additionalInformation: data.additionalInformation,
-      attachments: data.attachments,
-    },
-  }));
 
 export const withdrawRecordSchema = z.object({
   raiWithdrawEnabled: z.boolean(),
 });
 
-export type WithdrawRaiRecord = z.infer<typeof withdrawRaiSchema>;
 export type WithdrawRecord = z.infer<typeof withdrawRecordSchema>;
-export type WithdrawSinkRecord = z.infer<typeof raiActionSchema>;
