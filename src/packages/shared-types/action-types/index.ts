9332ddcbc9b6ca8bc73c851ba7f4d591e95dbded
Mike Dial <michael.dial.4@gmail.com>
2023-12-12 15:20:31 -0500

    rename file

--- src/packages/shared-types/action-types/index.ts
+++ src/packages/shared-types/action-types/index.ts
@@ -2,4 +2,4 @@ export * from "./toggle-withdraw-rai-enabled";
 export * from "./issue-rai";
 export * from "./respond-to-rai";
 export * from "./withdraw-rai";
-export * from "./withdrawPackage";
+export * from "./withdraw-package";
