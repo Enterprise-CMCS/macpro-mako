e079aa23568c573b5ef7b03d6c117f0e55b4030a
Kevin Haube <khaube@fearless.tech>
2023-12-14 14:50:08 -0500

    Add ReviewTeam UI

--- src/services/ui/src/components/SubmissionInfo/index.tsx
+++ src/services/ui/src/components/SubmissionInfo/index.tsx
@@ -1,6 +1,29 @@
 import { BLANK_VALUE } from "@/consts";
 import { OsMainSourceItem } from "shared-types";
+import { useMemo, useState } from "react";
 
+export const ReviewTeamList = ({ team }: { team: string[] | undefined }) => {
+  const [expanded, setExpanded] = useState(false);
+  const displayTeam = useMemo(
+    () => (expanded ? team : team?.slice(0, 3)),
+    [expanded, team]
+  );
+
+  return !displayTeam || !displayTeam.length ? null : (
+    <ul>
+      {displayTeam.map((reviewer, idx) => (
+        <li key={`reviewteam-ul-${reviewer}-${idx}`}>{reviewer}</li>
+      ))}
+      {team && team?.length > 3 && (
+        <li className={"text-xs text-sky-600 hover:cursor-pointer"}>
+          <button onClick={() => setExpanded((prev) => !prev)}>
+            {expanded ? "Show less" : "Show more"}
+          </button>
+        </li>
+      )}
+    </ul>
+  );
+};
 export const SubmissionInfo = (data: OsMainSourceItem) => {
   let cpocName = "";
   if (data.leadAnalystName) {
@@ -26,6 +49,10 @@ export const SubmissionInfo = (data: OsMainSourceItem) => {
     {
       label: "Submission Source",
       value: <p className="text-lg">{submissionSource()}</p>,
+    },
+    {
+      label: "Review Team",
+      value: <ReviewTeamList team={data.reviewTeam} />,
     },
   ];
   return (
