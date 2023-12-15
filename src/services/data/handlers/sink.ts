aea472dff306bb69477b9dc8fe75005b55dab6be
Paul Kim <pkim@gswell.com>
2023-12-15 09:48:29 -0700

    feat(OY-26537): OS changelog package activity

--- src/services/data/handlers/sink.ts
+++ src/services/data/handlers/sink.ts
@@ -24,12 +24,22 @@ import {
   Action,
 } from "shared-types";
 
+type Event = {
+  records: Record<
+    string,
+    {
+      key: string;
+      value: string;
+    }[]
+  >;
+};
+
 if (!process.env.osDomain) {
   throw "ERROR:  process.env.osDomain is required,";
 }
 const osDomain: string = process.env.osDomain;
 
-export const seatool: Handler = async (event) => {
+export const seatool: Handler<Event> = async (event) => {
   const seaToolRecords: (SeaToolTransform | SeaToolRecordsToDelete)[] = [];
   const docObject: Record<string, SeaToolTransform | SeaToolRecordsToDelete> =
     {};
@@ -47,7 +57,7 @@ export const seatool: Handler = async (event) => {
         const record = { id, ...JSON.parse(decode(value)) };
         const validPlanTypeIds = [122, 123, 124, 125];
         const result = transformSeatoolData(id).safeParse(record);
-        if (result.success === false) {
+        if (!result.success) {
           console.log(
             "SEATOOL Validation Error. The following record failed to parse: ",
             JSON.stringify(record),
@@ -108,7 +118,7 @@ export const seatool: Handler = async (event) => {
   }
 };
 
-export const onemac: Handler = async (event) => {
+export const onemac: Handler<Event> = async (event) => {
   const oneMacRecords: (
     | OnemacTransform
     | OnemacRecordsToDelete
@@ -122,139 +132,10 @@ export const onemac: Handler = async (event) => {
   )[] = [];
 
   for (const recordKey of Object.keys(event.records)) {
-    for (const onemacRecord of event.records[recordKey] as {
-      key: string;
-      value: string;
-    }[]) {
+    for (const onemacRecord of event.records[recordKey]) {
       const { key, value } = onemacRecord;
-      if (value) {
-        const id: string = decode(key);
-        const record = { id, ...JSON.parse(decode(value)) };
-        console.log("THE RECORD:");
-        console.log(JSON.stringify(record, null, 2));
-        if (record?.origin == "micro") {
-          console.log("MICRO EVENT");
-          const isActionType = "actionType" in record;
-          if (isActionType) {
-            console.log("MICRO ACTION");
-            switch (record.actionType) {
-              case Action.ENABLE_RAI_WITHDRAW:
-              case Action.DISABLE_RAI_WITHDRAW: {
-                const result = toggleWithdrawRaiEnabledSchema.safeParse(record);
-                if (result.success) {
-                  // write to opensearch
-                  // account for compaction
-                  oneMacRecords.push({
-                    id,
-                    ...result.data,
-                  });
-                } else {
-                  console.log(
-                    `ERROR: Invalid Payload for this action type (${record.actionType})`
-                  );
-                }
-                break;
-              }
-              case Action.ISSUE_RAI: {
-                const result = transformRaiIssue(id).safeParse(record);
-                if (result.success) {
-                  oneMacRecords.push(result.data);
-                } else {
-                  console.log(
-                    `ERROR: Invalid Payload for this action type (${record.actionType})`
-                  );
-                }
-                break;
-              }
-              case Action.RESPOND_TO_RAI: {
-                console.log("RESPONDING");
-                const result = transformRaiResponse(id).safeParse(record);
-                if (result.success) {
-                  console.log(result.data);
-                  oneMacRecords.push(result.data);
-                } else {
-                  console.log(
-                    `ERROR: Invalid Payload for this action type (${record.actionType})`
-                  );
-                }
-                break;
-              }
-              case Action.WITHDRAW_RAI: {
-                console.log("WITHDRAWING RAI");
-                console.log("Withdraw Record", record);
 
-                const result = transformRaiWithdraw(id).safeParse(record);
-                if (result.success === true) {
-                  oneMacRecords.push({
-                    ...result.data,
-                    raiWithdrawEnabled: null,
-                  });
-                } else {
-                  console.log(
-                    `ERROR: Invalid Payload for this action type (${record.actionType})`
-                  );
-                  console.log(
-                    "The error is the following: ",
-                    result.error.message
-                  );
-                }
-                break;
-              }
-              case Action.WITHDRAW_PACKAGE: {
-                console.log("WITHDRAWING PACKAGE");
-                console.log("Withdraw Package Record", record);
-
-                const result = transformWithdrawPackage(id).safeParse(record);
-                if (result.success === true) {
-                  oneMacRecords.push({
-                    ...result.data,
-                  });
-                } else {
-                  console.log(
-                    `ERROR: Invalid Payload for this action type (${record.actionType})`
-                  );
-                  console.log(
-                    "The error is the following: ",
-                    result.error.message
-                  );
-                }
-                break;
-              }
-            }
-          } else {
-            console.log("MICRO NEW SUBMISSION");
-            // This is a micro submission
-            const result = transformOnemac(id).safeParse(record);
-            if (result.success === false) {
-              console.log(
-                "ONEMAC Validation Error. The following record failed to parse: ",
-                JSON.stringify(record),
-                "Because of the following Reason(s):",
-                result.error.message
-              );
-            } else {
-              oneMacRecords.push(result.data);
-            }
-          }
-        } else if (
-          record?.sk === "Package" && // testing if this is a legacy onemac package record
-          record.submitterName &&
-          record.submitterName !== "-- --"
-        ) {
-          // This is a legacy onemac package record
-          const result = transformOnemacLegacy(id).safeParse(record);
-          if (result.success === false) {
-            console.log(
-              "ONEMAC Validation Error. The following record failed to parse: ",
-              JSON.stringify(record),
-              "Because of the following Reason(s):",
-              result.error.message
-            );
-          } else {
-            oneMacRecords.push(result.data);
-          }
-        }
-      } else {
+      if (!value) {
         const id: string = decode(key);
         const oneMacTombstone: OnemacRecordsToDelete = {
           id,
@@ -266,16 +147,178 @@ export const onemac: Handler = async (event) => {
         };
 
         oneMacRecords.push(oneMacTombstone);
-
         console.log(
           `Record ${id} has been nullified with the following data: `,
           JSON.stringify(oneMacTombstone)
         );
+        continue;
+      }
+
+      // value exists
+
+      const id: string = decode(key);
+      const record = { id, ...JSON.parse(decode(value)) };
+      console.log("THE RECORD:");
+      console.log(JSON.stringify(record, null, 2));
+      // legacy onemac vs new onemac
+      if (record?.origin == "micro") {
+        console.log("MICRO EVENT");
+        const isActionType = "actionType" in record;
+        // no actionType === new
+        if (isActionType) {
+          console.log("MICRO ACTION");
+          switch (record.actionType) {
+            case Action.ENABLE_RAI_WITHDRAW:
+            case Action.DISABLE_RAI_WITHDRAW: {
+              const result = toggleWithdrawRaiEnabledSchema.safeParse(record);
+              if (result.success) {
+                // write to opensearch
+                // account for compaction
+                oneMacRecords.push({
+                  id,
+                  ...result.data,
+                });
+              } else {
+                console.log(
+                  `ERROR: Invalid Payload for this action type (${record.actionType})`
+                );
+              }
+              break;
+            }
+            case Action.ISSUE_RAI: {
+              const result = transformRaiIssue(id).safeParse(record);
+              if (result.success) {
+                oneMacRecords.push(result.data);
+              } else {
+                console.log(
+                  `ERROR: Invalid Payload for this action type (${record.actionType})`
+                );
+              }
+              break;
+            }
+            case Action.RESPOND_TO_RAI: {
+              console.log("RESPONDING");
+              const result = transformRaiResponse(id).safeParse(record);
+              if (result.success) {
+                console.log(result.data);
+                oneMacRecords.push(result.data);
+              } else {
+                console.log(
+                  `ERROR: Invalid Payload for this action type (${record.actionType})`
+                );
+              }
+              break;
+            }
+            case Action.WITHDRAW_RAI: {
+              console.log("WITHDRAWING RAI");
+              console.log("Withdraw Record", record);
+
+              const result = transformRaiWithdraw(id).safeParse(record);
+              if (result.success === true) {
+                oneMacRecords.push({
+                  ...result.data,
+                  raiWithdrawEnabled: null,
+                });
+              } else {
+                console.log(
+                  `ERROR: Invalid Payload for this action type (${record.actionType})`
+                );
+                console.log(
+                  "The error is the following: ",
+                  result.error.message
+                );
+              }
+              break;
+            }
+            case Action.WITHDRAW_PACKAGE: {
+              console.log("WITHDRAWING PACKAGE");
+              console.log("Withdraw Package Record", record);
+
+              const result = transformWithdrawPackage(id).safeParse(record);
+              if (result.success === true) {
+                oneMacRecords.push({
+                  ...result.data,
+                });
+              } else {
+                console.log(
+                  `ERROR: Invalid Payload for this action type (${record.actionType})`
+                );
+                console.log(
+                  "The error is the following: ",
+                  result.error.message
+                );
+              }
+              break;
+            }
+          }
+        } else {
+          console.log("MICRO NEW SUBMISSION");
+          // This is a micro submission
+          const result = transformOnemac(id).safeParse(record);
+          if (result.success === false) {
+            console.log(
+              "ONEMAC Validation Error. The following record failed to parse: ",
+              JSON.stringify(record),
+              "Because of the following Reason(s):",
+              result.error.message
+            );
+          } else {
+            oneMacRecords.push(result.data);
+          }
+        }
+      } else if (
+        record?.sk === "Package" && // testing if this is a legacy onemac package record
+        record.submitterName &&
+        record.submitterName !== "-- --"
+      ) {
+        // This is a legacy onemac package record
+        const result = transformOnemacLegacy(id).safeParse(record);
+        if (result.success === false) {
+          console.log(
+            "ONEMAC Validation Error. The following record failed to parse: ",
+            JSON.stringify(record),
+            "Because of the following Reason(s):",
+            result.error.message
+          );
+        } else {
+          oneMacRecords.push(result.data);
+        }
       }
     }
   }
   try {
     await os.bulkUpdateData(osDomain, "main", oneMacRecords);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * {
+ *  actionType: PackageAction,
+ *  data: T
+ * }
+ */
+
+export const changelog: Handler<Event> = async (event) => {
+  const data = Object.values(event.records).reduce((ACC, RECORDS) => {
+    RECORDS.forEach((REC) => {
+      //TODO: handle delete
+      if (!REC.value) return;
+
+      // NO transform??
+      ACC.push({
+        id: decode(REC.key),
+        timestamp: new Date().toISOString(),
+        ...JSON.parse(decode(REC.value)),
+      });
+    });
+
+    return ACC;
+  }, [] as any[]);
+
+  try {
+    await os.bulkUpdateData(osDomain, "main", data);
   } catch (error) {
     console.error(error);
   }
