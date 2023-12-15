3615df0aceeb80f9203c1a93b70558b4a30092fb
Kevin Haube <khaube@fearless.tech>
2023-12-15 10:16:22 -0500

    Add user type gates

--- src/services/ui/src/components/SubmissionInfo/index.tsx
+++ src/services/ui/src/components/SubmissionInfo/index.tsx
@@ -1,6 +1,14 @@
 import { BLANK_VALUE } from "@/consts";
 import { OsMainSourceItem } from "shared-types";
-import { useMemo, useState } from "react";
+import { isStateUser, isCmsUser } from "shared-utils";
+import { ReactElement, ReactNode, useMemo, useState } from "react";
+import { OneMacUser, useGetUser } from "@/api/useGetUser";
+
+type DetailSectionItem = {
+  label: string;
+  value: ReactNode;
+  canView: (u: OneMacUser | undefined) => boolean;
+};
 
 export const ReviewTeamList = ({ team }: { team: string[] | undefined }) => {
   const [expanded, setExpanded] = useState(false);
@@ -27,6 +35,7 @@ export const ReviewTeamList = ({ team }: { team: string[] | undefined }) => {
   );
 };
 export const SubmissionInfo = (data: OsMainSourceItem) => {
+  const { data: user } = useGetUser();
   const submissionSource = () => {
     if (data?.origin?.toLowerCase() === "onemac") {
       return "OneMAC";
@@ -34,38 +43,44 @@ export const SubmissionInfo = (data: OsMainSourceItem) => {
       return BLANK_VALUE;
     }
   };
-  const submissionDetails = [
+  const submissionDetails: DetailSectionItem[] = [
     {
       label: "Submitted By",
       value: <p className="text-lg">{data?.submitterName || BLANK_VALUE}</p>,
+      canView: () => true,
     },
     {
       label: "Submission Source",
       value: <p className="text-lg">{submissionSource()}</p>,
+      canView: () => true,
     },
     {
       label: "Subject",
       value: <p>{data?.subject || BLANK_VALUE}</p>,
+      canView: (u) => (!u || !u.user ? false : isCmsUser(u.user)),
     },
     {
       label: "Description",
       value: <p>{data?.description || BLANK_VALUE}</p>,
+      canView: (u) => (!u || !u.user ? false : isCmsUser(u.user)),
     },
     {
       label: "CPOC",
       value: <p className="text-lg">{data?.leadAnalystName || BLANK_VALUE}</p>,
+      canView: () => true,
     },
     {
       label: "Review Team (SRT)",
       value: <ReviewTeamList team={data.reviewTeam} />,
+      canView: (u) => (!u || !u.user ? false : isCmsUser(u.user)),
     },
   ];
   return (
     <>
       <hr className="my-4" />
       <div className="grid grid-cols-2 gap-4">
-        {submissionDetails.map(({ label, value }) => {
-          return (
+        {submissionDetails.map(({ label, value, canView }) => {
+          return !canView(user) ? null : (
             <div key={label}>
               <h3 className="text-sm">{label}</h3>
               {value}
