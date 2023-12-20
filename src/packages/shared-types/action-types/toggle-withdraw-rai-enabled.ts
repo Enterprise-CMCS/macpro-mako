5ad95ff989bc4a2c24e355bd7a1a30203e76912d
Mike Dial <michael.dial.4@gmail.com>
2023-12-20 11:17:45 -0500

    fix(add missing id in schema):  Add id to schema for toggle enable and disable rai response withdraw

--- src/packages/shared-types/action-types/toggle-withdraw-rai-enabled.ts
+++ src/packages/shared-types/action-types/toggle-withdraw-rai-enabled.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const toggleWithdrawRaiEnabledSchema = z.object({
+  id: z.string(),
   authority: z.string(),
   origin: z.string(),
   raiWithdrawEnabled: z.boolean(),
