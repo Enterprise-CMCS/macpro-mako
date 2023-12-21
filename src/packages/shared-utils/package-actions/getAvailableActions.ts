2a0f4fb62389f4353d1572fbabc1407643d294ed
Kevin Haube <khaube@fearless.tech>
2023-12-21 12:03:02 -0500

    Oops, wrong allowed plan type

--- src/packages/shared-utils/package-actions/getAvailableActions.ts
+++ src/packages/shared-utils/package-actions/getAvailableActions.ts
@@ -4,14 +4,14 @@ import {
   PlanCheck,
   PlanType,
 } from "../../shared-types";
-import { getLatestRai } from "../rai-helper";
 import rules from "./rules";
+import { getLatestRai } from "../rai-helper";
 
 export const getAvailableActions = (
   user: CognitoUserAttributes,
   result: OsMainSourceItem
 ) =>
-  PlanCheck(result.planType).is([PlanType.CHIP_SPA])
+  PlanCheck(result.planType).is([PlanType.MED_SPA])
     ? rules
         .filter((r) => r.check(result, user, getLatestRai(result?.rais)))
         .map((a) => a.action)
