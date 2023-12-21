356cc4ce0e6161853c8c9fa8aaafe912446a4837
Kevin Haube <khaube@fearless.tech>
2023-12-21 11:44:56 -0500

    Fix med spa gate logic

--- src/packages/shared-utils/package-actions/getAvailableActions.ts
+++ src/packages/shared-utils/package-actions/getAvailableActions.ts
@@ -5,12 +5,16 @@ import {
 } from "../../shared-types";
 import { getLatestRai } from "../rai-helper";
 import rules from "./rules";
+import { removeUnderscoresAndCapitalize } from "ui/src/utils";
 
 export const getAvailableActions = (
   user: CognitoUserAttributes,
   result: OsMainSourceItem
 ) =>
-  result?.planType && [Authority.MED_SPA].includes(result.planType)
+  result?.planType &&
+  [Authority.MED_SPA].includes(
+    removeUnderscoresAndCapitalize(result.planType) as Authority
+  )
     ? rules
         .filter((r) => r.check(result, user, getLatestRai(result?.rais)))
         .map((a) => a.action)
