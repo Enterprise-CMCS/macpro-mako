fe9c37a13a0ac86ca463a8821d381f2b5e7a5084
Benjamin Paige <benjamin.paige@gmail.com>
2023-12-27 16:17:57 -0700

    pages deploy

--- docs/_deploy-metrics/pages/webforms/index.tsx
+++ docs/_deploy-metrics/pages/webforms/index.tsx
@@ -7,6 +7,9 @@ import {
 
 export const getStaticProps = async () => {
     const webformsAvailable = [{id: 1234, name: 'abp', version: 1}]
+
+    console.log(process.env.API_REST_URL)
+
   return {
     props: {
       webformsAvailable,
@@ -18,6 +21,7 @@ const WebformsDocs = ({
     webformsAvailable,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
 
+    console.log(process.env.API_REST_URL)
   return (
     <Container centerContent>
       <Heading as="h1">Available Webforms</Heading>
