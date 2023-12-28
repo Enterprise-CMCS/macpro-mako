d9ce24fcd1ba342a85ff641a5602d257a404742d
James Dinh <jdinh@collabralink.com>
2023-12-28 12:58:10 -0800

    adjust styling

--- src/services/ui/src/pages/guides/index.tsx
+++ src/services/ui/src/pages/guides/index.tsx
@@ -9,195 +9,200 @@ export const ImplmentationGuide = () => {
         </h1>
       </SubNavHeader>
       <section className="max-w-screen-xl m-auto px-4 lg:px-8 pt-8 gap-10">
-        <h2 className="font-bold">Implementation Guides:</h2>
-        <ul className="p-3">
-          <li className="mb-2">
-            <a
-              className="underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_AbpIntroduction.doc"
-              rel="noreferrer"
-            >
-              Introduction
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP1:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP1_AlternativeBenefitPlanPopulations.doc"
-              rel="noreferrer"
-            >
-              Alternative Benefit Plan Populations
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP2a:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP2a_VoluntaryBenefitPackageAssurances.doc"
-              rel="noreferrer"
-            >
-              Voluntary Benefit Package Selection Assurances - Eligibility Group
-              under Section 1902(a)(10)(A)(i)(VIII) of the Act
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP2b:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP2b_VoluntaryEnrollmentAssurances.doc"
-              rel="noreferrer"
-            >
-              Voluntary Enrollment Assurances for Eligibility Groups other than
-              the Adult Group under Section 1902(a)(10)(A)(i)(VIII) of the Act
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP2c:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP2c_EnrollmentAssurancesMandatoryParticipants.doc"
-              rel="noreferrer"
-            >
-              Enrollment Assurances - Mandatory Participants
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP3:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP3_SelectionOfBenchmark20190819-Final.docx"
-              rel="noreferrer"
-            >
-              Selection of Benchmark Benefit Package or Benchmark-Equivalent
-              Benefit Package
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP3.1:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP3.1_SelectionOfBenchmark20190819-Final.docx"
-              rel="noreferrer"
-            >
-              Selection of Benchmark Benefit Package or Benchmark-Equivalent
-              Benefit Package
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP4:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP4_AbpCostSharing.doc"
-              rel="noreferrer"
-            >
-              Alternative Benefit Plan Cost-Sharing
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP5:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP5_BenefitsDescription-Final.docx"
-              rel="noreferrer"
-            >
-              Benefits Description
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP6:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP6_BenchmarkEquivalentBenefit.doc"
-              rel="noreferrer"
-            >
-              Benchmark-Equivalent Benefit Package
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP7:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP7_BenefitAssurances.doce"
-              rel="noreferrer"
-            >
-              Benefit Assurance
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP8:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP8_ServiceDeliverySystems.doc"
-              rel="noreferrer"
-            >
-              Service Delivery Systems
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP9:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP9_EmployerSponsoredInsurance.doc"
-              rel="noreferrer"
-            >
-              Employer Sponsored Insurance and Payment of Premiums
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP10:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP10_GeneralAssurances.doc"
-              rel="noreferrer"
-            >
-              General Assurances
-            </a>
-          </li>
-          <li className="mb-2">
-            ABP11:
-            <a
-              className="ml-2 underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP11_PaymentMethodology.doc"
-              rel="noreferrer"
-            >
-              Payment Methodology
-            </a>
-          </li>
-        </ul>
-        <h2 className="font-bold">Training Materials:</h2>
-        <ul className="p-3">
-          <li className="mb-2">
-            <a
-              className="underline"
-              href="https://www.medicaid.gov/media/162926"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Alternative Benefit Plan State Training Webinar (2013-08-13 .wmv)
-            </a>
-          </li>
-          <li className="mb-2">
-            <a
-              className="underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/ABPStateTraining.pdf"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Alternative Benefit Plan State Training (2020-04-24 .pdf)
-            </a>
-          </li>
-          <li className="mb-2">
-            <a
-              className="underline"
-              href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/ABPSPAProcess.pdf"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Alternative Benefit Plan SPA Process (2016-08-01 .pdf)
-            </a>
-          </li>
-        </ul>
+        <div className="h-[5px] bg-gradient-to-r from-primary from-50% to-[#02bfe7] to-[66%] rounded-t"></div>
+        <div className="py-4 px-8 border-2 border-t-0 mt-0 mb-5">
+          <h2 className="font-bold">Implementation Guides:</h2>
+          <ul className="p-3">
+            <li className="mb-2">
+              <a
+                className="underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_AbpIntroduction.doc"
+                rel="noreferrer"
+              >
+                Introduction
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP1:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP1_AlternativeBenefitPlanPopulations.doc"
+                rel="noreferrer"
+              >
+                Alternative Benefit Plan Populations
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP2a:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP2a_VoluntaryBenefitPackageAssurances.doc"
+                rel="noreferrer"
+              >
+                Voluntary Benefit Package Selection Assurances - Eligibility
+                Group under Section 1902(a)(10)(A)(i)(VIII) of the Act
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP2b:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP2b_VoluntaryEnrollmentAssurances.doc"
+                rel="noreferrer"
+              >
+                Voluntary Enrollment Assurances for Eligibility Groups other
+                than the Adult Group under Section 1902(a)(10)(A)(i)(VIII) of
+                the Act
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP2c:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP2c_EnrollmentAssurancesMandatoryParticipants.doc"
+                rel="noreferrer"
+              >
+                Enrollment Assurances - Mandatory Participants
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP3:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP3_SelectionOfBenchmark20190819-Final.docx"
+                rel="noreferrer"
+              >
+                Selection of Benchmark Benefit Package or Benchmark-Equivalent
+                Benefit Package
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP3.1:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP3.1_SelectionOfBenchmark20190819-Final.docx"
+                rel="noreferrer"
+              >
+                Selection of Benchmark Benefit Package or Benchmark-Equivalent
+                Benefit Package
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP4:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP4_AbpCostSharing.doc"
+                rel="noreferrer"
+              >
+                Alternative Benefit Plan Cost-Sharing
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP5:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP5_BenefitsDescription-Final.docx"
+                rel="noreferrer"
+              >
+                Benefits Description
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP6:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP6_BenchmarkEquivalentBenefit.doc"
+                rel="noreferrer"
+              >
+                Benchmark-Equivalent Benefit Package
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP7:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP7_BenefitAssurances.doce"
+                rel="noreferrer"
+              >
+                Benefit Assurance
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP8:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP8_ServiceDeliverySystems.doc"
+                rel="noreferrer"
+              >
+                Service Delivery Systems
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP9:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP9_EmployerSponsoredInsurance.doc"
+                rel="noreferrer"
+              >
+                Employer Sponsored Insurance and Payment of Premiums
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP10:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP10_GeneralAssurances.doc"
+                rel="noreferrer"
+              >
+                General Assurances
+              </a>
+            </li>
+            <li className="mb-2">
+              ABP11:
+              <a
+                className="ml-2 underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/IG_ABP11_PaymentMethodology.doc"
+                rel="noreferrer"
+              >
+                Payment Methodology
+              </a>
+            </li>
+          </ul>
+          <h2 className="font-bold">Training Materials:</h2>
+          <ul className="p-3">
+            <li className="mb-2">
+              <a
+                className="underline"
+                href="https://www.medicaid.gov/media/162926"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Alternative Benefit Plan State Training Webinar (2013-08-13
+                .wmv)
+              </a>
+            </li>
+            <li className="mb-2">
+              <a
+                className="underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/ABPStateTraining.pdf"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Alternative Benefit Plan State Training (2020-04-24 .pdf)
+              </a>
+            </li>
+            <li className="mb-2">
+              <a
+                className="underline"
+                href="https://wms-mmdl-dev.cms.gov/MMDLDOC/abp/ABPSPAProcess.pdf"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Alternative Benefit Plan SPA Process (2016-08-01 .pdf)
+              </a>
+            </li>
+          </ul>
+        </div>
       </section>
     </>
   );
