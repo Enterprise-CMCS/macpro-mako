b25e4bb3ef0f028e3ea0f740c3c81abd9fed13d0
Kevin Haube <khaube@fearless.tech>
2024-01-05 09:52:54 -0500

    Update WithdrawPackage action form

--- src/services/ui/src/pages/actions/WithdrawPackage.tsx
+++ src/services/ui/src/pages/actions/WithdrawPackage.tsx
@@ -1,9 +1,7 @@
-import { Navigate, useNavigate, useParams } from "@/components/Routing";
+import { Navigate, useParams } from "@/components/Routing";
 import { Button } from "@/components/Inputs";
-import { ConfirmationModal } from "@/components/Modal/ConfirmationModal";
 import { useState } from "react";
 import { PlanType, ItemResult } from "shared-types";
-import { PackageActionForm } from "./PackageActionForm";
 import { ActionFormIntro, PackageInfo } from "./common";
 import { z } from "zod";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -14,6 +12,7 @@ import { AttachmentRecipe, buildActionUrl } from "@/lib";
 import { useGetUser } from "@/api/useGetUser";
 import { submit } from "@/api/submissionService";
 import { AttachmentsSizeTypesDesc } from "@/pages/form/content";
+import { useModalContext } from "@/pages/form/modals";
 
 // Temporary, will be refactored to an extendable schema with Brian/Mike's back-end
 // work.
@@ -35,12 +34,14 @@ const attachments: AttachmentRecipe<WithdrawPackageFormSchema>[] = [
   } as const,
 ];
 
-const WithdrawPackageForm: React.FC = ({ item }: { item?: ItemResult }) => {
-  const [successModalIsOpen, setSuccessModalIsOpen] = useState(false);
-  const [errorModalIsOpen, setErrorModalIsOpen] = useState(false);
-  const [cancelModalIsOpen, setCancelModalIsOpen] = useState(false);
-  const navigate = useNavigate();
+export const WithdrawPackage = ({ item }: { item?: ItemResult }) => {
   const { id, type } = useParams("/action/:id/:type");
+  const {
+    cancelModalOpen,
+    setCancelModalOpen,
+    setSuccessModalOpen,
+    setErrorModalOpen,
+  } = useModalContext();
   const { data: user } = useGetUser();
   const authority = item?._source.authority as PlanType;
   const form = useForm<WithdrawPackageFormSchema>({
@@ -52,7 +53,7 @@ const WithdrawPackageForm: React.FC = ({ item }: { item?: ItemResult }) => {
     data
   ) => {
     try {
-      if (!cancelModalIsOpen) {
+      if (!cancelModalOpen) {
         if (
           !data.attachments.supportingDocumentation &&
           !data.additionalInformation
@@ -70,12 +71,12 @@ const WithdrawPackageForm: React.FC = ({ item }: { item?: ItemResult }) => {
             user,
             authority,
           });
-          setSuccessModalIsOpen(true);
+          setSuccessModalOpen(true);
         }
       }
     } catch (err) {
       console.log(err);
-      setErrorModalIsOpen(true);
+      setErrorModalOpen(true);
     }
   };
 
@@ -84,162 +85,83 @@ const WithdrawPackageForm: React.FC = ({ item }: { item?: ItemResult }) => {
     <>
       {form.formState.isSubmitting && <LoadingSpinner />}
       <div>
-        <div className="px-14  py-5 ">
-          <ActionFormIntro title="Withdraw Medicaid SPA Package">
-            <p>
-              Complete this form to withdraw a package. Once complete, you will
-              not be able to resubmit this package. CMS will be notified and
-              will use this content to review your request. If CMS needs any
-              additional information, they will follow up by email.
-            </p>
-          </ActionFormIntro>
-          <PackageInfo item={item} />
-          <I.Form {...form}>
-            <form onSubmit={form.handleSubmit(handleSubmit)}>
-              {/* Change faqLink once we know the anchor */}
-              <AttachmentsSizeTypesDesc faqLink={"/faq"} />
-              {attachments.map(({ name, label, required }) => (
-                <I.FormField
-                  key={name}
-                  control={form.control}
-                  name={`attachments.${name}`}
-                  render={({ field }) => (
-                    <I.FormItem className="mt-8">
-                      <I.FormLabel>
-                        {label}
-                        {required ? <I.RequiredIndicator /> : ""}
-                      </I.FormLabel>
-                      <I.Upload
-                        files={field?.value ?? []}
-                        setFiles={field.onChange}
-                      />
-                      <I.FormMessage />
-                    </I.FormItem>
-                  )}
-                />
-              ))}
+        <ActionFormIntro title="Withdraw Medicaid SPA Package">
+          <p>
+            Complete this form to withdraw a package. Once complete, you will
+            not be able to resubmit this package. CMS will be notified and will
+            use this content to review your request. If CMS needs any additional
+            information, they will follow up by email.
+          </p>
+        </ActionFormIntro>
+        <PackageInfo item={item} />
+        <I.Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)}>
+            {/* Change faqLink once we know the anchor */}
+            <AttachmentsSizeTypesDesc faqLink={"/faq"} />
+            {attachments.map(({ name, label, required }) => (
               <I.FormField
+                key={name}
                 control={form.control}
-                name="additionalInformation"
+                name={`attachments.${name}`}
                 render={({ field }) => (
                   <I.FormItem className="mt-8">
-                    <h3 className="font-bold text-2xl font-sans">
-                      Additional Information
-                    </h3>
-                    <I.FormLabel className="font-normal">
-                      Explain your need for withdrawal or upload supporting
-                      documentation.
-                      <br />
-                      <p>
-                        <em className="italic">
-                          Once you submit this form, a confirmation email is
-                          sent to you and to CMS. CMS will use this content to
-                          review your package. If CMS needs any additional
-                          information, they will follow up by email.
-                        </em>{" "}
-                      </p>
-                      <br />
+                    <I.FormLabel>
+                      {label}
+                      {required ? <I.RequiredIndicator /> : ""}
                     </I.FormLabel>
-                    <I.Textarea {...field} className="h-[200px] resize-none" />
-                    <I.FormDescription>
-                      4,000 characters allowed
-                    </I.FormDescription>
+                    <I.Upload
+                      files={field?.value ?? []}
+                      setFiles={field.onChange}
+                    />
+                    <I.FormMessage />
                   </I.FormItem>
                 )}
               />
-              {errorMessage && (
-                <div className="text-red-500 mt-4">{errorMessage}</div>
+            ))}
+            <I.FormField
+              control={form.control}
+              name="additionalInformation"
+              render={({ field }) => (
+                <I.FormItem className="mt-8">
+                  <h3 className="font-bold text-2xl font-sans">
+                    Additional Information
+                  </h3>
+                  <I.FormLabel className="font-normal">
+                    Explain your need for withdrawal or upload supporting
+                    documentation.
+                    <br />
+                    <p>
+                      <em className="italic">
+                        Once you submit this form, a confirmation email is sent
+                        to you and to CMS. CMS will use this content to review
+                        your package. If CMS needs any additional information,
+                        they will follow up by email.
+                      </em>{" "}
+                    </p>
+                    <br />
+                  </I.FormLabel>
+                  <I.Textarea {...field} className="h-[200px] resize-none" />
+                  <I.FormDescription>
+                    4,000 characters allowed
+                  </I.FormDescription>
+                </I.FormItem>
               )}
-              <div className="flex gap-2 my-8">
-                <Button type="submit">Submit</Button>
-                <Button
-                  onClick={() => setCancelModalIsOpen(true)}
-                  variant="outline"
-                >
-                  Cancel
-                </Button>
-              </div>
-            </form>
-          </I.Form>
-        </div>
-        {/* Success Modal */}
-        <ConfirmationModal
-          open={successModalIsOpen}
-          onAccept={() => {
-            setSuccessModalIsOpen(false);
-            navigate({ path: "/details", query: { id } });
-          }}
-          onCancel={() => setSuccessModalIsOpen(false)} // Should be made optional
-          title="Withdraw Successful"
-          body={
-            <p>
-              Please be aware that it may take up to a minute for your status to
-              change on the Dashboard and Details pages.
-            </p>
-          }
-          cancelButtonVisible={false}
-          acceptButtonText="Go to Package Details"
-        />
-        {/* Error Modal */}
-        <ConfirmationModal
-          open={errorModalIsOpen}
-          onAccept={() => {
-            setErrorModalIsOpen(false);
-            navigate({ path: "/details", query: { id } });
-          }}
-          onCancel={() => setErrorModalIsOpen(false)}
-          title="Submission Error"
-          body={
-            <p>
-              An error occurred during issue.
-              <br />
-              You may close this window and try again, however, this likely
-              requires support.
-              <br />
-              <br />
-              Please contact the{" "}
-              <a
-                href="mailto:OneMAC_Helpdesk@cms.hhs.gov"
-                className="text-blue-500"
+            />
+            {errorMessage && (
+              <div className="text-red-500 mt-4">{errorMessage}</div>
+            )}
+            <div className="flex gap-2 my-8">
+              <Button type="submit">Submit</Button>
+              <Button
+                onClick={() => setCancelModalOpen(true)}
+                variant="outline"
               >
-                helpdesk
-              </a>{" "}
-              . You may include the following in your support request: <br />
-              <br />
-              <ul>
-                <li>SPA ID: {id}</li>
-                <li>Timestamp: {Date.now()}</li>
-              </ul>
-            </p>
-          }
-          cancelButtonVisible={true}
-          cancelButtonText="Return to Form"
-          acceptButtonText="Exit to Package Details"
-        />
-        {/* Cancel Modal */}
-        <ConfirmationModal
-          open={cancelModalIsOpen}
-          onAccept={() => {
-            setCancelModalIsOpen(false);
-            navigate({ path: "/details", query: { id } });
-          }}
-          onCancel={() => setCancelModalIsOpen(false)}
-          cancelButtonText="Return to Form"
-          acceptButtonText="Leave Page"
-          title="Are you sure you want to cancel?"
-          body={
-            <p>
-              If you leave this page you will lose your progress on this form
-            </p>
-          }
-        />
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </I.Form>
       </div>
     </>
   );
 };
-
-export const WithdrawPackage = () => (
-  <PackageActionForm>
-    <WithdrawPackageForm />
-  </PackageActionForm>
-);
