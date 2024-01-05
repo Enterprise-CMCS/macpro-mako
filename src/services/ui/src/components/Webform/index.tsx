5c42c78d33cca4cd0ea2496efefcd7e3d2ce4f0b
James Dinh <jdinh@collabralink.com>
2024-01-05 12:11:32 -0800

    cleanup

--- src/services/ui/src/components/Webform/index.tsx
+++ src/services/ui/src/components/Webform/index.tsx
@@ -15,7 +15,7 @@ export const Webforms = () => {
         <h1 className="text-xl font-medium">Webforms</h1>
       </SubNavHeader>
       <section className="block md:flex md:flex-row max-w-screen-xl m-auto px-4 lg:px-8 pt-8 gap-10">
-        <div className="flex-1">
+        <div className="flex-1 space-x-5">
           <Link
             path="/webform/:id/:version"
             params={{ id: "abp1", version: 1 }}
@@ -26,7 +26,7 @@ export const Webforms = () => {
             path="/webform/:id/:version"
             params={{ id: "abp3_1", version: 1 }}
           >
-            ABP 1
+            ABP 3.1
           </Link>
         </div>
       </section>
