6e3642a47438decfdf0754758602c731d9641dc2
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-01-09 11:02:37 -0500

    remove comments. they don't belong there

--- src/services/api/features/package-actions/handlers/withdraw-rai-lambda.ts
+++ src/services/api/features/package-actions/handlers/withdraw-rai-lambda.ts
@@ -1,15 +1,10 @@
 import { z } from "zod";
 import { type APIGatewayEvent } from "aws-lambda";
-import { response } from "@/shared/lambda-response";
 import { handleEvent } from "@/features/package-actions/lambda-handler";
 import { PackageActionWriteService } from "@/features/package-actions/services/package-action-write-service";
 import { seatoolConnection } from "@/features/package-actions/consts/sql-connection";
 import { kafkaConfig } from "@/features/package-actions/consts/kafka-connection";
 import { TOPIC_NAME } from "../consts/kafka-topic-name";
-
-// three types of schema (form schema, action kafka event schema, api schema)
-// can we share the same schema (form and api) some how
-// how can we build on top of the established form schema
 
 export const withdrawRaiLambda = async (event: APIGatewayEvent) => {
   return await handleEvent({
@@ -20,7 +15,6 @@ export const withdrawRaiLambda = async (event: APIGatewayEvent) => {
       raiWithdrawnDate: z.number(),
     }),
     async lambda(data) {
-      // withdraw rai
       const packageActionService =
         await PackageActionWriteService.createPackageActionWriteService(
           kafkaConfig,
@@ -34,7 +28,6 @@ export const withdrawRaiLambda = async (event: APIGatewayEvent) => {
         withdrawnDate: data.raiWithdrawnDate,
       });
 
-      // we need to update the status eventually here
       await packageActionService.changePackageStatus({
         id: data.id,
         status: "Pending",
