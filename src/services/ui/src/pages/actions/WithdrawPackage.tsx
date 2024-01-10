9cadbaf5b9625bbfc18ab119e52a0a1458404d28
Kevin Haube <khaube@fearless.tech>
2024-01-10 16:09:15 -0500

    extract template and implement with WithdrawPackage

--- src/services/ui/src/pages/actions/WithdrawPackage.tsx
+++ src/services/ui/src/pages/actions/WithdrawPackage.tsx
@@ -1,22 +1,13 @@
 import { Navigate } from "@/components/Routing";
-import { Button } from "@/components/Inputs";
 import { PlanType, ItemResult } from "shared-types";
-import { ActionFormIntro, PackageInfo } from "./common";
+import { ActionFormIntro } from "./common";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as I from "@/components/Inputs";
-import { LoadingSpinner } from "@/components";
-import { AttachmentRecipe } from "@/lib";
-import { AttachmentsSizeTypesDesc } from "@/pages/form/content";
-import { useModalContext } from "@/pages/form/modals";
 import { useActionSubmitHandler } from "@/hooks/useActionFormController";
-import {
-  SlotAdditionalInfo,
-  SlotAttachments,
-} from "@/pages/actions/renderSlots";
+import { ActionFormTemplate } from "@/pages/actions/template";
 
-const withdrawPackageFormSchema = z.object({
+const formSchema = z.object({
   additionalInformation: z
     .string()
     .max(4000, "This field may only be up to 4000 characters.")
@@ -25,24 +16,12 @@ const withdrawPackageFormSchema = z.object({
     supportingDocumentation: z.array(z.instanceof(File)).optional(),
   }),
 });
-const attachments: AttachmentRecipe<
-  z.infer<typeof withdrawPackageFormSchema>
->[] = [
-  {
-    name: "supportingDocumentation",
-    label: "Supporting Documentation",
-    required: false,
-  } as const,
-];
 
 export const WithdrawPackage = ({ item }: { item?: ItemResult }) => {
-  const { setCancelModalOpen } = useModalContext();
-  const form = useForm<z.infer<typeof withdrawPackageFormSchema>>({
-    resolver: zodResolver(withdrawPackageFormSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
   });
-  const handleSubmit = useActionSubmitHandler<
-    z.infer<typeof withdrawPackageFormSchema>
-  >({
+  const handleSubmit = useActionSubmitHandler<z.infer<typeof formSchema>>({
     formHookReturn: form,
     authority: item?._source.authority as PlanType,
     addDataConditions: [
@@ -61,12 +40,14 @@ export const WithdrawPackage = ({ item }: { item?: ItemResult }) => {
     ],
   });
 
-  if (!item) return <Navigate path={"/"} />; // Prevents optional chains below
+  if (!item) return <Navigate path={"/"} />; // Prevents optionals below
   return (
-    <>
-      {form.formState.isSubmitting && <LoadingSpinner />}
-      <div>
-        <ActionFormIntro title="Withdraw Medicaid SPA Package">
+    <ActionFormTemplate<z.infer<typeof formSchema>>
+      item={item}
+      formController={form}
+      submitHandler={handleSubmit}
+      intro={
+        <ActionFormIntro title={`Withdraw ${item._source.planType} Package`}>
           <p>
             Complete this form to withdraw a package. Once complete, you will
             not be able to resubmit this package. CMS will be notified and will
@@ -74,55 +55,21 @@ export const WithdrawPackage = ({ item }: { item?: ItemResult }) => {
             information, they will follow up by email.
           </p>
         </ActionFormIntro>
-        <PackageInfo item={item} />
-        <h3 className="font-bold text-2xl font-sans">Attachments</h3>
+      }
+      attachments={[
+        {
+          name: "supportingDocumentation",
+          label: "Supporting Documentation",
+          required: false,
+        } as const,
+      ]}
+      attachmentFaqLink={"/faq"}
+      attachmentInstructions={
         <p className="font-normal mb-4">
           Upload your supporting documentation for withdrawal or explain your
           need for withdrawal in the <em>Additional Information section.</em>
         </p>
-        <I.Form {...form}>
-          <form onSubmit={form.handleSubmit(handleSubmit)}>
-            {/* Change faqLink once we know the anchor */}
-            <AttachmentsSizeTypesDesc faqLink={"/faq"} />
-            {attachments.map(({ name, label, required }) => (
-              <I.FormField
-                key={name}
-                control={form.control}
-                name={`attachments.${name}`}
-                render={SlotAttachments({
-                  label: (
-                    <>
-                      {label}
-                      {required ? <I.RequiredIndicator /> : ""}
-                    </>
-                  ),
-                  message: <I.FormMessage />,
-                  className: "my-4",
-                })}
-              />
-            ))}
-            <I.FormField
-              control={form.control}
-              name="additionalInformation"
-              render={SlotAdditionalInfo({
-                label:
-                  "Add anything else you would like to share with the state.",
-                description: "4,000 characters allowed",
-                className: "pt-6",
-              })}
-            />
-            <div className="flex gap-2 my-8">
-              <Button type="submit">Submit</Button>
-              <Button
-                onClick={() => setCancelModalOpen(true)}
-                variant="outline"
-              >
-                Cancel
-              </Button>
-            </div>
-          </form>
-        </I.Form>
-      </div>
-    </>
+      }
+    />
   );
 };
---
+++ src/services/ui/src/pages/actions/template.tsx
@@ -0,0 +1,88 @@
+import { LoadingSpinner } from "@/components";
+import { PackageInfo } from "@/pages/actions/common";
+import { AttachmentsSizeTypesDesc } from "@/pages/form/content";
+import {
+  SlotAdditionalInfo,
+  SlotAttachments,
+} from "@/pages/actions/renderSlots";
+import {
+  Form,
+  Button,
+  FormField,
+  FormMessage,
+  RequiredIndicator,
+} from "@/components/Inputs";
+import { ReactElement } from "react";
+import { ItemResult } from "shared-types";
+import {
+  FieldValues,
+  Path,
+  SubmitHandler,
+  UseFormReturn,
+} from "react-hook-form";
+import { AttachmentRecipe } from "@/lib";
+import { useModalContext } from "@/pages/form/modals";
+
+export const ActionFormTemplate = <D extends FieldValues>({
+  item,
+  formController,
+  submitHandler,
+  intro,
+  attachments,
+  attachmentFaqLink,
+  attachmentInstructions,
+}: {
+  item: ItemResult;
+  formController: UseFormReturn<D>;
+  submitHandler: SubmitHandler<D>;
+  intro: ReactElement;
+  attachments: AttachmentRecipe<D>[];
+  attachmentFaqLink: string;
+  attachmentInstructions?: ReactElement;
+}) => {
+  const { setCancelModalOpen } = useModalContext();
+  return (
+    <Form {...formController}>
+      <form onSubmit={formController.handleSubmit(submitHandler)}>
+        {formController.formState.isSubmitting && <LoadingSpinner />}
+        {intro}
+        <PackageInfo item={item} />
+        <h3 className="font-bold text-2xl font-sans">Attachments</h3>
+        {attachmentInstructions}
+        <AttachmentsSizeTypesDesc faqLink={attachmentFaqLink} />
+        {attachments.map(({ name, label, required }) => (
+          <FormField
+            key={String(name)}
+            control={formController.control}
+            name={`attachments.${String(name)}` as Path<D>}
+            render={SlotAttachments({
+              label: (
+                <>
+                  {label}
+                  {required ? <RequiredIndicator /> : ""}
+                </>
+              ),
+              message: <FormMessage />,
+              className: "my-4",
+            })}
+          />
+        ))}
+        <FormField
+          control={formController.control}
+          name={"additionalInformation" as Path<D>}
+          render={SlotAdditionalInfo({
+            label: "Add anything else you would like to share with the state.",
+            description: "4,000 characters allowed",
+            className: "pt-6",
+          })}
+        />
+        <div className="flex gap-2 my-8">
+          <Button type="submit">Submit</Button>
+          <Button onClick={() => setCancelModalOpen(true)} variant="outline">
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
