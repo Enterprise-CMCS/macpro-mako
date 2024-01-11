864e59a953a9586c37dab2e7ed3c41d9d6bcf365
Adewale Sangobiyi <asangobiyi@adewale-mac.mynetworksettings.com>
2024-01-11 08:27:46 -0500

    fix(2nd clock): drop 2nd clock to new line

--- src/services/ui/src/pages/detail/index.tsx
+++ src/services/ui/src/pages/detail/index.tsx
@@ -53,16 +53,13 @@ const StatusCard = (data: opensearch.main.Document) => {
             ? transformedStatuses.cmsStatus
             : transformedStatuses.stateStatus}
         </h2>
-        {checker.hasEnabledRaiWithdraw && (
-          <em className="text-xs my-4 mr-2">
-            {"Withdraw Formal RAI Response - Enabled"}
-          </em>
-        )}
         {user?.isCms && checker.isInSecondClock && (
-          <>
-            <br />
-            <span id="secondclock">2nd Clock</span>
-          </>
+          <span id="secondclock">2nd Clock</span>
+        )}
+        {checker.hasEnabledRaiWithdraw && (
+          <div className="text-xs italic">
+            {"Withdraw Formal RAI Response - Enabled"}
+          </div>
         )}
       </div>
     </DetailCardWrapper>
