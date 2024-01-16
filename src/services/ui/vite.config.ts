38ddf046fe3884dadc9ffdf47fc4a492e51f3b9f
Kristin Grue <k.grue@theta-llc.com>
2024-01-16 15:48:40 -0500

    feat(GA): put the tags back into the feature branch for the demo

--- src/services/ui/vite.config.ts
+++ src/services/ui/vite.config.ts
@@ -9,10 +9,36 @@ export default defineConfig({
     react(),
     VitePluginRadar({
       enableDev: true,
+      // analytics: [
+      //   {
+      //     id: `${process.env.VITE_GOOGLE_ANALYTICS_GTAG}-isthetag`,
+      //     disable: false, // process.env.VITE_GOOGLE_ANALYTICS_DISABLE === "true",
+      //   },
+      // ],
       analytics: [
         {
           id: `${process.env.VITE_GOOGLE_ANALYTICS_GTAG}-isthetag`,
           disable: false, // process.env.VITE_GOOGLE_ANALYTICS_DISABLE === "true",
+        },
+        {
+          // feature branch, add-ga @ https://dobocggqhtrxb.cloudfront.net/
+          id: "G-PTZMHTCC56",
+          disable: process.env.VITE_NODE_ENV === "production",
+        },
+        {
+          // master branch @ https://mako-dev.cms.gov/
+          id: "G-CT1LJB6NL5",
+          disable: process.env.VITE_NODE_ENV === "production",
+        },
+        {
+          // val branch @ https://mako-val.cms.gov/
+          id: "G-5K2N0LTR1B",
+          disable: process.env.VITE_NODE_ENV === "production",
+        },
+        {
+          // production branch @ https://mako.cms.gov/
+          id: "G-58KK49TE7V",
+          disable: process.env.VITE_NODE_ENV !== "production",
         },
       ],
     }),
