2c0aa50d7c5c6a4ef78e078f7934b102f66475fb
James Dinh <jdinh8124@gmail.com>
2024-01-16 11:47:01 -0800

    feat(implementation-guide): add implementation guide (#293)
    * added implementation guide
    
    * cleanup
    
    * adjust styling
    
    * adjust logic
    
    * add route
    
    * cleanup and upload files
    
    * add table from MUI and clean up data
    
    * Update index.tsx
    
    * add target blank logic
    
    * Update index.tsx
    
    * Update index.tsx
    
    * add guide type
    
    * cleanup using native table

--- src/services/ui/src/router.tsx
+++ src/services/ui/src/router.tsx
@@ -71,6 +71,7 @@ export const router = createBrowserRouter([
       { path: ROUTES.WEBFORMS, element: <C.Webforms /> },
       { path: ROUTES.WEBFORM, element: <C.Webform /> },
       { path: ROUTES.PROFILE, element: <P.Profile /> },
+      { path: ROUTES.ABPGUIDE, element: <P.ABPGuide /> },
     ],
     loader: rootLoader(queryClient),
   },
