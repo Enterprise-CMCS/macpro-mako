4407d7b1c6c4ad53cd70041fe94433c595da1d5a
Kevin Haube <khaube@fearless.tech>
2024-01-17 10:02:53 -0500

    Remove old additional requirement method

--- src/services/ui/src/hooks/useActionFormController.ts
+++ src/services/ui/src/hooks/useActionFormController.ts
@@ -6,33 +6,17 @@ import { submit } from "@/api/submissionService";
 import { buildActionUrl } from "@/lib";
 import { PlanType } from "shared-types";
 
-export type DataConditionError = {
-  message: string;
-};
-
 export const useActionSubmitHandler = <D extends FieldValues>({
   authority,
-  addDataConditions,
 }: {
   formHookReturn: UseFormReturn<D>;
   authority: PlanType;
-  /** Reserved for things zod cannot check. */
-  addDataConditions?: ((data: D) => DataConditionError | null)[];
 }): SubmitHandler<D> => {
   const { id, type } = useParams("/action/:id/:type");
   const { data: user } = useGetUser();
   const { setSuccessModalOpen, setErrorModalOpen } = useModalContext();
   return async (data) => {
     try {
-      if (addDataConditions?.length) {
-        const errors = addDataConditions
-          .map((fn) => fn(data))
-          .filter((err) => err) // Filter out nulls
-          .map((err) => err?.message);
-        if (errors.length)
-          throw Error(`Additional conditions were not met: ${errors}`);
-      }
-      // TODO: Type update for submit generic
       await submit<D & { id: string }>({
         data: { ...data, id: id! },
         endpoint: buildActionUrl(type!),
