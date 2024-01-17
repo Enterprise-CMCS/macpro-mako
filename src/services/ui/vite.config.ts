0a3f5074c2808cbddce7a2fcfe5f97e1f845b020
Kristin Grue <k.grue@theta-llc.com>
2024-01-17 13:14:44 -0500

    remove the hard-coded tags now that the demo is done

--- src/services/ui/vite.config.ts
+++ src/services/ui/vite.config.ts
@@ -19,26 +19,6 @@ export default defineConfig(({ mode }) => {
             id: env.VITE_GOOGLE_ANALYTICS_GTAG,
             disable: env.VITE_GOOGLE_ANALYTICS_DISABLE === "true",
           },
-          {
-            // feature branch, add-ga @ https://dobocggqhtrxb.cloudfront.net/
-            id: "G-PTZMHTCC56",
-            disable: env.VITE_NODE_ENV === "production",
-          },
-          {
-            // master branch @ https://mako-dev.cms.gov/
-            id: "G-CT1LJB6NL5",
-            disable: env.VITE_NODE_ENV === "production",
-          },
-          {
-            // val branch @ https://mako-val.cms.gov/
-            id: "G-5K2N0LTR1B",
-            disable: env.VITE_NODE_ENV === "production",
-          },
-          {
-            // production branch @ https://mako.cms.gov/
-            id: "G-58KK49TE7V",
-            disable: env.VITE_NODE_ENV !== "production",
-          },
         ],
       }),
     ],
