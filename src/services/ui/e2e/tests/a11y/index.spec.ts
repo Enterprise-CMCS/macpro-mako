46e7dc0663ac5e4f89472db101304dcd508c4f62
Benjamin Paige <benjamin.paige@gmail.com>
2024-01-22 15:06:33 -0700

    add webform a11y tests and resolve issues

--- src/services/ui/e2e/tests/a11y/index.spec.ts
+++ src/services/ui/e2e/tests/a11y/index.spec.ts
@@ -19,3 +19,28 @@ test.describe("test a11y on static routes", () => {
     });
   }
 });
+
+const webformRoutes = [
+  "/guides/abp",
+  "/webform/abp10/1",
+  "webform/abp3_1/1",
+  "/webform/abp3/1",
+  "/webform/abp1/1",
+];
+
+test.describe("test a11y on webform routes", () => {
+  for (const route of webformRoutes) {
+    test(`${route} should not have any automatically detectable accessibility issues`, async ({
+      page,
+    }) => {
+      await page.goto(route);
+      await page.waitForLoadState("networkidle");
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+      console.log(
+        `${route} violations: `,
+        accessibilityScanResults.violations.length
+      );
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  }
+});
