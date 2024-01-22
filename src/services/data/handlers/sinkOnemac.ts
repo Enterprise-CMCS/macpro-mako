630e0c0723e5071b19c38513868d2dee8f509e27
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2024-01-21 21:54:39 -0500

    refactor onemac

--- src/services/data/handlers/sinkOnemac.ts
+++ src/services/data/handlers/sinkOnemac.ts
@@ -16,77 +16,59 @@ export const handler: Handler<KafkaEvent> = async (event) => {
   await onemac_changelog(event);
 };
 
-export const onemacDataTransform = (props: { key: string; value?: string }) => {
-  const id: string = decode(props.key);
-
-  // is delete
-  if (!props.value) {
-    return {
-      id,
-      additionalInformation: null,
-      raiWithdrawEnabled: null,
-      attachments: null,
-      submitterEmail: null,
-      submitterName: null,
-    };
-  }
-
-  const record = { id, ...JSON.parse(decode(props.value)) };
-
-  // is Legacy
-  const isLegacy = record?.origin !== "micro";
-  if (isLegacy) {
-    const notPackageView = record?.sk !== "Package";
-    if (notPackageView) return null;
-
-    const notOriginatingFromOnemacLegacy =
-      !record.submitterName || record.submitterName === "-- --";
-    if (notOriginatingFromOnemacLegacy) return null;
-
-    const result = main.transforms.transformOnemacLegacy(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  // NOTE: Make official decision on initial type by MVP - timebomb
-  const isNewRecord = !record?.actionType;
-  if (isNewRecord) {
-    const result = main.transforms.transformOnemac(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  // --------- Package-Actions ---------//
-
-  if (
-    record.actionType === Action.DISABLE_RAI_WITHDRAW ||
-    record.actionType === Action.ENABLE_RAI_WITHDRAW
-  ) {
-    const result = main.transforms
-      .transformToggleWithdrawRaiEnabled(id)
-      .safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  if (record.actionType === Action.WITHDRAW_RAI) {
-    const result = main.transforms.transformRaiWithdraw(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  if (record.actionType === Action.WITHDRAW_PACKAGE) {
-    const result = main.transforms
-      .transformWithdrawPackage(id)
-      .safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  return null;
-};
-
 export const onemac_main = async (event: KafkaEvent) => {
   const records = Object.values(event.records).reduce((ACC, RECORDS) => {
     RECORDS.forEach((REC) => {
-      const dataTransform = onemacDataTransform(REC);
-      if (!dataTransform) return;
-      ACC.push(dataTransform);
+      const id: string = decode(REC.key);
+
+      // Handle deletes and return
+      if (!REC.value) {
+        ACC.push({
+          id,
+          additionalInformation: null,
+          raiWithdrawEnabled: null,
+          attachments: null,
+          submitterEmail: null,
+          submitterName: null,
+        });
+        return;
+      }
+
+      const record = JSON.parse(decode(REC.value));
+
+      // Handle legacy and return
+      if (
+        record?.origin !== "micro" && // Is Legacy
+        record?.sk === "Package" && // Is a Package View
+        record?.submitterName && // Is originally from Legacy
+        record?.submitterName !== "-- --" // Is originally from Legacy
+      ) {
+        const result = main.transforms
+          .transformOnemacLegacy(id)
+          .safeParse(record);
+        if (result.success) ACC.push(result.data);
+        return;
+      }
+
+      // Handle everything else
+      const result = (() => {
+        switch (record?.actionType) {
+          case undefined:
+            return main.transforms.transformOnemac(id).safeParse(record);
+          case Action.DISABLE_RAI_WITHDRAW:
+          case Action.ENABLE_RAI_WITHDRAW:
+            return main.transforms
+              .transformToggleWithdrawRaiEnabled(id)
+              .safeParse(record);
+          case Action.WITHDRAW_RAI:
+            return main.transforms.transformRaiWithdraw(id).safeParse(record);
+          case Action.WITHDRAW_PACKAGE:
+            return main.transforms
+              .transformWithdrawPackage(id)
+              .safeParse(record);
+        }
+      })();
+      if (result?.success) ACC.push(result.data);
     });
 
     return ACC;
@@ -102,14 +84,15 @@ export const onemac_main = async (event: KafkaEvent) => {
 export const onemac_changelog = async (event: KafkaEvent) => {
   const data = Object.values(event.records).reduce((ACC, RECORDS) => {
     RECORDS.forEach((REC) => {
-      // omit delete event
+      // Handle deletes and return
       if (!REC.value) return;
 
       const record = JSON.parse(decode(REC.value));
-      // omit legacy record
+
+      // Handle legacy and return
       if (record?.origin !== "micro") return;
 
-      // include package actions
+      // Handle everything else
       const packageId = decode(REC.key);
       ACC.push({
         ...record,
