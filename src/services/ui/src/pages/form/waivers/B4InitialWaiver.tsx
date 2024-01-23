a7bf9561b9a5168afdc53d7c31c423df5adc83b2
Kevin Haube <khaube@fearless.tech>
2024-01-23 13:23:16 -0500

    Update form intros

--- src/services/ui/src/pages/form/waivers/B4InitialWaiver.tsx
+++ src/services/ui/src/pages/form/waivers/B4InitialWaiver.tsx
@@ -24,7 +24,17 @@ const Form = () => {
       formController={form}
       submitHandler={handleSubmit}
       title={"Formal RAI Details"}
-      description={<p className="font-light mb-6 max-w-4xl"></p>}
+      description={
+        <p className="mb-6 max-w-4xl">
+          Once you submit this form, a confirmation email is sent to you and to
+          CMS. CMS will use this content to review your package, and you will
+          not be able to edit this form. If CMS needs any additional
+          information, they will follow up by email.{" "}
+          <strong>
+            If you leave this page, you will lose your progress on this form.
+          </strong>
+        </p>
+      }
       preSubmitMessage={""}
       attachments={attachments}
       attachmentFaqLink={"/faq"}
