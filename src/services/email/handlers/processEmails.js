3c5bf35ccb9693d4772a01d23cf7d9070eb97b9f
Kristin Grue <k.grue@theta-llc.com>
2024-01-25 11:09:47 -0500

    feat(email): more invistigating

--- src/services/email/handlers/processEmails.js
+++ src/services/email/handlers/processEmails.js
@@ -1,12 +1,23 @@
-import { SESClient, ListConfigurationSetsCommand } from "@aws-sdk/client-ses";
+import { SESClient, ListConfigurationSetsCommand, ListIdentitiesCommand } from "@aws-sdk/client-ses";
+
+const createListIdentitiesCommand = () =>
+    new ListIdentitiesCommand({ IdentityType: "EmailAddress", MaxItems: 10 });
 
 const SES = new SESClient({ region: process.env.region });
 
 export const main = async (event, context, callback) => {
+    let response;
     console.log("Received event (stringified):", JSON.stringify(event, null, 4));
+    const listIdentitiesCommand = createListIdentitiesCommand();
 
-    const response = await SES.send(new ListConfigurationSetsCommand());
+    try {
+        response = await SES.send(listIdentitiesCommand);
+        console.log("listIdentitiesCommand response: ", response);
 
-    console.log("got this response: ", response);
+        response = await SES.send(new ListConfigurationSetsCommand({ NextToken: "", MaxItems: 10 }));
+        console.log("ListConfigurationSetsCommand response: ", response);
+    } catch (err) {
+        console.log("Failed to list identities.", err);
+    }
     callback(null, "Success");
 };
