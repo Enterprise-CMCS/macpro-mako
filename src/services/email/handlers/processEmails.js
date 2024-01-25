463d0c00ba6c8238d7e3f2d82c8617c9a8e91a13
Kristin Grue <k.grue@theta-llc.com>
2024-01-25 09:46:05 -0500

    feat(email): add very basic SES

--- src/services/email/handlers/processEmails.js
+++ src/services/email/handlers/processEmails.js
@@ -1,4 +1,17 @@
+import { SESClient, ListConfigurationSetsCommand } from "@aws-sdk/client-ses";
+
+const SES = new SESClient({ region: process.env.region });
+
 export const main = async (event, context, callback) => {
     console.log("Received event (stringified):", JSON.stringify(event, null, 4));
+
+    const input = { // ListConfigurationSetsRequest
+        NextToken: "STRING_VALUE",
+        MaxItems: Number("int"),
+    };
+    const command = new ListConfigurationSetsCommand(input);
+    const response = await client.send(command);
+
+    console.log("got this response: ", response);
     callback(null, "Success");
 };
