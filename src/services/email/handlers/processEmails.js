49234e581bf6df5dd0c39ca218527df5591c9670
Kristin Grue <k.grue@theta-llc.com>
2024-02-01 11:12:49 -0500

    feat(email): try to not have it in separate functions

--- src/services/email/handlers/processEmails.js
+++ src/services/email/handlers/processEmails.js
@@ -2,43 +2,23 @@ import {
     CognitoIdentityProviderClient,
     ListUsersCommand,
     // UserType as CognitoUserType,
-  } from "@aws-sdk/client-cognito-identity-provider";
+} from "@aws-sdk/client-cognito-identity-provider";
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 // import { CognitoUserAttributes } from "shared-types";
 
 const SES = new SESClient({ region: process.env.region });
-
 const Cognito = new CognitoIdentityProviderClient({
     region: process.env.region,
-  });
+});
 
-  const getEmailRecipients = async () => {
-    const commandListUsers = new ListUsersCommand({
-        UserPoolId: "grue-user-pool",
-        // Filter: subFilter,
-      });
-      try {
-        const listUsersResponse = await Cognito.send(commandListUsers);
-    
-        console.log("listUsers response: ", listUsersResponse);
-    
-      } catch (error) {
-        throw new Error("Error fetching user from Cognito");
-      }
-    return [
-        "k.grue.stateuser@gmail.com",
-    ];
-};
-
-const createSendEmailCommand = async (event) => {
-
-    try {
-    const recipientList = await getEmailRecipients();
-    return new SendEmailCommand({
+const createSendEmailCommand = async (event) =>
+    new SendEmailCommand({
         Source: "kgrue@fearless.tech",
         Destination: {
-            ToAddresses: recipientList,
+            ToAddresses: [
+                "k.grue.stateuser@gmail.com",
+            ],
         },
         Message: {
             Subject: {
@@ -58,17 +38,21 @@ const createSendEmailCommand = async (event) => {
         },
         ConfigurationSetName: process.env.emailConfigSet,
     });
-} catch (error) {
-    throw new Error("Error fetching user from Cognito");
-  }
-};
 
 export const main = async (event, context, callback) => {
     let response;
     console.log("Received event (stringified):", JSON.stringify(event, null, 4));
+    const commandListUsers = new ListUsersCommand({
+        UserPoolId: "grue-user-pool",
+        // Filter: subFilter,
+    });
+
     const sendEmailCommand = createSendEmailCommand(event);
 
     try {
+        const listUsersResponse = await Cognito.send(commandListUsers);
+        console.log("listUsers response: ", listUsersResponse);
+
         response = await SES.send(sendEmailCommand);
         console.log("sendEmailCommand response: ", response);
     } catch (err) {
