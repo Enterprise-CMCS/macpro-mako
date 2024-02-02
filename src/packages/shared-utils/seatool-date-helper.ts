441e8fa7dc635dc66fc87b3747d07812001ba094
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2024-02-02 10:20:52 -0500

    Update per paul's comments

--- src/packages/shared-utils/seatool-date-helper.ts
+++ src/packages/shared-utils/seatool-date-helper.ts
@@ -22,19 +22,17 @@ export const formatSeatoolDate = (date: string): string => {
   return moment(date).tz("UTC").format("MM/DD/yyyy")
 }
 
-export const getNextBusinessDayTimestamp = (date?: Date): number => {
-  if(!date) {
-    date = new Date();
-  }
-
+export const getNextBusinessDayTimestamp = (date: Date = new Date()): number => {
   let localeStringDate = date.toLocaleString("en-US", { timeZone: "America/New_York", dateStyle: "short" });
   let localeStringHours24 = date.toLocaleString("en-US", { timeZone: "America/New_York", hour: 'numeric', hour12: false });
   let localeDate = new Date(localeStringDate);
-  // Some logging
+
   console.log(`Evaluating ${localeStringDate} at ${localeStringHours24}`);
 
-  // If it's after 5pm eastern, a holiday, or a weekend, recurse using the next day.
-  if(parseInt(localeStringHours24,10) >= 17 || fedHolidays.isAHoliday(localeDate) || !(localeDate.getDay() % 6)) {
+  const after5pmEST = parseInt(localeStringHours24,10) >= 17
+  const isHoliday = fedHolidays.isAHoliday(localeDate)
+  const isWeekend = !(localeDate.getDay() % 6)
+  if(after5pmEST || isHoliday || isWeekend) {
     let nextDate = localeDate;
     nextDate.setDate(nextDate.getDate() + 1);
     nextDate.setHours(12,0,0,0)
@@ -42,7 +40,7 @@ export const getNextBusinessDayTimestamp = (date?: Date): number => {
     return getNextBusinessDayTimestamp(nextDate)
   }
 
-  // If it's none of the above, return the time of the localeDate; equivalent to the next business day's epoch for midnight UTC
+  // Return the next business day's epoch for midnight UTC
   let ret = offsetForUtc(localeDate).getTime();
   console.log('Current date is a valid business date.  Will return ' + ret);
   return ret;
