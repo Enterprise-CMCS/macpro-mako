66480edf2ed5c0068bdf4c4128a357fba308b8b8
Kristin Grue <k.grue@theta-llc.com>
2024-02-02 08:39:17 -0500

    feat(email): add in the decoding and transforming of the events

--- src/services/email/handlers/processEmails.js
+++ src/services/email/handlers/processEmails.js
@@ -5,12 +5,97 @@ import {
 } from "@aws-sdk/client-cognito-identity-provider";
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
+import {
+    transformOnemac,
+    transformOnemacLegacy,
+    transformRaiIssue,
+    transformRaiResponse,
+    transformRaiWithdraw,
+    transformWithdrawPackage,
+    transformToggleWithdrawRaiEnabled,
+    Action,
+  } from "shared-types";
+
 // import { CognitoUserAttributes } from "shared-types";
 
 const SES = new SESClient({ region: process.env.region });
 const Cognito = new CognitoIdentityProviderClient({
     region: process.env.region,
 });
+
+export const onemacDataTransform = (props) => {
+    const id = decode(props.key);
+  
+    // is delete
+    if (!props.value) {
+      return {
+        id,
+        additionalInformation: null,
+        raiWithdrawEnabled: null,
+        attachments: null,
+        submitterEmail: null,
+        submitterName: null,
+      };
+    }
+  
+    const record = { id, ...JSON.parse(decode(props.value)) };
+  
+    // is Legacy
+    const isLegacy = record?.origin !== "micro";
+    if (isLegacy) {
+      const notPackageView = record?.sk !== "Package";
+      if (notPackageView) return null;
+  
+      const notOriginatingFromOnemacLegacy =
+        !record.submitterName || record.submitterName === "-- --";
+      if (notOriginatingFromOnemacLegacy) return null;
+  
+      const result = transformOnemacLegacy(id).safeParse(record);
+      return result.success ? result.data : null;
+    }
+  
+    // NOTE: Make official decision on initial type by MVP - timebomb
+    const isNewRecord = !record?.actionType;
+    if (isNewRecord) {
+      const result = transformOnemac(id).safeParse(record);
+      return result.success ? result.data : null;
+    }
+  
+    // --------- Package-Actions ---------//
+    // TODO: remove transform package-action below
+  
+    if (record.actionType === Action.ENABLE_RAI_WITHDRAW) {
+      const result = transformToggleWithdrawRaiEnabled(id).safeParse(record);
+      return result.success ? result.data : null;
+    }
+  
+    if (record.actionType === Action.DISABLE_RAI_WITHDRAW) {
+      const result = transformToggleWithdrawRaiEnabled(id).safeParse(record);
+      return result.success ? result.data : null;
+    }
+  
+    if (record.actionType === Action.ISSUE_RAI) {
+      const result = transformRaiIssue(id).safeParse(record);
+      return result.success ? result.data : null;
+    }
+  
+    if (record.actionType === Action.RESPOND_TO_RAI) {
+      const result = transformRaiResponse(id).safeParse(record);
+      return result.success ? result.data : null;
+    }
+  
+    if (record.actionType === Action.WITHDRAW_RAI) {
+      const result = transformRaiWithdraw(id).safeParse(record);
+      return result.success ? result.data : null;
+    }
+  
+    if (record.actionType === Action.WITHDRAW_PACKAGE) {
+      const result = transformWithdrawPackage(id).safeParse(record);
+      return result.success ? result.data : null;
+    }
+  
+    return null;
+  };
 
 const createSendEmailCommand = (event) =>
     new SendEmailCommand({
@@ -42,6 +127,19 @@ const createSendEmailCommand = (event) =>
 export const main = async (event, context, callback) => {
     let response;
     console.log("Received event (stringified):", JSON.stringify(event, null, 4));
+
+    const records = Object.values(event.records).reduce((ACC, RECORDS) => {
+        RECORDS.forEach((REC) => {
+          const dataTransform = onemacDataTransform(REC);
+          if (!dataTransform) return;
+          ACC.push(dataTransform);
+        });
+    
+        return ACC;
+      }, []);
+
+    console.log("the transformed records are: ", records);
+    
     const commandListUsers = new ListUsersCommand({
         UserPoolId: process.env.cognitoPoolId,
     });
