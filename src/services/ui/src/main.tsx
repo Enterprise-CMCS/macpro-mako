9937f46f1fe669b7d2cc7fc8027ea9180a1d2cef
Kevin Haube <khaube@fearless.tech>
2024-02-05 14:20:50 -0500

    Plug in context provider

--- src/services/ui/src/main.tsx
+++ src/services/ui/src/main.tsx
@@ -7,12 +7,15 @@ import { queryClient, router } from "./router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { UserContextProvider } from "./components/Context/userContext";
+import { ModalProvider } from "@/components/Context/modalContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <UserContextProvider>
-        <RouterProvider router={router} />
+        <ModalProvider>
+          <RouterProvider router={router} />
+        </ModalProvider>
       </UserContextProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
