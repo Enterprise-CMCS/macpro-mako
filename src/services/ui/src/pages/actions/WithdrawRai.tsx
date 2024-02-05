42789ad910650c8f8f89aafc6345806e2c55e516
Kevin Haube <khaube@fearless.tech>
2024-02-05 12:09:32 -0500

    Undo template: WithdrawRai

--- src/services/ui/src/pages/actions/WithdrawRai.tsx
+++ src/services/ui/src/pages/actions/WithdrawRai.tsx
@@ -1,13 +1,26 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Path, useForm } from "react-hook-form";
 import { z } from "zod";
 import { opensearch, PlanType } from "shared-types";
-import { ActionFormTemplate } from "@/pages/actions/template";
 import { useActionSubmitHandler } from "@/hooks/useActionFormController";
 import { FormSetup } from "@/pages/actions/setups";
+import {
+  Button,
+  Form,
+  FormField,
+  FormMessage,
+  RequiredIndicator,
+} from "@/components/Inputs";
+import { Alert, LoadingSpinner } from "@/components";
+import { ActionFormIntro, PackageInfo } from "@/pages/actions/common";
+import { AttachmentsSizeTypesDesc } from "@/pages/form/content";
+import {
+  SlotAdditionalInfo,
+  SlotAttachments,
+} from "@/pages/actions/renderSlots";
+import { Info } from "lucide-react";
+import { useModalContext } from "@/pages/form/modals";
 
-const preSubmitMessage =
-  "Once complete, you and CMS will receive an email confirmation.";
 export const WithdrawRai = ({
   item,
   schema,
@@ -21,24 +34,90 @@ export const WithdrawRai = ({
     formHookReturn: form,
     authority: item?._source.authority as PlanType,
   });
+  const { setCancelModalOpen } = useModalContext();
 
   return (
-    <ActionFormTemplate<z.infer<typeof schema>>
-      item={item}
-      formController={form}
-      submitHandler={handleSubmit}
-      title={"Withdraw Formal RAI Response Details"}
-      description={
-        <p className="font-light mb-6 max-w-4xl">
-          Complete this form to withdraw the Formal RAI response.{" "}
-          {preSubmitMessage}
-        </p>
-      }
-      preSubmitMessage={preSubmitMessage}
-      attachments={attachments}
-      attachmentFaqLink={"/faq/#medicaid-spa-rai-attachments"}
-      requireAddlInfo
-      addlInfoInstructions={<p>Explain your need for withdrawal.</p>}
-    />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)}>
+        {form.formState.isSubmitting && <LoadingSpinner />}
+        {/* Intro */}
+        <ActionFormIntro title={"Withdraw Formal RAI Response Details"}>
+          <RequiredIndicator /> Indicates a required field
+          <p className="font-light mb-6 max-w-4xl">
+            Complete this form to withdraw the Formal RAI response. Once
+            complete, you and CMS will receive an email confirmation.
+          </p>
+        </ActionFormIntro>
+        {/* Package ID and type info */}
+        <PackageInfo item={item} />
+        {/* Attachments */}
+        <h3 className="font-bold text-2xl font-sans">Attachments</h3>
+        <AttachmentsSizeTypesDesc
+          faqLink={"/faq/#medicaid-spa-rai-attachments"}
+        />
+        {attachments.map(({ name, label, required }) => (
+          <FormField
+            key={String(name)}
+            control={form.control}
+            name={`attachments.${String(name)}` as Path<typeof schema>}
+            render={SlotAttachments({
+              label: (
+                <>
+                  {label}
+                  {required ? <RequiredIndicator /> : ""}
+                </>
+              ),
+              message: <FormMessage />,
+              className: "my-4",
+            })}
+          />
+        ))}
+        {/* Additional Info */}
+        <FormField
+          control={form.control}
+          name={"additionalInformation" as Path<typeof schema>}
+          render={SlotAdditionalInfo({
+            label: <p>Explain your need for withdrawal.</p>,
+            description: "4,000 characters allowed",
+            className: "pt-6",
+            required: true,
+          })}
+        />
+        {/* Error banner */}
+        {Object.keys(form.formState.errors).length !== 0 && (
+          <Alert className="my-6" variant="destructive">
+            Input validation error(s)
+            <ul className="list-disc">
+              {Object.values(form.formState.errors).map(
+                (err, idx) =>
+                  err?.message && (
+                    <li className="ml-8 my-2" key={idx}>
+                      {err.message as string}
+                    </li>
+                  )
+              )}
+            </ul>
+          </Alert>
+        )}
+        {/* Pre-submit message banner */}
+        <Alert variant={"infoBlock"} className="my-2 w-full flex-row text-sm">
+          <Info />
+          <p className="ml-2">
+            Once complete, you and CMS will receive an email confirmation.
+          </p>
+        </Alert>
+        {/* Buttons */}
+        <div className="flex gap-2 my-8">
+          <Button type="submit">Submit</Button>
+          <Button
+            type="button"
+            onClick={() => setCancelModalOpen(true)}
+            variant="outline"
+          >
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 };
