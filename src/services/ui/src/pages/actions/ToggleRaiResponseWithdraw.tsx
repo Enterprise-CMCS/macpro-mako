7181e6d2a389858ca5dccf7a8171d1c56328310f
Kevin Haube <khaube@fearless.tech>
2024-02-05 15:19:20 -0500

    Implement ToggleRaiWithdraw cancel modal

--- src/services/ui/src/pages/actions/ToggleRaiResponseWithdraw.tsx
+++ src/services/ui/src/pages/actions/ToggleRaiResponseWithdraw.tsx
@@ -1,30 +1,30 @@
-import { Navigate, useParams } from "@/components/Routing";
+import { Navigate, useNavigate, useParams } from "@/components/Routing";
 import { Alert, LoadingSpinner } from "@/components";
 import { Action, PlanType, opensearch } from "shared-types";
 import { Button } from "@/components/Inputs";
-import { FC, useEffect, useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useSubmissionService } from "@/api/submissionService";
 import { buildActionUrl } from "@/lib";
 import { useGetUser } from "@/api/useGetUser";
 import { ActionFormIntro, PackageInfo } from "@/pages/actions/common";
-import { useModalContext } from "@/pages/form/modals";
+import { useModalContext } from "@/components/Context/modalContext";
 
 export const ToggleRaiResponseWithdraw = ({
   item,
 }: {
   item?: opensearch.main.ItemResult;
 }) => {
+  const navigate = useNavigate();
   const { id, type } = useParams("/action/:id/:type");
   const { data: user } = useGetUser();
-  const authority = item?._source.authority as PlanType;
-  const { setCancelModalOpen, setSuccessModalOpen } = useModalContext();
+  const { setModalOpen, setContent, setAcceptPath } = useModalContext();
   const { mutate, isLoading, isSuccess, error } = useSubmissionService<{
     id: string;
   }>({
     data: { id: id! },
     endpoint: buildActionUrl(type!),
     user,
-    authority,
+    authority: item?._source.authority as PlanType,
   });
 
   const ACTION_WORD = useMemo(
@@ -33,7 +33,7 @@ export const ToggleRaiResponseWithdraw = ({
   );
 
   useEffect(() => {
-    if (isSuccess) setSuccessModalOpen(true);
+    if (isSuccess) navigate({ path: "/dashboard" });
   }, [isSuccess]);
 
   if (!item) return <Navigate path={"/dashboard"} />; // Prevents optional chains below
@@ -62,7 +62,19 @@ export const ToggleRaiResponseWithdraw = ({
       )}
       <div className="flex gap-2">
         <Button onClick={() => mutate()}>Submit</Button>
-        <Button onClick={() => setCancelModalOpen(true)} variant="outline">
+        <Button
+          onClick={() => {
+            setContent({
+              header: "Stop form submission?",
+              body: "All information you've entered on this form will be lost if you leave this page.",
+              acceptButtonText: "Yes, leave form",
+              cancelButtonText: "Return to form",
+            });
+            setAcceptPath("/dashboard");
+            setModalOpen(true);
+          }}
+          variant="outline"
+        >
           Cancel
         </Button>
       </div>
