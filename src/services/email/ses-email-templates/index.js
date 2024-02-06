6583bb3527c710ad5f279b9b65fee2ee40a0b2e9
Kristin Grue <k.grue@theta-llc.com>
2024-02-06 11:02:09 -0500

    feat(email): needs specific name?

--- src/services/email/ses-email-templates/index.js
+++ src/services/email/ses-email-templates/index.js
@@ -3,11 +3,11 @@
  * @param {Object} options - runtime options
  * @returns {Promise<{name: string, subject: string, html: string, text}[]>}
  */
-const allTemplates = async (serverless, options) => [{
+const templateConfiguration = async (serverless, options) => [{
     name: "initial-submission-cms",
     subject:  "{{authority}} {{id}} Submitted",
     html: "<p>The OneMAC Submission Portal received a {{authority}} Submission:</p><ul><li>The submission can be accessed in the OneMAC application, which you can find at <a href='{{applicationEndpoint}}'>this link</a>.</li><li>If you are not already logged in, please click the \"Login\" link at the top of the page and log in using your Enterprise User Administration (EUA) credentials.</li><li>After you have logged in, you will be taken to the OneMAC application. The submission will be listed on the dashboard page, and you can view its details by clicking on its ID number.</li></ul>{{PackageDetails}}{{PackageWarnings}}<br><p>If the contents of this email seem suspicious, do not open them, and instead forward this email to <a href='mailto:SPAM@cms.hhs.gov'>SPAM@cms.hhs.gov</a>.</p><p>Thank you!</p>",
     text: "The OneMAC Submission Portal received a {{authority}} Submission:\n\nThe submission can be accessed in the OneMAC application, which you can find at {{applicationEndpoint}}.\n\nIf you are not already logged in, please click the \"Login\" link at the top of the page and log in using your Enterprise User Administration (EUA) credentials.\n\nAfter you have logged in, you will be taken to the OneMAC application. The submission will be listed on the dashboard page, and you can view its details by clicking on its ID number.\n\n{{PackageDetails}}{{PackageWarnings}}\n\nIf the contents of this email seem suspicious, do not open them, and instead forward this email to SPAM@cms.hhs.gov.\n\nThank you!",
 }];
 
-export default allTemplates
+export default templateConfiguration
