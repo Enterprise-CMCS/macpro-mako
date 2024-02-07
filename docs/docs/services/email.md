319a1f743929c600b795d33712a61a063c9d8c48
Kristin Grue <k.grue@theta-llc.com>
2024-02-07 16:11:10 -0500

    feat(email): first attempt at driving emails from actions (two emails from one action)

--- docs/docs/services/email.md
+++ docs/docs/services/email.md
@@ -14,6 +14,6 @@ The email service deploys the lambdas, SNS topics, and Configuration Sets needed
 AWS SES is an account-wide service for basic sending and receiving of email.  By creating lambdas to build the emails and sending the email with a branch-specific configuration set, we can follow the events of email sending and take action based on those events.
 
 ### SSM Parameters
-- emailSource: the single email address to place in the From: field of the email.  Must be a verified email in the AWS SES service being used.
-- emailRecipients: a JSON object containing the email address type to actual email addresses mappings
-{ "OSG":"spa@cms.hhs.gov","}
+The workflow will not successfully deploy unless the following ssm parameters are set:
+- {project}/default/emailSource or {project}/{stage}/emailSource: the single email address to place in the From: field of the email.  Must be verified at the account level.
+- {project}/default/osgEmail or {project}/{stage}/osgEmail: the actual email address used for emails configured to be sent to the OSG Email.
--- src/services/email/handlers/processEmails.js
+++ src/services/email/handlers/processEmails.js
@@ -6,17 +6,6 @@ import {
 } from "@aws-sdk/client-cognito-identity-provider";
 import { SESClient, SendTemplatedEmailCommand } from "@aws-sdk/client-ses";
 
-import {
-  transformOnemac,
-  transformOnemacLegacy,
-  transformRaiIssue,
-  transformRaiResponse,
-  transformRaiWithdraw,
-  transformWithdrawPackage,
-  transformToggleWithdrawRaiEnabled,
-  Action,
-} from "shared-types";
-
 // import { CognitoUserAttributes } from "shared-types";
 
 const SES = new SESClient({ region: process.env.region });
@@ -24,109 +13,35 @@ const Cognito = new CognitoIdentityProviderClient({
   region: process.env.region,
 });
 
-export const onemacDataTransform = (props) => {
-  const id = decode(props.key);
-
-  // is delete
-  if (!props.value) {
-    return {
-      id,
-      additionalInformation: null,
-      raiWithdrawEnabled: null,
-      attachments: null,
-      submitterEmail: null,
-      submitterName: null,
-    };
-  }
-  const mapActionToTransform = {
-    [Action.ENABLE_RAI_WITHDRAW]: transformToggleWithdrawRaiEnabled,
-  };
-  const record = { id, ...JSON.parse(decode(props.value)) };
-
-  console.log("here is the decoded record: ", record);
-
-  // events to ignore: everything except micro's actions
-  if (record?.origin !== "micro") return null;
-
-  console.log("is not ignored");
-
-  // looks like initial submissions do not have an action type yet
-  const whichEmails = record.actionType ? record.actionType : "initial-submission";
-
-  console.log("whichEmail: ", whichEmails);
-
-  const cmsTemplateName = whichEmails + '-cms';
-  const stateTemplateName = whichEmails + '-state';
-
-  console.log("cms template: ", cmsTemplateName);
-  console.log("state Template: ", stateTemplateName);
-
-  // is Legacy
-  const isLegacy = record?.origin !== "micro";
-  if (isLegacy) {
-    const notPackageView = record?.sk !== "Package";
-    if (notPackageView) return null;
-
-    const notOriginatingFromOnemacLegacy =
-      !record.submitterName || record.submitterName === "-- --";
-    if (notOriginatingFromOnemacLegacy) return null;
-
-    const result = transformOnemacLegacy(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  // NOTE: Make official decision on initial type by MVP - timebomb
-  const isNewRecord = !record?.actionType;
-  if (isNewRecord) {
-    const result = transformOnemac(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  // --------- Package-Actions ---------//
-  // TODO: remove transform package-action below
-
-  if (record.actionType === Action.ENABLE_RAI_WITHDRAW) {
-    const result = mapActionToTransform[record.actionType](id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  if (record.actionType === Action.DISABLE_RAI_WITHDRAW) {
-    const result = transformToggleWithdrawRaiEnabled(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  if (record.actionType === Action.ISSUE_RAI) {
-    const result = transformRaiIssue(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  if (record.actionType === Action.RESPOND_TO_RAI) {
-    const result = transformRaiResponse(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  if (record.actionType === Action.WITHDRAW_RAI) {
-    const result = transformRaiWithdraw(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  if (record.actionType === Action.WITHDRAW_PACKAGE) {
-    const result = transformWithdrawPackage(id).safeParse(record);
-    return result.success ? result.data : null;
-  }
-
-  return null;
-};
+const emailsToSend = {
+  "initial-submission-default": [{
+    "templateBase": "initial-submission-cms",
+    "ToAddresses": process.env.osgEmail,
+    "CcAddresses": process.env.testingEmail,
+  }, {
+    "templateBase": "initial-submission-state",
+    "ToAddresses": "submitterEmail",
+    "CcAddresses": process.env.testingEmail,
+  }],
+  "initial-submission-chip": [{
+    "templateBase": "initial-submission-cms",
+    "ToAddresses": process.env.chipEmail,
+    "CcAddresses": `${process.env.chipCCList};${process.env.testingEmail}`,
+  }, {
+    "templateBase": "initial-submission-state",
+    "ToAddresses": "submitterEmail",
+    "CcAddresses": process.env.testingEmail,
+  }],
+}
 
 const createSendTemplatedEmailCommand = (data) =>
   new SendTemplatedEmailCommand({
     Source: process.env.emailSource ?? "k.grue@fearless.tech",
-    Destination: process.env.emailDestination ?? {
-      ToAddresses: [
-        "k.grue.stateuser@gmail.com",
-      ],
+    Destination: {
+      ToAddresses: data.ToAddresses,
+      CcAddresses: data.CcAddresses,
     },
-    TemplateData: JSON.stringify({ authority: "An Authority", id: "the ID", applicationEndpoint: "onemac.cms.gov", packageDetails: "some details", packageWarnings: "looks like missing attributes are rendering failures... that is not good", extraData: "what if I add more attributes than needed??" }),
+    TemplateData: JSON.stringify({ applicationEndpoint: "onemac.cms.gov", packageDetails: "some details", packageWarnings: "looks like missing attributes are rendering failures... that is not good", ...data }),
     Template: `initial-submission-cms_${process.env.stage}`,
     ConfigurationSetName: process.env.emailConfigSet,
   });
@@ -139,30 +54,27 @@ export const main = async (event, context, callback) => {
   // Verify we have all the information we need, set defaults where possible
 
   // decode and transform the incoming events
-  const records = Object.values(event.records).reduce((ACC, RECORDS) => {
+  const emails = Object.values(event.records).reduce((ACC, RECORDS) => {
     RECORDS.forEach((REC) => {
-      const dataTransform = onemacDataTransform(REC);
-      if (!dataTransform) return;
-      ACC.push(dataTransform);
+      if (!REC.value) return;
+      const record = { id: decode(props.key), ...JSON.parse(decode(REC.value)) };
+      console.log("here is the decoded record: ", record);
+      if (record?.origin !== "micro") return;
+      if (!record?.actionType) record.actionType = "initial-submission";
+      if (!emailsToSend[`${record.actionType}-default`]) return;
+      emailsToSend[`${record.actionType}-default`].forEach((anEmail) => {
+        ACC.push({...anEmail, ...record});
+      })
     });
 
     return ACC;
   }, []);
-  console.log("the transformed records are: ", records);
-
+  console.log("the emails to send are: ", emails);
 
   try {
     await Promise.all(
-      records.map(async (oneEvent) => {
-        // verify that we have details for this action
-        const emailDestination = process.env.emailDestination[oneEvent.action];
-        if (!emailDestination) {
-          console.log("no email Destination for this action?  Should alert someone", oneEvent);
-          return null;
-        }
-
-        // check if this action needs emails from Cognito
-        if (emailDestination.ToAddresses.includes("allStateUsers")) {
+      emails.map(async (oneEmail) => {
+        if (oneEmail.ToAddresses.includes("allStateUsers")) {
           try {
             const commandListUsers = new ListUsersCommand({
               UserPoolId: process.env.cognitoPoolId,
@@ -170,17 +82,17 @@ export const main = async (event, context, callback) => {
             const listUsersResponse = await Cognito.send(commandListUsers);
             console.log("listUsers response: ", JSON.stringify(listUsersResponse, null, 4));
           } catch (err) {
-            console.log("Failed to List users.", err, JSON.stringify(oneEvent, null, 4));
+            console.log("Failed to List users.", err, JSON.stringify(oneEmail, null, 4));
           }
         }
 
         try {
-          const sendTemplatedEmailCommand = createSendTemplatedEmailCommand(oneEvent);
+          const sendTemplatedEmailCommand = createSendTemplatedEmailCommand(oneEmail);
           console.log("the sendTemplatedEmailCommand is: ", JSON.stringify(sendTemplatedEmailCommand, null, 4));
           response = await SES.send(sendTemplatedEmailCommand);
           console.log("sendEmailCommand response: ", JSON.stringify(response, null, 4));
         } catch (err) {
-          console.log("Failed to process oneEvent.", err, JSON.stringify(oneEvent, null, 4));
+          console.log("Failed to process oneEmail.", err, JSON.stringify(oneEmail, null, 4));
         }
       })
     );
