2579ddb9e834e968b25ace970ce4dc76eae48722
Kevin Haube <khaube@fearless.tech>
2024-02-08 13:27:37 -0500

    Add modalContext tests

--- src/services/ui/src/components/Context/modalContext.test.tsx
+++ src/services/ui/src/components/Context/modalContext.test.tsx
@@ -1,10 +1,11 @@
 import { act, render, renderHook, screen } from "@testing-library/react";
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, vi } from "vitest";
 import { PropsWithChildren, useEffect } from "react";
 import {
   useModalContext,
   ModalProvider,
 } from "@/components/Context/modalContext";
+import { MemoryRouter } from "react-router-dom";
 
 const wrapper = ({ children }: PropsWithChildren) => (
   <MemoryRouter>
@@ -15,29 +16,57 @@ const wrapper = ({ children }: PropsWithChildren) => (
 describe("useAlertContext", () => {
   test("hook provides content values and setter", () => {
     const ctx = renderHook(useModalContext, { wrapper });
+    expect(ctx.result.current.content).toStrictEqual({
+      header: "No header given",
+      body: "No body given",
+    });
+    act(() =>
+      ctx.result.current.setContent({
+        header: "New header",
+        body: "New body",
+      })
+    );
+    expect(ctx.result.current.content).toStrictEqual({
+      header: "New header",
+      body: "New body",
+    });
   });
-  test("hook provides switch for showing the banner, defaults to off", () => {
+  test("hook provides switch for showing the modal, defaults to off", () => {
     const ctx = renderHook(useModalContext, { wrapper });
+    expect(ctx.result.current.modalOpen).toBe(false);
+    act(() => ctx.result.current.setModalOpen(true));
+    expect(ctx.result.current.modalOpen).toBe(true);
   });
-  test("hook provides route-specific display criteria and setter", () => {
+  test("hook provides onAccept action property and setter", () => {
     const ctx = renderHook(useModalContext, { wrapper });
+    const action = vi.fn(() => console.log("test"));
+    expect(ctx.result.current.onAccept).toBe(void {});
+    act(() => ctx.result.current.setOnAccept(() => action));
+    act(() => ctx.result.current.onAccept());
+    expect(action).toHaveBeenCalledOnce();
   });
 });
 
-const TestChild = () => {
-  const alert = useAlertContext();
-  useEffect(() => {
-    alert.setBannerShow(true);
-  }, []);
-  return <h1>test</h1>;
-};
-describe("AlertProvider", () => {
-  test("renders alert component above children", () => {
+describe("ModalProvider", () => {
+  test("accept button is operational", () => {
+    const action = vi.fn(() => console.log("test"));
+    const TestChild = () => {
+      const modal = useModalContext();
+      useEffect(() => {
+        modal.setContent({
+          header: "Test header",
+          body: "Test body",
+          acceptButtonText: "Accept",
+          cancelButtonText: "Cancel",
+        });
+        modal.setOnAccept(() => action);
+        modal.setModalOpen(true);
+      }, []);
+      return <h1>test</h1>;
+    };
     render(<TestChild />, { wrapper });
-    const child = screen.getByText("test");
-    const alert = screen.getByText("No header given");
-    expect(child.compareDocumentPosition(alert)).toBe(
-      Node.DOCUMENT_POSITION_FOLLOWING
-    );
+    const accept = screen.getByText("Accept");
+    accept.click();
+    expect(action).toHaveBeenCalledOnce();
   });
 });
