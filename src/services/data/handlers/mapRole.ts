099fdaf57ba1a6ecc907da2080f2d271c39f7dc4
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2024-02-12 12:13:28 -0500

    move

--- src/services/data/handlers/mapRole.ts
+++ src/services/data/handlers/mapRole.ts
@@ -1,38 +1,35 @@
 import { Handler } from "aws-lambda";
-import { send, SUCCESS, FAILED } from "cfn-response-async";
-type ResponseStatus = typeof SUCCESS | typeof FAILED;
 import * as os from "./../../../libs/opensearch-lib";
 
-export const handler: Handler = async (event, context) => {
+export const handler: Handler = async (event, context, callback) => {
   console.log("request:", JSON.stringify(event, undefined, 2));
-  const responseData = {};
-  let responseStatus: ResponseStatus = SUCCESS;
+  const response = {
+    statusCode: 200,
+  };
   try {
-    if (event.RequestType == "Create" || event.RequestType == "Update") {
-      if (!event.ResourceProperties.MasterRoleToAssume) {
-        throw "ERROR:  Property MasterRoleToAssume is required, but was not supplied.";
-      }
-      if (!event.ResourceProperties.OSRoleName) {
-        throw "ERROR:  Property OSRoleName is required, but was not supplied.";
-      }
-      if (!event.ResourceProperties.IAMRoleName) {
-        throw "ERROR:  Property IAMRoleName is required, but was not supplied.";
-      }
-      if (!process.env.osDomain) {
-        throw "ERROR:  process.env.osDomain must be defined";
-      }
-      await os.mapRole(
-        process.env.osDomain,
-        event.ResourceProperties.MasterRoleToAssume,
-        event.ResourceProperties.OSRoleName,
-        event.ResourceProperties.IAMRoleName
-      );
+    if (!event.MasterRoleToAssume) {
+      throw "ERROR:  Property MasterRoleToAssume is required, but was not supplied.";
     }
+    if (!event.OSRoleName) {
+      throw "ERROR:  Property OSRoleName is required, but was not supplied.";
+    }
+    if (!event.IAMRoleName) {
+      throw "ERROR:  Property IAMRoleName is required, but was not supplied.";
+    }
+    if (!process.env.osDomain) {
+      throw "ERROR:  process.env.osDomain must be defined";
+    }
+    let reply = await os.mapRole(
+      process.env.osDomain,
+      event.ResourceProperties.MasterRoleToAssume,
+      event.ResourceProperties.OSRoleName,
+      event.ResourceProperties.IAMRoleName
+    );
+    console.log(reply);
   } catch (error) {
     console.log(error);
-    responseStatus = FAILED;
+    response.statusCode = 500;
   } finally {
-    console.log("finally");
-    await send(event, context, responseStatus, responseData);
+    callback(null, response);
   }
 };
