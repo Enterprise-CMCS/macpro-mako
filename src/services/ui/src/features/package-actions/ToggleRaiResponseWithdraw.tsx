9d4de6f40e08cafb1268a53558afe7a9249f90b6
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-02-12 14:29:03 -0500

    finish the look of togglerai

--- src/services/ui/src/features/package-actions/ToggleRaiResponseWithdraw.tsx
+++ src/services/ui/src/features/package-actions/ToggleRaiResponseWithdraw.tsx
@@ -1,15 +1,12 @@
 import { submit } from "@/api/submissionService";
 import { getUser } from "@/api/useGetUser";
 import * as SC from "@/features/package-actions/shared-components";
-import { ActionFunction } from "react-router-dom";
+import { ActionFunction, useActionData } from "react-router-dom";
 import { PlanType } from "shared-types";
 import { z } from "zod";
 
 export const toggleRaiResponseWithdrawSchema = z.object({
-  raiWithdrawEnabled: z
-    .boolean()
-    .or(z.string())
-    .transform((enabled) => Boolean(enabled)),
+  raiWithdrawEnabled: z.string().transform((enabled) => Boolean(enabled)),
 });
 
 export const onValidSubmission: ActionFunction = async ({ request }) => {
@@ -48,15 +45,27 @@ type Props = {
 
 export const ToggleRaiResponseWithdraw = ({ isEnabled }: Props) => {
   const { formMethods, handleSubmit } = SC.useSubmitForm();
+  const raiTypeText = isEnabled ? "Enable" : "Disable";
+  const raiDescriptionText = isEnabled
+    ? "Once you submit this form, the most recent Formal RAI Response for this package will be able to be withdrawn by the state. "
+    : "Once you submit this form, you will disable the previous Formal RAI Response Withdraw - Enabled action. The State will not be able to withdraw the Formal RAI Response. ";
 
   if (!formMethods.getValues("raiWithdrawEnabled")) {
-    console.log("hello");
     formMethods.setValue("raiWithdrawEnabled", isEnabled);
   }
 
   return (
     <>
-      <SC.Heading title="Enable and Disable Stuff" />
+      <SC.Heading
+        title={`${raiTypeText} Formal RAI Response Withdraw Details`}
+      />
+      <SC.ActionDescription>
+        {raiDescriptionText}
+        <strong className="font-bold">
+          If you leave this page, you will lose your progress on this form.
+        </strong>
+      </SC.ActionDescription>
+      <SC.PackageSection id="test-spa-id" type="testing" />
       <form onSubmit={handleSubmit}>
         <SC.SubmissionButtons />
       </form>
