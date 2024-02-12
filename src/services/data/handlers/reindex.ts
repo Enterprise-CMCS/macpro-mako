b549802f5dd2a3efa52403253ab1ac3a0e52d5d2
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2024-02-12 13:33:51 -0500

    breakout

--- src/services/data/handlers/reindex.ts
+++ src/services/data/handlers/reindex.ts
@@ -39,40 +39,6 @@ export const toggleTriggers: Handler = async () => {
   }
 };
 
-export const toggleTriggersTwo: Handler = async (event, context, callback) => {
-  console.log("request:", JSON.stringify(event, undefined, 2));
-  const response = {
-    statusCode: 200,
-  };
-  try {
-    let enabled = event.Enabled
-    for (const functionName of event.Functions) {
-      const lambdaClient = new LambdaClient({});
-      const response = await lambdaClient.send(
-        new ListEventSourceMappingsCommand({ FunctionName: functionName })
-      );
-      for (const eventSourceMapping of response.EventSourceMappings || []) {
-        if (eventSourceMapping.SelfManagedKafkaEventSourceConfig) {
-          console.log(
-            `Disabling all Kafka triggers for function:  ${functionName}`
-          );
-          await lambdaClient.send(
-            new UpdateEventSourceMappingCommand({
-              UUID: eventSourceMapping.UUID,
-              Enabled: enabled,
-            })
-          );
-        }
-      }
-    }
-  } catch (error: any) {
-    response.statusCode = 500;
-    callback(error, response);
-  } finally {
-    callback(null, response);
-  }
-};
-
 export const getConsumerGroupStatus: Handler = async (
   event,
   context,
