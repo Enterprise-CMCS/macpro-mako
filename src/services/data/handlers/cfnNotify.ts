57d3e2048edf79d4e1f2475bfafda28ce80d155c
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2024-02-13 07:49:16 -0500

    lint

--- src/services/data/handlers/cfnNotify.ts
+++ src/services/data/handlers/cfnNotify.ts
@@ -8,12 +8,12 @@ export const handler:Handler = async (event, _, callback) => {
   let errorResponse = null;
   const responseData = {};
   try {
-    let cfnEvent = event.Context.Execution.Input?.cfnEvent;
-    let cfnContext = event.Context.Execution.Input?.cfnContext;
+    const cfnEvent = event.Context.Execution.Input?.cfnEvent;
+    const cfnContext = event.Context.Execution.Input?.cfnContext;
     if(!cfnEvent){
-      console.log("No cfnEvent.  No one to notify.  Will proceed")
+      console.log("No cfnEvent.  No one to notify.  Will proceed");
     } else {
-      console.log("Sending notification to CFN... Success: " + event.Success)
+      console.log("Sending notification to CFN... Success: " + event.Success);
       const result = event.Success ? SUCCESS : FAILED
       await send(cfnEvent, cfnContext, result, responseData, "static");
     }
