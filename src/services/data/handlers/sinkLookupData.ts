b40a930fa7ed6fa20f946fb72551bbf75e75f3c6
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2024-02-16 02:44:02 -0500

    middle ground ish

--- src/services/data/handlers/sinkLookupData.ts
+++ src/services/data/handlers/sinkLookupData.ts
@@ -1,8 +1,9 @@
 import { Handler } from "aws-lambda";
 import { decode } from "base-64";
 import * as os from "./../../../libs/opensearch-lib";
-import { KafkaRecord, opensearch } from "shared-types";
+import { opensearch } from "shared-types";
 import { KafkaEvent } from "shared-types";
+import { Index } from "shared-types/opensearch";
 
 const osDomain: string =
   process.env.osDomain ||
@@ -14,90 +15,57 @@ export const getTableName = (recordKey: string) => {
   return recordKey.split(".").pop()?.split("-").slice(0, -1).join("-") || "";
 };
 
+const tables: {
+  [key: string]: {
+    index: Index;
+    transform: any;
+  };
+} = {
+  SPA_Type: {
+    index: "types",
+    transform: opensearch.types.spa_type,
+  },
+  Type: {
+    index: "subtypes",
+    transform: opensearch.subtypes.type,
+  },
+};
+
 export const handler: Handler<KafkaEvent> = async (event) => {
   for (const recordKey of Object.keys(event.records)) {
     const tableName = getTableName(recordKey);
-    switch (tableName) {
-      case "SPA_Type":
-        console.log("would process type records");
-        await typeIndexer(event.records[recordKey]);
-        break;
-      case "Type":
-        console.log("would process sub type records");
-        await subtypeIndexer(event.records[recordKey]);
-        break;
-      default:
-        console.log(`ERROR:  Unknown topic event source ${recordKey}`);
-    }
-  }
-};
 
-export const typeIndexer = async (records: KafkaRecord[]) => {
-  const docs: any = [];
-  for (const kafkaRecord of records) {
-    const { key, value } = kafkaRecord;
-    try {
-      const id: string = JSON.parse(decode(key));
-      const record = JSON.parse(decode(value)).payload.after;
-      if (!record) {
-        console.log("delete detected... TODO:  handle deletes");
-        continue;
-      }
-      const result = opensearch.types.spa_type.transform(id).safeParse(record);
-      if (!result.success) {
+    const docs: any = [];
+    for (const kafkaRecord of event.records[recordKey]) {
+      const { key, value } = kafkaRecord;
+      try {
+        const id: string = JSON.parse(decode(key));
+        const record = JSON.parse(decode(value)).payload.after;
+        if (!record) {
+          console.log("delete detected... TODO:  handle deletes");
+          continue;
+        }
+        const result = tables[tableName].transform(id).safeParse(record);
+        if (!result.success) {
+          console.log(
+            "TYPES Validation Error. The following record failed to parse: ",
+            JSON.stringify(record),
+            "Because of the following Reason(s):",
+            result.error.message
+          );
+        } else {
+          docs.push(result.data);
+        }
+      } catch (error) {
         console.log(
-          "TYPES Validation Error. The following record failed to parse: ",
-          JSON.stringify(record),
-          "Because of the following Reason(s):",
-          result.error.message
+          `ERROR UKNOWN:  An unknown error occurred.  Loop continuing.  Error:  ${error}`
         );
-      } else {
-        docs.push(result.data);
       }
-    } catch (error) {
-      console.log(
-        `ERROR UKNOWN:  An unknown error occurred.  Loop continuing.  Error:  ${error}`
-      );
     }
-  }
-  try {
-    await os.bulkUpdateData(osDomain, "types", docs);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const subtypeIndexer = async (records: KafkaRecord[]) => {
-  const docs: any = [];
-  for (const kafkaRecord of records) {
-    const { key, value } = kafkaRecord;
     try {
-      const id: string = JSON.parse(decode(key));
-      const record = JSON.parse(decode(value)).payload.after;
-      if (!record) {
-        console.log("delete detected... TODO:  handle deletes");
-        continue;
-      }
-      const result = opensearch.subtypes.type.transform(id).safeParse(record);
-      if (!result.success) {
-        console.log(
-          "SUBTYPES Validation Error. The following record failed to parse: ",
-          JSON.stringify(record),
-          "Because of the following Reason(s):",
-          result.error.message
-        );
-      } else {
-        docs.push(result.data);
-      }
+      await os.bulkUpdateData(osDomain, tables[tableName].index, docs);
     } catch (error) {
-      console.log(
-        `ERROR UKNOWN:  An unknown error occurred.  Loop continuing.  Error:  ${error}`
-      );
+      console.error(error);
     }
-  }
-  try {
-    await os.bulkUpdateData(osDomain, "subtypes", docs);
-  } catch (error) {
-    console.error(error);
   }
 };
