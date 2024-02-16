bc341a2d60c137882365e966e8e620d7d6b8c8f7
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2024-02-16 03:16:09 -0500

    asdf

--- src/packages/shared-types/seatool-tables/type.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const seaTypeSchema = z.object({
-  SPA_Type_ID: z.number(),
-  SPA_Type_Name: z.string(),
-  Plan_Type_ID: z.number().optional(),
-});
-export type SeaType = z.infer<typeof seaTypeSchema>;
