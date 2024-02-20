a6b3667fcc2966027145da235805b0da5d8e74fd
James Dinh <jdinh@collabralink.com>
2024-02-20 12:26:23 -0800

    Update loginPage.ts

--- src/services/ui/e2e/pages/loginPage.ts
+++ src/services/ui/e2e/pages/loginPage.ts
@@ -13,9 +13,9 @@ export class LoginPage {
     await this.page.getByRole("textbox", { name: "name@host.com" }).fill(email);
     await this.page.getByRole("textbox", { name: "Password" }).fill(password);
     await this.page.getByRole("button", { name: "submit" }).click();
-    // await this.page.waitForLoadState("networkidle");
-    // expect(
-    //   await this.page.getByRole("link", { name: "Dashboard" })
-    // ).toBeVisible();
+    await this.page.waitForLoadState("networkidle");
+    expect(
+      await this.page.getByRole("link", { name: "Dashboard" })
+    ).toBeVisible();
   }
 }
