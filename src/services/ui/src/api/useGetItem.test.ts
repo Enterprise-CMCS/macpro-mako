cfec19004ba002c10d2b606f2b48f79a1d2f57e5
Kevin Haube <khaube@fearless.tech>
2024-02-20 13:55:59 -0500

    Setup msw and write first handler

--- src/services/ui/src/api/useGetItem.test.ts
+++ src/services/ui/src/api/useGetItem.test.ts
@@ -22,21 +22,19 @@ describe("getItem", () => {
   });
 });
 
-describe("item helpers", () => {
-  beforeAll(() => {
-    mockItem.server.listen();
-  });
-  afterEach(() => mockItem.server.resetHandlers());
-  afterAll(() => mockItem.server.close());
-  // describe("idIsUnique", () => {
-  //   it("confirms no matching record is returned", async () => {
-  //     API.post = vi.fn((apiName, path, init) =>
-  //       fetch("/item-mock-server", { ...init, method: "post" })
-  //     );
-  //     expect(await idIsUnique("existing-id")).toBe(false);
-  //   });
-  // });
-  // describe("idIsApproved", () => {
-  //   it("", () => {});
-  // });
-});
+// describe("item helpers", () => {
+//   beforeAll(() => mockItem.server.listen());
+//   afterEach(() => mockItem.server.resetHandlers());
+//   afterAll(() => mockItem.server.close());
+//   describe("idIsUnique", () => {
+//     it("confirms no matching record is returned", async () => {
+//       API.post = vi.fn((apiName, path, init) =>
+//         fetch("/item-mock-server", { ...init, method: "post" })
+//       );
+//       expect(await idIsUnique("existing-id")).toBe(false);
+//     });
+//   });
+//   describe("idIsApproved", () => {
+//     it("", () => {});
+//   });
+// });
