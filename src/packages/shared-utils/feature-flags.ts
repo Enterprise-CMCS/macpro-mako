29c8e5152e2ab4f537283385a6e8666d13690ff4
James Dinh <jdinh@collabralink.com>
2024-02-26 10:13:15 -0800

    cleanup

--- src/packages/shared-utils/feature-flags.ts
+++ src/packages/shared-utils/feature-flags.ts
@@ -2,7 +2,7 @@
  * Source of truth for feature flags in application.
  * Ensures some type safety around flag names when we're enabling/disabling features in our code.
  *
- * This file also used to generate types for our Jest unit tests and Cypress tests validation logic.
+ *
  */
 
 export const featureFlags = {
@@ -20,14 +20,5 @@ export const featureFlags = {
   CLEAR_DATA_BUTTON: {
     flag: "clear-data-button",
     defaultValue: false,
-  },
-
-  /**
-   * Used in testing to simulate errors in fetching flag value.
-   * This flag does not exist in LaunchDarkly dashboard so fetching this will return the defaultValue.
-   */
-  TEST_ERROR_FETCHING_FLAG: {
-    flag: "test-error-fetching-flag",
-    defaultValue: undefined,
   },
 } as const;
