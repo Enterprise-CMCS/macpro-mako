1eda05a7f8b88e0d57b5ccc62b118fe992e04c2a
Kristin Grue <k.grue@theta-llc.com>
2024-02-29 12:01:27 -0500

    re-add Cognito lookup

--- src/services/email/libs/cognito-lib.js
+++ src/services/email/libs/cognito-lib.js
@@ -1,19 +1,23 @@
+import {
+  CognitoIdentityProviderClient,
+  ListUsersCommand,
+  // UserType as CognitoUserType,
+} from "@aws-sdk/client-cognito-identity-provider";
+
+const Cognito = new CognitoIdentityProviderClient({
+  region: process.env.region,
+});
+
 export const getCognitoData = async (id) => {
     try {
-      // if (!process.env.osDomain) {
-      //   throw new Error("process.env.osDomain must be defined");
-      // }
-  
-      // const osInsightsItem = await os.getItem(
-      //   process.env.osDomain,
-      //   "insights",
-      //   id
-      // );
-      // console.log("The OpenSearch Item index Insights for %s is: ", id, JSON.stringify(osInsightsItem, null, 4));
-      // const cpoc = osInsightsItem?._source?.LEAD_ANALYST ? osInsightsItem._source.LEAD_ANALYST : "LEAD_ANALYST IS null?";
-      // const srt = osInsightsItem?._source?.ACTION_OFFICERS ? osInsightsItem._source.ACTION_OFFICERS : "ACTION_OFFICERS IS null?";
-      // console.log("CPOC and SRT are: ", cpoc, srt);
-      // if (osInsightsItem) return "'CPOC Insights' <k.grue.cmsapprover@gmail.com>;'SRT Insights' <k.grue.stateadmn@gmail.com>";
+
+      const commandListUsers = new ListUsersCommand({
+        UserPoolId: process.env.cognitoPoolId,
+        // Filter: subFilter,
+    });
+    const listUsersResponse = await Cognito.send(commandListUsers);
+        console.log("listUsers response: ", listUsersResponse);
+
       return { "allState": `'${id} State 1' <k.grue.stateuser@gmail.com>;'${id} State 2' <k.grue.stateadmn@gmail.com>`};
     } catch (error) {
       console.log("Cognito error is: ", error);
