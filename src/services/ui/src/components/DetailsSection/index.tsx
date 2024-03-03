299d71653985c52470472369ef34c9538b7fd31b
Benjamin Paige <benjamin.paige@gmail.com>
2024-03-03 16:18:27 -0700

    pushing pixels

--- src/services/ui/src/components/DetailsSection/index.tsx
+++ src/services/ui/src/components/DetailsSection/index.tsx
@@ -12,9 +12,9 @@ export const DetailsSection: React.FC<DetailsSectionProps> = ({
   id,
 }: DetailsSectionProps) => {
   return (
-    <div id={id} className="mb-8 border-[1px] p-4">
+    <div id={id} className="mb-8 border border-slate-600 p-4">
       <h2 className="text-3xl font-semibold mb-2">{title}</h2>
-      <hr className="my-4" />
+      <hr className="my-4 bg-gray-700 border " />
 
       {description && <p className="mb-4 text-sm">{description}</p>}
 
