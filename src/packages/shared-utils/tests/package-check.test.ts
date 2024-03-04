5211490cae79f2774778463e1d67b0d61a71fe0e
Kevin Haube <khaube@fearless.tech>
2024-03-04 14:04:28 -0500

    Add Package Check tests

--- src/packages/shared-utils/tests/package-check.test.ts
+++ src/packages/shared-utils/tests/package-check.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { baseNewSubmissionObj, testCmsUser } from "./testData";
 import { PackageCheck } from "../package-check";
-import { Authority } from "shared-types";
+import { Authority, SEATOOL_STATUS } from "shared-types";
 
 // Build Mock Package data for:
 //   - make it basic, like a new submission
@@ -10,16 +10,95 @@ import { Authority } from "shared-types";
 
 describe("PackageCheck", () => {
   describe("Plan Checks", () => {
-    it("checks if isSpa", () => {});
-    it("checks if isWaiver", () => {});
-    it("checks against input", () => {});
+    it("checks if isSpa", () => {
+      let packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        authority: Authority.MED_SPA,
+      });
+      expect(packageCheck.isSpa).toBe(true);
+      packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        authority: Authority.CHIP_SPA,
+      });
+      expect(packageCheck.isSpa).toBe(true);
+      packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        authority: Authority["1915b"],
+      });
+      expect(packageCheck.isSpa).toBe(false);
+    });
+    it("checks if isWaiver", () => {
+      let packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        authority: Authority["1915b"],
+      });
+      expect(packageCheck.isWaiver).toBe(true);
+      packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        authority: Authority.CHIP_SPA,
+      });
+      expect(packageCheck.isWaiver).toBe(false);
+    });
+    it("checks against input", () => {
+      let packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        authority: Authority["1915b"],
+      });
+      expect(packageCheck.authorityIs([Authority["1915b"]])).toBe(true);
+    });
   });
 
   describe("Status Checks", () => {
-    it("checks if isInActivePendingStatus", () => {});
-    it("checks if isInSecondClock", () => {});
-    it("checks if isNotWithdrawn", () => {});
-    it("checks against input", () => {});
+    it("checks if isInActivePendingStatus", () => {
+      let packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        seatoolStatus: SEATOOL_STATUS.PENDING,
+      });
+      expect(packageCheck.isInActivePendingStatus).toBe(true);
+      packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        seatoolStatus: SEATOOL_STATUS.APPROVED,
+      });
+      expect(packageCheck.isInActivePendingStatus).toBe(false);
+    });
+    it("checks if isInSecondClock", () => {
+      let packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        authority: Authority.CHIP_SPA, // Chip Spas don't have 2nd clock
+        seatoolStatus: SEATOOL_STATUS.PENDING,
+        raiRequestedDate: "exists",
+        raiReceivedDate: "exists",
+      });
+      expect(packageCheck.isInSecondClock).toBe(false);
+      packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        authority: Authority.MED_SPA,
+        seatoolStatus: SEATOOL_STATUS.PENDING,
+        raiRequestedDate: "exists",
+        raiReceivedDate: "exists",
+      });
+      expect(packageCheck.isInSecondClock).toBe(true);
+    });
+    it("checks if isNotWithdrawn", () => {
+      let packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        seatoolStatus: SEATOOL_STATUS.WITHDRAWN,
+      });
+      expect(packageCheck.isNotWithdrawn).toBe(false);
+      packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        seatoolStatus: SEATOOL_STATUS.APPROVED,
+      });
+      expect(packageCheck.isNotWithdrawn).toBe(true);
+    });
+    it("checks against input", () => {
+      let packageCheck = PackageCheck({
+        ...baseNewSubmissionObj._source,
+        seatoolStatus: SEATOOL_STATUS.WITHDRAWN,
+      });
+      expect(packageCheck.hasStatus(SEATOOL_STATUS.PENDING_RAI)).toBe(false);
+      expect(packageCheck.hasStatus(SEATOOL_STATUS.WITHDRAWN)).toBe(true);
+    });
   });
 
   describe("RAI Checks", () => {
