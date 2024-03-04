1126ecad3ab7a370dfef229d54bb92a6f4226b86
Kevin Haube <khaube@fearless.tech>
2024-03-04 15:09:28 -0500

    Refactor user-helper

--- src/packages/shared-utils/user-helper.ts
+++ src/packages/shared-utils/user-helper.ts
@@ -4,52 +4,27 @@ import {
   CMS_READ_ONLY_ROLES,
   CognitoUserAttributes,
   STATE_ROLES,
-} from "../shared-types";
+  UserRoles,
+  UserRolesString,
+} from "shared-types";
 
-export const isCmsUser = (user: CognitoUserAttributes) => {
-  const userRoles = user["custom:cms-roles"];
-
-  for (const cmsRole of CMS_ROLES) {
-    if (userRoles.includes(cmsRole)) {
-      return true;
-    }
-  }
-  return false;
+const userHasAuthorizedRole = (
+  user: CognitoUserAttributes | null,
+  authorized: UserRoles[]
+) => {
+  if (!user) return false;
+  const userRoles = user["custom:cms-roles"].split(",") as UserRoles[];
+  return userRoles.filter((role) => authorized.includes(role)).length > 0;
 };
 
-export const isCmsWriteUser = (user: CognitoUserAttributes) => {
-  const userRoles = user["custom:cms-roles"];
+export const isCmsUser = (user: CognitoUserAttributes) =>
+  userHasAuthorizedRole(user, CMS_ROLES);
 
-  for (const role of CMS_WRITE_ROLES) {
-    if (userRoles.includes(role)) {
-      return true;
-    }
-  }
-  return false;
-};
+export const isCmsWriteUser = (user: CognitoUserAttributes) =>
+  userHasAuthorizedRole(user, CMS_WRITE_ROLES);
 
-export const isCmsReadonlyUser = (user: CognitoUserAttributes) => {
-  const userRoles = user["custom:cms-roles"];
+export const isCmsReadonlyUser = (user: CognitoUserAttributes) =>
+  userHasAuthorizedRole(user, CMS_READ_ONLY_ROLES);
 
-  for (const role of CMS_READ_ONLY_ROLES) {
-    if (userRoles.includes(role)) {
-      return true;
-    }
-  }
-  return false;
-};
-
-export const isStateUser = (user: CognitoUserAttributes | null) => {
-  if (!user) {
-    return false;
-  } else {
-    const userRoles = user["custom:cms-roles"];
-
-    for (const role of STATE_ROLES) {
-      if (userRoles.includes(role)) {
-        return true;
-      }
-    }
-    return false;
-  }
-};
+export const isStateUser = (user: CognitoUserAttributes | null) =>
+  userHasAuthorizedRole(user, STATE_ROLES);
