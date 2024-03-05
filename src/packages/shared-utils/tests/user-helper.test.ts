9f3f9afcaf5aa378e746c0071fe9f8710fe07e8f
Kevin Haube <khaube@fearless.tech>
2024-03-05 11:21:48 -0500

    Add isIDM tests

--- src/packages/shared-utils/tests/user-helper.test.ts
+++ src/packages/shared-utils/tests/user-helper.test.ts
@@ -13,6 +13,7 @@ import {
   testStateIDMUser,
 } from "./testData";
 import { CognitoUserAttributes } from "shared-types";
+import { OneMacUser } from "ui/src/api";
 
 const cmsHelpDeskUser: CognitoUserAttributes = {
   ...testCMSCognitoUser.user,
@@ -76,6 +77,10 @@ describe("isStateUser", () => {
   it("returns false for State users", () => {
     expect(isStateUser(stateSubmitterUser)).toEqual(true);
   });
+  // Maybe we should refactor to eliminate this
+  it("returns false for null args", () => {
+    expect(isStateUser(null)).toBe(false);
+  });
 });
 
 describe("isIDM", () => {
@@ -86,5 +91,28 @@ describe("isIDM", () => {
   it("returns true if a user has the IDM Cognito identity attribute", () => {
     expect(isIDM(testStateIDMUser.user.identities)).toBe(true);
     expect(isIDM(testCMSIDMUser.user.identities)).toBe(true);
+  });
+  it("returns false if a user has the Cognito identity attribute but it is not 'IDM'", () => {
+    const rogueIdentityUser: OneMacUser = {
+      isCms: testCMSCognitoUser.isCms,
+      user: {
+        ...testCMSCognitoUser.user,
+        identities:
+          '[{"dateCreated":"1709308952587","userId":"abc123","providerName":"NOT-IDM","providerType":"OIDC","issuer":null,"primary":"true"}]',
+      },
+    };
+    expect(isIDM(rogueIdentityUser.user.identities)).toBe(false);
+  });
+
+  it("returns false if a user has a malformed identities string", () => {
+    const rogueIdentityUser: OneMacUser = {
+      isCms: testCMSCognitoUser.isCms,
+      user: {
+        ...testCMSCognitoUser.user,
+        identities:
+          '[{"someThing":"1709308952587","someOne":"abc123","someWhere":"NOT-IDM","someHow":"OIDC"}]',
+      },
+    };
+    expect(isIDM(rogueIdentityUser.user.identities)).toBe(false);
   });
 });
