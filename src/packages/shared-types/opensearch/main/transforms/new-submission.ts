56080c2d6975274fe8c93c3b418390ae3ff2782f
Mike Dial <michael.dial.4@gmail.com>
2024-03-12 22:10:01 -0400

    lets see what breaks

--- src/packages/shared-types/opensearch/main/transforms/new-submission.ts
+++ src/packages/shared-types/opensearch/main/transforms/new-submission.ts
@@ -21,49 +21,41 @@ const getIdByAuthorityName = (authorityName: string) => {
 export const transform = (id: string) => {
   return onemacSchema.transform((data) => {
     if (data.seaActionType === "Extend") {
-      // We should have a separate transform for TE new submission... todo
+      // We should have a separate transform for TE new submission, and possibly for each new-submission that's unique (appk)... todo
       const seatoolStatus = SEATOOL_STATUS.PENDING;
       const { stateStatus, cmsStatus } = getStatus(seatoolStatus);
       return {
         id,
-        flavor: "WAIVER",
-        actionType: data.seaActionType,
-        // actionTypeId: 999999, // There isnt an action type id.. do we want this set or not set at all?
-        approvedEffectiveDate: null,
-        description: null, // This will be avilable when 4 fields come in.
-        finalDispositionDate: null,
-        leadAnalystOfficerId: null,
-        initialIntakeNeeded: null,
-        leadAnalystName: null,
-        authorityId: getIdByAuthorityName(data.authority),
-        authority: data.authority,
-        // types and subtypes will very soon be collected by the frontend and be avialable to be set.  these are null for now.
-        typeId: null,
-        typeName: null,
-        subTypeId: null,
-        subTypeName: null,
-        proposedDate: null,
-        raiReceivedDate: null,
-        raiRequestedDate: null,
-        raiWithdrawnDate: null,
-        reviewTeam: null,
-        state: id.split("-")[0],
-        stateStatus,
-        cmsStatus,
-        seatoolStatus,
-        subject: null,
-        secondClock: null,
-        raiWithdrawEnabled: null,
-
         attachments: data.attachments,
+        appkParentId: data.appkParentId,
+        raiWithdrawEnabled: data.raiWithdrawEnabled,
         additionalInformation: data.additionalInformation,
         submitterEmail: data.submitterEmail,
         submitterName:
           data.submitterName === "-- --" ? null : data.submitterName,
         origin: "OneMAC",
+        // ----------
+        // The fields below are usually set by way of seatool and the ksql output, but must be set here for TEs.
+        flavor: "WAIVER",
+        state: id.split("-")[0],
+        actionType: data.seaActionType,
+        actionTypeId: 9999,
+        authorityId: getIdByAuthorityName(data.authority),
+        authority: data.authority,
+        stateStatus,
+        cmsStatus,
+        seatoolStatus,
         statusDate: data.statusDate,
         submissionDate: data.submissionDate,
         changedDate: data.changedDate,
+        // type, subtype, subject, description will soon be collected and available in data; this will need updating then.
+        subject: null,
+        description: null,
+        typeId: null,
+        typeName: null,
+        subTypeId: null,
+        subTypeName: null,
+        // ----------
       };
     } else {
       return {
