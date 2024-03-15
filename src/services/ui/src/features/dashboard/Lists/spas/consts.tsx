f9489a7804c538146959e1367900f7d72248e9a8
Benjamin Paige <benjamin.paige@gmail.com>
2024-03-14 22:25:26 -0600

    feat(four fields): This change add required fields to the submission forms  (#428)
    * Update site title
    
    * init fields to medicaid submittion form
    
    * submission comments
    
    * update isprod check for next domain
    
    * update title check test
    
    * adjust layout width
    
    * Revert "adjust layout width"
    
    This reverts commit 9f6889fc71acabb3860aeb552473a67a0fc66840.
    
    * add subject and description to submit forms
    
    * adjust some fields did some refactoring
    
    * remove temo form
    
    * lots of refactoring
    
    * move dashboard to feature
    
    * getAllForms hood refactor structure
    
    * add types and subtypes schemas
    
    * add subject and description
    
    * add useGetPackageTypes
    
    * refactor authority, type, and subtype.  move to a pattern of lookup indices.
    
    * remove tests... little value, just confusing at this point
    
    * reset file to clear junk diff
    
    * remove commented lines
    
    * remove unused func
    
    * remove comments
    
    * lint
    
    * remove comment
    
    * remove file
    
    * lint
    
    * type to clear vs code red line
    
    * lint
    
    * remove unused vars
    
    * Correct custom resource replies
    
    * added some api calls and hooks
    
    * add subtypes stuff
    
    * filter for do not use
    
    * update query and waiver implimentation
    
    * update submit query
    
    * fix table name typo
    
    * refactor
    
    * refactor inports
    
    * updates per paul
    
    * prettier"
    
    * middle ground ish
    
    * yes
    
    * rename
    
    * asdf
    
    * Add back
    
    * ok
    
    * ok
    
    * asdf
    
    * align names
    
    * asdf
    
    * break the seatool index sink away, for operational speed.
    
    * correct logic for disabled triggers
    
    * update decode record util
    
    * added shared comps
    
    * Refactor our sinks and reindex workflow
    
    * Set status to undefined when undefined
    
    * skip records without a status
    
    * types
    
    * fix conflicts
    
    * update getAllForms
    
    * update types and detail and page
    
    * fix unit tests
    
    * refactor
    
    * lots of refactors
    
    * refactor
    
    * add deconde thing back
    
    * temp comment four fields stuff out
    
    * clean up
    
    * remove unused api/util/seatoolservice
    
    * Remove remaining repack stuff
    
    * uncomment additinal seatool fields
    
    * update details page fields
    
    * update type and subtype fields and stuff
    
    * update more stuff and queries
    
    * update styles
    
    * update that details page sidebar thing
    
    * style updates
    
    * pushing pixels
    
    * pushing pixels
    
    * use a transaction with commit and rollback for submit handler
    
    * write types and subtypes as a single insert to reduce pressure on ksql and increase performance
    
    * collapse action type into main query
    
    * i am danerous, iceman
    
    * fixing an issue... pulling authority id from the static map
    
    * remove transmit begin
    
    * Bring back review team thing
    
    * Bug fixes
    
    * small bug fixes
    
    * substitute some weird characters in type and subtype
    
    * better
    
    * Adding fetchTypes tests and adjust styling
    
    * Add subtype tests and prettierrc file :0
    
    * Combined type/subtype hooks and tests and mocks
    
    * Remove unused files
    
    * Pushing pixels
    
    * Pixel pushing
    
    * Update sidebar for details page
    
    * typo
    
    * Merge with master
    
    * Fixed sidebar bug
    
    * fixing bugs
    
    * fixing bugs
    
    * Bug Fixes
    
    * Add size to query
    
    * Bug fixes withn subtypes
    
    * Remainnig bug squashing
    
    * Squashed the rest of the bugs
    
    ---------
    
    Co-authored-by: Mike Dial <48921055+mdial89f@users.noreply.github.com>
    Co-authored-by: Mike Dial <michael.dial.4@gmail.com>

--- src/services/ui/src/features/dashboard/Lists/spas/consts.tsx
+++ src/services/ui/src/features/dashboard/Lists/spas/consts.tsx
@@ -31,7 +31,7 @@ export const useSpaTableColumns = (): OsTableColumn[] => {
     },
     {
       field: "authority.keyword",
-      label: "Type",
+      label: "Authority",
       cell: (data) =>
         data?.authority
           ? removeUnderscoresAndCapitalize(data.authority)
@@ -84,7 +84,7 @@ export const useSpaTableColumns = (): OsTableColumn[] => {
     },
     {
       field: "raiReceivedDate",
-      label: "Formal RAI Response",
+      label: "Formal RAI Received",
       cell: (data) => {
         if (!data.raiReceivedDate || data.raiWithdrawnDate) return null;
         return formatSeatoolDate(data.raiReceivedDate);
--- src/services/ui/src/features/submission/waiver/contracting/contracting-1915-b-waiver-renewal.tsx
+++ src/services/ui/src/features/submission/waiver/contracting/contracting-1915-b-waiver-renewal.tsx
@@ -36,6 +36,7 @@ import {
   DescriptionInput,
   SubTypeSelect,
   SubjectInput,
+  TypeSelect,
 } from "@/features";
 
 const formSchema = z
@@ -43,11 +44,18 @@ const formSchema = z
     waiverNumber: zRenewalOriginalWaiverNumberSchema,
     id: zRenewalWaiverNumberSchema,
     proposedEffectiveDate: z.date(),
-    // TODO: FFF
-    // subject: z.string(),
-    // description: z.string(),
-    // typeId: z.string().default("111"),
-    // subTypeId: z.string(),
+    subject: z
+      .string()
+      .trim()
+      .min(1, { message: "This field is required" })
+      .max(120, { message: "Subject should be under 120 characters" }),
+    description: z
+      .string()
+      .trim()
+      .min(1, { message: "This field is required" })
+      .max(4000, { message: "Description should be under 4000 characters" }),
+    typeIds: z.array(z.number()).min(1, { message: "Required" }),
+    subTypeIds: z.array(z.number()).min(1, { message: "Required" }),
     attachments: z.object({
       b4WaiverApplication: zAttachmentRequired({ min: 1 }),
       b4IndependentAssessment: zAttachmentOptional,
@@ -115,7 +123,7 @@ export const Contracting1915BWaiverRenewalPage = () => {
     navigate(originPath ? { path: originPath } : { path: "/dashboard" });
   }, []);
   const handleSubmit: SubmitHandler<Waiver1915BContractingRenewal> = async (
-    formData
+    formData,
   ) => {
     try {
       // AK-0260.R04.02
@@ -135,7 +143,7 @@ export const Contracting1915BWaiverRenewalPage = () => {
         // when any queries are added, such as the case of /details?id=...
         urlQuery.get(ORIGIN)
           ? originRoute[urlQuery.get(ORIGIN)! as Origin]
-          : "/dashboard"
+          : "/dashboard",
       );
       navigate(originPath ? { path: originPath } : { path: "/dashboard" });
     } catch (e) {
@@ -252,16 +260,24 @@ export const Contracting1915BWaiverRenewalPage = () => {
                 </Inputs.FormItem>
               )}
             />
-            {/* // TODO: FFF */}
-            {/* <SubTypeSelect
+            <SubjectInput
               control={form.control}
-              typeId={"111"}
-              name="subTypeId"
+              name="subject"
+              helperText="The title or purpose of the Waiver"
+            />
+            <DescriptionInput
+              control={form.control}
+              name="description"
+              helperText="A summary of the Waiver. This should include details about a reduction or increase, the amount of the reduction or increase, Federal Budget impact, and fiscal year. If there is a reduction, indicate if the EPSDT population is or isn’t exempt from the reduction."
+            />
+            <TypeSelect
+              control={form.control}
+              name="typeIds"
+              authorityId={122}
+            />
+            <SubTypeSelect
               authorityId={122} // waivers authority
             />
-
-            <SubjectInput control={form.control} name="subject" />
-            <DescriptionInput control={form.control} name="description" /> */}
           </SectionCard>
           <SectionCard title="Attachments">
             <Content.AttachmentsSizeTypesDesc faqLink="/faq/medicaid-spa-attachments" />
@@ -276,7 +292,10 @@ export const Contracting1915BWaiverRenewalPage = () => {
                       {label}
                       {required ? <Inputs.RequiredIndicator /> : null}
                     </Inputs.FormLabel>
-                    <Inputs.Upload files={field?.value ?? []} setFiles={field.onChange}  />
+                    <Inputs.Upload
+                      files={field?.value ?? []}
+                      setFiles={field.onChange}
+                    />
                     <Inputs.FormMessage />
                   </Inputs.FormItem>
                 )}
