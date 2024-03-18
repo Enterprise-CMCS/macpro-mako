d9ed2534c9ffc54bdcf79cef396d12e1be4d5a8a
Mike Dial <michael.dial.4@gmail.com>
2024-03-18 10:12:57 -0400

    Fix unit test

--- src/packages/shared-types/tests/onemac-legacy.test.ts
+++ src/packages/shared-types/tests/onemac-legacy.test.ts
@@ -5,22 +5,20 @@ import { opensearch } from "..";
 describe("onemac has valid data", () => {
   it("has valid data", () => {
     for (const record of onemacRecords) {
-      if (
-        record.GSI1pk?.startsWith("OneMAC#submit") &&
-        [
-          "waivernew",
-          "medicaidspa",
-          "chipspa",
-          "waiverappk",
-          "waiveramendment",
-          "waiverrenewal",
-        ].includes(record.GSI1pk.split("OneMAC#submit")[1])
-      ) {
-        const transformedData = opensearch.main.legacySubmission
-          .transform("randomid")
-          .parse(record);
-        expect(transformedData).toHaveProperty(["submitterName"]);
-      }
+      console.log(record);
+      console.log(record);
+      // Note:  what do we do here?
+      // In our sink, we filter our "-- --" records, as it indicates a non-onemac originating record.
+      // But here we're parsing un filtered records, and our zod schema is rightly failing.
+      // I added a filter here, to not fail the parse, but curious if theres a right way
+      if (record.submitterName === "-- --") continue;
+      const transformedData = opensearch.main.legacyPackageView
+        .transform("randomid")
+        .parse(record);
+      expect(transformedData).toHaveProperty("id");
+      expect(transformedData).toHaveProperty("submitterName");
+      expect(transformedData).toHaveProperty("submitterEmail");
+      expect(transformedData).toHaveProperty("origin");
     }
   });
 });
