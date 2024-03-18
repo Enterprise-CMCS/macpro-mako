3f04e87bf2b4d46a5ee8762f6fc66075521b600e
Mike Dial <michael.dial.4@gmail.com>
2024-03-18 12:12:22 -0400

    Remove double function call for readability

--- src/packages/shared-types/opensearch/changelog/transforms/legacy-event.ts
+++ src/packages/shared-types/opensearch/changelog/transforms/legacy-event.ts
@@ -4,63 +4,70 @@ export const transform = (id: string, offset: number) => {
   return legacyEventSchema.transform((data) => {
     // Resolve the action type based on the GSI1pk
     const eventType = data.GSI1pk.split("OneMAC#")[1];
-    const actionType = (() => {
-      switch (eventType) {
-        // These are the NOSO events.  I want to leave this here to make it easier to pick up in a bit.
-        // case "createchipspa":
-        // case "createmedicaidspa":
-        // case "createwaiveramendment":
-        //   console.log(
-        //     `NOSO event ${eventType} detected.  This event is not yet supported.  Doing nothing...`,
-        //   );
-        //   return undefined;
-        case "enableRAIResponseWithdraw":
-          return Action.ENABLE_RAI_WITHDRAW;
-        case "submitchipspa":
-        case "submitmedicaidspa":
-        case "submitwaiveramendment":
-        case "submitwaiverappk":
-        case "submitwaiverextension":
-        case "submitwaiverextensionb":
-        case "submitwaiverextensionc":
-        case "submitwaivernew":
-        case "submitwaiverrenewal":
-          return "new-submission";
-        case "submitchipsparai":
-        case "submitmedicaidsparai":
-        case "submitwaiveramendmentrai":
-        case "submitwaiverappkrai":
-        case "submitwaiverrai":
-          return Action.RESPOND_TO_RAI;
-        case "submitchipspawithdraw":
-        case "submitmedicaidspawithdraw":
-        case "submitwaiveramendmentwithdraw":
-        case "submitwaiverappkwithdraw":
-        case "submitwaivernewwithdraw":
-        case "submitwaiverrenewalwithdraw":
-          return Action.WITHDRAW_PACKAGE;
-        case "submitrairesponsewithdraw":
-          return Action.WITHDRAW_RAI; // This should be a separate action thats just a request
-        // These are seemingly deprecated event types.  They are not show in the legacy app.
-        // They each have a new event couterpart, so we can ignore them
-        // I'd like to leave this commented out for a bit, in case we need to make changes.
-        // case "withdrawchipspa":
-        // case "withdrawmedicaidspa":
-        // case "withdrawwaiveramendment":
-        // case "withdrawwaiverappk":
-        // case "withdrawwaivernew":
-        // case "withdrawwaiverrenewal":
-        //   console.log(
-        //     `Deprecated action ${eventType} detected.  Doing nothing...`,
-        //   );
-        //   return undefined;
-        default:
-          console.log(
-            `Unhandled event type for ${id}:  ${eventType}.  Doing nothing and continuing.`,
-          );
-          return undefined;
-      }
-    })();
+
+    let actionType;
+
+    switch (eventType) {
+      // These are the NOSO events.  I want to leave this here to make it easier to pick up in a bit.
+      // case "createchipspa":
+      // case "createmedicaidspa":
+      // case "createwaiveramendment":
+      //   console.log(
+      //     `NOSO event ${eventType} detected.  This event is not yet supported.  Doing nothing...`,
+      //   );
+      //   return undefined;
+      case "enableRAIResponseWithdraw":
+        actionType = Action.ENABLE_RAI_WITHDRAW;
+        break;
+      case "submitchipspa":
+      case "submitmedicaidspa":
+      case "submitwaiveramendment":
+      case "submitwaiverappk":
+      case "submitwaiverextension":
+      case "submitwaiverextensionb":
+      case "submitwaiverextensionc":
+      case "submitwaivernew":
+      case "submitwaiverrenewal":
+        actionType = "new-submission";
+        break;
+      case "submitchipsparai":
+      case "submitmedicaidsparai":
+      case "submitwaiveramendmentrai":
+      case "submitwaiverappkrai":
+      case "submitwaiverrai":
+        actionType = Action.RESPOND_TO_RAI;
+        break;
+      case "submitchipspawithdraw":
+      case "submitmedicaidspawithdraw":
+      case "submitwaiveramendmentwithdraw":
+      case "submitwaiverappkwithdraw":
+      case "submitwaivernewwithdraw":
+      case "submitwaiverrenewalwithdraw":
+        actionType = Action.WITHDRAW_PACKAGE;
+        break;
+      case "submitrairesponsewithdraw":
+        actionType = Action.WITHDRAW_RAI; // This should be a separate action thats just a request
+        break;
+      // These are seemingly deprecated event types.  They are not show in the legacy app.
+      // They each have a new event couterpart, so we can ignore them
+      // I'd like to leave this commented out for a bit, in case we need to make changes.
+      // case "withdrawchipspa":
+      // case "withdrawmedicaidspa":
+      // case "withdrawwaiveramendment":
+      // case "withdrawwaiverappk":
+      // case "withdrawwaivernew":
+      // case "withdrawwaiverrenewal":
+      //   console.log(
+      //     `Deprecated action ${eventType} detected.  Doing nothing...`,
+      //   );
+      //   return undefined;
+      default:
+        console.log(
+          `Unhandled event type for ${id}:  ${eventType}.  Doing nothing and continuing.`,
+        );
+        actionType = undefined;
+        break;
+    }
 
     // Return if the actionType is unhandled
     if (actionType === undefined) return undefined;
