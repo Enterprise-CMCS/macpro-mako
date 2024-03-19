bade4f796b23137b0f058dbebb52b586d6caeea6
Kevin Haube <khaube@fearless.tech>
2024-03-19 10:39:55 -0400

    Add messages in zod check

--- src/services/ui/src/utils/zod.ts
+++ src/services/ui/src/utils/zod.ts
@@ -98,7 +98,10 @@ export const zAmendmentOriginalWaiverNumberSchema = z
     message:
       "According to our records, this 1915(b) Waiver Number does not yet exist. Please check the 1915(b) Initial or Renewal Waiver Number and try entering it again.",
   })
-  .refine(async (value) => canBeRenewedOrAmended(value), { message: "" })
+  .refine(async (value) => canBeRenewedOrAmended(value), {
+    message:
+      "The 1915(b) Waiver Number entered does not seem to match our records. Please enter an approved 1915(b) Initial or Renewal Waiver Number, using a dash after the two character state abbreviation.",
+  })
   .refine(async (value) => idIsApproved(value), {
     message:
       "According to our records, this 1915(b) Waiver Number is not approved. You must supply an approved 1915(b) Initial or Renewal Waiver Number.",
@@ -118,7 +121,10 @@ export const zRenewalOriginalWaiverNumberSchema = z
     message:
       "According to our records, this 1915(b) Waiver Number does not yet exist. Please check the 1915(b) Initial or Renewal Waiver Number and try entering it again.",
   })
-  .refine(async (value) => canBeRenewedOrAmended(value), { message: "" })
+  .refine(async (value) => canBeRenewedOrAmended(value), {
+    message:
+      "The 1915(b) Waiver Number entered does not seem to match our records. Please enter an approved 1915(b) Initial or Renewal Waiver Number, using a dash after the two character state abbreviation.",
+  })
   .refine(async (value) => idIsApproved(value), {
     message:
       "According to our records, this 1915(b) Waiver Number is not approved. You must supply an approved 1915(b) Initial or Renewal Waiver Number.",
