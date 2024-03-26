af4acecfcd94da1c9e5292f3f03feeb8759efbda
James Dinh <jdinh@collabralink.com>
2024-03-25 18:24:02 -0700

    Update index.ts

--- src/services/api/webforms/index.ts
+++ src/services/api/webforms/index.ts
@@ -1,5 +1,5 @@
 import * as ABP1 from "./ABP1";
-import * as ABP2A from "./ABP2a";
+import * as ABP2a from "./ABP2a";
 import * as ABP10 from "./ABP10";
 import * as ABP11 from "./ABP11";
 import * as ABP3 from "./ABP3";
@@ -11,8 +11,8 @@ export const webformVersions: Record<string, Record<string, FormSchema>> = {
     v202401: ABP1.v202401,
     v202402: ABP1.v202402,
   },
-  ABP2A: {
-    v202401: ABP2A.v202401,
+  ABP2a: {
+    v202401: ABP2a.v202401,
   },
   ABP3: {
     v202401: ABP3.v202401,
