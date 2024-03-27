c3686d5c5b4f89039287be43ea24c7ed4cf03d29
Mike Dial <michael.dial.4@gmail.com>
2024-03-27 09:18:42 -0400

    Revert "rm unused"
    This reverts commit 294ead58794254fc7eeae661acbf4ad4f3ced69f.

--- src/services/ui/src/features/package-actions/index.tsx
+++ src/services/ui/src/features/package-actions/index.tsx
@@ -1,5 +1,13 @@
 import { Navigate, RouteObject } from "react-router-dom";
 import {
+  ToggleRaiResponseWithdraw,
+  onValidSubmission as toggleRaiWithdrawSubmission,
+} from "./ToggleRaiResponseWithdraw";
+import {
+  WithdrawPackage,
+  onValidSubmission as withdrawPackageSubmission,
+} from "./WithdrawPackage";
+import {
   TemporaryExtension,
   onValidSubmission as temporaryExtensionSubmission,
 } from "./TemporaryExtension";
@@ -10,6 +18,21 @@ export const packageActionRoutes: RouteObject = {
   path: "/action/:authority/:id",
   element: <ActionWrapper />,
   children: [
+    {
+      path: "enable-rai-withdraw",
+      element: <ToggleRaiResponseWithdraw isEnabled />,
+      action: toggleRaiWithdrawSubmission,
+    },
+    {
+      path: "disable-rai-withdraw",
+      element: <ToggleRaiResponseWithdraw isEnabled={false} />,
+      action: toggleRaiWithdrawSubmission,
+    },
+    {
+      path: "withdraw-package",
+      element: <WithdrawPackage />,
+      action: withdrawPackageSubmission,
+    },
     {
       path: "temporary-extension",
       element: <TemporaryExtension />,
