64b316320dff1f9b0fd90ef0e9569ad8abf4c402
Valencia McMurray <vmcmurray@collabralink.com>
2024-04-02 19:54:57 -0400

    ensure cypress axe is installed
    trying without implicitly installing in the deploy yml

--- src/services/cypress/cypress.config.js
+++ src/services/cypress/cypress.config.js
@@ -7,7 +7,7 @@ const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esb
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
-
+  cypressAxe(on);
   on("file:preprocessor",
     createBundler({
       plugins: [createEsbuildPlugin.default(config)],
