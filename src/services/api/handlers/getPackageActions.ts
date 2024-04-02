e628eb3588f19fad380ca107887bbbb2cfe9f72b
Mike Dial <michael.dial.4@gmail.com>
2024-04-02 14:23:29 -0400

    drift detectio, common actions only, yayyy

--- src/services/api/handlers/getPackageActions.ts
+++ src/services/api/handlers/getPackageActions.ts
@@ -7,6 +7,8 @@ import {
   lookupUserAttributes,
 } from "../libs/auth/user";
 import { response } from "../libs/handler";
+import { getAppkChildren } from "../libs/package";
+import { Action } from "shared-types";
 
 type GetPackageActionsBody = {
   id: string;
@@ -24,7 +26,7 @@ export const getPackageActions = async (event: APIGatewayEvent) => {
     const result = await getPackage(body.id);
     const passedStateAuth = await isAuthorizedToGetPackageActions(
       event,
-      result._source.state
+      result._source.state,
     );
     if (!passedStateAuth)
       return response({
@@ -39,13 +41,58 @@ export const getPackageActions = async (event: APIGatewayEvent) => {
     const authDetails = getAuthDetails(event);
     const userAttr = await lookupUserAttributes(
       authDetails.userId,
-      authDetails.poolId
+      authDetails.poolId,
     );
+
+    if (!result._source.appkParent) {
+      return response({
+        statusCode: 200,
+        body: {
+          actions: getAvailableActions(userAttr, result._source),
+        },
+      });
+    }
+
+    // Actions available on an appk need to consider child waivers, so they're handled differently
+    const appkChildren = await getAppkChildren(result._source.id);
+    const allAppkMembers = [result, ...appkChildren.hits.hits];
+
+    const allActions = [result, ...appkChildren.hits.hits].map((child) =>
+      getAvailableActions(userAttr, child._source as any),
+    );
+
+    // only want common actions; omit actions not in every appk package
+    let commonActions = allActions.reduce((acc, currentActions) => {
+      if (!acc.length) return currentActions;
+      return acc.filter((action) => currentActions.includes(action));
+    }, []); // Initial value of acc is null to handle the first iteration
+
+    // Remove RAI actions unless the RAI requested date is identical across all appk members
+    // Scenario: If the parent and the children all have respond to rai as an available action
+    //           But one of those children's latest rai has a different requested date (raitable in seetool)
+    //           There's drift, and there will be a failure when going to write the response
+    //           Further, we can't reliably know what RAI is correct.
+    const allRaiRequestedDates = allAppkMembers.map(
+      (member) => member._source.raiRequestedDate,
+    );
+    const isRaiRequestedDateIdentical = allRaiRequestedDates.every(
+      (date, _, arr) => date === arr[0],
+    );
+    if (!isRaiRequestedDateIdentical) {
+      const actionsToRemove = [
+        Action.RESPOND_TO_RAI,
+        Action.WITHDRAW_RAI,
+        Action.ISSUE_RAI,
+      ];
+      commonActions = commonActions.filter(
+        (action) => !actionsToRemove.includes(action),
+      );
+    }
 
     return response({
       statusCode: 200,
       body: {
-        actions: getAvailableActions(userAttr, result._source),
+        actions: commonActions,
       },
     });
   } catch (err) {
