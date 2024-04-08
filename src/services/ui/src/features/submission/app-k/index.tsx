b5f9b218671c4f7694cfeffc9ac7acf3f08ba39d
Kevin Haube <khaube@fearless.tech>
2024-04-08 09:19:39 -0400

    Fix appk double heading

--- src/services/ui/src/features/submission/app-k/index.tsx
+++ src/services/ui/src/features/submission/app-k/index.tsx
@@ -180,6 +180,7 @@ export const AppKSubmissionForm = () => {
               control={form.control}
               name={"additionalInformation"}
               render={SlotAdditionalInfo({
+                withoutHeading: true,
                 label: (
                   <p>
                     Add anything else you would like to share with CMS, limited
