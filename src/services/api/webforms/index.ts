cb7790d0137e600cb23762ff09021916b87edbfc
James Dinh <jdinh@collabralink.com>
2024-04-11 12:13:35 -0700

    add abp 9 logic

--- src/services/api/webforms/index.ts
+++ src/services/api/webforms/index.ts
@@ -4,6 +4,7 @@ import * as ABP2C from "./ABP2C";
 import * as ABP2B from "./ABP2B";
 import * as ABP3 from "./ABP3";
 import * as ABP3_1 from "./ABP3_1";
+import * as ABP9 from "./ABP9";
 import * as ABP10 from "./ABP10";
 import * as ABP11 from "./ABP11";
 import { FormSchema } from "shared-types";
@@ -27,6 +28,9 @@ export const webformVersions: Record<string, Record<string, FormSchema>> = {
   },
   ABP3_1: {
     v202401: ABP3_1.v202401,
+  },
+  ABP9: {
+    v202401: ABP9.v202401,
   },
   ABP10: {
     v202401: ABP10.v202401,
