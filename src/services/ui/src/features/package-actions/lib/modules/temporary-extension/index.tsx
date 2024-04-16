b70cc387c1fb690e65aaca66e24995e98ce9ef46
Kevin Haube <khaube@fearless.tech>
2024-04-16 15:28:28 -0400

    adapt Andie's TE alert changes

--- src/services/ui/src/features/package-actions/lib/modules/temporary-extension/index.tsx
+++ src/services/ui/src/features/package-actions/lib/modules/temporary-extension/index.tsx
@@ -102,8 +102,7 @@ export const defaultTempExtContent: FormContentHydrator = (document) => ({
   preSubmitNotice:
     "Once you submit this form, a confirmation email is sent to you and to CMS. CMS will use this content to review your package, and you will not be able to edit this form. If CMS needs any additional information, they will follow up by email.",
   successBanner: {
-    header: "Temporary Extension issued",
-    // TODO: figure out parent ID in success banner
-    body: `The Temporary Extension Request for ${"[parentId]"} has been submitted.`,
+    header: "Temporary Extension Request submitted.",
+    body: "Your submission has been received.",
   },
 });
