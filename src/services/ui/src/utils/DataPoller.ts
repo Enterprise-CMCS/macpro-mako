8ea718451c2a94bfdd97d49365bec118c7c0a7c6
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-04-24 12:31:35 -0400

    Add first iteration of new data poller that works with new stuff

---
+++ src/services/ui/src/utils/DataPoller.ts
@@ -0,0 +1,49 @@
+export type Options = {
+  fetcher: () => Promise<unknown>;
+  checkStatus: (data: any) => boolean;
+  interval: number;
+  pollAttempts: number;
+};
+
+export class DataPoller {
+  private options: Options;
+  isCorrectStatus = false;
+
+  constructor(options: Options) {
+    this.options = options;
+  }
+
+  async startPollingData() {
+    return new Promise<{
+      maxAttemptsReached: boolean;
+      correctStatusFound: boolean;
+    }>((resolve, reject) => {
+      let timesPolled = 0;
+
+      const intervalId = setInterval(async () => {
+        timesPolled++;
+        try {
+          if (timesPolled <= this.options.pollAttempts) {
+            const data = await this.options.fetcher();
+            const correctStatus = this.options.checkStatus(data);
+
+            if (correctStatus) {
+              resolve({
+                correctStatusFound: true,
+                maxAttemptsReached: false,
+              });
+            }
+          } else {
+            resolve({
+              correctStatusFound: false,
+              maxAttemptsReached: true,
+            });
+            clearInterval(intervalId);
+          }
+        } catch (err: unknown) {
+          reject(err);
+        }
+      }, this.options.interval);
+    });
+  }
+}
