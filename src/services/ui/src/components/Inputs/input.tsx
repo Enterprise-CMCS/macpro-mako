8dd65d594c2c71bbfe23981fc136cbc162db475d
Gavin St. Ours <gst-ours@fearless.tech>
2024-04-30 16:10:30 -0400

    Lint

--- src/services/ui/src/components/Inputs/input.tsx
+++ src/services/ui/src/components/Inputs/input.tsx
@@ -15,7 +15,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           className={cn(
             "flex h-9 w-full rounded-sm border border-black bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
             icon && "pl-6",
-            className
+            className,
           )}
           ref={ref}
           id={props.name}
@@ -23,7 +23,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         />
       </div>
     );
-  }
+  },
 );
 
 Input.displayName = "Input";
