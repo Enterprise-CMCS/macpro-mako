1f1e5a9b9cc9b7a160f488aadb2695a462826bd6
Kevin Haube <khaube@fearless.tech>
2024-04-30 09:29:48 -0400

    fix button wording

--- src/services/ui/src/features/package-actions/lib/modules/withdraw-package/waiver/withdraw-appk.tsx
+++ src/services/ui/src/features/package-actions/lib/modules/withdraw-package/waiver/withdraw-appk.tsx
@@ -6,7 +6,7 @@ export const appKWithdrawPackageContent: FormContentHydrator = (document) => ({
   confirmationModal: {
     header: "Are you sure you want to withdraw this package?",
     body: `You are about to withdraw ${document.authority} ${document.id}. Completing this action will conclude the review of this ${document.authority} package, and all packages associated with ${document.id} will also be withdrawn. If you are not sure this is the correct action to select, contact your CMS point of contact for assistance`,
-    acceptButtonText: "Yes, withdraw",
+    acceptButtonText: "Yes, withdraw package",
     cancelButtonText: "Cancel",
   },
 });
