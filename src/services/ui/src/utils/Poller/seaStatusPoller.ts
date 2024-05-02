a6635efc6779f2c5ed2e5f69584e170acb407598
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-05-02 13:08:47 -0400

    simplify record exists logic

--- src/services/ui/src/utils/Poller/seaStatusPoller.ts
+++ src/services/ui/src/utils/Poller/seaStatusPoller.ts
@@ -2,24 +2,8 @@ import { getItem } from "@/api/useGetItem";
 import { DataPoller } from "./DataPoller";
 import { PackageCheck } from "shared-utils";
 
-/** These are checks that simply can't be added into the packagecheck logic.
- * A prime example being the ability to check for the existance of the data
- * coming back. This won't work in something like PackageCheck because it is
- * already expecting data
- */
-export const pollerSpecificChecks = (
-  data: Awaited<ReturnType<typeof getItem>> | undefined | null,
-) => {
-  const recordExists = !!data;
-
-  return {
-    recordExists,
-  };
-};
-
 export type CheckStatusFunction = (
-  check: ReturnType<typeof PackageCheck> &
-    ReturnType<typeof pollerSpecificChecks>,
+  check: ReturnType<typeof PackageCheck> & { recordExists: boolean },
 ) => boolean;
 
 export const seaStatusPoller = (
@@ -31,9 +15,8 @@ export const seaStatusPoller = (
     pollAttempts: 20,
     onPoll: (data) => {
       const check = PackageCheck(data._source);
-      const pollerChecks = pollerSpecificChecks(data);
 
-      return statusToCheck({ ...check, ...pollerChecks });
+      return statusToCheck({ ...check, recordExists: !!data });
     },
     fetcher: () => getItem(id),
   });
