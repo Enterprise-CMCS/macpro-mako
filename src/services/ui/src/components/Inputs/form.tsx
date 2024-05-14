9663a2e8b58d053c7efbdaef0304049efe70ce63
Gavin St. Ours <gst-ours@fearless.tech>
2024-05-14 09:31:39 -0400

    Use paragraph styling for form descriptions

--- src/services/ui/src/components/Inputs/form.tsx
+++ src/services/ui/src/components/Inputs/form.tsx
@@ -133,12 +133,7 @@ const FormDescription = React.forwardRef<
   const { formDescriptionId } = useFormField();
 
   return (
-    <p
-      ref={ref}
-      id={formDescriptionId}
-      className={cn("text-[0.8rem] text-muted-foreground", className)}
-      {...props}
-    />
+    <p ref={ref} id={formDescriptionId} className={className} {...props} />
   );
 });
 FormDescription.displayName = "FormDescription";
