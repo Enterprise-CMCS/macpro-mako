a8605bcae509c11bc6f0236f1fd36b5a4e1a50ca
Gavin St. Ours <gst-ours@fearless.tech>
2024-06-17 14:53:43 -0400

    Ensure border style matches other elements

--- src/services/ui/src/components/Inputs/multiselect.tsx
+++ src/services/ui/src/components/Inputs/multiselect.tsx
@@ -1,6 +1,17 @@
 import { type FC } from "react";
-import Select from "react-select";
+import Select, { StylesConfig } from "react-select";
 import { MultiselectOption } from "shared-types";
+
+const customStyles: StylesConfig = {
+  control: (provided) => ({
+    ...provided,
+    border: "1px solid black",
+    boxShadow: "none",
+    "&:hover": {
+      border: "1px solid black",
+    },
+  }),
+};
 
 export const Multiselect: FC<{
   options: MultiselectOption[];
@@ -15,6 +26,7 @@ export const Multiselect: FC<{
       options={props.options}
       closeMenuOnSelect={false}
       placeholder
+      styles={customStyles}
     />
   );
 };
