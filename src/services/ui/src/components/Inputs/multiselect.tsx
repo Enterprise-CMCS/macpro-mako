c56866c45386a98236223c673587aae91f41377c
Gavin St. Ours <gst-ours@fearless.tech>
2024-06-17 14:16:23 -0400

    Update multiselect styling

--- src/services/ui/src/components/Inputs/multiselect.tsx
+++ src/services/ui/src/components/Inputs/multiselect.tsx
@@ -1,20 +1,22 @@
 import { type FC } from "react";
 import Select from "react-select";
 import { MultiselectOption } from "shared-types";
+import { cn } from "@/utils";
 
 export const Multiselect: FC<{
   options: MultiselectOption[];
   value: string[];
   onChange: (values: string[]) => void;
-}> = (props) => {
+  className?: string;
+}> = (className, props) => {
   return (
     <Select<any, any>
       isMulti={true}
-      value={props.value?.map((str) => ({ value: str, label: str }))}
+      value={props.value?.map((str: string) => ({ value: str, label: str }))}
       onChange={(val) => props.onChange(val.map((s: any) => s.value) || [])}
       options={props.options}
       closeMenuOnSelect={false}
-      placeholder
+      className={cn("rounded-sm border border-black", className)}
     />
   );
 };
