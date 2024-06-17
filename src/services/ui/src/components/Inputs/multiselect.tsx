daab396465f0424a669dafe780c79790b3bae4e9
Gavin St. Ours <gst-ours@fearless.tech>
2024-06-17 14:42:37 -0400

    Revert breaking change

--- src/services/ui/src/components/Inputs/multiselect.tsx
+++ src/services/ui/src/components/Inputs/multiselect.tsx
@@ -1,22 +1,20 @@
 import { type FC } from "react";
 import Select from "react-select";
 import { MultiselectOption } from "shared-types";
-import { cn } from "@/utils";
 
 export const Multiselect: FC<{
   options: MultiselectOption[];
   value: string[];
   onChange: (values: string[]) => void;
-  className?: string;
-}> = (className, props) => {
+}> = (props) => {
   return (
     <Select<any, any>
       isMulti={true}
-      value={props.value?.map((str: string) => ({ value: str, label: str }))}
+      value={props.value?.map((str) => ({ value: str, label: str }))}
       onChange={(val) => props.onChange(val.map((s: any) => s.value) || [])}
       options={props.options}
       closeMenuOnSelect={false}
-      className={cn("rounded-sm border border-black", className)}
+      placeholder
     />
   );
 };
