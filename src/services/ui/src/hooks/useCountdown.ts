7801330b53a68bf06e4999bbc5a2d0855e557ef9
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-06-27 11:50:04 -0400

    feat: create `useCountdown` hook

---
+++ src/services/ui/src/hooks/useCountdown.ts
@@ -0,0 +1,55 @@
+// credit: https://usehooks-ts.com/react-hook/use-countdown
+
+import { useCallback, useEffect, useState } from "react";
+
+type CountdownControllers = {
+  startCountdown: () => void;
+  stopCountdown: () => void;
+  resetCountdown: () => void;
+};
+
+export const useCountdown = (
+  countStart: number,
+): [number, CountdownControllers] => {
+  const [count, setCount] = useState(countStart);
+  const [isCountdownRunning, setIsCountdownRunning] = useState(false);
+
+  const startCountdown = () => {
+    setIsCountdownRunning(true);
+  };
+
+  const stopCountdown = () => {
+    setIsCountdownRunning(false);
+  };
+
+  // Will set running false and reset the seconds to initial value
+  const resetCountdown = useCallback(() => {
+    stopCountdown();
+    setCount(countStart);
+  }, [stopCountdown]);
+
+  const countdownCallback = useCallback(() => {
+    if (count === 0) {
+      stopCountdown();
+      return;
+    }
+
+    setCount((oldCount) => oldCount - 1);
+  }, [count, stopCountdown]);
+
+  useEffect(() => {
+    if (isCountdownRunning === false) {
+      return;
+    }
+
+    const id = setInterval(() => {
+      countdownCallback();
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [isCountdownRunning, countdownCallback]);
+
+  return [count, { startCountdown, stopCountdown, resetCountdown }];
+};
