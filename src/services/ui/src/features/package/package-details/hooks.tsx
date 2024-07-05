6652697573157d361591ecdc8867e7c0b78feb4a
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-07-05 15:48:47 -0400

    feat: convert states names in Package Details

--- src/services/ui/src/features/package/package-details/hooks.tsx
+++ src/services/ui/src/features/package/package-details/hooks.tsx
@@ -7,7 +7,7 @@ import { OneMacUser } from "@/api/useGetUser";
 
 import { formatSeatoolDate } from "shared-utils";
 import { useMemo, useState } from "react";
-import { LABELS } from "@/utils";
+import { convertStateAbbrToFullName, LABELS } from "@/utils";
 
 export const ReviewTeamList: FC<opensearch.main.Document> = (props) => {
   const [expanded, setExpanded] = useState(false);
@@ -60,7 +60,7 @@ export const recordDetails = (
   },
   {
     label: "State",
-    value: data.state,
+    value: convertStateAbbrToFullName(data.state),
     canView: () => true,
   },
   {
