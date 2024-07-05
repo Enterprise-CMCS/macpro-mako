f1f4b6df1c3c6c6fc6e810f0b6ccaa96a1d62009
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-07-05 11:45:05 -0400

    fix: hide bold reminder

--- src/services/ui/src/features/package-actions/lib/modules/toggle-rai-withdraw/index.tsx
+++ src/services/ui/src/features/package-actions/lib/modules/toggle-rai-withdraw/index.tsx
@@ -19,7 +19,7 @@ export const defaultEnableRaiWithdrawContent: FormContentHydrator = () => ({
 });
 
 export const defaultDisableRaiWithdrawFields: ReactElement[] = [
-  <ActionFormDescription boldReminder key={"section-description"}>
+  <ActionFormDescription key={"section-description"}>
     The state will not be able to withdraw its RAI response. It may take up to a
     minute for this change to be applied.
   </ActionFormDescription>,
