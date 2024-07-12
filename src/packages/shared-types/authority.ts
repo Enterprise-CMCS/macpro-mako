1a359181bd52720fca9b963af6984274c08dc202
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-07-12 10:43:36 -0400

    feat: create new `Authority` unions and maps

--- src/packages/shared-types/authority.ts
+++ src/packages/shared-types/authority.ts
@@ -6,9 +6,24 @@ export enum Authority {
   "1915c" = "1915(c)",
 }
 
-/* Authority values as a union string type. Replaces old Authority enum. */
 export type AuthorityUnion =
   | "Medicaid SPA"
   | "CHIP SPA"
   | "1915(b)"
   | "1915(c)";
+
+export type AuthorityAPI = "medicaid spa" | "chip spa" | "1915(b)" | "1915(c)";
+
+export const AUTHORITY_FE_TO_API: Record<AuthorityUnion, AuthorityAPI> = {
+  "CHIP SPA": "chip spa",
+  "Medicaid SPA": "medicaid spa",
+  "1915(b)": "1915(b)",
+  "1915(c)": "1915(c)",
+};
+
+export const AUTHORITY_API_TO_FE: Record<AuthorityAPI, AuthorityUnion> = {
+  "1915(b)": "1915(b)",
+  "1915(c)": "1915(c)",
+  "chip spa": "CHIP SPA",
+  "medicaid spa": "Medicaid SPA",
+};
