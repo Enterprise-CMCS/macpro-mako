6cd70e57cfe36f291dc2b30d8474d8c78d596bfc
Mike Dial <michael.dial.4@gmail.com>
2024-07-12 13:26:45 -0400

    stuff

--- src/services/api/handlers/package-actions/toggle-rai-response-withdraw/toggle-rai-response-withdraw.test.ts
+++ src/services/api/handlers/package-actions/toggle-rai-response-withdraw/toggle-rai-response-withdraw.test.ts
@@ -1,46 +1,7 @@
-// toggle-rai-response-withdraw.test.ts
-
 import { toggleRaiResponseWithdraw } from "./toggle-rai-response-withdraw";
 import { vi, describe, it, expect, beforeEach } from "vitest";
-import {
-  IssueRaiDto,
-  PackageWriteClass,
-  RemoveAppkChildDto,
-  RespondToRaiDto,
-  UpdateIdDto,
-  WithdrawPackageDto,
-  WithdrawRaiDto,
-} from "../services/package-action-write-service";
-import { ToggleRaiResponseDto } from "../services/mako-write-service";
-import { CompleteIntakeDto } from "../services/seatool-write-service";
-import { response } from "../../../libs/handler";
-
-class MockPackageActionWriteService implements PackageWriteClass {
-  async issueRai(data: IssueRaiDto) {
-    console.log("hello");
-  }
-  async respondToRai(data: RespondToRaiDto) {
-    console.log("hello");
-  }
-  async withdrawRai(data: WithdrawRaiDto) {
-    console.log("hello");
-  }
-  async toggleRaiResponseWithdraw(data: ToggleRaiResponseDto) {
-    console.log("hello");
-  }
-  async removeAppkChild(data: RemoveAppkChildDto) {
-    console.log("hello");
-  }
-  async withdrawPackage(data: WithdrawPackageDto) {
-    console.log("hello");
-  }
-  async updateId(data: UpdateIdDto) {
-    console.log("hello");
-  }
-  async completeIntake(data: CompleteIntakeDto) {
-    console.log("hello");
-  }
-}
+import { MockPackageActionWriteService } from "../services/package-action-write-service";
+const mockPackageWrite = new MockPackageActionWriteService();
 
 describe("toggleRaiResponseWithdraw", async () => {
   beforeEach(() => {
@@ -48,8 +9,6 @@ describe("toggleRaiResponseWithdraw", async () => {
   });
 
   it("should return a server error response if given bad body", async () => {
-    const mockPackageWrite = new MockPackageActionWriteService();
-
     const toggleRaiWithdraw = await toggleRaiResponseWithdraw(
       { hello: "world" },
       true,
