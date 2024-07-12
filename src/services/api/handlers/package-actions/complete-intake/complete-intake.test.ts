f675592e7c04dd99cec00e4793cf645531dae475
Mike Dial <michael.dial.4@gmail.com>
2024-07-12 06:04:16 -0400

    fix up test running

--- src/services/api/handlers/package-actions/complete-intake/complete-intake.test.ts
+++ src/services/api/handlers/package-actions/complete-intake/complete-intake.test.ts
@@ -1,6 +1,8 @@
-import { describe, expect } from "vitest";
+import { describe, it, expect } from "vitest";
 
 // this is a placeholder. i will rewrite these shortly
 describe("example test", () => {
-  expect(1).toEqual(1);
+  it("should equal 1", () => {
+    expect(1).toEqual(1);
+  });
 });
