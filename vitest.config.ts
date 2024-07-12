af34fac1d23e9fe7a6779fbab11c7b108118006d
Mike Dial <michael.dial.4@gmail.com>
2024-07-12 06:35:57 -0400

    report

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+import { join } from 'path';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: join(__dirname, 'coverage'),
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    },
+  },
+});
