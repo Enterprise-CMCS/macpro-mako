997d63514915d608d1efec54335cf4680df07e77
Mike Dial <michael.dial.4@gmail.com>
2024-07-15 14:31:00 -0400

    upload to cc

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config';
-import { join } from 'path';
+import { defineConfig } from "vitest/config";
+import { join } from "path";
 
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'v8',
-      reportsDirectory: join(__dirname, 'coverage'),
-      reporter: ['text', 'json-summary', 'json'],
+      provider: "v8",
+      reportsDirectory: join(__dirname, "coverage"),
+      reporter: ["text", "json-summary", "json", "lcov"],
       reportOnFailure: true,
     },
   },
