2211f1d9686c884867b3d91bce428d473fd5d3d9
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-07-16 13:04:14 -0400

    feat: convert seatool services to function modules

--- src/services/api/handlers/package-actions/services/seatool-write-service.ts
+++ src/services/api/handlers/package-actions/services/seatool-write-service.ts
@@ -1,6 +1,7 @@
-import { ConnectionPool, Transaction, config, connect } from "mssql";
+import { ConnectionPool, Transaction, connect } from "mssql";
 import { buildStatusMemoQuery } from "../../../libs/statusMemo";
 import { formatSeatoolDate } from "shared-utils";
+import { config as sqlConfg } from "../consts";
 
 export type CompleteIntakeDto = {
   id: string;
@@ -55,48 +56,42 @@ export type UpdateIdDto = {
   newId: string;
 };
 
-export class SeatoolWriteService {
-  #pool: ConnectionPool;
-  trx: Transaction;
+let pool: ConnectionPool | null = null;
+export const getTrx = async () => {
+  pool = pool || (await connect(sqlConfg));
 
-  constructor(pool: ConnectionPool) {
-    this.#pool = pool;
-    this.trx = new Transaction(pool);
-  }
+  return { trx: new Transaction(pool) };
+};
 
-  public static async createSeatoolService(config: config) {
-    const pool = await connect(config);
+export const completeIntakeSeatool = async ({
+  typeIds,
+  subTypeIds,
+  id,
+  cpoc,
+  description,
+  subject,
+  submitterName,
+}: CompleteIntakeDto) => {
+  const { trx } = await getTrx();
 
-    return new SeatoolWriteService(pool);
-  }
+  // Generate INSERT statements for typeIds
+  const typeIdsValues = typeIds
+    .map((typeId: number) => `('${id}', '${typeId}')`)
+    .join(",\n");
+  const typeIdsInsert = typeIdsValues
+    ? `INSERT INTO SEA.dbo.State_Plan_Service_Types (ID_Number, Service_Type_ID) VALUES ${typeIdsValues};`
+    : "";
 
-  async completeIntake({
-    typeIds,
-    subTypeIds,
-    id,
-    cpoc,
-    description,
-    subject,
-    submitterName,
-  }: CompleteIntakeDto) {
-    // Generate INSERT statements for typeIds
-    const typeIdsValues = typeIds
-      .map((typeId: number) => `('${id}', '${typeId}')`)
-      .join(",\n");
-    const typeIdsInsert = typeIdsValues
-      ? `INSERT INTO SEA.dbo.State_Plan_Service_Types (ID_Number, Service_Type_ID) VALUES ${typeIdsValues};`
-      : "";
+  // Generate INSERT statements for subTypeIds
+  const subTypeIdsValues = subTypeIds
+    .map((subTypeId: number) => `('${id}', '${subTypeId}')`)
+    .join(",\n");
 
-    // Generate INSERT statements for subTypeIds
-    const subTypeIdsValues = subTypeIds
-      .map((subTypeId: number) => `('${id}', '${subTypeId}')`)
-      .join(",\n");
+  const subTypeIdsInsert = subTypeIdsValues
+    ? `INSERT INTO SEA.dbo.State_Plan_Service_SubTypes (ID_Number, Service_SubType_ID) VALUES ${subTypeIdsValues};`
+    : "";
 
-    const subTypeIdsInsert = subTypeIdsValues
-      ? `INSERT INTO SEA.dbo.State_Plan_Service_SubTypes (ID_Number, Service_SubType_ID) VALUES ${subTypeIdsValues};`
-      : "";
-
-    await this.trx.request().query(`
+  await trx.request().query(`
       UPDATE SEA.dbo.State_Plan
         SET 
           Title_Name = ${subject ? `'${subject.replace("'", "''")}'` : "NULL"},
@@ -113,19 +108,25 @@ export class SeatoolWriteService {
         -- Insert all types into State_Plan_Service_SubTypes
         ${subTypeIdsInsert}
     `);
-  }
+};
 
-  async issueRai({ id, spwStatus, today }: IssueRaiDto) {
-    // Issue RAI
-    const query1 = `
+export const issueRaiSeatool = async ({
+  id,
+  spwStatus,
+  today,
+}: IssueRaiDto) => {
+  const { trx } = await getTrx();
+
+  // Issue RAI
+  const query1 = `
     Insert into SEA.dbo.RAI (ID_Number, RAI_Requested_Date)
       values ('${id}'
       ,dateadd(s, convert(int, left(${today}, 10)), cast('19700101' as datetime)))
     `;
-    await this.trx.request().query(query1);
+  await trx.request().query(query1);
 
-    // Update Status
-    const query2 = `
+  // Update Status
+  const query2 = `
     UPDATE SEA.dbo.State_Plan
     SET 
       SPW_Status_ID = (SELECT SPW_Status_ID FROM SEA.dbo.SPW_Status WHERE SPW_Status_DESC = '${spwStatus}'),
@@ -133,45 +134,47 @@ export class SeatoolWriteService {
       Status_Memo = ${buildStatusMemoQuery(id, "RAI Issued")}
     WHERE ID_Number = '${id}'
     `;
-    await this.trx.request().query(query2);
+  await trx.request().query(query2);
+};
+
+export const respondToRaiSeatool = async ({
+  id,
+  raiReceivedDate,
+  raiWithdrawnDate,
+  today,
+  raiToRespondTo,
+  spwStatus,
+}: RespondToRaiDto) => {
+  const { trx } = await getTrx();
+
+  let statusMemoUpdate = "";
+  if (raiReceivedDate && raiWithdrawnDate) {
+    statusMemoUpdate = buildStatusMemoQuery(
+      id,
+      `RAI Response Received.  This overwrites the previous response received on ${formatSeatoolDate(raiReceivedDate)} and withdrawn on ${formatSeatoolDate(raiWithdrawnDate)}`,
+    );
+  } else if (raiWithdrawnDate) {
+    statusMemoUpdate = buildStatusMemoQuery(
+      id,
+      `RAI Response Received.  This overwrites a previous response withdrawn on ${formatSeatoolDate(raiWithdrawnDate)}`,
+    );
+  } else {
+    statusMemoUpdate = buildStatusMemoQuery(id, "RAI Response Received");
   }
 
-  async respondToRai({
-    id,
-    raiReceivedDate,
-    raiWithdrawnDate,
-    today,
-    raiToRespondTo,
-    spwStatus,
-  }: RespondToRaiDto) {
-    let statusMemoUpdate = "";
-    if (raiReceivedDate && raiWithdrawnDate) {
-      statusMemoUpdate = buildStatusMemoQuery(
-        id,
-        `RAI Response Received.  This overwrites the previous response received on ${formatSeatoolDate(raiReceivedDate)} and withdrawn on ${formatSeatoolDate(raiWithdrawnDate)}`,
-      );
-    } else if (raiWithdrawnDate) {
-      statusMemoUpdate = buildStatusMemoQuery(
-        id,
-        `RAI Response Received.  This overwrites a previous response withdrawn on ${formatSeatoolDate(raiWithdrawnDate)}`,
-      );
-    } else {
-      statusMemoUpdate = buildStatusMemoQuery(id, "RAI Response Received");
-    }
-
-    // Respond to RAI
-    const query1 = `
+  // Respond to RAI
+  const query1 = `
           UPDATE SEA.dbo.RAI
             SET 
               RAI_RECEIVED_DATE = DATEADD(s, CONVERT(int, LEFT('${today}', 10)), CAST('19700101' AS DATETIME)),
               RAI_WITHDRAWN_DATE = NULL
             WHERE ID_Number = '${id}' AND RAI_REQUESTED_DATE = DATEADD(s, CONVERT(int, LEFT('${raiToRespondTo}', 10)), CAST('19700101' AS DATETIME))
         `;
-    const result1 = await this.trx.request().query(query1);
-    console.log(result1);
+  const result1 = await trx.request().query(query1);
+  console.log(result1);
 
-    // Update Status
-    const query2 = `
+  // Update Status
+  const query2 = `
           UPDATE SEA.dbo.State_Plan
             SET 
               SPW_Status_ID = (SELECT SPW_Status_ID FROM SEA.dbo.SPW_Status WHERE SPW_Status_DESC = '${spwStatus}'),
@@ -179,26 +182,28 @@ export class SeatoolWriteService {
               Status_Memo = ${statusMemoUpdate}
             WHERE ID_Number = '${id}'
         `;
-    const result2 = await this.trx.request().query(query2);
-    console.log(result2);
-  }
+  const result2 = await trx.request().query(query2);
+  console.log(result2);
+};
 
-  async withdrawRai({
-    id,
-    today,
-    raiToWithdraw,
-    spwStatus,
-    raiRequestedDate,
-    raiReceivedDate,
-  }: WithdrawRaiDto) {
-    await this.trx.request().query(`
+export const withdrawRaiSeatool = async ({
+  id,
+  today,
+  raiToWithdraw,
+  spwStatus,
+  raiRequestedDate,
+  raiReceivedDate,
+}: WithdrawRaiDto) => {
+  const { trx } = await getTrx();
+
+  await trx.request().query(`
       UPDATE SEA.dbo.RAI
         SET 
           RAI_WITHDRAWN_DATE = DATEADD(s, CONVERT(int, LEFT('${today}', 10)), CAST('19700101' AS DATETIME))
       WHERE ID_Number = '${id}' AND RAI_REQUESTED_DATE = DATEADD(s, CONVERT(int, LEFT('${raiToWithdraw}', 10)), CAST('19700101' AS DATETIME))
     `);
-    // Set Status to Pending - RAI
-    await this.trx.request().query(`
+  // Set Status to Pending - RAI
+  await trx.request().query(`
       UPDATE SEA.dbo.State_Plan
         SET 
           SPW_Status_ID = (SELECT SPW_Status_ID FROM SEA.dbo.SPW_Status WHERE SPW_Status_DESC = '${spwStatus}'),
@@ -209,10 +214,16 @@ export class SeatoolWriteService {
           )}
         WHERE ID_Number = '${id}'
     `);
-  }
+};
 
-  async removeAppkChild({ id, today, spwStatus }: RemoveAppkChildDto) {
-    await this.trx.request().query(`
+export const removeAppkChildSeatool = async ({
+  id,
+  today,
+  spwStatus,
+}: RemoveAppkChildDto) => {
+  const { trx } = await getTrx();
+
+  await trx.request().query(`
       UPDATE SEA.dbo.State_Plan
         SET 
           SPW_Status_ID = (SELECT SPW_Status_ID FROM SEA.dbo.SPW_Status WHERE SPW_Status_DESC = '${spwStatus}'),
@@ -220,10 +231,16 @@ export class SeatoolWriteService {
           Status_Memo = ${buildStatusMemoQuery(id, "Package Withdrawn")}
         WHERE ID_Number = '${id}'
     `);
-  }
+};
 
-  async withdrawPackage({ id, today, spwStatus }: WithdrawPackageDto) {
-    const query = `
+export const withdrawPackageSeatool = async ({
+  id,
+  today,
+  spwStatus,
+}: WithdrawPackageDto) => {
+  const { trx } = await getTrx();
+
+  const query = `
         UPDATE SEA.dbo.State_Plan
           SET 
             SPW_Status_ID = (SELECT SPW_Status_ID FROM SEA.dbo.SPW_Status WHERE SPW_Status_DESC = '${spwStatus}'),
@@ -231,12 +248,19 @@ export class SeatoolWriteService {
             Status_Memo = ${buildStatusMemoQuery(id, "Package Withdrawn")}
           WHERE ID_Number = '${id}'
       `;
-    await this.trx.request().query(query);
-  }
+  await trx.request().query(query);
+};
 
-  async updateId({ id, today, spwStatus, newId }: UpdateIdDto) {
-    await this.trx.request().query(
-      `
+export const updateIdSeatool = async ({
+  id,
+  today,
+  spwStatus,
+  newId,
+}: UpdateIdDto) => {
+  const { trx } = await getTrx();
+
+  await trx.request().query(
+    `
       DECLARE @columns NVARCHAR(MAX), @sql NVARCHAR(MAX), @newId NVARCHAR(50), @originalId NVARCHAR(50);
 
       SET @newId = '${newId}';
@@ -250,46 +274,46 @@ export class SeatoolWriteService {
       SET @sql = 'INSERT INTO SEA.dbo.State_Plan (ID_Number, ' + @columns + ') SELECT ''' + @newId + ''' as ID_Number, ' + @columns + ' FROM SEA.dbo.State_Plan WHERE ID_Number = ''' + @originalId + '''';
       EXEC sp_executesql @sql;
     `,
-    );
+  );
 
-    await this.trx.request().query(
-      `
+  await trx.request().query(
+    `
       INSERT INTO RAI (ID_Number, RAI_REQUESTED_DATE, RAI_RECEIVED_DATE, RAI_WITHDRAWN_DATE)
         SELECT '${newId}', RAI_REQUESTED_DATE, RAI_RECEIVED_DATE, RAI_WITHDRAWN_DATE
         FROM RAI
         WHERE ID_Number = '${id}';
     `,
-    );
+  );
 
-    await this.trx.request().query(
-      `
+  await trx.request().query(
+    `
       INSERT INTO State_Plan_Service_Types (ID_Number, Service_Type_ID)
         SELECT '${newId}', Service_Type_ID
         FROM State_Plan_Service_Types
         WHERE ID_Number = '${id}';
     `,
-    );
+  );
 
-    await this.trx.request().query(
-      `
+  await trx.request().query(
+    `
       INSERT INTO State_Plan_Service_SubTypes (ID_Number, Service_SubType_ID)
         SELECT '${newId}', Service_SubType_ID
         FROM State_Plan_Service_SubTypes
         WHERE ID_Number = '${id}';
     `,
-    );
+  );
 
-    await this.trx.request().query(
-      `
+  await trx.request().query(
+    `
       INSERT INTO Action_Officers (ID_Number, Officer_ID)
         SELECT '${newId}', Officer_ID
         FROM Action_Officers
         WHERE ID_Number = '${id}';
     `,
-    );
+  );
 
-    await this.trx.request().query(
-      `
+  await trx.request().query(
+    `
       UPDATE SEA.dbo.State_Plan
       SET 
         SPW_Status_ID = (SELECT SPW_Status_ID FROM SEA.dbo.SPW_Status WHERE SPW_Status_DESC = '${spwStatus}'),
@@ -297,15 +321,14 @@ export class SeatoolWriteService {
         Status_Memo = ${buildStatusMemoQuery(id, `Package Terminated via ID Update: this package was copied to ${newId} and then terminated.`)}
       WHERE ID_Number = '${id}'
     `,
-    );
+  );
 
-    await this.trx.request().query(
-      `
+  await trx.request().query(
+    `
     UPDATE SEA.dbo.State_Plan
       SET 
         Status_Memo = ${buildStatusMemoQuery(id, `Package Created via ID Update: this package was copied from ${id}.`)}
       WHERE ID_Number = '${newId}'
     `,
-    );
-  }
-}
+  );
+};
