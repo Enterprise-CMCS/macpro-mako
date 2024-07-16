47f27b62a2ef1fb8eea804026174c295d7c9b9a3
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-07-16 13:07:29 -0400

    feat: convert package action write services to function modules

--- src/services/api/handlers/package-actions/services/package-action-write-service.ts
+++ src/services/api/handlers/package-actions/services/package-action-write-service.ts
@@ -1,6 +1,5 @@
-import { Action } from "shared-types";
+import { getIdsToUpdate } from "../get-id-to-update";
 import {
-  MakoWriteService,
   IssueRaiDto as MakoIssueRaiDto,
   CompleteIntakeDto as MakoCompleteIntake,
   RespondToRaiDto as MakoRespondToRai,
@@ -9,9 +8,15 @@ import {
   RemoveAppkChildDto as MakoRemoveAppkChild,
   WithdrawPackageDto as MakoWithdrawPackage,
   UpdateIdDto as MakoUpdateId,
+  completeIntakeMako,
+  issueRaiMako,
+  respondToRaiMako,
+  withdrawPackageMako,
+  toggleRaiResponseWithdrawMako,
+  removeAppkChildMako,
+  updateIdMako,
 } from "./mako-write-service";
 import {
-  SeatoolWriteService,
   IssueRaiDto as SeaIssueRaiDto,
   CompleteIntakeDto as SeaCompleteIntake,
   RespondToRaiDto as SeaRespondToRai,
@@ -19,9 +24,15 @@ import {
   RemoveAppkChildDto as SeaRemoveAppkChild,
   WithdrawPackageDto as SeaWithdrawPackage,
   UpdateIdDto as SeaUpdateId,
+  getTrx,
+  completeIntakeSeatool,
+  issueRaiSeatool,
+  respondToRaiSeatool,
+  withdrawRaiSeatool,
+  removeAppkChildSeatool,
+  withdrawPackageSeatool,
+  updateIdSeatool,
 } from "./seatool-write-service";
-
-type IdsToUpdateFunction = (lookupId: string) => Promise<string[]>;
 
 export type CompleteIntakeDto = MakoCompleteIntake & SeaCompleteIntake;
 export type IssueRaiDto = SeaIssueRaiDto & MakoIssueRaiDto;
@@ -31,337 +42,300 @@ export type RemoveAppkChildDto = SeaRemoveAppkChild & MakoRemoveAppkChild;
 export type WithdrawPackageDto = SeaWithdrawPackage & MakoWithdrawPackage;
 export type UpdateIdDto = SeaUpdateId & MakoUpdateId;
 
-export type PackageWriteClass = {
-  completeIntake: (data: CompleteIntakeDto) => Promise<void>;
-  issueRai: (data: IssueRaiDto) => Promise<void>;
-  respondToRai: (data: RespondToRaiDto) => Promise<void>;
-  withdrawRai: (data: WithdrawRaiDto) => Promise<void>;
-  toggleRaiResponseWithdraw: (data: ToggleRaiResponseDto) => Promise<void>;
-  removeAppkChild: (data: RemoveAppkChildDto) => Promise<void>;
-  withdrawPackage: (data: WithdrawPackageDto) => Promise<void>;
-  updateId: (data: UpdateIdDto) => Promise<void>;
+export const completeIntakeAction = async ({
+  action,
+  cpoc,
+  description,
+  id,
+  subTypeIds,
+  subject,
+  submitterName,
+  timestamp,
+  topicName,
+  typeIds,
+  ...data
+}: CompleteIntakeDto) => {
+  const { trx } = await getTrx();
+
+  try {
+    await trx.begin();
+    await completeIntakeSeatool({
+      typeIds,
+      subTypeIds,
+      id,
+      cpoc,
+      description,
+      subject,
+      submitterName,
+    });
+    await completeIntakeMako({
+      action,
+      id,
+      timestamp,
+      topicName,
+      ...data,
+    });
+    await trx.commit();
+  } catch (err: unknown) {
+    console.log("AN ERROR OCCURED: ", err);
+    trx.rollback();
+  }
 };
 
-export class MockPackageActionWriteService implements PackageWriteClass {
-  async issueRai(data: IssueRaiDto) {
-    console.log("hello");
-  }
-  async respondToRai(data: RespondToRaiDto) {
-    console.log("hello");
-  }
-  async withdrawRai(data: WithdrawRaiDto) {
-    console.log("hello");
-  }
-  async toggleRaiResponseWithdraw(data: ToggleRaiResponseDto) {
-    console.log("hello");
-  }
-  async removeAppkChild(data: RemoveAppkChildDto) {
-    console.log("hello");
-  }
-  async withdrawPackage(data: WithdrawPackageDto) {
-    console.log("hello");
-  }
-  async updateId(data: UpdateIdDto) {
-    console.log("hello");
-  }
-  async completeIntake(data: CompleteIntakeDto) {
-    console.log("hello");
-  }
-}
+export const issueRaiAction = async ({
+  action,
+  id,
+  spwStatus,
+  today,
+  timestamp,
+  topicName,
+  ...data
+}: IssueRaiDto) => {
+  const { trx } = await getTrx();
 
-export class PackageActionWriteService implements PackageWriteClass {
-  #seatoolWriteService: SeatoolWriteService;
-  #makoWriteService: MakoWriteService;
-  #getIdsToUpdate: IdsToUpdateFunction;
+  try {
+    await trx.begin();
+    const idsToUpdate = await getIdsToUpdate(id);
 
-  constructor(
-    seatool: SeatoolWriteService,
-    mako: MakoWriteService,
-    idsToUpdateFunction: IdsToUpdateFunction,
-  ) {
-    this.#makoWriteService = mako;
-    this.#seatoolWriteService = seatool;
-    this.#getIdsToUpdate = idsToUpdateFunction;
-  }
-
-  async completeIntake({
-    action,
-    cpoc,
-    description,
-    id,
-    subTypeIds,
-    subject,
-    submitterName,
-    timestamp,
-    topicName,
-    typeIds,
-    ...data
-  }: CompleteIntakeDto) {
-    try {
-      await this.#seatoolWriteService.trx.begin();
-      await this.#seatoolWriteService.completeIntake({
-        typeIds,
-        subTypeIds,
-        id,
-        cpoc,
-        description,
-        subject,
-        submitterName,
-      });
-      await this.#makoWriteService.completeIntake({
+    for (const id of idsToUpdate) {
+      await issueRaiSeatool({ id, spwStatus, today });
+      await issueRaiMako({
         action,
         id,
         timestamp,
         topicName,
         ...data,
       });
-      await this.#seatoolWriteService.trx.commit();
-    } catch (err: unknown) {
-      console.log("AN ERROR OCCURED: ", err);
-      this.#seatoolWriteService.trx.rollback();
     }
+
+    await trx.commit();
+  } catch (err: unknown) {
+    await trx.rollback();
+
+    console.error(err);
   }
+};
 
-  async issueRai({
-    action,
-    id,
-    spwStatus,
-    today,
-    timestamp,
-    topicName,
-    ...data
-  }: IssueRaiDto) {
-    try {
-      await this.#seatoolWriteService.trx.begin();
-      const idsToUpdate = await this.#getIdsToUpdate(id);
+export const respondToRaiAction = async ({
+  action,
+  id,
+  raiReceivedDate,
+  raiToRespondTo,
+  raiWithdrawnDate,
+  responseDate,
+  spwStatus,
+  today,
+  timestamp,
+  topicName,
+  ...data
+}: RespondToRaiDto) => {
+  const { trx } = await getTrx();
 
-      for (const id of idsToUpdate) {
-        await this.#seatoolWriteService.issueRai({ id, spwStatus, today });
-        await this.#makoWriteService.issueRai({
-          action,
-          id,
-          timestamp,
-          topicName,
-          ...data,
-        });
-      }
+  try {
+    await trx.begin();
+    const idsToUpdate = await getIdsToUpdate(id);
 
-      await this.#seatoolWriteService.trx.commit();
-    } catch (err: unknown) {
-      await this.#seatoolWriteService.trx.rollback();
-
-      console.error(err);
+    for (const id of idsToUpdate) {
+      await respondToRaiSeatool({
+        id,
+        spwStatus,
+        today,
+        raiReceivedDate,
+        raiToRespondTo,
+        raiWithdrawnDate,
+      });
+      await respondToRaiMako({
+        action,
+        id,
+        topicName,
+        timestamp,
+        responseDate,
+        ...data,
+      });
     }
+
+    await trx.commit();
+  } catch (err: unknown) {
+    await trx.rollback();
+
+    console.error(err);
   }
+};
 
-  async respondToRai({
-    action,
-    id,
-    raiReceivedDate,
-    raiToRespondTo,
-    raiWithdrawnDate,
-    responseDate,
-    spwStatus,
-    today,
-    timestamp,
-    topicName,
-    ...data
-  }: RespondToRaiDto) {
-    try {
-      await this.#seatoolWriteService.trx.begin();
-      const idsToUpdate = await this.#getIdsToUpdate(id);
+export const withdrawRaiAction = async ({
+  id,
+  spwStatus,
+  today,
+  timestamp,
+  raiReceivedDate,
+  raiToWithdraw,
+  raiRequestedDate,
+  action,
+  topicName,
+  withdrawnDate,
+  ...data
+}: WithdrawRaiDto) => {
+  const { trx } = await getTrx();
 
-      for (const id of idsToUpdate) {
-        await this.#seatoolWriteService.respondToRai({
-          id,
-          spwStatus,
-          today,
-          raiReceivedDate,
-          raiToRespondTo,
-          raiWithdrawnDate,
-        });
-        await this.#makoWriteService.respondToRai({
-          action,
-          id,
-          topicName,
-          timestamp,
-          responseDate,
-          ...data,
-        });
-      }
+  try {
+    await trx.begin();
+    const idsToUpdate = await getIdsToUpdate(id);
 
-      await this.#seatoolWriteService.trx.commit();
-    } catch (err: unknown) {
-      await this.#seatoolWriteService.trx.rollback();
-
-      console.error(err);
+    for (const id of idsToUpdate) {
+      await withdrawRaiSeatool({
+        id,
+        spwStatus,
+        today,
+        raiReceivedDate,
+        raiToWithdraw,
+        raiRequestedDate,
+      });
+      await withdrawPackageMako({
+        action,
+        id,
+        topicName,
+        timestamp,
+        withdrawnDate,
+        ...data,
+      });
     }
+
+    await trx.commit();
+  } catch (err: unknown) {
+    await trx.rollback();
+
+    console.error(err);
   }
+};
 
-  async withdrawRai({
-    id,
-    spwStatus,
-    today,
-    timestamp,
-    raiReceivedDate,
-    raiToWithdraw,
-    raiRequestedDate,
-    action,
-    topicName,
-    withdrawnDate,
-    ...data
-  }: WithdrawRaiDto) {
-    try {
-      await this.#seatoolWriteService.trx.begin();
-      const idsToUpdate = await this.#getIdsToUpdate(id);
+export const toggleRaiResponseWithdrawAction = async (
+  data: ToggleRaiResponseDto,
+) => {
+  try {
+    const idsToUpdate = await getIdsToUpdate(data.id);
 
-      for (const id of idsToUpdate) {
-        await this.#seatoolWriteService.withdrawRai({
-          id,
-          spwStatus,
-          today,
-          raiReceivedDate,
-          raiToWithdraw,
-          raiRequestedDate,
-        });
-        await this.#makoWriteService.withdrawRai({
-          action,
-          id,
-          topicName,
-          timestamp,
-          withdrawnDate,
-          ...data,
-        });
-      }
-
-      await this.#seatoolWriteService.trx.commit();
-    } catch (err: unknown) {
-      await this.#seatoolWriteService.trx.rollback();
-
-      console.error(err);
+    for (const id of idsToUpdate) {
+      await toggleRaiResponseWithdrawMako({
+        ...data,
+        id,
+      });
     }
+  } catch (err: unknown) {
+    console.error(err);
   }
+};
 
-  async toggleRaiResponseWithdraw(data: ToggleRaiResponseDto) {
-    try {
-      const idsToUpdate = await this.#getIdsToUpdate(data.id);
+export const removeAppkChildAction = async ({
+  id,
+  timestamp,
+  today,
+  spwStatus,
+  action,
+  topicName,
+  ...data
+}: RemoveAppkChildDto) => {
+  const { trx } = await getTrx();
 
-      for (const id of idsToUpdate) {
-        await this.#makoWriteService.toggleRaiResponseWithdraw({
-          ...data,
-          id,
-        });
-      }
-    } catch (err: unknown) {
-      console.error(err);
+  try {
+    await trx.begin();
+    const idsToUpdate = [id];
+
+    for (const id of idsToUpdate) {
+      await removeAppkChildSeatool({
+        id,
+        spwStatus,
+        today,
+      });
+      await removeAppkChildMako({
+        action,
+        id,
+        topicName,
+        timestamp,
+        ...data,
+      });
     }
+
+    await trx.commit();
+  } catch (err: unknown) {
+    await trx.rollback();
+
+    console.error(err);
   }
+};
 
-  async removeAppkChild({
-    id,
-    timestamp,
-    today,
-    spwStatus,
-    action,
-    topicName,
-    ...data
-  }: RemoveAppkChildDto) {
-    try {
-      await this.#seatoolWriteService.trx.begin();
-      const idsToUpdate = [id];
+export const withdrawPackageAction = async ({
+  id,
+  timestamp,
+  today,
+  spwStatus,
+  action,
+  topicName,
+  ...data
+}: WithdrawPackageDto) => {
+  const { trx } = await getTrx();
 
-      for (const id of idsToUpdate) {
-        await this.#seatoolWriteService.removeAppkChild({
-          id,
-          spwStatus,
-          today,
-        });
-        await this.#makoWriteService.removeAppkChild({
-          action,
-          id,
-          topicName,
-          timestamp,
-          ...data,
-        });
-      }
+  try {
+    await trx.begin();
+    const idsToUpdate = await getIdsToUpdate(id);
 
-      await this.#seatoolWriteService.trx.commit();
-    } catch (err: unknown) {
-      await this.#seatoolWriteService.trx.rollback();
-
-      console.error(err);
+    for (const id of idsToUpdate) {
+      await withdrawPackageSeatool({
+        id,
+        spwStatus,
+        today,
+      });
+      await withdrawPackageMako({
+        action,
+        id,
+        topicName,
+        timestamp,
+        ...data,
+      });
     }
+    await trx.commit();
+  } catch (err: unknown) {
+    await trx.rollback();
+
+    console.error(err);
   }
+};
 
-  async withdrawPackage({
-    id,
-    timestamp,
-    today,
-    spwStatus,
-    action,
-    topicName,
-    ...data
-  }: WithdrawPackageDto) {
-    try {
-      await this.#seatoolWriteService.trx.begin();
-      const idsToUpdate = await this.#getIdsToUpdate(id);
+export const updateIdAction = async ({
+  id,
+  timestamp,
+  today,
+  spwStatus,
+  action,
+  topicName,
+  newId,
+  ...data
+}: UpdateIdDto) => {
+  const { trx } = await getTrx();
 
-      for (const id of idsToUpdate) {
-        await this.#seatoolWriteService.withdrawPackage({
-          id,
-          spwStatus,
-          today,
-        });
-        await this.#makoWriteService.withdrawPackage({
-          action,
-          id,
-          topicName,
-          timestamp,
-          ...data,
-        });
-      }
-      await this.#seatoolWriteService.trx.commit();
-    } catch (err: unknown) {
-      await this.#seatoolWriteService.trx.rollback();
+  try {
+    await trx.begin();
+    const idsToUpdate = [id];
 
-      console.error(err);
+    for (const id of idsToUpdate) {
+      await updateIdSeatool({
+        id,
+        spwStatus,
+        today,
+        newId,
+      });
+
+      await updateIdMako({
+        action,
+        id,
+        timestamp,
+        topicName,
+        ...data,
+      });
     }
+    await trx.commit();
+  } catch (err: unknown) {
+    await trx.rollback();
+
+    console.error(err);
   }
-
-  async updateId({
-    id,
-    timestamp,
-    today,
-    spwStatus,
-    action,
-    topicName,
-    newId,
-    ...data
-  }: UpdateIdDto) {
-    try {
-      await this.#seatoolWriteService.trx.begin();
-      const idsToUpdate = [id];
-
-      for (const id of idsToUpdate) {
-        await this.#seatoolWriteService.updateId({
-          id,
-          spwStatus,
-          today,
-          newId,
-        });
-        await this.#makoWriteService.updateId({
-          action,
-          id,
-          timestamp,
-          topicName,
-          ...data,
-        });
-      }
-      await this.#seatoolWriteService.trx.commit();
-    } catch (err: unknown) {
-      await this.#seatoolWriteService.trx.rollback();
-
-      console.error(err);
-    }
-  }
-}
+};
