a65e5a7a7580cd64025817aa67420b3479bd8a26
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-07-16 13:06:34 -0400

    feat: update `issue-rai` and tests to functions

--- src/services/api/handlers/package-actions/respond-to-rai/respond-to-rai.test.ts
+++ src/services/api/handlers/package-actions/respond-to-rai/respond-to-rai.test.ts
@@ -1,10 +1,14 @@
 import { respondToRai } from "./respond-to-rai";
 import { vi, describe, it, expect, beforeEach } from "vitest";
-import { MockPackageActionWriteService } from "../services/package-action-write-service";
-import { Action, raiResponseSchema } from "shared-types";
+import { raiResponseSchema } from "shared-types";
 import { generateMock } from "@anatine/zod-mock";
-import { ExtendedItemResult } from "../../../libs/package";
-const mockPackageWrite = new MockPackageActionWriteService();
+import * as packageActionWriteService from "../services/package-action-write-service";
+
+vi.mock("../services/package-action-write-service", () => {
+  return {
+    respondToRaiAction: vi.fn(),
+  };
+});
 
 describe("respondToRai", async () => {
   beforeEach(() => {
@@ -19,52 +23,42 @@ describe("respondToRai", async () => {
         raiReceivedDate: "999",
         raiWithdrawnDate: "999",
       },
-      mockPackageWrite,
     );
     expect(response.statusCode).toBe(400);
   });
 
   it("should return a 400 when no requested date is sent", async () => {
     const mockData = generateMock(raiResponseSchema);
-    const response = await respondToRai(
-      mockData,
-      {
-        raiRequestedDate: null,
-        raiReceivedDate: null,
-        raiWithdrawnDate: null,
-      },
-      mockPackageWrite,
-    );
+    const response = await respondToRai(mockData, {
+      raiRequestedDate: null,
+      raiReceivedDate: null,
+      raiWithdrawnDate: null,
+    });
     expect(response.statusCode).toBe(400);
   });
 
   it("should return a 400 when a bad requestDate is sent", async () => {
     const mockData = generateMock(raiResponseSchema);
-    const response = await respondToRai(
-      mockData,
-      {
-        raiRequestedDate: "123456789", // should be an isoString
-        raiReceivedDate: null,
-        raiWithdrawnDate: null,
-      },
-      mockPackageWrite,
-    );
+    const response = await respondToRai(mockData, {
+      raiRequestedDate: "123456789", // should be an isoString
+      raiReceivedDate: null,
+      raiWithdrawnDate: null,
+    });
     expect(response.statusCode).toBe(400);
     expect(JSON.parse(response.body).message).toBe("Event validation error");
   });
 
   it("should return a 200 when a good payload is sent", async () => {
-    const packageWriteSpy = vi.spyOn(mockPackageWrite, "respondToRai");
-    const mockData = generateMock(raiResponseSchema);
-    const response = await respondToRai(
-      mockData,
-      {
-        raiRequestedDate: new Date().toISOString(),
-        raiReceivedDate: null,
-        raiWithdrawnDate: null,
-      },
-      mockPackageWrite,
+    const packageWriteSpy = vi.spyOn(
+      packageActionWriteService,
+      "respondToRaiAction",
     );
+    const mockData = generateMock(raiResponseSchema);
+    const response = await respondToRai(mockData, {
+      raiRequestedDate: new Date().toISOString(),
+      raiReceivedDate: null,
+      raiWithdrawnDate: null,
+    });
     expect(packageWriteSpy).toHaveBeenCalledOnce();
     expect(response.statusCode).toBe(200);
   });
