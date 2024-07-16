2927ea85876fc47ff451ab815ce798434c855ef5
Mike Dial <michael.dial.4@gmail.com>
2024-07-16 19:00:35 -0400

    Footer update

--- src/services/ui/src/features/package-actions/lib/modules/withdraw-rai/index.tsx
+++ src/services/ui/src/features/package-actions/lib/modules/withdraw-rai/index.tsx
@@ -43,8 +43,7 @@ export const defaultWithdrawRaiContent: FormContentHydrator = (document) => {
     : `${document.authority} Withdraw Formal RAI Response Details`;
   return {
     title,
-    preSubmitNotice:
-      "Complete this form to withdraw the Formal RAI response. Once complete, you and CMS will receive an email confirmation.",
+    preSubmitNotice: "Complete this form to withdraw the Formal RAI response.",
     confirmationModal: {
       header: "Withdraw RAI response?",
       body: `The RAI response for ${document.id} will be withdrawn, and CMS will be notified.`,
