fe5f57af1d09d10ae6831b2993cb955d3aaf46a3
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-07-19 10:57:13 -0400

    feat: lift banner/alert functions into `ActionForm`; use simplified origin function

--- src/services/ui/src/features/package-actions/ActionForm.tsx
+++ src/services/ui/src/features/package-actions/ActionForm.tsx
@@ -3,113 +3,141 @@ import {
   Form,
   LoadingSpinner,
   PreSubmitNotice,
+  Route,
   useAlertContext,
   useModalContext,
-  useNavigate,
 } from "@/components";
 import { useGetUser } from "@/api/useGetUser";
-import { useOriginPath } from "@/utils";
+import { getOrigin } from "@/utils";
 import { FormSetup } from "./lib";
 import { submitActionForm } from "@/features/package-actions/lib";
 import { useGetItem } from "@/api";
-import { useCallback, useEffect, useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useParams, ActionFormHeaderCard } from "@/components";
-import { successCheckSwitch } from "./lib/successCheckSwitch";
+import { ActionFormHeaderCard } from "@/components";
 import { SubmitAndCancelBtnSection } from "../submission/waiver/shared-components";
+import { useNavigate } from "react-router-dom";
+import { Action, AuthorityUnion } from "shared-types";
 
-export const ActionForm = ({ setup }: { setup: FormSetup }) => {
-  const { id, type, authority } = useParams("/action/:authority/:id/:type");
+type ActionFormProps = {
+  setup: FormSetup;
+  id: string;
+  authority: AuthorityUnion;
+  actionType: Action;
+};
+
+export const ActionForm = ({
+  setup,
+  id,
+  authority,
+  actionType,
+}: ActionFormProps) => {
   const navigate = useNavigate();
-  const origin = useOriginPath();
   const alert = useAlertContext();
   const modal = useModalContext();
   const { data: user } = useGetUser();
-  const { data: item } = useGetItem(id!);
-  const content = useMemo(
-    () => (item?._source ? setup.content(item._source) : null),
-    [item],
-  );
-  const form = useForm({
+  const { data: item } = useGetItem(id);
+
+  const form = useForm<Record<string, string>>({
     resolver: setup.schema ? zodResolver(setup.schema) : undefined,
     mode: "onChange",
+    defaultValues: { id },
   });
 
-  if (type === "temporary-extension") {
+  if (!item || !user) {
+    return <LoadingSpinner />;
+  }
+
+  if (actionType === "temporary-extension") {
     form.setValue("seaActionType", "Extend");
   }
 
-  // Submission Handler
-  const handler = form.handleSubmit(async (data) => {
-    await submitActionForm({
-      data,
-      id: data?.id ?? id,
-      type: type!,
-      authority: authority!,
-      user: user!,
-      alert,
-      navigate,
-      originRoute: origin,
-      statusToCheck: successCheckSwitch(type),
-    });
-  });
-  useEffect(() => {
-    content?.successBanner && alert.setContent(content.successBanner);
-  }, [content]);
-  // Adapted handler for destructive confirmation modal use
-  const confirmSubmitCallback = useCallback(() => {
-    modal.setModalOpen(false);
-    handler();
-  }, [content]);
+  const content = setup.content(item._source);
 
-  if (content === null) {
-    // Still loading item and no content hydrated
-    return <LoadingSpinner />;
-  } else {
-    return (
-      <Form {...form}>
-        {form.formState.isSubmitting && <LoadingSpinner />}
-        <form onSubmit={handler}>
-          {setup?.fields &&
-            setup.fields.map((field, index) => {
-              // only some forms will need to have the title in a card along with the description
-              if (index === 0) {
-                return (
-                  <ActionFormHeaderCard
-                    title={content.title}
-                    hasRequiredField={setup.schema !== null}
-                    isTE={field.key === "te-content-description"}
-                    key="content-description"
-                  >
-                    {field}
-                  </ActionFormHeaderCard>
-                );
-              } else {
-                return field;
-              }
-            })}
-          <ErrorBanner />
-          {content?.preSubmitNotice && (
-            <PreSubmitNotice
-              message={content.preSubmitNotice}
-              hasProgressLossReminder={setup.schema !== null}
-            />
-          )}
-          {content?.confirmationModal ? (
-            <SubmitAndCancelBtnSection
-              confirmWithdraw={() => {
-                modal.setContent(content.confirmationModal!);
-                modal.setOnAccept(() => confirmSubmitCallback);
-                modal.setModalOpen(true);
-              }}
-              enableSubmit={content.enableSubmit}
-            />
-          ) : (
-            <SubmitAndCancelBtnSection enableSubmit={content.enableSubmit} />
-          )}
-        </form>
-      </Form>
-    );
-  }
+  const onSubmit = form.handleSubmit(async (data) => {
+    try {
+      await submitActionForm({
+        data,
+        id,
+        actionType,
+        authority,
+        user,
+      });
+
+      alert.setBannerStyle("success");
+      alert.setBannerShow(true);
+      alert.setContent(content.successBanner);
+
+      const originPath = getOrigin({
+        id: data?.newId ?? id,
+        authority,
+      });
+
+      navigate(originPath);
+
+      alert.setBannerDisplayOn(originPath.pathname as Route);
+    } catch (error) {
+      alert.setContent({
+        header: "An unexpected error has occurred:",
+        body: error instanceof Error ? error.message : String(error),
+      });
+      alert.setBannerStyle("destructive");
+      alert.setBannerDisplayOn(window.location.pathname as Route);
+      alert.setBannerShow(true);
+      window.scrollTo(0, 0);
+    }
+  });
+
+  const onConfirmWithdraw = () => {
+    if (content.confirmationModal) {
+      modal.setContent(content.confirmationModal);
+    }
+
+    modal.setModalOpen(true);
+
+    modal.setOnAccept(() => () => {
+      modal.setModalOpen(false);
+      onSubmit();
+    });
+  };
+
+  return (
+    <Form {...form}>
+      {form.formState.isSubmitting && <LoadingSpinner />}
+      <form onSubmit={onSubmit}>
+        {setup.fields.map((field, index) => {
+          // only some forms will need to have the title in a card along with the description
+          if (index === 0) {
+            return (
+              <ActionFormHeaderCard
+                title={content.title}
+                hasRequiredField={setup.schema !== null}
+                isTE={field.key === "te-content-description"}
+                key="content-description"
+              >
+                {field}
+              </ActionFormHeaderCard>
+            );
+          }
+
+          return field;
+        })}
+        <ErrorBanner />
+        {content.preSubmitNotice && (
+          <PreSubmitNotice
+            message={content.preSubmitNotice}
+            hasProgressLossReminder={setup.schema !== null}
+          />
+        )}
+        {content.confirmationModal ? (
+          <SubmitAndCancelBtnSection
+            confirmWithdraw={onConfirmWithdraw}
+            enableSubmit={content.enableSubmit}
+          />
+        ) : (
+          <SubmitAndCancelBtnSection enableSubmit={content.enableSubmit} />
+        )}
+      </form>
+    </Form>
+  );
 };
--- src/services/ui/src/features/package-actions/lib/index.ts
+++ src/services/ui/src/features/package-actions/lib/index.ts
@@ -2,17 +2,12 @@ import { Action, AUTHORITY_FE_TO_API, AuthorityUnion } from "shared-types";
 import { getSchemaFor } from "@/features/package-actions/lib/schemaSwitch";
 import { getFieldsFor } from "@/features/package-actions/lib/fieldsSwitch";
 import { OneMacUser, submit } from "@/api";
-import {
-  buildActionUrl,
-  getDashboardTabForAuthority,
-  useOriginPath,
-} from "@/utils";
-import { Route, useAlertContext, useNavigate } from "@/components";
+import { buildActionUrl } from "@/utils";
 import { FieldValues } from "react-hook-form";
 import { getContentFor } from "@/features/package-actions/lib/contentSwitch";
-import { successCheckSwitch } from "./successCheckSwitch";
 import { documentPoller } from "@/utils/Poller/documentPoller";
-import { stripQueryStringFromURL } from "@/utils/stripQueryString";
+import { successCheckSwitch } from "./successCheckSwitch";
+
 export type FormSetup = {
   schema: ReturnType<typeof getSchemaFor>;
   fields: ReturnType<typeof getFieldsFor>;
@@ -30,65 +25,29 @@ export const getSetupFor = (a: Action, p: AuthorityUnion): FormSetup => ({
 export const submitActionForm = async ({
   data,
   id,
-  type,
+  actionType,
   user,
   authority,
-  alert,
-  navigate,
-  originRoute,
-  statusToCheck,
 }: {
   data: FieldValues;
   id: string;
-  type: Action;
+  actionType: Action;
   user: OneMacUser;
   authority: AuthorityUnion;
-  originRoute: ReturnType<typeof useOriginPath>;
-  alert: ReturnType<typeof useAlertContext>;
-  navigate: ReturnType<typeof useNavigate>;
-  statusToCheck: ReturnType<typeof successCheckSwitch>;
 }) => {
-  const path = originRoute ? originRoute : "/dashboard";
-  const actionsThatUseSubmitEndpoint: Action[] = [Action.TEMP_EXTENSION];
-  try {
-    await submit({
-      data: { ...data, id: id },
-      endpoint: !actionsThatUseSubmitEndpoint.includes(type)
-        ? buildActionUrl(type) // "/action/{type}"
-        : "/submit",
-      user,
-      authority: AUTHORITY_FE_TO_API[authority],
-    });
-    alert.setBannerStyle("success");
-    alert.setBannerShow(true);
-    // banner display doesn't work with url queries
-    alert.setBannerDisplayOn(path.split("?")[0] as Route);
+  await submit({
+    data: { ...data, id: id },
+    endpoint:
+      actionType === Action.TEMP_EXTENSION
+        ? "/submit"
+        : buildActionUrl(actionType),
+    user,
+    authority: AUTHORITY_FE_TO_API[authority],
+  });
 
-    const poller = documentPoller(id, statusToCheck);
-    await poller.startPollingData();
-
-    // path has to be stripped in case it contains a query string in it already
-    // this will break things if you try to add any below in the navigate function
-    // an example of this is details?id=example-123
-    const strippedPath = stripQueryStringFromURL(path);
-
-    navigate({
-      path: strippedPath.path as Route,
-      query: {
-        tab: getDashboardTabForAuthority(authority),
-      },
-    });
-  } catch (e: unknown) {
-    console.error(e);
-    alert.setContent({
-      header: "An unexpected error has occurred:",
-      body: e instanceof Error ? e.message : String(e),
-    });
-    alert.setBannerStyle("destructive");
-    alert.setBannerDisplayOn(window.location.pathname as Route);
-    alert.setBannerShow(true);
-    window.scrollTo(0, 0);
-  }
+  const poller = documentPoller(
+    data?.newId ?? id,
+    successCheckSwitch(actionType),
+  );
+  await poller.startPollingData();
 };
-
-export * from "../../../components/Form/old-content";
