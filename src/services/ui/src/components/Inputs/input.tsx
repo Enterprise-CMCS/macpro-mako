a1dbc6e1653c240ea5a6bbea11dccdfad037f60a
James Dinh <jdinh@collabralink.com>
2024-07-22 15:32:30 -0700

    Update input.tsx

--- src/services/ui/src/components/Inputs/input.tsx
+++ src/services/ui/src/components/Inputs/input.tsx
@@ -16,7 +16,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           className={cn(
             "flex h-9 w-full rounded-sm border border-[#212121] bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-            icon && (iconRight ? "pr-6" : "pl-6"),
+            icon && (iconRight ? "pr-10" : "pl-10"),
             className,
           )}
           ref={ref}
