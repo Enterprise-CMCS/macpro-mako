ac7f78615964f853800e30e1de46e0a32e87dcfe
Mike Dial <michael.dial.4@gmail.com>
2024-07-28 17:06:24 -0400

    fix issue where security group wasnt reachable.  fwiw this syntax is very strange, always has been.  you can check out the createTriggers lambda to see where else we use it

--- lib/stacks/email.ts
+++ lib/stacks/email.ts
@@ -216,7 +216,7 @@ export class Email extends cdk.NestedStack {
         })),
         {
           type: "VPC_SECURITY_GROUP",
-          uri: lambdaSecurityGroupId,
+          uri: `security_group:${lambdaSecurityGroupId}`,
         },
       ],
       startingPosition: "LATEST",
