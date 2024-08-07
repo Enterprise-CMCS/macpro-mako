84d706abfac3bc765c43e9091f7e8636644479b3
Benjamin Paige <benjamin.paige@gmail.com>
2024-08-06 22:03:50 -0600

    placeholder test

---
+++ lib/libs/email/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { emailTemplates } from ".";
+
+describe("emailTemplates", () => {
+  it("placeholder", () => {
+    expect(1).to.equal(1);
+  });
+});
