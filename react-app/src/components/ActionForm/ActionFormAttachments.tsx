908cc3080105d6b700ac9dcb5c2cd37c2031c1d2
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-08-22 11:11:16 -0400

    feat: decouple attachments logic from form; add attachments properties

---
+++ react-app/src/components/ActionForm/ActionFormAttachments.tsx
@@ -0,0 +1,107 @@
+import { useFormContext } from "react-hook-form";
+import { z } from "zod";
+import {
+  SectionCard,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  RequiredIndicator,
+  Upload,
+  FormDescription,
+} from "@/components";
+import { Link } from "react-router-dom";
+import { FAQ_TAB } from "../Routing";
+
+const DEFAULT_ATTACHMENTS_INSTRUCTIONS =
+  "Maximum file size of 80 MB per attachment. You can add multiple files per attachment type.";
+
+type EnforceSchemaProps<Shape extends z.ZodRawShape> = Shape & {
+  attachments?: z.ZodObject<{
+    [Key in keyof Shape]: z.ZodObject<{
+      label: z.ZodDefault<z.ZodString>;
+      files: z.ZodTypeAny;
+    }>;
+  }>;
+  additionalInformation?: z.ZodDefault<z.ZodNullable<z.ZodString>>;
+};
+
+export type SchemaWithEnforcableProps<Shape extends z.ZodRawShape> =
+  z.ZodObject<EnforceSchemaProps<Shape>, "strip", z.ZodTypeAny>;
+
+type ActionFormAttachmentsProps<Schema extends z.ZodRawShape> = {
+  schema: SchemaWithEnforcableProps<Schema>;
+  specialInstructions?: string;
+  faqLink: string;
+  fileInstructions?: {
+    [key: string]: string;
+  };
+};
+
+export const ActionFormAttachments = <Schema extends z.ZodRawShape>({
+  schema,
+  specialInstructions = DEFAULT_ATTACHMENTS_INSTRUCTIONS,
+  faqLink,
+  fileInstructions = {},
+}: ActionFormAttachmentsProps<Schema>) => {
+  const form = useFormContext();
+  const attachementsFromSchema = Object.entries(schema.shape.attachments.shape);
+
+  return (
+    <SectionCard title="Attachments">
+      <div className="text-gray-700 font-light">
+        <p>
+          {specialInstructions} Read the description for each of the attachment
+          types on the{" "}
+          <Link
+            to={faqLink}
+            target={FAQ_TAB}
+            rel="noopener noreferrer"
+            className="text-blue-900 underline"
+          >
+            FAQ Page
+          </Link>
+          .
+        </p>
+        <br />
+        <p>
+          We accept the following file formats:{" "}
+          <strong>.doc, .docx, .pdf, .jpg, .xlsx, and more. </strong>{" "}
+          <Link
+            to={"/faq/acceptable-file-formats"}
+            target={FAQ_TAB}
+            rel="noopener noreferrer"
+            className="text-blue-900 underline"
+          >
+            See the full list
+          </Link>
+          .
+        </p>
+      </div>
+      {attachementsFromSchema.map(([key, value]) => (
+        <FormField
+          key={key}
+          control={form.control}
+          name={`attachments.${key}.files`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                {value.shape.label._def.defaultValue()}{" "}
+                {value.shape.files instanceof z.ZodOptional ? null : (
+                  <RequiredIndicator />
+                )}
+                {fileInstructions[key] && (
+                  <FormDescription data-testid={fileInstructions[key]}>
+                    {fileInstructions[key]}
+                  </FormDescription>
+                )}
+              </FormLabel>
+              <Upload files={field.value ?? []} setFiles={field.onChange} />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      ))}
+    </SectionCard>
+  );
+};
--- react-app/src/components/ActionForm/index.tsx
+++ react-app/src/components/ActionForm/index.tsx
@@ -12,18 +12,12 @@ import {
   FormField,
   banner,
   userPrompt,
-  FormItem,
-  RequiredIndicator,
-  FormLabel,
-  FormMessage,
   formCrumbsFromPath,
-  Upload,
 } from "@/components";
 import {
   DefaultValues,
   FieldPath,
   useForm,
-  useFormContext,
   UseFormReturn,
 } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -37,75 +31,38 @@ import {
 } from "@/utils/Poller/documentPoller";
 import { API } from "aws-amplify";
 import { Authority } from "shared-types";
+import {
+  ActionFormAttachments,
+  SchemaWithEnforcableProps,
+} from "./ActionFormAttachments";
 
-type EnforceSchemaProperties<Shape extends z.ZodRawShape> = Shape & {
-  attachments?: z.ZodObject<{
-    [Key in keyof Shape]: z.ZodObject<{
-      label: z.ZodDefault<z.ZodString>;
-      files: z.ZodTypeAny;
-    }>;
-  }>;
-  additionalInformation?: z.ZodDefault<z.ZodNullable<z.ZodString>>;
-};
-
-type CombinedSchema<Shape extends z.ZodRawShape> = z.ZodObject<
-  EnforceSchemaProperties<Shape>,
-  "strip",
-  z.ZodTypeAny
->;
-
-type ActionFormAttachmentsProps<Schema extends z.ZodRawShape> = {
-  schema: CombinedSchema<Schema>;
-};
-
-const ActionFormAttachments = <Schema extends z.ZodRawShape>({
-  schema,
-}: ActionFormAttachmentsProps<Schema>) => {
-  const form = useFormContext();
-  const attachementsFromSchema = Object.entries(schema.shape.attachments.shape);
-
-  return (
-    <SectionCard title="Attachments">
-      {attachementsFromSchema.map(([key, value]) => (
-        <FormField
-          key={key}
-          control={form.control}
-          name={`attachments.${key}.files`}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                {value.shape.label._def.defaultValue()}{" "}
-                {value.shape.files instanceof z.ZodOptional ? null : (
-                  <RequiredIndicator />
-                )}
-              </FormLabel>
-              <Upload files={field.value ?? []} setFiles={field.onChange} />
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      ))}
-    </SectionCard>
-  );
-};
-
-type ActionFormProps<Schema extends z.ZodObject<any>> = {
-  schema: Schema;
-  defaultValues?: DefaultValues<z.TypeOf<Schema>>;
-  title: string;
-  fieldsLayout?: ({ children }: { children: ReactNode }) => ReactNode;
-  fields: (form: UseFormReturn<z.TypeOf<Schema>>) => ReactNode[];
-  bannerPostSubmission: Omit<Banner, "pathnameToDisplayOn">;
-  promptPreSubmission?: Omit<UserPrompt, "onAccept">;
-  promptOnLeavingForm?: Omit<UserPrompt, "onAccept">;
-  documentPollerArgs: {
-    property: keyof z.TypeOf<Schema> & string;
-    documentChecker: CheckDocumentFunction;
+type ActionFormProps<Schema extends SchemaWithEnforcableProps<z.ZodRawShape>> =
+  {
+    schema: Schema;
+    defaultValues?: DefaultValues<z.TypeOf<Schema>>;
+    title: string;
+    fieldsLayout?: ({ children }: { children: ReactNode }) => ReactNode;
+    fields: (form: UseFormReturn<z.TypeOf<Schema>>) => ReactNode[];
+    bannerPostSubmission: Omit<Banner, "pathnameToDisplayOn">;
+    promptPreSubmission?: Omit<UserPrompt, "onAccept">;
+    promptOnLeavingForm?: Omit<UserPrompt, "onAccept">;
+    attachments: {
+      faqLink: string;
+      specialInstructions?: string;
+      fileInstructions: {
+        [Key in keyof z.TypeOf<Schema>["attachments"]]: string;
+      };
+    };
+    documentPollerArgs: {
+      property: keyof z.TypeOf<Schema> & string;
+      documentChecker: CheckDocumentFunction;
+    };
+    footer?: () => ReactNode;
   };
-  footer?: () => ReactNode;
-};
 
-export const ActionForm = <Schema extends CombinedSchema<z.ZodRawShape>>({
+export const ActionForm = <
+  Schema extends SchemaWithEnforcableProps<z.ZodRawShape>,
+>({
   schema,
   defaultValues,
   title,
@@ -115,13 +72,14 @@ export const ActionForm = <Schema extends CombinedSchema<z.ZodRawShape>>({
   promptOnLeavingForm,
   promptPreSubmission,
   documentPollerArgs,
+  attachments,
   footer: Footer,
 }: ActionFormProps<Schema>) => {
   const { id, authority } = useParams<{ id: string; authority: Authority }>();
   const location = useLocation();
   const navigate = useNavigate();
 
-  const form = useForm<z.infer<Schema>>({
+  const form = useForm<z.TypeOf<Schema>>({
     resolver: zodResolver(schema),
     mode: "onChange",
     defaultValues: {
@@ -134,15 +92,18 @@ export const ActionForm = <Schema extends CombinedSchema<z.ZodRawShape>>({
       await API.post("os", "/submit", {
         body: formData,
       });
+
       documentPoller(
         documentPollerArgs.property,
         documentPollerArgs.documentChecker,
       );
+
       const formOrigins = getFormOrigin();
       banner({
         ...bannerPostSubmission,
         pathnameToDisplayOn: formOrigins.pathname,
       });
+
       navigate(formOrigins);
     } catch (error) {
       console.error(error);
@@ -155,11 +116,6 @@ export const ActionForm = <Schema extends CombinedSchema<z.ZodRawShape>>({
       window.scrollTo(0, 0);
     }
   });
-
-  const isAdditionalInformationRequired =
-    schema instanceof z.ZodObject && "additionalInformation" in schema.shape;
-  const isAttachmentsInSchema =
-    schema instanceof z.ZodObject && "attachments" in schema.shape;
 
   return (
     <SimplePageContainer>
@@ -180,9 +136,10 @@ export const ActionForm = <Schema extends CombinedSchema<z.ZodRawShape>>({
               <Fields {...form} />
             </SectionCard>
           )}
-
-          {isAttachmentsInSchema && <ActionFormAttachments schema={schema} />}
-          {isAdditionalInformationRequired && (
+          {schema.shape.attachments && (
+            <ActionFormAttachments schema={schema} {...attachments} />
+          )}
+          {schema.shape.additionalInformation && (
             <SectionCard title="Additional Information">
               <FormField
                 control={form.control}
