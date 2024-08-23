bfa3b2cfd4805ca29bfa03b2ec93bd21799b607c
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-08-23 13:48:13 -0400

    chore: remove redundant elements from SectionCard

--- react-app/src/components/Cards/SectionCard.tsx
+++ react-app/src/components/Cards/SectionCard.tsx
@@ -14,21 +14,17 @@ export const SectionCard = ({
   id,
 }: SectionCardProps) => {
   return (
-    <div
+    <section
       id={id}
       className={cn("mb-8 p-4 border rounded-sm border-cardBorder", className)}
     >
-      <section>
-        {title && (
-          <>
-            <h1 className="text-3xl font-semibold mb-2">{title}</h1>
-            <hr className="my-6 bg-slate-200" />
-          </>
-        )}
-        <div className="gap-8 flex flex-col" data-testid="attachments-section">
-          {children}
-        </div>
-      </section>
-    </div>
+      {title && (
+        <>
+          <h1 className="text-3xl font-semibold mb-2">{title}</h1>
+          <hr className="my-6 bg-slate-200" />
+        </>
+      )}
+      <div className="gap-8 flex flex-col">{children}</div>
+    </section>
   );
 };
