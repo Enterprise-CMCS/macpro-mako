3d91f1f78d74f410d16e31b9d9cc85acb213489c
Tiffany Vu <tiffanyvu@itadminss-MBP.home>
2024-08-26 12:14:46 -0700

    delete other unused code

--- react-app/src/components/Tooltip/Tooltip.test.tsx
+++ react-app/src/components/Tooltip/Tooltip.test.tsx
@@ -5,59 +5,19 @@ import { describe, expect, test, beforeAll } from "vitest";
 import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from ".";
 import { OsExportData } from "../Opensearch";
 
-// const wrapper = ({ children }: { children: ReactNode }) => (
-//   <MemoryRouter initialEntries={["/dashboard"]}>
-//     <Routes>
-//       <Route
-//         path="/dashboard"
-//         element={<OsExportData columns={[]}/>}
-//       />
-//     </Routes>
-//     {children}
-//   </MemoryRouter>
-// );
-
-const renderWithRouter = (element) => (
-  render(
-    <MemoryRouter initialEntries={['/dashboard']}>
-      <Routes>
-        <Route path='/dashboard' element={element} />
-      </Routes>
-    </MemoryRouter>
-  )
-);
-
 describe("Tooltip Component", async () => {
   beforeAll(() => {
-    // const memoryRouter = createMemoryRouter(
-    //   [
-    //     { path: "/dashboard", element: <OsExportData columns={[]}/> },
-    //   ],
-    //   {
-    //     initialEntries: ["/dashboard"],
-    //   },
-    // );
-    // render(<OsExportData columns={[]}/>)
-
-    // render(<RouterProvider router={memoryRouter} />);
-    // render(<OsExportData columns={[]}/>, { wrapper })
+    const memoryRouter = createMemoryRouter(
+      [
+        { path: "/dashboard", element: <OsExportData columns={[]}/> },
+      ],
+      {
+        initialEntries: ["/dashboard"],
+      },
+    );
+    render(<RouterProvider router={memoryRouter} />);
   });
-    // render(
-      // <OsExportData columns={[]}/>
-      // <TooltipProvider>
-      //   <Tooltip disableHoverableContent={true}>
-      //     <TooltipTrigger asChild>
-      //       <button data-testid="tooltip-trigger">Test Button</button>
-      //     </TooltipTrigger>
-      //     <TooltipContent role="tooltip-content">
-      //       Test Tooltip Info
-      //     </TooltipContent>
-      //   </Tooltip>
-      // </TooltipProvider>,
-    // );
-  // });
   test("Tooltip content hidden when not hovering", () => {
-    renderWithRouter(<OsExportData columns={[]}/>)
     const tooltipTrigger = screen.getByRole("tooltip-trigger")
     expect(tooltipTrigger).toBeInTheDocument();
     // const tooltipContent = screen.queryByTestId("tooltip-content");
