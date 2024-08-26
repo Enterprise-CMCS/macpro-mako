25eab13378adc2be781ed167ce0eb7d73931c4b8
Tiffany Vu <tiffanyvu@itadminss-MBP.home>
2024-08-26 14:19:01 -0700

    useOsData is not a function

--- react-app/src/components/Tooltip/Tooltip.test.tsx
+++ react-app/src/components/Tooltip/Tooltip.test.tsx
@@ -8,32 +8,51 @@ import { Dashboard, dashboardLoader } from "@/features";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 
-const renderWithClient = (ui: ReactJSXElement) => {
-  const queryClient = new QueryClient();
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <MemoryRouter initialEntries={["/dashboard"]}>
+    <Routes>
+      <Route
+        path="/dashboard"
+        element={<Dashboard />}
+      />
+    </Routes>
+    {children}
+  </MemoryRouter>
+);
 
-  return {
-    ...render(<QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>),
-    queryClient,
-  };
-}
+// const renderWithClient = (ui: ReactJSXElement) => {
+//   const queryClient = new QueryClient();
+
+//   return {
+//     ...render(<QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>),
+//     queryClient,
+//   };
+// }
+
+// const testing = <MemoryRouter initialEntries={["/dashboard"]}>
+// <Routes>
+//   <Route path="/dashboard" element={<Dashboard/>}/> 
+// </Routes>
+// </MemoryRouter>
 
 describe("Tooltip Component", async () => {
   beforeAll(() => {
     // const queryClient = new QueryClient();
-    const memoryRouter = createMemoryRouter(
-      [
-        { path: "/dashboard", element:<Dashboard/> },
-      ],
-      {
-        initialEntries: ["/dashboard"],
-      },
-    );
+    // const memoryRouter = createMemoryRouter(
+    //   [
+    //     { path: "/dashboard", element:<Dashboard/>, },
+    //   ],
+    //   {
+    //     initialEntries: ["/dashboard"],
+    //   },
+    // );
+    // render(<RouterProvider router={memoryRouter} />);
     // renderWithClient(<RouterProvider router={memoryRouter} />);
-    renderWithClient(<Dashboard />);
+    // renderWithClient(<Dashboard/>);
   });
   test("Tooltip content hidden when not hovering", () => {
-    // renderWithClient(<Dashboard/>)
-    const tooltipTrigger = screen.getByText("Dashboard")
+    render(<OsExportData columns={[]}/>, { wrapper })
+    const tooltipTrigger = screen.getByText("SPAs")
     expect(tooltipTrigger).toBeInTheDocument();
     // const tooltipContent = screen.queryByTestId("tooltip-content");
     // expect(tooltipContent).not.toBeInTheDocument();
