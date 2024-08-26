3937d734597d51d8c46c2a0de3a3c58172f7ef44
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-08-26 16:04:45 -0400

    feat: create fe schemas for renewal and initial forms

--- lib/packages/shared-types/events/capitated-waivers.ts
+++ lib/packages/shared-types/events/capitated-waivers.ts
@@ -4,9 +4,7 @@ import {
   attachmentArraySchemaOptional,
 } from "../attachments";
 
-export const amendmentFeSchema = z.object({
-  // zAmendmentOriginalWaiverNumberSchema
-  waiverNumber: z.number(),
+const baseCapitatedWaiverSchema = z.object({
   // zAmendmentWaiverNumberSchema
   id: z.string(),
   proposedEffectiveDate: z.date(),
@@ -37,5 +35,54 @@ export const amendmentFeSchema = z.object({
     }),
   }),
   additionalInformation: z.string().max(4000).nullable().default(null),
+});
+
+export const amendmentFeSchema = baseCapitatedWaiverSchema.extend({
+  // zAmendmentOriginalWaiverNumberSchema
+  waiverNumber: z.number(),
   seaActionType: z.string().default("Amend"),
+});
+
+export const renewalFeSchema = baseCapitatedWaiverSchema.extend({
+  // zAmendmentOriginalWaiverNumberSchema
+  waiverNumber: z.number(),
+  seaActionType: z.string().default("Renew"),
+  attachments: z.object({
+    bCapWaiverApplication: z.object({
+      label: z
+        .string()
+        .default(
+          "1915(b) Comprehensive (Capitated) Waiver Application Pre-print",
+        ),
+      files: attachmentArraySchema(),
+    }),
+    bCapCostSpreadsheets: z.object({
+      label: z
+        .string()
+        .default(
+          "1915(b) Comprehensive (Capitated) Waiver Cost Effectiveness Spreadsheets",
+        ),
+      files: attachmentArraySchema(),
+    }),
+    bCapIndependentAssessment: z.object({
+      label: z
+        .string()
+        .default(
+          "1915(b) Comprehensive (Capitated) Waiver Independent Assessment (first two renewals only)",
+        ),
+      files: attachmentArraySchemaOptional(),
+    }),
+    tribalConsultation: z.object({
+      label: z.string().default("Tribal Consultation"),
+      files: attachmentArraySchemaOptional(),
+    }),
+    other: z.object({
+      label: z.string().default("Other"),
+      files: attachmentArraySchemaOptional(),
+    }),
+  }),
+});
+
+export const initialFeSchema = baseCapitatedWaiverSchema.extend({
+  seaActionType: z.string().default("New"),
 });
