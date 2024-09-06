6a2b0061b6d73aff22098d07aead60e995c1a9e8
Gavin St. Ours <gst-ours@fearless.tech>
2024-09-06 14:47:32 -0400

    Set up initial CS7 files

---
+++ lib/libs/webforms/CS7/v202401.ts
@@ -0,0 +1,8 @@
+import { FormSchema } from "shared-types";
+
+export const v202401: FormSchema = {
+  header: "CS 7: Eligibility for Medicaid expansion program",
+  subheader: "42 CFR 457.320(a)(2) and (3)",
+  formId: "cs7",
+  sections: [],
+};
