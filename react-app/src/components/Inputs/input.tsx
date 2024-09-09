7b1cfce5269a06f5930b57c90cf1f5c02be139b9
Benjamin Paige <benjamin.paige@gmail.com>
2024-09-09 11:36:00 -0600

    updated input icon padding

--- react-app/src/components/Inputs/input.tsx
+++ react-app/src/components/Inputs/input.tsx
@@ -25,8 +25,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           className={cn(
             "flex h-9 w-full rounded-sm border border-[#212121] bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-            icon && !iconRight && "pl-10",
             className,
+            icon && (iconRight ? "pr-10" : "pl-10"),
           )}
           ref={ref}
           id={props.name}
