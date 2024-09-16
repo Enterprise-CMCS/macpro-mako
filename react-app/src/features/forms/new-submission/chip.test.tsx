5bafea242819639fbbaed68b1bc8eb2846d60fc9
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-09-16 13:26:18 -0400

    chore: more tests for `chip.test`

--- react-app/src/features/forms/new-submission/chip.test.tsx
+++ react-app/src/features/forms/new-submission/chip.test.tsx
@@ -14,12 +14,6 @@ vi.mock("@/components/Inputs/upload.utilities", () => ({
   uploadToS3: vi.fn(async () => {}),
   extractBucketAndKeyFromUrl: vi.fn(() => ({ bucket: "hello", key: "world" })),
 }));
-vi.mock("@/api/itemExists", () => ({
-  itemExists: vi.fn(async () => false),
-}));
-vi.mock("@/utils/user", () => ({
-  isAuthorizedState: vi.fn(async () => true),
-}));
 
 let container: HTMLElement;
 
@@ -34,8 +28,29 @@ describe("CHIP SPA", () => {
 
   test("SPA ID", async () => {
     const spaIdInput = screen.getByLabelText(/SPA ID/);
-    await userEvent.type(spaIdInput, "MD-24-9291");
-    const spaIdLabel = container.querySelector('[for="id"]');
+    const spaIdLabel = screen.getByTestId("spaid-label");
+
+    // test id validations
+    // fails if item exists
+    await userEvent.type(spaIdInput, "MD-00-0000");
+    const recordExistsErrorText = screen.getByText(
+      /According to our records, this SPA ID already exists. Please check the SPA ID and try entering it again./,
+    );
+    expect(recordExistsErrorText).toBeInTheDocument();
+
+    await userEvent.clear(spaIdInput);
+
+    // fails if state entered is not a valid state
+    await userEvent.type(spaIdInput, "AK-00-0000");
+    const invalidStateErrorText = screen.getByText(
+      /You can only submit for a state you have access to. If you need to add another state, visit your IDM user profile to request access./,
+    );
+    expect(invalidStateErrorText).toBeInTheDocument();
+
+    await userEvent.clear(spaIdInput);
+
+    // end of test id validations
+    await userEvent.type(spaIdInput, "MD-00-0001");
 
     expect(spaIdLabel).not.toHaveClass("text-destructive");
   });
