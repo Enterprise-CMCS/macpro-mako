946a8b9e0468dc7a05613f44df8de4922a6dc722
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-09-16 13:54:34 -0400

    chore: add waiver numbers for capitated initial

--- react-app/src/features/forms/waiver/capitated/initial.test.tsx
+++ react-app/src/features/forms/waiver/capitated/initial.test.tsx
@@ -2,7 +2,10 @@ import { screen } from "@testing-library/react";
 import { beforeAll, describe, expect, test, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 import { renderForm } from "@/utils/test-helpers/renderForm";
-import { skipCleanup } from "@/utils/test-helpers/skipCleanup";
+import {
+  mockApiRefinements,
+  skipCleanup,
+} from "@/utils/test-helpers/skipCleanup";
 import { InitialForm } from "./Initial";
 import { uploadFiles } from "@/utils/test-helpers/uploadFiles";
 import { formSchemas } from "@/formSchemas";
@@ -20,6 +23,7 @@ let container: HTMLElement;
 describe("Capitated Initial", () => {
   beforeAll(() => {
     skipCleanup();
+    mockApiRefinements();
 
     const { container: renderedContainer } = renderForm(<InitialForm />);
 
@@ -29,7 +33,24 @@ describe("Capitated Initial", () => {
   test("1915(B) WAIVER NUMBER", async () => {
     const waiverInitialInput = screen.getByLabelText(/Initial Waiver Number/);
     const waiverInitialLabel = screen.getByTestId("1915b-waiver-initial-label");
-    await userEvent.type(waiverInitialInput, "SS-8382.R98.01");
+
+    await userEvent.type(waiverInitialInput, "MD-0000.R00.01");
+    const recordExistsErrorText = screen.getByText(
+      /The Initial Waiver Number must be in the format of SS-####.R00.00 or SS-#####.R00.00/,
+    );
+    expect(recordExistsErrorText).toBeInTheDocument();
+
+    await userEvent.clear(waiverInitialInput);
+
+    await userEvent.type(waiverInitialInput, "AK-0000.R00.00");
+    const invalidStateErrorText = screen.getByText(
+      /You can only submit for a state you have access to. If you need to add another state, visit your IDM user profile to request access./,
+    );
+    expect(invalidStateErrorText).toBeInTheDocument();
+
+    await userEvent.clear(waiverInitialInput);
+
+    await userEvent.type(waiverInitialInput, "MD-0000.R00.00");
 
     expect(waiverInitialLabel).not.toHaveClass("text-destructive");
   });
