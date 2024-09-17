62218a250cdaa2db3eab09cba0741f3b249e02ec
Gavin St. Ours <gst-ours@fearless.tech>
2024-09-17 12:59:11 -0400

    Begin "Statewide income standards" section

--- lib/libs/webforms/CS9/v202401.ts
+++ lib/libs/webforms/CS9/v202401.ts
@@ -50,13 +50,14 @@ export const v202401: FormSchema = {
     {
       title: "Age standard",
       sectionId: "age-standard",
+      subsection: true,
       form: [
         {
           slots: [
             {
               rhf: "Select",
               label: "The age standard is from conception to end of pregnancy.",
-              labelClassName: "text-base",
+              labelClassName: "text-black",
               description:
                 "Does the state have an additional age definition or other age-related conditions?",
               descriptionAbove: true,
@@ -83,7 +84,7 @@ export const v202401: FormSchema = {
             {
               rhf: "Textarea",
               label: "Describe",
-              labelClassName: "text-base font-bold",
+              labelClassName: "text-black font-bold",
               name: "age-standard-description",
               formItemClassName:
                 "ml-[0.6rem] px-4 my-2 border-l-4 border-l-primary",
@@ -103,6 +104,123 @@ export const v202401: FormSchema = {
                   },
                 ],
                 effect: { type: "show" },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Statewide income standards",
+      sectionId: "statewide-income-standards",
+      subsection: true,
+      form: [
+        {
+          slots: [
+            {
+              rhf: "Select",
+              label: "Are income standards applied statewide?",
+              labelClassName: "text-black font-bold",
+              name: "statewide-income-standards",
+              rules: {
+                required: "* Required",
+              },
+              props: {
+                options: [
+                  {
+                    label: "Yes",
+                    value: "yes",
+                  },
+                  {
+                    label: "No",
+                    value: "no",
+                  },
+                ],
+                className: "w-[125px]",
+              },
+            },
+            {
+              rhf: "Input",
+              label: "Statewide income standard",
+              labelClassName: "text-black font-bold",
+              name: "statewide-income-standard-from",
+              description: "From zero up to",
+              descriptionAbove: true,
+              descriptionClassName: "font-bold",
+              rules: {
+                pattern: {
+                  value: /^[0-9]\d*$/,
+                  message: "Must be a positive integer value",
+                },
+                required: "* Required",
+              },
+              props: {
+                icon: "% FPL",
+                iconRight: true,
+                className: "w-[159px]",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Income standard exceptions",
+      sectionId: "income-standard-exceptions",
+      subsection: true,
+      form: [
+        {
+          slots: [
+            {
+              rhf: "Select",
+              label:
+                "Are there any exceptions, such as populations in a county that may qualify under either a statewide income standard or a county income standard?",
+              labelClassName: "text-black font-bold",
+              name: "are-there-exceptions",
+              rules: {
+                required: "* Required",
+              },
+              props: {
+                options: [
+                  {
+                    label: "Yes",
+                    value: "yes",
+                  },
+                  {
+                    label: "No",
+                    value: "no",
+                  },
+                ],
+                className: "w-[125px]",
+              },
+            },
+            {
+              rhf: "Textarea",
+              label:
+                "Explain, including a description of the overlapping geographic area and the reason for having different income standards.",
+              labelClassName: "text-black font-bold",
+              name: "income-standard-exceptions-description",
+              formItemClassName:
+                "ml-[0.6rem] px-4 my-2 border-l-4 border-l-primary",
+              rules: {
+                pattern: {
+                  value: noLeadingTrailingWhitespace,
+                  message: "Must not have leading or trailing whitespace.",
+                },
+                required: "* Required",
+              },
+              dependency: {
+                conditions: [
+                  {
+                    name: "cs9_age-standard_age-standard",
+                    type: "expectedValue",
+                    expectedValue: "yes",
+                  },
+                ],
+                effect: { type: "show" },
+              },
+              props: {
+                className: "min-h-[76px]",
               },
             },
           ],
