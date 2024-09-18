f2aec5be524c4fc044b6703a3603c3075e31bcf0
Tiffany Vu <tiffanyvu@itadminss-MacBook-Pro.local>
2024-09-18 08:35:20 -0700

    format code

--- react-app/src/components/Opensearch/main/index.tsx
+++ react-app/src/components/Opensearch/main/index.tsx
@@ -33,28 +33,28 @@ export const OsMainView: FC<{
   return (
     <div className="flex flex-col">
       <div className="w-full my-2 max-w-screen-xl self-center px-4 lg:px-8">
-      <OsFiltering onToggle={onToggle} columns={osColumns} />
-      <FilterChips/>
+        <OsFiltering onToggle={onToggle} columns={osColumns} />
+        <FilterChips />
       </div>
       <div className="px-4 lg:px-8">
-      <OsTable onToggle={onToggle} columns={osColumns} />
-      <Pagination
-        pageNumber={url.state.pagination.number}
-        count={context.data?.total?.value || 0}
-        pageSize={url.state.pagination.size}
-        onPageChange={(number) =>
-          url.onSet((s) => ({
-            ...s,
-            pagination: { ...s.pagination, number },
-          }))
-        }
-        onSizeChange={(size) =>
-          url.onSet((s) => ({
-            ...s,
-            pagination: { number: 0, size },
-          }))
-        }
-      />
+        <OsTable onToggle={onToggle} columns={osColumns} />
+        <Pagination
+          pageNumber={url.state.pagination.number}
+          count={context.data?.total?.value || 0}
+          pageSize={url.state.pagination.size}
+          onPageChange={(number) =>
+            url.onSet((s) => ({
+              ...s,
+              pagination: { ...s.pagination, number },
+            }))
+          }
+          onSizeChange={(size) =>
+            url.onSet((s) => ({
+              ...s,
+              pagination: { number: 0, size },
+            }))
+          }
+        />
       </div>
     </div>
   );
