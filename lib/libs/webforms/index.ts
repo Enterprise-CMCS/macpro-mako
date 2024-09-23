9cf543a97afd5a5166bdaa4a7c817a804256a4db
Gavin St. Ours <gst-ours@fearless.tech>
2024-09-23 17:00:53 -0400

    feat(webforms): Add CS 9 to webforms (#777)
    * Create new CS 9 dir and files
    
    * Add CS 9 to index
    
    * Add first conditional logic
    
    * Begin "Statewide income standards" section
    
    * Adjust spacing and layout
    
    * Adjust dependency
    
    * Add rest of CS 9 form
    
    * Add validation rules
    
    * Ensure both checkboxes at bottom are required
    
    * Remove unnecessary checkbox group label
    
    * Ensure labels have the correct classes
    
    * Ensure correct validation for text input fields
    
    * Add missing Textarea validation pattern
    
    * Revert test code

--- lib/libs/webforms/index.ts
+++ lib/libs/webforms/index.ts
@@ -14,6 +14,7 @@ import * as ABP10 from "./ABP10";
 import * as ABP11 from "./ABP11";
 import * as CS3 from "./CS3";
 import * as CS7 from "./CS7";
+import * as CS9 from "./CS9";
 import * as CS8 from "./CS8";
 import * as G2A from "./G2A";
 import * as G1 from "./G1";
@@ -74,6 +75,9 @@ export const webformVersions: Record<string, Record<string, FormSchema>> = {
   },
   CS8: {
     v202401: CS8.v202401,
+  },
+  CS9: {
+    v202401: CS9.v202401,
   },
   G1: {
     v202401: G1.v202401,
