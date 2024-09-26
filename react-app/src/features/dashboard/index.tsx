0f76baf8b1b9f5c97484e6b8e3121bd502dd5373
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-09-25 20:29:14 -0400

    fix: use `custom:cms-roles` to determine whether user can submit

--- react-app/src/features/dashboard/index.tsx
+++ react-app/src/features/dashboard/index.tsx
@@ -1,7 +1,6 @@
-import { useMemo } from "react";
 import { QueryClient } from "@tanstack/react-query";
 import { Plus as PlusIcon } from "lucide-react";
-import { getUser } from "@/api";
+import { getUser, useGetUser } from "@/api";
 import { WaiversList } from "./Lists/waivers";
 import { SpasList } from "./Lists/spas";
 import {
@@ -9,7 +8,6 @@ import {
   type OsTab,
   useOsData,
   FilterDrawerProvider,
-  useUserContext,
   Tabs,
   TabsContent,
   TabsList,
@@ -43,17 +41,16 @@ const loader = (queryClient: QueryClient) => {
 export const dashboardLoader = loader;
 
 export const Dashboard = () => {
-  const userContext = useUserContext();
+  const { data: userObj } = useGetUser();
   const osData = useOsData();
   useScrollToTop();
 
-  const role = useMemo(() => {
-    return userContext?.user?.["custom:cms-roles"] ? true : false;
-  }, []);
-
-  if (!role) {
+  if (userObj === undefined) {
     return <Navigate path={"/"} />;
   }
+
+  const canUserSubmit =
+    userObj.user["custom:cms-roles"] === "onemac-micro-statesubmitter";
 
   return (
     <OsProvider
@@ -65,10 +62,9 @@ export const Dashboard = () => {
     >
       <div>
         <FilterDrawerProvider>
-          {/* Header  */}
-            <div className="flex flex-col w-full self-center mx-auto max-w-screen-xl xs:flex-row justify-between p-4 lg:px-8">
+          <div className="flex flex-col w-full self-center mx-auto max-w-screen-xl xs:flex-row justify-between p-4 lg:px-8">
             <h1 className="text-xl font-bold mb-4 md:mb-0">Dashboard</h1>
-            {!userContext?.isCms && (
+            {canUserSubmit && (
               <Link
                 path="/new-submission"
                 className="flex items-center text-white font-bold bg-primary border-none px-10 py-2 rounded cursor-pointer"
@@ -78,7 +74,6 @@ export const Dashboard = () => {
               </Link>
             )}
           </div>
-          {/* Tabs */}
           <div className="w-full">
             <div className="flex flex-col">
               <Tabs
@@ -96,14 +91,14 @@ export const Dashboard = () => {
                 }
               >
                 <div className="flex max-w-screen-xl mx-auto px-4 lg:px-8">
-                <TabsList>
-                  <TabsTrigger value="spas" className="px-6 py-2">
-                    <h2 className="font-bold text-[1.3em]">SPAs</h2>
-                  </TabsTrigger>
-                  <TabsTrigger value="waivers" className="px-6 py-2">
-                    <h2 className="font-bold text-[1.3em]">Waivers</h2>
-                  </TabsTrigger>
-                </TabsList>
+                  <TabsList>
+                    <TabsTrigger value="spas" className="px-6 py-2">
+                      <h2 className="font-bold text-[1.3em]">SPAs</h2>
+                    </TabsTrigger>
+                    <TabsTrigger value="waivers" className="px-6 py-2">
+                      <h2 className="font-bold text-[1.3em]">Waivers</h2>
+                    </TabsTrigger>
+                  </TabsList>
                 </div>
                 <TabsContent value="spas">
                   <SpasList />
