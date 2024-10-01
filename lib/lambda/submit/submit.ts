690bc332b0d5e7c2d2de274f40d885299c766a1a
Tiffany Vu <tiffanyvu@itadminss-MBP.home>
2024-10-01 09:23:35 -0700

    add logs

--- lib/lambda/submit/submit.ts
+++ lib/lambda/submit/submit.ts
@@ -6,7 +6,9 @@ import { produceMessage } from "../../libs/api/kafka";
 import { BaseSchemas } from "shared-types/events";
 
 export const submit = async (event: APIGatewayEvent) => {
+  console.log('IN SUBMIT?')
   if (!event.body) {
+    console.log('NO EVENT BODY')
     return response({
       statusCode: 400,
       body: "Event body required",
@@ -15,10 +17,10 @@ export const submit = async (event: APIGatewayEvent) => {
 
   const body: BaseSchemas = JSON.parse(event.body);
 
-  console.log(body);
-
+  console.log(body, 'AFTER BODY');
   // If there's no event, we reject
   if (!body.event) {
+    console.log('NO BODY EVENT')
     return response({
       statusCode: 400,
       body: { message: "Bad Request - Missing event name in body" },
@@ -34,8 +36,9 @@ export const submit = async (event: APIGatewayEvent) => {
   }
 
   try {
+    console.log('IN THIS TRY')
     const eventBody = await submissionPayloads[body.event](event);
-
+    console.log(eventBody, 'event body')
     await produceMessage(
       process.env.topicName as string,
       body.id,
