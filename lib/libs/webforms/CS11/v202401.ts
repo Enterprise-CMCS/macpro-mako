9684771ab3a05012c41c991881fa2c8d5e830de4
Gavin St. Ours <gst-ours@fearless.tech>
2024-10-11 10:05:05 -0400

    Import CS 11 in index.ts

---
+++ lib/libs/webforms/CS11/v202401.ts
@@ -0,0 +1,10 @@
+import { FormSchema } from "shared-types";
+
+export const v202401: FormSchema = {
+  header:
+    "CS 11: Separate CHIP eligibility—Pregnant women who have access to public employee coverage",
+  subheader:
+    "Sections 2110(b)(2)(B) and (b)(6) of the Social Security Act (SSA)",
+  formId: "CS11",
+  sections: [],
+};
