da290d804304eab523b07ee78b75c55a91d753f4
Gavin St. Ours <gst-ours@fearless.tech>
2024-10-11 10:14:35 -0400

    Add Overview

--- lib/libs/webforms/CS11/v202401.ts
+++ lib/libs/webforms/CS11/v202401.ts
@@ -6,5 +6,49 @@ export const v202401: FormSchema = {
   subheader:
     "Sections 2110(b)(2)(B) and (b)(6) of the Social Security Act (SSA)",
   formId: "CS11",
-  sections: [],
+  sections: [
+    {
+      title: "Overview",
+      sectionId: "overview",
+      form: [
+        {
+          slots: [
+            {
+              rhf: "Checkbox",
+              name: "",
+              styledLabel: [
+                {
+                  text: "Otherwise eligible",
+                  type: "default",
+                  classname: "text-black",
+                },
+                {
+                  text: " targeted low-income pregnant women who have access to public employee coverage",
+                  type: "bold",
+                  classname: "text-black",
+                },
+                {
+                  text: " on the basis of their own or a family member's employment",
+                  type: "default",
+                  classname: "text-black",
+                },
+              ],
+              rules: {
+                required: "* Required",
+              },
+              props: {
+                options: [
+                  {
+                    label:
+                      "The CHIP agency operates this covered group in accordance with the following provisions.",
+                    value: "true",
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
