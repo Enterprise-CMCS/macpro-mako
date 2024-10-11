2338c159fcf7ee23d84806a8e1e6db33c08f8bac
Tiffany Vu <tiffanyvu@itadminss-MacBook-Pro.local>
2024-10-11 10:44:48 -0700

    event

--- lib/packages/shared-types/events/toggle-withdraw-rai.ts
+++ lib/packages/shared-types/events/toggle-withdraw-rai.ts
@@ -14,6 +14,7 @@ import { z } from "zod";
 // >;
 
 export const baseSchema = z.object({
+  event: z.literal("toggle-withdraw-rai").default("toggle-withdraw-rai"),
   id: z.string(),
   authority: z.string(),
   raiWithdrawEnabled: z.boolean(),
