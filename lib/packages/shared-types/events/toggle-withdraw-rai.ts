f9f1b6af5f915fab980f634a3087512af39b6046
tiffanyvu <tiffany.v330@gmail.com>
2024-10-15 14:09:36 -0700

    Update lib/packages/shared-types/events/toggle-withdraw-rai.ts
    Co-authored-by: 13bfrancis <40218571+13bfrancis@users.noreply.github.com>

--- lib/packages/shared-types/events/toggle-withdraw-rai.ts
+++ lib/packages/shared-types/events/toggle-withdraw-rai.ts
@@ -1,17 +1,5 @@
 import { z } from "zod";
 
-// export const toggleWithdrawRaiEnabledSchema = z.object({
-//   id: z.string(),
-//   authority: z.string(),
-//   origin: z.string(),
-//   raiWithdrawEnabled: z.boolean(),
-//   submitterName: z.string(),
-//   submitterEmail: z.string(),
-//   timestamp: z.number().optional(),
-// });
-// export type ToggleWithdrawRaiEnabled = z.infer<
-//   typeof toggleWithdrawRaiEnabledSchema
-// >;
 
 export const baseSchema = z.object({
   event: z.literal("toggle-withdraw-rai").default("toggle-withdraw-rai"),
