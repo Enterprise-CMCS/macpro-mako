2845230b545846d2ddfbc465f5195dea470163ff
Rafiq Muntaqim <rmuntaqim@fearless.tech>
2024-10-16 16:27:36 -0400

    restoring to previous version

--- lib/lambda/item.ts
+++ lib/lambda/item.ts
@@ -10,7 +10,6 @@ import { validateEnvVariable } from "shared-utils";
 
 export const getItemData = async (event: APIGatewayEvent) => {
   validateEnvVariable("osDomain");
-  console.log("Testing works!");
   if (!event.body) {
     return response({
       statusCode: 400,
@@ -20,15 +19,11 @@ export const getItemData = async (event: APIGatewayEvent) => {
   try {
     const body = JSON.parse(event.body);
     const stateFilter = await getStateFilter(event);
-    const packageResult = await getPackage(body.id); // Add another line here for getting the intened parent to pass on later for the title
-
+    const packageResult = await getPackage(body.id);
 
     let appkChildren: any[] = [];
     if (packageResult._source.appkParent) {
       const children = await getAppkChildren(body.id);
-      //Testing
-
-      //console.log("These are the children...", children);
       appkChildren = children.hits.hits;
     }
     const filter = [];
@@ -59,82 +54,6 @@ export const getItemData = async (event: APIGatewayEvent) => {
       });
     }
 
-    // Return the whole package
-    //console.log('This is the Package Result', packageResult);
-
-    // see if its a child appk that has been withdrawn
-    const appKChildRemoved = changelog.hits.hits.find(changeLogItem => changeLogItem._source.actionType === "remove-appk-child");
-    const getChildID = changelog.hits.hits.find(changeLogItem => changeLogItem._source.appkParentId !== null);
-    getChildID?._source.appkParentId;
-
-    const theActionType = appKChildRemoved?._source.actionType;
-    //const getChildID = changelog.hits.hits['parentID'];
-    //const getParendID = appKChildRemoved?._source.appkParentId;
-
-    // console.log("Hello child..", getChildID);
-    //console.log("Hello parent..", getChildID);
-    console.log("appkChild Removed here action TYPE: ..", appKChildRemoved?._source.actionType);
-    if (theActionType === "remove-appk-child") {
-      console.log("App Child is being Removed..GO");
-      console.log("Getting Parent ID..", appKChildRemoved?._source.appkParentId);
-      console.log("Getting the child ID..", appKChildRemoved?._source.packageId);
-
-      // fetch parent id from it
-      // await getPackage(appKChildRemoved._source.appkParentId)
-      // packageResult: set the appkTitle
-      //console.log("Step 1..check if childappk is removed", appKChildRemoved._source.appkParentId)
-      //console.log("Step 2.. get parentid", appKChildRemoved._source)
-    }
-
-    // Return the title from the parent
-    const pTitle = packageResult._source.appkTitle;
-    //console.log("This is the", pTitle);
-
-    // Check to see if appkchild is in withdrawn state
-    const appChildWithdrawnState = packageResult._source.actionType;
-
-    //console.log("This is the", appChildWithdrawnState);
-
-    // Probably have to loop over the change log to get the
-
-    // array of the history of everything that has happened to the record
-    // if there is an entry that say it used to be a app-k child and was withdrawn
-    // determine if this record is an app-k child
-    // query for the parent id from that entry
-
-    //console.log("what the heck is in the changelog (B.F.):", JSON.stringify(changelog));
-
-    //const checkActionType = changelog.hits.hits.find(action => action._source.actionType === "remove-appk-child");
-    //console.log("this is the action type", checkActionType?._source.actionType);
-
-    //console.log("This is the parent id: ", packageResult._id);
-
-
-
-
-    //packageResult._source.appkParent
-    //           "appkParent": true,
-
-
-    // The title that you got from the parent pass to the child
-    const theChildren = packageResult._source.appkChildren;
-    //console.log('This title should pass to the ', theChildren);
-
-    /*
-    if(packageResult){
-      // get the pTitle
-      
-      // check to see if package is withdrawn
-      if(appChildWithdrawnState){
-
-      }
-    }
-      
-
-       
-    */
-
-
     if (!packageResult.found) {
       return response({
         statusCode: 404,
@@ -164,11 +83,3 @@ export const getItemData = async (event: APIGatewayEvent) => {
 };
 
 export const handler = getItemData;
-
-// Refactor to return appkchild title property with appkchild Document
-// When appkchild is withdrawn, check to the changelog to see if it was a
-// withdrawn appkchild ... And if it is withdrawn i should be looking 
-// for its parent to get the appropiate title. 
-
-// Check the changelog to see if its a withdrawn appkchild and if so pass 
-// the title to the return statement. 
