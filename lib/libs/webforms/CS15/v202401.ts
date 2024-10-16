e9720e7e4d38c6aab9760191b2d270f9018fd907
Gavin St. Ours <gst-ours@fearless.tech>
2024-10-16 11:48:11 -0400

    Add initial CS 15 files

---
+++ lib/libs/webforms/CS15/v202401.ts
@@ -0,0 +1,9 @@
+import { FormSchema } from "shared-types";
+
+export const v202401: FormSchema = {
+  formId: "cs15",
+  header: "CS 15: Separate CHIP MAGI-based income methodologies",
+  subheader:
+    "2102(b)(1)(B)(v) of the Social Security Act (SSA) and 42 CFR 457.315",
+  sections: [],
+};
