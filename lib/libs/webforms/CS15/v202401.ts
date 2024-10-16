32a8dcd46623cffc872c2b928105d0e47284513e
Gavin St. Ours <gst-ours@fearless.tech>
2024-10-16 11:52:33 -0400

    Add Overview

--- lib/libs/webforms/CS15/v202401.ts
+++ lib/libs/webforms/CS15/v202401.ts
@@ -5,5 +5,32 @@ export const v202401: FormSchema = {
   header: "CS 15: Separate CHIP MAGI-based income methodologies",
   subheader:
     "2102(b)(1)(B)(v) of the Social Security Act (SSA) and 42 CFR 457.315",
-  sections: [],
+  sections: [
+    {
+      title: "Overview",
+      sectionId: "overview",
+      form: [
+        {
+          slots: [
+            {
+              rhf: "Checkbox",
+              name: "chip-agency-apply-magi",
+              props: {
+                options: [
+                  {
+                    label:
+                      "The CHIP agency will apply modified adjusted gross income (MAGI) methodologies for all separate CHIP covered groups, as described below and consistent with 42 CFR 457.315 and 435.603(b) through (i).",
+                    value: "true",
+                  },
+                ],
+              },
+              rules: {
+                required: "* Required",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
