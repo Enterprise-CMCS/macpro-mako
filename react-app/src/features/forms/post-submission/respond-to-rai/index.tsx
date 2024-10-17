0fe10bbbaf87023d02594d797eaa8ef3162ce5dc
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-10-17 14:32:23 -0400

    fix lint errors

--- react-app/src/features/forms/post-submission/respond-to-rai/index.tsx
+++ react-app/src/features/forms/post-submission/respond-to-rai/index.tsx
@@ -1,102 +1,81 @@
-import {
-    ActionForm,
-    RequiredFieldDescription,
-    SectionCard,
-    ActionFormDescription,
-    PackageSection,
-} from "@/components";
+import { ActionForm, PackageSection } from "@/components";
 import { formSchemas } from "@/formSchemas";
-import { SEATOOL_STATUS } from "shared-types";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 export const RespondToRaiMedicaid = () => {
-    const { authority, id } = useParams();
-    console.log({ authority })
-    return (
-        <ActionForm
-            schema={formSchemas["respond-to-rai-medicaid"]}
-            title={`${authority} Formal RAI Response Details`}
-            fields={({ control }) => (
-                <>
-                    <PackageSection />
-                </>
-            )}
-            defaultValues={{ id }}
-            attachments={{
-                faqLink: "/faq",
-            }}
-            documentPollerArgs={{
-                property: "id",
-                documentChecker: (check) =>
-                    check.recordExists,
-            }}
-            breadcrumbText="Respond to Formal RAI"
-            preSubmissionMessage="Once complete, you and CMS will receive an email confirmation."
-            bannerPostSubmission={{
-                header: "RAI response submitted",
-                body: `The RAI response for ${id} has been submitted.`,
-                variant: "success",
-            }} />
-    );
-}
+  const { authority, id } = useParams();
+  console.log({ authority });
+  return (
+    <ActionForm
+      schema={formSchemas["respond-to-rai-medicaid"]}
+      title={`${authority} Formal RAI Response Details`}
+      fields={() => <PackageSection />}
+      defaultValues={{ id }}
+      attachments={{
+        faqLink: "/faq",
+      }}
+      documentPollerArgs={{
+        property: "id",
+        documentChecker: (check) => check.recordExists,
+      }}
+      breadcrumbText="Respond to Formal RAI"
+      preSubmissionMessage="Once complete, you and CMS will receive an email confirmation."
+      bannerPostSubmission={{
+        header: "RAI response submitted",
+        body: `The RAI response for ${id} has been submitted.`,
+        variant: "success",
+      }}
+    />
+  );
+};
 export const RespondToRaiWaiver = () => {
-    const { authority, id } = useParams();
-    console.log({ authority })
-    return (
-        <ActionForm
-            schema={formSchemas["respond-to-rai-waiver"]}
-            title={`${authority} Waiver Formal RAI Response Details`}
-            fields={({ control }) => (
-                <>
-                    <PackageSection />
-                </>
-            )}
-            defaultValues={{ id }}
-            attachments={{
-                faqLink: "/faq",
-            }}
-            documentPollerArgs={{
-                property: "id",
-                documentChecker: (check) =>
-                    check.recordExists,
-            }}
-            breadcrumbText="Respond to Formal RAI"
-            preSubmissionMessage="Once complete, you and CMS will receive an email confirmation."
-            bannerPostSubmission={{
-                header: "RAI response submitted",
-                body: `The RAI response for ${id} has been submitted.`,
-                variant: "success",
-            }}
-        />
-    );
-}
+  const { authority, id } = useParams();
+  console.log({ authority });
+  return (
+    <ActionForm
+      schema={formSchemas["respond-to-rai-waiver"]}
+      title={`${authority} Waiver Formal RAI Response Details`}
+      fields={() => <PackageSection />}
+      defaultValues={{ id }}
+      attachments={{
+        faqLink: "/faq",
+      }}
+      documentPollerArgs={{
+        property: "id",
+        documentChecker: (check) => check.recordExists,
+      }}
+      breadcrumbText="Respond to Formal RAI"
+      preSubmissionMessage="Once complete, you and CMS will receive an email confirmation."
+      bannerPostSubmission={{
+        header: "RAI response submitted",
+        body: `The RAI response for ${id} has been submitted.`,
+        variant: "success",
+      }}
+    />
+  );
+};
 export const RespondToRaiChip = () => {
-    const { authority, id } = useParams();
-    return (
-        <ActionForm
-            schema={formSchemas["respond-to-rai-chip"]}
-            title={`${authority} Formal RAI Response Details`}
-            fields={({ control }) => (
-                <>
-                    <PackageSection />
-                </>
-            )}
-            defaultValues={{ id }}
-            attachments={{
-                faqLink: "/faq",
-            }}
-            documentPollerArgs={{
-                property: "id",
-                documentChecker: (check) =>
-                    check.recordExists,
-            }}
-            breadcrumbText="Respond to Formal RAI"
-            preSubmissionMessage="Once complete, you and CMS will receive an email confirmation."
-            bannerPostSubmission={{
-                header: "RAI response submitted",
-                body: `The RAI response for ${id} has been submitted.`,
-                variant: "success",
-            }}
-        />
-    );
-}
+  const { authority, id } = useParams();
+  return (
+    <ActionForm
+      schema={formSchemas["respond-to-rai-chip"]}
+      title={`${authority} Formal RAI Response Details`}
+      fields={() => <PackageSection />}
+      defaultValues={{ id }}
+      attachments={{
+        faqLink: "/faq",
+      }}
+      documentPollerArgs={{
+        property: "id",
+        documentChecker: (check) => check.recordExists,
+      }}
+      breadcrumbText="Respond to Formal RAI"
+      preSubmissionMessage="Once complete, you and CMS will receive an email confirmation."
+      bannerPostSubmission={{
+        header: "RAI response submitted",
+        body: `The RAI response for ${id} has been submitted.`,
+        variant: "success",
+      }}
+    />
+  );
+};
