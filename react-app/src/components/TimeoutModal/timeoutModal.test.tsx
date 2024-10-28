cdf55a1540482c544813c115fdb30e4b843c9f20
tiffanyvu <tiffany.v330@gmail.com>
2024-10-28 11:59:26 -0700

    close modal in signout for test

--- react-app/src/components/TimeoutModal/timeoutModal.test.tsx
+++ react-app/src/components/TimeoutModal/timeoutModal.test.tsx
@@ -2,6 +2,7 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { vi, describe, it, expect, beforeEach, afterEach } from "vitest";
 import { TimeoutModal } from ".";
+import { useCountdown } from "@/hooks";
 
 const ParentComponent = () => (
   <div>
@@ -12,15 +13,22 @@ const ParentComponent = () => (
   </div>
 );
 
+vi.mock("@/api", () => ({
+  useGetUser: vi
+    .fn()
+    .mockReturnValue({ data: { user: { name: "reviewer" }, isCms: true } }),
+}));
+
+vi.mock("aws-amplify", () => ({
+  Auth: {
+    signOut: vi.fn(),
+  },
+}));
+
 beforeEach(() => {
   vi.useFakeTimers({ shouldAdvanceTime: true });
-  vi.mock("@/api", () => ({
-    useGetUser: vi
-      .fn()
-      .mockReturnValue({ data: { user: { name: "reviewer" }, isCms: true } }),
-  }));
   vi.mock("@/hooks", () => ({
-    useIdle: vi.fn().mockReturnValueOnce(true),
+    useIdle: vi.fn().mockReturnValue(true),
     useCountdown: vi.fn().mockReturnValue([
       200,
       {
@@ -38,25 +46,7 @@ afterEach(() => {
 });
 
 describe("Timeout Modal", () => {
-  // it("doesn't display a modal before 20 minutes", () => {
-  // vi.mock("@/hooks", () => ({
-  //   useIdle: vi.fn().mockReturnValue(false),
-  //   useCountdown: vi.fn().mockReturnValue([
-  //     200000,
-  //     {
-  //       startCountdown: vi.fn(),
-  //       stopCountdown: vi.fn(),
-  //       resetCountdown: vi.fn(),
-  //     },
-  //   ]),
-  // }));
-  // const { queryByText } = render(<ParentComponent />);
-  // // vi.advanceTimersByTime(1000);
-  // const sessionExpirationWarning = queryByText(/Session expiring soon/i);
-  // expect(sessionExpirationWarning).not.toBeInTheDocument();
-  // });
-
-  it("displays a modal after idle", async () => {
+  it("closes after user extends session", async () => {
     render(<ParentComponent />);
 
     const sessionExpirationWarning = screen.getByText(
@@ -68,6 +58,25 @@ describe("Timeout Modal", () => {
     expect(extendBtn).toBeInTheDocument();
 
     await userEvent.click(extendBtn);
+    await waitFor(() => {
+      expect(
+        screen.queryByText(/Your session will expire in/i),
+      ).not.toBeInTheDocument();
+    });
+  });
+
+  it("closes the modal if user clicks sign out", async () => {
+    render(<ParentComponent />);
+
+    const sessionExpirationWarning = screen.getByText(
+      /Your session will expire in/i,
+    );
+    expect(sessionExpirationWarning).toBeInTheDocument();
+
+    const signOutBtn = screen.getByText(/No, sign out/i);
+    expect(signOutBtn).toBeInTheDocument();
+
+    await userEvent.click(signOutBtn);
     await waitFor(() => {
       expect(
         screen.queryByText(/Your session will expire in/i),
