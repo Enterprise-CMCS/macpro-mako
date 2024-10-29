8991f98aeef8732d3b0acf306d82e6738e9864dc
tiffanyvu <tiffany.v330@gmail.com>
2024-10-29 14:00:38 -0700

    add tests for maintenance and simplepage container

--- react-app/src/components/MaintenanceBanner/maintenanceBanner.test.tsx
+++ react-app/src/components/MaintenanceBanner/maintenanceBanner.test.tsx
@@ -1,63 +1,26 @@
 import { render, screen } from "@testing-library/react";
-import { describe, vi, it, expect, beforeEach, afterEach } from "vitest";
+import { describe, vi, it, expect, afterEach } from "vitest";
 import { MaintenanceBanner } from ".";
-import { useLDClient } from "launchdarkly-react-client-sdk";
-// import * as useLDClient from "launchdarkly-react-client-sdk";
 
-// const useLDClient = vi.hoisted(() => vi.fn());
-
-// vi.mock("launchdarkly-react-client-sdk", () => ({
-//   useLDClient,
-// }));
-// vi.mock("launchdarkly-react-client-sdk", () => ({
-//   useLDClient: vi.fn(),
-// }));
-// vi.mock("useLDClient");
 vi.mock("launchdarkly-react-client-sdk", () => ({
   useLDClient: vi.fn().mockReturnValueOnce({
     variation: () => "SCHEDULED",
   }),
 }));
 
+// TODO: test unscheduled, need to mock different return value in each test
 describe("MaintenanceBanner", () => {
   afterEach(() => {
     vi.clearAllMocks();
   });
 
   it("renders the scheduled maintenance banner", () => {
-    // vi.mocked(useLDClient).mockReturnValueOnce({
-    //   variation: () => "SCHEDULED",
-    // });
-    // vi.mock("launchdarkly-react-client-sdk", () => ({
-    //   useLDClient: vi.fn().mockReturnValueOnce({
-    //     variation: () => "SCHEDULED",
-    //   }),
-    // }));
-    // useLDClient.mockImplementation();
     render(<MaintenanceBanner />);
 
     expect(screen.getByText("Scheduled Maintenance Flag")).toBeVisible();
   });
 
-  // it("renders the UNSCHEDULED maintenance banner", () => {
-  //   // vi.mocked(useLDClient).mockReturnValueOnce({
-  //   //   variation: () => "UNSCHEDULED",
-  //   // });
-  //   render(<MaintenanceBanner />);
-
-  //   expect(screen.getByText("Unschedule Maintenance Flag")).toBeVisible();
-  // });
-
   it("renders nothing if no maintenance flag is set", () => {
-    // vi.mocked(useLDClient).mockReturnValueOnce({
-    //   variation: () => undefined,
-    // });
-    // vi.mock("launchdarkly-react-client-sdk", () => ({
-    //   useLDClient: vi.fn().mockReturnValueOnce({
-    //     variation: () => undefined,
-    //   }),
-    // }));
-
     const { container } = render(<MaintenanceBanner />);
 
     expect(container.firstChild).toBeNull();
