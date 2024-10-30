41780f2bf0c2d5df295be673404ba2b93bd9d7fa
ndebrah-fearless <ndebrah@fearless.tech>
2024-10-30 10:43:36 -0400

    Adding high-level test steps for CHIP and Medicaid SPAs.

--- test/e2e/tests/home/chipspa.spec.ts
+++ test/e2e/tests/home/chipspa.spec.ts
@@ -3,5 +3,21 @@ import { test } from "@playwright/test";
 test.describe.skip("Form Submission", async () => {
   // comment this out until we need it
   // test.beforeAll();
-  test("Create and submit a CHIP SPA", () => {});
+  test("Create and submit a CHIP SPA", () => {
+    /* Login */
+    // Navigate to the OneMAC Home Page using the home.page.ts file. 
+    // Login to MAKO as a State User, using the loginPage.ts script to navigate to the Login prompt. 
+    // Refer to the users.ts file to view the available users for MAKO.
+
+    /* Dashboard */
+    // Select New Submission on the Dashboard page. 
+    // Select State Plan Amendment (SPA) on the Submission Type page. 
+    // Select CHIP SPA on the SPA Type page. 
+    // Select All Other CHIP SPA Submissions for CHIP SPA Type. 
+    // This should take the user to the CHIP SPA Details page. 
+
+    /* CHIP SPA Details */
+
+
+  });
 });
--- test/e2e/tests/home/medicaidspa.spec.ts
+++ test/e2e/tests/home/medicaidspa.spec.ts
@@ -3,5 +3,19 @@ import { test } from "@playwright/test";
 test.describe.skip("Form Submission", async () => {
   // comment this out until we need it
   // test.beforeAll();
-  test("Create and submit a Medicaid SPA", () => {});
+  test("Create and submit a Medicaid SPA", () => {
+    /* Login */
+    // Navigate to the OneMAC Home Page using the home.page.ts file. 
+    // Login to MAKO as a State User, using the loginPage.ts script to navigate to the Login prompt. 
+    // Refer to the users.ts file to view the available users for MAKO.
+
+    /* Dashboard */
+    // Select New Submission on the Dashboard page. 
+    // Select State Plan Amendment (SPA) on the Submission Type page. 
+    // Select Medicaid SPA on the SPA Type page. 
+    // Select All Other Medicaid SPA Submissions for Medicaid SPA Type. 
+    // This should take the user to the Medicaid SPA Details page. 
+
+    /* Medicaid SPA Details */
+  });
 });
