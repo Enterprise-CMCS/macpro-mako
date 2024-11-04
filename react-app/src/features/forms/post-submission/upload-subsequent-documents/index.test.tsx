7d302f7d2909cef4c344441d8c6fe60a028fa8a4
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-11-04 10:08:40 -0500

    feat: write test of `upload-subsequent-documentation`

---
+++ react-app/src/features/forms/post-submission/upload-subsequent-documents/index.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { describe, test, expect, beforeAll } from "vitest";
+import { formSchemas } from "@/formSchemas";
+import { uploadFiles } from "@/utils/test-helpers/uploadFiles";
+import {
+  mockApiRefinements,
+  skipCleanup,
+} from "@/utils/test-helpers/skipCleanup";
+import { UploadSubsequentDocuments } from ".";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
+const upload = uploadFiles<(typeof formSchemas)["new-medicaid-submission"]>();
+
+describe("Upload Subsequent Documents (for Medicaid SPA)", () => {
+  beforeAll(() => {
+    skipCleanup();
+    mockApiRefinements();
+
+    render(<UploadSubsequentDocuments />, {
+      wrapper: ({ children }) => (
+        <MemoryRouter initialEntries={["/test/12345/Medicaid SPA"]}>
+          <Routes>
+            <Route path="/test/:id/:authority" element={children} />
+          </Routes>
+        </MemoryRouter>
+      ),
+    });
+  });
+
+  test("CMS FORM 179", async () => {
+    const currentStatePlanLabel = await upload("cmsForm179");
+    expect(currentStatePlanLabel).not.toHaveClass("text-destructive");
+  });
+
+  test("ADDITIONAL INFORMATION", async () => {
+    const additionalInfoInput = screen.getByLabelText(
+      /Explain why additional documents are being submitted/,
+    );
+    const additionalInfoLabel = screen.getByTestId("addl-info-label");
+
+    await userEvent.type(
+      additionalInfoInput,
+      "this is additional information in the test",
+    );
+
+    expect(additionalInfoLabel).not.toHaveClass("text-destructive");
+  });
+
+  test("submit button is enabled", async () => {
+    waitFor(() =>
+      expect(screen.getByTestId("submit-action-form")).toBeEnabled(),
+    );
+  });
+});
