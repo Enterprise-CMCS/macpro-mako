3a20f5e95ba35bf8b96e4b6258855dd3e56af2c0
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-11-06 15:30:29 -0500

    remove schema

--- lib/packages/shared-types/events/withdraw-package.ts
+++ lib/packages/shared-types/events/withdraw-package.ts
@@ -1,37 +1,8 @@
 import { z } from "zod";
 import {
-  attachmentSchema,
   attachmentArraySchemaOptional,
   attachmentArraySchema,
 } from "../attachments";
-
-// Temporary, will be refactored to an extendable schema with Brian/Mike's back-end
-// work.
-export const withdrawPackageSchema = z.object({
-  id: z.string(),
-  authority: z.string(),
-  origin: z.string(),
-  additionalInformation: z.preprocess(
-    (value) => (typeof value === "string" ? value.trimStart() : value),
-    z
-      .string()
-      .max(4000)
-      .refine((value) => value !== "", {
-        message: "Additional Information is required.",
-      })
-      .refine((value) => value.trim().length > 0, {
-        message: "Additional Information can not be only whitespace.",
-      }),
-  ),
-  // .optional(),
-  attachments: z.array(attachmentSchema).nullish(),
-  submitterName: z.string(),
-  submitterEmail: z.string(),
-  timestamp: z.number().optional(),
-  submissionDate: z.number().optional(),
-});
-
-export type WithdrawPackage = z.infer<typeof withdrawPackageSchema>;
 
 export const attachmentsDefault = z.object({
   supportingDocumentation: z.object({
@@ -60,5 +31,3 @@ export const schema = baseSchema.extend({
   submitterEmail: z.string().email(),
   timestamp: z.number(),
 });
-
-// export type RaiWithdraw = z.infer<typeof baseSchema>;
