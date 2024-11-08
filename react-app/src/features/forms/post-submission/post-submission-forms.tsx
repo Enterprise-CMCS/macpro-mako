ee0e19f6073bbec5953ff9e41229f7465aa33e33
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-11-08 11:36:59 -0500

    feat: add temp-ext form to route; add some undefined guarding and types

--- react-app/src/features/forms/post-submission/post-submission-forms.tsx
+++ react-app/src/features/forms/post-submission/post-submission-forms.tsx
@@ -1,62 +1,62 @@
-import { LoaderFunction, useParams } from "react-router-dom";
+import { LoaderFunction, Navigate, useParams } from "react-router-dom";
+import { Action, AuthorityUnion } from "shared-types";
 import {
   WithdrawPackageAction,
   WithdrawPackageActionChip,
   WithdrawPackageActionWaiver,
 } from "./withdraw-package";
-import {
-  RespondToRaiChip,
-  RespondToRaiMedicaid,
-  RespondToRaiWaiver,
-} from "./respond-to-rai";
+import { RespondToRaiChip, RespondToRaiMedicaid, RespondToRaiWaiver } from "./respond-to-rai";
 import { queryClient } from "../../../router";
 import { getItem } from "@/api";
 import { WithdrawRaiForm } from "./withdraw-rai";
-import {
-  DisableWithdrawRaiForm,
-  EnableWithdrawRaiForm,
-} from "./toggle-withdraw-rai";
+import { DisableWithdrawRaiForm, EnableWithdrawRaiForm } from "./toggle-withdraw-rai";
+import { TemporaryExtensionForm } from "../waiver/temporary-extension";
 
-// the keys will relate to this part of the route /actions/{key of postSubmissionForms}/authority/id
-export const postSubmissionForms: Record<
-  string,
-  Record<string, () => React.ReactNode>
+export const postSubmissionForms: Partial<
+  Record<Action, Partial<Record<AuthorityUnion, () => React.ReactNode>>>
 > = {
   "withdraw-package": {
-    ["1915(b)"]: WithdrawPackageActionWaiver,
-    ["1915(c)"]: WithdrawPackageActionWaiver,
-    ["Medicaid SPA"]: WithdrawPackageAction,
-    ["CHIP SPA"]: WithdrawPackageActionChip,
+    "1915(b)": WithdrawPackageActionWaiver,
+    "1915(c)": WithdrawPackageActionWaiver,
+    "Medicaid SPA": WithdrawPackageAction,
+    "CHIP SPA": WithdrawPackageActionChip,
   },
   "respond-to-rai": {
-    ["1915(b)"]: RespondToRaiWaiver,
-    ["1915(c)"]: RespondToRaiWaiver,
-    ["Medicaid SPA"]: RespondToRaiMedicaid,
-    ["CHIP SPA"]: RespondToRaiChip,
+    "1915(b)": RespondToRaiWaiver,
+    "1915(c)": RespondToRaiWaiver,
+    "Medicaid SPA": RespondToRaiMedicaid,
+    "CHIP SPA": RespondToRaiChip,
   },
   "withdraw-rai": {
-    ["1915(b)"]: WithdrawRaiForm,
-    ["1915(c)"]: WithdrawRaiForm,
-    ["Medicaid SPA"]: WithdrawRaiForm,
-    ["CHIP SPA"]: WithdrawRaiForm,
+    "1915(b)": WithdrawRaiForm,
+    "1915(c)": WithdrawRaiForm,
+    "Medicaid SPA": WithdrawRaiForm,
+    "CHIP SPA": WithdrawRaiForm,
   },
   "enable-rai-withdraw": {
-    ["1915(b)"]: EnableWithdrawRaiForm,
-    ["1915(c)"]: EnableWithdrawRaiForm,
-    ["Medicaid SPA"]: EnableWithdrawRaiForm,
-    ["CHIP SPA"]: EnableWithdrawRaiForm,
+    "1915(b)": EnableWithdrawRaiForm,
+    "1915(c)": EnableWithdrawRaiForm,
+    "Medicaid SPA": EnableWithdrawRaiForm,
+    "CHIP SPA": EnableWithdrawRaiForm,
   },
   "disable-rai-withdraw": {
-    ["1915(b)"]: DisableWithdrawRaiForm,
-    ["1915(c)"]: DisableWithdrawRaiForm,
-    ["Medicaid SPA"]: DisableWithdrawRaiForm,
-    ["CHIP SPA"]: DisableWithdrawRaiForm,
+    "1915(b)": DisableWithdrawRaiForm,
+    "1915(c)": DisableWithdrawRaiForm,
+    "Medicaid SPA": DisableWithdrawRaiForm,
+    "CHIP SPA": DisableWithdrawRaiForm,
+  },
+  "temporary-extension": {
+    "1915(b)": TemporaryExtensionForm,
   },
 };
 
 export const PostSubmissionWrapper = () => {
-  const { type, authority } = useParams();
-  const PostSubmissionForm = postSubmissionForms[type][authority];
+  const { type, authority } = useParams<{ authority: AuthorityUnion; type: string }>();
+  const PostSubmissionForm = postSubmissionForms?.[type]?.[authority];
+
+  if (PostSubmissionForm === undefined) {
+    return <Navigate to="/" />;
+  }
 
   return <PostSubmissionForm />;
 };
