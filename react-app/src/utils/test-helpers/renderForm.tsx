31074d013fe58bdf27d6fca2df96b5f208831f7e
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-11-13 11:20:28 -0500

    chore: fix tests for temp-ext

--- react-app/src/utils/test-helpers/renderForm.tsx
+++ react-app/src/utils/test-helpers/renderForm.tsx
@@ -1,19 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import { ReactElement } from "react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 export const renderForm = (form: ReactElement) =>
   render(form, {
-    wrapper: ({ children }) => <MemoryRouter>{children}</MemoryRouter>,
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={new QueryClient()}>
+        <MemoryRouter>{children}</MemoryRouter>,
+      </QueryClientProvider>
+    ),
   });
 
 export const renderFormWithPackageSection = (form: ReactElement) =>
   render(form, {
     wrapper: ({ children }) => (
-      <MemoryRouter initialEntries={["/test/12345/Medicaid SPA"]}>
-        <Routes>
-          <Route path="/test/:id/:authority" element={children} />
-        </Routes>
-      </MemoryRouter>
+      <QueryClientProvider client={new QueryClient()}>
+        <MemoryRouter initialEntries={["/test/12345/Medicaid SPA"]}>
+          <Routes>
+            <Route path="/test/:id/:authority" element={children} />
+          </Routes>
+        </MemoryRouter>
+      </QueryClientProvider>
     ),
   });
