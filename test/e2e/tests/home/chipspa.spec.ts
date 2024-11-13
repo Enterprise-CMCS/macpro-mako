32c484f6b151809a03bf5629b0104f471b092772
ndebrah-fearless <ndebrah@fearless.tech>
2024-11-13 13:22:03 -0500

    Added login check to the CHIP and Medicaid scripts

--- test/e2e/tests/home/chipspa.spec.ts
+++ test/e2e/tests/home/chipspa.spec.ts
@@ -1,10 +1,22 @@
 import { test } from "@playwright/test";
+import { HomePage, LoginPage } from "pages";
 
 test.describe.skip("Form Submission", async () => {
   // comment this out until we need it
-  test.beforeAll(async ({}) => {
-    // Call home.page.ts to go to the MACPRO Home Page 
-    
+  test.beforeAll(async ({ page }) => {
+    /* Login */
+    // Navigate to the OneMAC Home Page using the home.page.ts file. 
+    // Login to MAKO as a State User, using the loginPage.ts script to navigate to the Login prompt. 
+    // Refer to the users.ts file to view the available users for MAKO.
+
+    const homePage = new HomePage(page);
+    const loginPage = new LoginPage(page);
+    const email = "george@example.com";
+    const password = "Testing";
+
+    await homePage;
+    await loginPage.login(email, password);
+
   });
   test("Create and submit a CHIP SPA", () => {
     /* Login */
