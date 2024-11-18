86e9caebd942fdc47c41f2b5a3dade153d26787c
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-11-18 15:13:48 -0500

    feat: simplify `PackageActivity` logic

--- react-app/src/features/package/package-activity/hook.ts
+++ react-app/src/features/package/package-activity/hook.ts
@@ -1,29 +1,31 @@
-import { getAttachmentUrl, useGetItem } from "@/api";
+import { getAttachmentUrl } from "@/api";
 import JSZip from "jszip";
 import { saveAs } from "file-saver";
 import { opensearch } from "shared-types";
 import { useMutation } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
 
-type Attachments = NonNullable<opensearch.changelog.Document["attachments"]>;
+export type Attachments = NonNullable<opensearch.changelog.Document["attachments"]>;
 
 export const useAttachmentService = ({ packageId }: { packageId: string }) => {
-  const { mutateAsync, error, isLoading } = useMutation(
-    (att: Attachments[number]) =>
-      getAttachmentUrl(packageId, att.bucket, att.key, att.filename),
+  const { mutateAsync, error, isLoading } = useMutation((attachment: Attachments[number]) =>
+    getAttachmentUrl(packageId, attachment.bucket, attachment.key, attachment.filename),
   );
 
   const onZip = (attachments: Attachments) => {
+    if (attachments.length === 0) {
+      return;
+    }
+
     const zip = new JSZip();
 
-    const remoteZips = attachments.map(async (ATT, index) => {
-      const url = await mutateAsync(ATT);
+    const remoteZips = attachments.map(async (attachment, index) => {
+      const url = await mutateAsync(attachment);
       if (!url) return;
       const data = await fetch(url).then((res) => res.blob());
 
       // append index for uniqueness (fileone.md -> fileone(1).md)
       const filename = (() => {
-        const pieces = ATT.filename.split(".");
+        const pieces = attachment.filename.split(".");
         const ext = pieces.pop();
         return `${pieces.join(".")}(${index + 1}).${ext}`;
       })();
@@ -44,32 +46,4 @@ export const useAttachmentService = ({ packageId }: { packageId: string }) => {
   };
 
   return { loading: isLoading, error, onUrl: mutateAsync, onZip };
-};
-
-export const usePackageActivities = () => {
-  const { id } = useParams();
-  const { data: submission } = useGetItem(id);
-
-  const service = useAttachmentService({ packageId: submission._id });
-
-  const onDownloadAll = () => {
-    const attachmentsAggregate = submission._source.changelog?.reduce(
-      (ACC, ATT) => {
-        if (!ATT._source.attachments) return ACC;
-        return ACC.concat(ATT._source.attachments);
-      },
-      [] as any,
-    );
-
-    if (attachmentsAggregate.length === 0) return;
-
-    service.onZip(attachmentsAggregate);
-  };
-
-  return {
-    data: submission._source.changelog,
-    accordianDefault: [submission._source.changelog?.[0]?._source?.id],
-    onDownloadAll,
-    ...service,
-  };
 };
