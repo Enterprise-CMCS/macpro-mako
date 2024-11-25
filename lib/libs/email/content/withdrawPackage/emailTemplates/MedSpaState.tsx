4bda929e83b52489e9f0760b9c97dd3e88af3e58
Asharon Baltazar <asharonbaltazar@outlook.com>
2024-11-25 15:09:35 -0500

    feat: create Medicaid SPA State Withdraw Request email & Preview

--- lib/libs/email/content/withdrawPackage/emailTemplates/MedSpaState.tsx
+++ lib/libs/email/content/withdrawPackage/emailTemplates/MedSpaState.tsx
@@ -1,22 +1,37 @@
-import { CommonEmailVariables } from "shared-types";
-import { WithdrawPackage } from "shared-types";
-import { Container, Html } from "@react-email/components";
-import { ContactStateLead } from "../../email-components";
+import { CommonEmailVariables, Events } from "shared-types";
+import { Link, Section, Text } from "@react-email/components";
+import { BasicFooter, EMAIL_CONFIG, PackageDetails } from "../../email-components";
+import { BaseEmailTemplate } from "../../email-templates";
 
-export const MedSpaStateEmail = (props: { variables: WithdrawPackage & CommonEmailVariables }) => {
-  const variables = props.variables;
-  return (
-    <Html
-      lang="en"
-      dir="ltr"
-    >
-      <Container>
-        <h3>
-          This email is to confirm Medicaid SPA {variables.id} was withdrawn by
-          {variables.submitterName}. The review of Medicaid SPA {variables.id} has concluded.
-        </h3>
-        <ContactStateLead />
-      </Container>
-    </Html>
-  );
-};
+export const MedSpaStateEmail = ({
+  variables,
+}: {
+  variables: Events["WithdrawPackage"] & CommonEmailVariables;
+}) => (
+  <BaseEmailTemplate
+    previewText={`SPA Package ${variables.id} Withdraw Request`}
+    heading="This is confirmation that you have requested to withdraw the package below. The package will no longer be considered for CMS review:"
+    applicationEndpointUrl={variables.applicationEndpointUrl}
+    footerContent={<BasicFooter />}
+  >
+    <PackageDetails
+      details={{
+        "State or territory": variables.territory,
+        Name: variables.submitterName,
+        Email: variables.submitterEmail,
+        "Medicaid SPA Package ID": variables.id,
+        Summary: variables.additionalInformation,
+      }}
+    />
+    <Section>
+      <Text style={{ marginTop: "8px", fontSize: "14px" }}>
+        If you have any questions or did not expect this email, please contact{" "}
+        <Link href={`mailto:${EMAIL_CONFIG.SPA_EMAIL}`} style={{ textDecoration: "underline" }}>
+          {EMAIL_CONFIG.SPA_EMAIL}
+        </Link>{" "}
+        or your state lead.
+      </Text>
+      <Text>Thank you.</Text>
+    </Section>
+  </BaseEmailTemplate>
+);
