b014d32edac618dad38af0a5486b59ed8a695b7e
James Dinh <jdinh@collabralink.com>
2024-11-25 09:54:39 -0800

    remove unused

--- lib/libs/email/preview/Withdraw Rai/CMS/CHIP_SPA.tsx
+++ lib/libs/email/preview/Withdraw Rai/CMS/CHIP_SPA.tsx
@@ -1,7 +1,6 @@
 import { ChipSpaCMSEmail } from "lib/libs/email/content/upload-subsequent-documents/emailTemplates";
 import { emailTemplateValue } from "lib/libs/email/mock-data/upload-subsequent-documents";
-import { relatedEvent } from "./AppK";
 
 export default () => {
-  return <ChipSpaCMSEmail relatedEvent={relatedEvent} variables={emailTemplateValue} />;
+  return <ChipSpaCMSEmail variables={emailTemplateValue} />;
 };
--- lib/libs/email/preview/Withdraw Rai/State/CHIP_SPA.tsx
+++ lib/libs/email/preview/Withdraw Rai/State/CHIP_SPA.tsx
@@ -1,7 +1,6 @@
 import { ChipSpaStateEmail } from "lib/libs/email/content/upload-subsequent-documents/emailTemplates";
 import { emailTemplateValue } from "lib/libs/email/mock-data/upload-subsequent-documents";
-import { relatedEvent } from "../CMS/AppK";
 
 export default () => {
-  return <ChipSpaStateEmail relatedEvent={relatedEvent} variables={emailTemplateValue} />;
+  return <ChipSpaStateEmail variables={emailTemplateValue} />;
 };
