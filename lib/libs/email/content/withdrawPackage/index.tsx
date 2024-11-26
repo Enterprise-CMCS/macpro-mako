4538c73faa40417f4e9748c79b1d946144977804
Andie Swift <andie.swift5@gmail.com>
2024-11-26 11:37:58 -0600

    verified all subject lines and added waivers where needed for index.tsx files

--- lib/libs/email/content/withdrawPackage/index.tsx
+++ lib/libs/email/content/withdrawPackage/index.tsx
@@ -1,10 +1,12 @@
-import { Authority, CommonEmailVariables, EmailAddresses } from "shared-types";
+import { Events, Authority, CommonEmailVariables, EmailAddresses } from "shared-types";
 import { AuthoritiesWithUserTypesTemplate } from "../..";
 import {
   MedSpaCMSEmail,
   MedSpaStateEmail,
   ChipSpaCMSEmail,
   ChipSpaStateEmail,
+  WaiverCMSEmail,
+  WaiverStateEmail,
 } from "./emailTemplates";
 import { render } from "@react-email/render";
 
@@ -45,6 +47,55 @@ export const withdrawPackage: AuthoritiesWithUserTypesTemplate = {
         cc: variables.emails.chipCcList,
         subject: `CHIP SPA Package ${variables.id} Withdrawal Confirmation`,
         body: await render(<ChipSpaStateEmail variables={variables} />),
+      };
+    },
+  },
+  [Authority["1915b"]]: {
+    cms: async (
+      variables:
+        | (Events["CapitatedInitial"] & CommonEmailVariables & { emails: EmailAddresses })
+        | (Events["ContractingInitial"] & CommonEmailVariables & { emails: EmailAddresses }),
+    ) => {
+      return {
+        to: variables.emails.osgEmail,
+        subject: `Waiver Package ${variables.id} Withdraw Request`,
+        body: await render(<WaiverCMSEmail variables={variables} />),
+      };
+    },
+    state: async (
+      variables:
+        | (Events["CapitatedInitial"] & CommonEmailVariables & { emails: EmailAddresses })
+        | (Events["ContractingInitial"] & CommonEmailVariables & { emails: EmailAddresses }),
+    ) => {
+      return {
+        to: [`${variables.submitterName} <${variables.submitterEmail}>`], // TODO: change to ALL state users
+        subject: `Waiver Package ${variables.id} Withdraw Request`,
+        body: await render(<WaiverStateEmail variables={variables} />),
+      };
+    },
+  },
+
+  [Authority["1915c"]]: {
+    cms: async (
+      variables: Events["NewAppKSubmission"] & CommonEmailVariables & { emails: EmailAddresses },
+    ) => {
+      return {
+        to: [
+          ...variables.emails.osgEmail,
+          ...variables.emails.cpocEmail,
+          ...variables.emails.srtEmails,
+        ],
+        subject: `Waiver Package ${variables.id} Withdraw Request`,
+        body: await render(<WaiverCMSEmail variables={variables} />),
+      };
+    },
+    state: async (
+      variables: Events["NewAppKSubmission"] & CommonEmailVariables & { emails: EmailAddresses },
+    ) => {
+      return {
+        to: [`${variables.submitterName} <${variables.submitterEmail}>`], // TODO: change to ALL state users
+        subject: `Waiver Package ${variables.id} Withdraw Request`,
+        body: await render(<WaiverStateEmail variables={variables} />),
       };
     },
   },
