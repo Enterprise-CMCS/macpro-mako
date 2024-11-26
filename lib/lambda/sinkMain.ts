4791235153111e369b147dde8e3dcd7406dd4611
tiffanyvu <tiffany.v330@gmail.com>
2024-11-26 10:06:22 -0800

    temp bypass record event

--- lib/lambda/sinkMain.ts
+++ lib/lambda/sinkMain.ts
@@ -87,11 +87,9 @@ const processAndIndex = async ({
       // If we're not a mako event, continue
       // TODO:  handle legacy.  for now, just continue
       console.log(record, "RECORD?????");
-      if (!record.event || record?.origin !== "mako") {
-        console.log("IN HERE???", record.event, "RECORD EVENT");
-        console.log(record.origin, "RECORD ORIGIN");
-        continue;
-      }
+      // if (!record.event || record?.origin !== "mako") {
+      //   continue;
+      // }
       console.log("AFTER THIS IF");
 
       // TODO: revisit
