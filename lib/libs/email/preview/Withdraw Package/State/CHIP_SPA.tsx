8872d8353c6099fc272c1d64086585597a3b4de6
James Dinh <jdinh@collabralink.com>
2024-12-02 08:11:04 -0800

    Delete CHIP_SPA.tsx

--- lib/libs/email/preview/Withdraw Package/State/CHIP_SPA.tsx
+++
@@ -1,6 +0,0 @@
-import { ChipSpaStateEmail } from "lib/libs/email/content/widthdrawPackage/emailTemplates";
-import { emailTemplateValue } from "lib/libs/email/mock-data/new-submission";
-
-export default () => {
-  return <ChipSpaStateEmail variables={emailTemplateValue} />;
-};
