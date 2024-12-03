b3ab659836449fb5a71b0b5d8b604af2048f081f
Gavin St. Ours <gst-ours@fearless.tech>
2024-12-03 09:51:33 -0500

    Open correct FAQ section

--- react-app/src/features/welcome/index.tsx
+++ react-app/src/features/welcome/index.tsx
@@ -58,7 +58,7 @@ export const Welcome = () => {
                 Medicaid Alternative Benefit Plan, Premium and Cost Sharing, and CHIP Eligibility
                 SPA templates and implementation guides are now available in OneMAC. New submissions
                 for these SPA types are submitted through the OneMAC system effective [add date].{" "}
-                <Link to="/faq" className="underline text-[#0071bc]">
+                <Link to="/faq/abp-spa-templates" className="underline text-[#0071bc]">
                   Learn more
                 </Link>
               </p>
