ff56d57020228bd4c9473dcf46e5925c744878ce
Benjamin Paige <benjaminpaige@users.noreply.github.com>
2024-12-04 12:03:25 -0700

    Update

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,7 +15,20 @@ export default defineConfig({
       reportsDirectory: join(__dirname, "coverage"),
       reporter: ["html", "text", "json-summary", "json", "lcovonly"],
       reportOnFailure: true,
-      exclude: [...configDefaults.exclude],
+      exclude: [
+        ...configDefaults.exclude,
+        "./lib/config/deployment-config.test.ts",
+        "./react-app/src/hooks/useDebounce.test.ts",
+        "./react-app/src/hooks/useMediaQuery.test.ts",
+        "./react-app/src/utils/buildStatusMemoQuery.test.ts",
+        "./react-app/src/components/HowItWorks/index.test.tsx",
+        "./react-app/src/components/SimplePageContainer/index.test.tsx",
+        "./react-app/src/components/Inputs/upload.utilities.test.ts",
+        "./react-app/src/components/Banner/index.test.tsx",
+        "./react-app/src/hooks/useIdle.test.ts",
+        "./react-app/src/hooks/useCountdown.test.ts",
+        "./react-app/src/components/DependencyWrapper/index.test.tsx"
+      ],
     },
     environment: 'jsdom',
     deps: {
