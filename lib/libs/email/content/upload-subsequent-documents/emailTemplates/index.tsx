1593129a9ee87838b657c7cd062fbde5e23a7190
James Dinh <jdinh@collabralink.com>
2024-12-06 12:12:06 -0800

    add all logic for upload subsequent documents

--- lib/libs/email/content/upload-subsequent-documents/emailTemplates/index.tsx
+++ lib/libs/email/content/upload-subsequent-documents/emailTemplates/index.tsx
@@ -1,2 +1,4 @@
 export { ChipSpaCMSEmail } from "./ChipSpaCMS";
 export { ChipSpaStateEmail } from "./ChipSpaState";
+export { MedSpaCMSEmail } from "./MedSpaCMS";
+export { MedSpaStateEmail } from "./MedSpaState";
