319e3c3f0e4edc88ab03d196f160001f1b4d5eed
Benjamin Paige <benjaminpaige@users.noreply.github.com>
2024-12-12 11:02:44 -0700

    remove reindexConfig

--- lib/lambda/runReindex.ts
+++ lib/lambda/runReindex.ts
@@ -14,20 +14,6 @@ export const handler: Handler = async (event, context) => {
         input: JSON.stringify({
           cfnEvent: event,
           cfnContext: context,
-          reindexConfig: {
-            script: {
-              source: `
-                // Ensure consistent types
-                ctx._source.id = String.valueOf(ctx._source.id);
-                ctx._source.changedDate = ctx._source.changedDate != null ? 
-                  new Date(ctx._source.changedDate).getTime() : null;
-                ctx._source.statusDate = ctx._source.statusDate != null ? 
-                  ZonedDateTime.parse(ctx._source.statusDate).toString() : null;
-                ctx._source.submissionDate = ctx._source.submissionDate != null ? 
-                  ZonedDateTime.parse(ctx._source.submissionDate).toString() : null;
-              `
-            }
-          }
         }),
       });
 
