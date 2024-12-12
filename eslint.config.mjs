00bf7433a05db924cd45679bcf31593bb58d178e
Ty Bolt <tybolt@gmail.com>
2024-12-12 11:17:56 -0500

    adds eslint <-> prettier integration, making eslint flag formatting issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,66 @@
 // @ts-check
+import { fixupPluginRules, includeIgnoreFile } from "@eslint/compat";
+import eslint from "@eslint/js";
 import react from "eslint-plugin-react";
 import eslintReactHooks from "eslint-plugin-react-hooks";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import { fixupPluginRules, includeIgnoreFile } from "@eslint/compat";
-import { fileURLToPath } from "url";
 import path from "path";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, includeIgnoreFile(gitignorePath), {
-  plugins: {
-    // @ts-expect-error Types mismatch for eslint-plugin-react
-    react,
-    // @ts-expect-error https://github.com/facebook/react/pull/28773#issuecomment-2147149016
-    "react-hooks": fixupPluginRules(eslintReactHooks),
-  },
-  languageOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  includeIgnoreFile(gitignorePath),
+  eslintConfigPrettier,
+  {
+    plugins: {
+      // @ts-expect-error Types mismatch for eslint-plugin-react
+      react,
+      // @ts-expect-error https://github.com/facebook/react/pull/28773#issuecomment-2147149016
+      "react-hooks": fixupPluginRules(eslintReactHooks),
+      prettier,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
 
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
-  },
 
-  settings: {
-    react: {
-      version: "detect",
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/ban-ts-comment": [
+        "off",
+        {
+          "ts-ignore": false,
+          "ts-nocheck": false,
+          "ts-check": true,
+          "ts-expect-error": true,
+        },
+      ],
     },
   },
-
-  rules: {
-    "@typescript-eslint/no-empty-interface": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/ban-ts-comment": [
-      "off",
-      {
-        "ts-ignore": false,
-        "ts-nocheck": false,
-        "ts-check": true,
-        "ts-expect-error": true,
-      },
-    ],
-  },
-});
+);
