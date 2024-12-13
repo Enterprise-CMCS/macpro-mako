a7c325a5650416ded4a9629ae28fbb97dcd901c6
ranyem <rmclendon@fearless.tech>
2024-12-13 11:41:27 -0500

    fix: updated changes and removed svg

--- react-app/src/components/Banner/MMDLSpaBanner.tsx
+++ react-app/src/components/Banner/MMDLSpaBanner.tsx
@@ -1,54 +1,41 @@
 import { useState } from "react";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import { InfoCircledIcon } from "@radix-ui/react-icons";
-
 const MMDLAlertBanner = () => {
   const [isVisible, setIsVisible] = useState(true);
-
   const handleDismiss = () => {
     setIsVisible(false);
   };
-
   if (!isVisible) return null;
-
   return (
-    <div className="bg-[#E1F3F8] w-full flex flex-col md:flex-row relative box-border h-auto md:h-[180px] lg:h-[98px] p-4 md:p-3 gap-4">
-      <div className="bg-[#00A6D2] w-[8px] h-full absolute left-0 top-0"></div>
-      <div className="flex items-center flex-grow mt-2">
-        <InfoCircledIcon
-          className="object-contain w-[38px] h-[38px]  mx-4 mb-14"
-          aria-hidden="true"
-        />
+    <div className="bg-[#E1F3F8] grid md:grid-cols-[min-content_auto_min-content] md:grid-rows-[auto_auto] grid-cols-[auto_auto] gap-4 md:gap-x-4 border-l-[8px] border-[#00A6D2] p-3">
+      <InfoCircledIcon className="w-10 h-10" aria-hidden="true" />
+      <div className="flex gap-x-4 flex-grow">
         <div className="flex flex-col flex-grow">
-          <h3 className="font-bold text-black text-[18px] break-words pr-4">
+          <h3 className="font-bold text-black text-lg break-words">
             MMDL SPA forms available in OneMAC
           </h3>
-          <p className="text-black leading-normal text-[16px] pr-4 break-words mb-2">
+          <p className="text-black leading-normal break-words">
             Medicaid Alternative Benefit Plan, Premium and Cost Sharing, and CHIP Eligibility SPA
             templates and implementation guides are now available in OneMAC. New submissions for
             these SPA types are submitted through the OneMAC system effective [add date].
           </p>
         </div>
       </div>
-      <div className="flex space-x-4">
+      <div className="flex space-x-4 col-start-2 md:col-start-auto">
         <a
           href="/faq#mmdl-section"
           target="_blank"
           rel="noopener noreferrer"
-          className="border-2 border-black rounded h-[38px] px-4 text-[16px] font-open-sans font-bold text-center decoration-transparent [text-decoration-skip-ink:none] whitespace-nowrap flex items-center justify-center"
+          className="border-2 border-black rounded h-[38px] px-4 text font-bold text-center whitespace-nowrap"
         >
           Go to FAQs
         </a>
-        <button
-          onClick={handleDismiss}
-          aria-label="Dismiss"
-          className="rounded-full w-[24px] h-[24px] flex justify-center items-center"
-        >
-          <Cross2Icon className="object-contain w-full h-full" aria-hidden="true" />
+        <button onClick={handleDismiss} aria-label="Dismiss" className="rounded-full w-6 h-6">
+          <Cross2Icon className="w-full h-full" aria-hidden="true" />
         </button>
       </div>
     </div>
   );
 };
-
 export default MMDLAlertBanner;
