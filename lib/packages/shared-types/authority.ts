483844067de6bafa31d0a8802a022bfe29a3327e
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-12-16 13:31:40 -0500

    Revert "fix authority?"
    This reverts commit 18800d17d388eca895648f76d7310ac9c7848d5e.

--- lib/packages/shared-types/authority.ts
+++ lib/packages/shared-types/authority.ts
@@ -1,9 +1,13 @@
 // TODO: Refactor out
 export enum Authority {
-  MED_SPA = "Medicaid SPA",
-  CHIP_SPA = "CHIP SPA",
+  MED_SPA = "medicaid spa",
+  CHIP_SPA = "chip spa",
   "1915b" = "1915(b)",
   "1915c" = "1915(c)",
 }
 
-export type AuthorityUnion = "Medicaid SPA" | "CHIP SPA" | "1915(b)" | "1915(c)";
+export type AuthorityUnion =
+  | "Medicaid SPA"
+  | "CHIP SPA"
+  | "1915(b)"
+  | "1915(c)";
