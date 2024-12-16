18800d17d388eca895648f76d7310ac9c7848d5e
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-12-16 13:26:58 -0500

    fix authority?

--- lib/packages/shared-types/authority.ts
+++ lib/packages/shared-types/authority.ts
@@ -1,13 +1,9 @@
 // TODO: Refactor out
 export enum Authority {
-  MED_SPA = "medicaid spa",
-  CHIP_SPA = "chip spa",
+  MED_SPA = "Medicaid SPA",
+  CHIP_SPA = "CHIP SPA",
   "1915b" = "1915(b)",
   "1915c" = "1915(c)",
 }
 
-export type AuthorityUnion =
-  | "Medicaid SPA"
-  | "CHIP SPA"
-  | "1915(b)"
-  | "1915(c)";
+export type AuthorityUnion = "Medicaid SPA" | "CHIP SPA" | "1915(b)" | "1915(c)";
