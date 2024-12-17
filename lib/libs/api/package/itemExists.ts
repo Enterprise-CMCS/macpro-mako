6beed07986b557919bc221f18f974f3cacf4b3eb
Tiffany Forkner <tforkner@fearless.tech>
2024-12-17 13:01:38 -0500

    catching 404 in getItem and throwing the rest of the errors

--- lib/libs/api/package/itemExists.ts
+++ lib/libs/api/package/itemExists.ts
@@ -1,4 +1,3 @@
-import { errors as OpensearchErrors } from "@opensearch-project/opensearch";
 import * as os from "../../../libs/opensearch-lib";
 
 export async function itemExists(params: {
@@ -6,20 +5,10 @@ export async function itemExists(params: {
   osDomain?: string;
   indexNamespace?: string;
 }): Promise<boolean> {
-  try {
-    const packageResult = await os.getItem(
-      params.osDomain || process.env.osDomain!,
-      `${params.indexNamespace || process.env.indexNamespace!}main`,
-      params.id,
-    );
-    return !!packageResult?._source;
-  } catch (error) {
-    if (error instanceof OpensearchErrors.ResponseError && error.statusCode === 404 || error.meta?.statusCode === 404) {
-      console.log("Error (404) retrieving in OpenSearch:", error);
-      return false;
-    }
-
-    console.error("Error checking item existence in OpenSearch:", error);
-    throw error;
-  }
+  const packageResult = await os.getItem(
+    params.osDomain || process.env.osDomain!,
+    `${params.indexNamespace || process.env.indexNamespace!}main`,
+    params.id,
+  );
+  return !!packageResult?._source;
 }
