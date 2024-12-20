296a670f939c401824cd7d01e87530f353067d99
Benjamin Paige <benjaminpaige@users.noreply.github.com>
2024-12-20 15:37:28 -0700

    Update

--- lib/libs/api/package/getPackage.ts
+++ lib/libs/api/package/getPackage.ts
@@ -1,5 +1,5 @@
 import { ItemResult } from "lib/packages/shared-types/opensearch/main";
-import * as os from "libs/opensearch-lib";
+import { getItem } from "libs/opensearch-lib";
 
 export interface ExtendedItemResult extends ItemResult {
   appkChildren?: any[];
@@ -8,11 +8,11 @@ export const getPackage = async (id: string): Promise<ItemResult | undefined> =>
   if (!process.env.osDomain) {
     throw new Error("process.env.osDomain must be defined");
   }
-  const packageResult = await os.getItem(
+  const packageResult = await getItem(
     process.env.osDomain,
     `${process.env.indexNamespace}main`,
     id,
   );
 
-  return packageResult;
+  return packageResult as any;
 };
