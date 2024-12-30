da9988ab03420c47021bf4085b735ad036ad4695
ndebrah-fearless <ndebrah@fearless.tech>
2024-12-30 14:48:49 -0500

    Removed unused variables and updated branch

--- test/e2e/tests/home/appendixKamendment.spec.ts
+++ test/e2e/tests/home/appendixKamendment.spec.ts
@@ -1,14 +1,10 @@
 import { test } from "@playwright/test";
-import { HomePage, LoginPage } from "pages";
+//import { HomePage, LoginPage } from "pages";
 
 test.describe.skip("Form Submission", async () => {
   // comment this out until we need it
 
-  test.beforeAll(async ({ page }) => {
-    /* Login */
-    // Navigate to the OneMAC Home Page using the home.page.ts file. 
-    // Login to MAKO as a State User, using the loginPage.ts script to navigate to the Login prompt. 
-    // Refer to the users.ts file to view the available users for MAKO.
+  test.beforeAll(async () => {
 
   });
   test("Create and submit an Appendix K Amendment Waiver", () => { });
