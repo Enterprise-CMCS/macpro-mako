eb9e633fcbeda9ac0f7e3750b5972073dca3acda
Thomas Walker <thomas.e.walker88@gmail.com>
2025-01-02 11:00:33 -0500

    Updating data for snapshots (#963)

--- lib/libs/email/mock-data/new-submission.ts
+++ lib/libs/email/mock-data/new-submission.ts
@@ -1,12 +1,16 @@
 export const emailTemplateValue = {
   territory: "CO",
   applicationEndpointUrl: "https://mako-dev.cms.gov/",
-  timestamp: Date.now(),
+  get timestamp() {
+    return Date.now();
+  },
   submitterName: "George Harrison",
   submitterEmail: "george@example.com",
   additionalInformation:
     "This submission includes necessary documentation for requested updates to the state’s Medicaid plan, in alignment with CMS requirements.",
   origin: "mako" as const,
   appkParentId: null,
-  proposedEffectiveDate: Date.now() + 5184000000,
+  get proposedEffectiveDate() {
+    return Date.now() + 5184000000;
+  },
 };
