bf4a9328fdab3e6dd3c7bf116e957f0d14368881
Benjamin Paige <benjamin.paige@gmail.com>
2025-01-08 02:13:19 -0700

    Update

--- lib/libs/api/package/itemExists.ts
+++ lib/libs/api/package/itemExists.ts
@@ -1,5 +1,6 @@
 import * as os from "libs/opensearch-lib";
-import { getDomain } from "libs/utils";
+import { getDomain, getNamespace } from "libs/utils";
+import { BaseIndex } from "lib/packages/shared-types/opensearch";
 
 export async function itemExists(params: {
   id: string;
@@ -7,7 +8,9 @@ export async function itemExists(params: {
   indexNamespace?: string;
 }): Promise<boolean> {
   const domain = params.osDomain || getDomain();
-  const index = `${params.indexNamespace ?? ""}main` as `${string}main`;
+  const index: `${string}${BaseIndex}` = params.indexNamespace
+    ? `${params.indexNamespace}main`
+    : getNamespace("main");
 
   const packageResult = await os.getItem(domain, index, params.id);
   return !!packageResult?._source;
