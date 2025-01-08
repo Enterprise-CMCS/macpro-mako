c2b4132b664d2322a90f8368b7bfd5f62843e9e8
Jonathan Schaffer <jschaffer@gswell.com>
2025-01-08 18:52:16 -0500

    Fix: updated per review comments

--- test/e2e/tests/perf/index.spec.ts
+++ test/e2e/tests/perf/index.spec.ts
@@ -1,41 +1,17 @@
 import { test } from "@playwright/test";
+import * as routes from "../fixtures/routes";
 
-const staticRoutes = [
-  "/",
-  "/dashboard",
-  "/faq",
-  "/profile",
-  "/new-submission",
-  "/new-submission/spa",
-  "/new-submission/spa/medicaid",
-  "/new-submission/spa/chip",
-  "/new-submission/waiver",
-  "/new-submission/waiver/b",
-  "/new-submission/waiver/b/b4",
-  "/new-submission/waiver/b/capitated",
-  "/new-submission/spa/medicaid/landing/medicaid-eligibility",
-  "/new-submission/waiver/b/capitated/amendment/create",
-  "/new-submission/waiver/b/capitated/renewal/create",
-  "/new-submission/waiver/b/capitated/initial/create",
-  "/new-submission/waiver/b/b4/initial/create",
-  "/new-submission/waiver/b/b4/amendment/create",
-  "/new-submission/waiver/b/b4/renewal/create",
-  "/new-submission/spa/medicaid/create",
-  "/new-submission/spa/chip/create",
-  "/new-submission/waiver/app-k",
-  "/new-submission/waiver/temporary-extensions",
-];
+const STATIC_ROUTES = routes.STATIC;
 
 test.describe("test performance on static routes", { tag: ["@perf"] }, () => {
-  for (const route of staticRoutes) {
-    test(`Time to First Byte for ${route}`, async({ page }) => {
+  for (const route of STATIC_ROUTES) {
+    test(`Time to First Byte for ${route}`, { tag: ["@ttfb"] }, async({ page }) => {
       await page.goto(route);
-      // const ttfb = response.timing().responseStart - response.timing().requestStart;
       const ttfb = await page.evaluate(() => performance.timing.responseStart - performance.timing.requestStart)
       console.log(`TTFB for ${route}: ${ttfb} ms`);
     });
 
-    test(`Largest Contentful Paint for ${route}`, async ({ page }) => {
+    test(`Largest Contentful Paint for ${route}`, { tag: ["@lcp"] }, async ({ page }) => {
       await page.goto(route);
       const lcp = await page.evaluate(async () => {
         return new Promise((resolve) => {
@@ -49,7 +25,7 @@ test.describe("test performance on static routes", { tag: ["@perf"] }, () => {
       console.log(`Largest Contentful Paint for ${route} is: ${lcp.startTime} ms`);
     });
 
-    test(`First Contentful Paint for ${route}`, async ({ page }) => {
+    test(`First Contentful Paint for ${route}`, { tag: ["@fcp"] }, async ({ page }) => {
       await page.goto(route);
       const fcp = await page.evaluate(async () => {
         return new Promise((resolve) => {
