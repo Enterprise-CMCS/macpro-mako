b4c577a2c32ef067dd04c0c591fa3d8c8c9ac441
Jonathan Schaffer <jschaffer@gswell.com>
2025-01-10 13:32:37 -0500

    Feat: add dashboard spec

--- test/e2e/tests/home/filters.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "@playwright/test";
-
-test.describe.skip("Form Submission", async () => {
-  // comment this out until we need it
-  // test.beforeAll();
-  test("Verify records displayed on the Dashboard based on the filter selection by clicking the Filters button", () => {});
-});
