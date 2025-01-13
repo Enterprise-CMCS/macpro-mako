e345e330d2096494e53c440f2c8c748649586e1d
Benjamin Paige <benjamin.paige@gmail.com>
2025-01-13 14:38:48 -0700

    fix(waivers): Change to ensure waiver emails are sending correctly (#1009)
    * Adjusted for missing Waiver types and prob broke some tests.
    
    * Update
    
    * Removed unused emails and updated waiver mapping
    
    * Update ProcesEmails
    
    * get authority from item in main for respond-to-rai
    
    * update cpoc and srt getter methods
    
    * Update cpoc and srt logic
    
    * Merge main into e-wave
    
    * feat(test) additional coverage for get from (#1010)
    
    * feat(test) add test for getfrom
    
    * feat(test) additional coverage for getfrom
    
    * fix(ui): issues at `700px` (#924)
    
    ---------
    
    Co-authored-by: Thomas Walker <thomas.e.walker88@gmail.com>
    Co-authored-by: rmuntaqim <146774930+rmuntaqim@users.noreply.github.com>

--- lib/packages/shared-types/events/index.ts
+++ lib/packages/shared-types/events/index.ts
@@ -58,4 +58,6 @@ export type Events = {
   RespondToRai: z.infer<typeof respondToRai.schema>;
   UploadSubsequentDocuments: z.infer<typeof uploadSubsequentDocuments.schema>;
   WithdrawPackage: z.infer<typeof withdrawPackage.schema>;
+  WithdrawRai: z.infer<typeof withdrawRai.schema>;
+  ToggleWithdrawRai: z.infer<typeof toggleWithdrawRai.schema>;
 };
