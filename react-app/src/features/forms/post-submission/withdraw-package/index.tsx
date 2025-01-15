65b60787c5e0dbe25cccf568f7f6f975eca1acd5
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2025-01-15 14:17:13 -0500

    Update react-app/src/features/forms/post-submission/withdraw-package/index.tsx
    Co-authored-by: asharonbaltazar <58940073+asharonbaltazar@users.noreply.github.com>

--- react-app/src/features/forms/post-submission/withdraw-package/index.tsx
+++ react-app/src/features/forms/post-submission/withdraw-package/index.tsx
@@ -137,7 +137,7 @@ export const WithdrawPackageActionChip = () => {
       }}
       bannerPostSubmission={{
         header: "Withdraw package request has been submitted",
-        body: `If CMS needs any additional information, they will follow up by email.`,
+        body: "If CMS needs any additional information, they will follow up by email.",
         variant: "success",
       }}
       documentPollerArgs={{
