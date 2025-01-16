1e799ae512393c038a47d8d531b66a55ffdf4d53
Thomas <thomas.e.walker88@gmail.com>
2025-01-16 11:17:39 -0500

    add respondto rai and fix event in changelog

--- lib/lambda/processEmailsHandler.test.ts
+++ lib/lambda/processEmailsHandler.test.ts
@@ -12,9 +12,33 @@ const withdrawPackage = "withdraw-package";
 const contractingInitial = "contracting-initial";
 const capitatedInitial = "capitated-initial";
 const withdrawRai = "withdraw-rai";
-
+const respondToRai = "respond-to-rai"
 describe("process emails  Handler", () => {
   it.each([
+        [
+      `should send an email for ${respondToRai} with ${Authority.MED_SPA}`,
+      Authority.MED_SPA,
+      respondToRai,
+      SIMPLE_ID,
+    ],
+    [
+      `should send an email for ${respondToRai} with ${Authority.CHIP_SPA}`,
+      Authority.CHIP_SPA,
+      respondToRai,
+      SIMPLE_ID,
+    ],
+    [
+      `should send an email for ${respondToRai} with ${Authority["1915b"]}`,
+      Authority["1915b"],
+      respondToRai,
+      SIMPLE_ID,
+    ],
+    [
+      `should send an email for ${respondToRai} with ${Authority["1915c"]}`,
+      Authority["1915c"],
+      respondToRai,
+      SIMPLE_ID,
+    ],
     [
       `should send an email for ${nms} with ${Authority.MED_SPA}`,
       Authority.MED_SPA,
