f8a22c249a83889a4c86a7dd856c653c3dc1e041
tiffanyvu <tiffany.v330@gmail.com>
2025-01-17 09:02:46 -0800

    log error

--- lib/libs/opensearch-lib.ts
+++ lib/libs/opensearch-lib.ts
@@ -183,15 +183,23 @@ export async function getItem(
   client = client || (await getClient(host));
   console.log(client, "CLIENT???");
   console.log(id, index, "ID AND INDEX???");
-  console.log("CLIENT STUCK", await client.get({ id, index }));
-  const response = await client.get({ id, index });
-  console.log(response, "RESPONSE FROM OS");
-  const item = decodeUtf8(response).body;
-  console.log(item, "ITEM FROM OS");
-  if (item.found === false || !item._source) {
-    return undefined;
+  try {
+    console.log("CLIENT STUCK", await client.get({ id, index }));
+    const response = await client.get({ id, index });
+    console.log(response, "RESPONSE FROM OS");
+    const item = decodeUtf8(response).body;
+    console.log(item, "ITEM FROM OS");
+    if (item.found === false || !item._source) {
+      return undefined;
+    }
+    return item;
+  } catch (error) {
+    console.error("ERRRORORR", error);
+    if (error instanceof Error) {
+      console.error("ERR MSG:", error.message);
+    }
+    throw error;
   }
-  return item;
 }
 
 export async function getItems(ids: string[]): Promise<OSDocument[]> {
