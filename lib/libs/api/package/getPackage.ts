7771b76b744eadd71d458cff7d0b6352595c2549
tiffanyvu <tiffany.v330@gmail.com>
2025-01-17 12:13:39 -0800

    revert

--- lib/libs/api/package/getPackage.ts
+++ lib/libs/api/package/getPackage.ts
@@ -7,10 +7,7 @@ export interface ExtendedItemResult extends ItemResult {
 }
 export const getPackage = async (id: string): Promise<ItemResult | undefined> => {
   const { domain, index } = getDomainAndNamespace("main");
-console.log(domain, "DOMAIN");
-console.log(index, "INDEX");
-console.log(id, "WHATS THE ID");
   const packageResult = await os.getItem(domain, index, id);
-  console.log(packageResult, "WHAT?");
+
   return packageResult;
 };
