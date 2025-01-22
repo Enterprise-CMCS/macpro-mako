1d6c96c9b06ae3058a4231ebb1f5eadca72fd4db
tiffanyvu <tiffany.v330@gmail.com>
2025-01-22 13:01:25 -0800

    cleanup

--- lib/lambda/submit/splitSPAId.ts
+++ lib/lambda/submit/splitSPAId.ts
@@ -1,5 +1,4 @@
 import { search } from "libs/opensearch-lib";
-// import { getPackage } from "libs/api/package";
 import { getDomainAndNamespace } from "libs/utils";
 
 export const getNextSplitSPAId = async (spaId: string) => {
@@ -12,33 +11,29 @@ export const getNextSplitSPAId = async (spaId: string) => {
     },
   };
   // get existing split SPAs for this package id
-  const matchingSplitSPAs = await search(domain, index, query);
+  const existingSplitSPAs = await search(domain, index, query);
 
   let newSplitSPAId: string;
 
-  if (matchingSplitSPAs.hits.hits.length === 0) {
-    // make sure package exists
-    // const currentPackage = await getPackage(spaId);
-    // if (currentPackage && currentPackage.found) {
+  if (existingSplitSPAs.hits.hits.length === 0) {
+    // first split SPA
     newSplitSPAId = `${spaId}-A`;
-    return newSplitSPAId;
-    // }
   } else {
-    newSplitSPAId = "";
-    const ids = matchingSplitSPAs.hits.hits.map((hit: any) => hit._source.id);
-    console.log(ids);
+    // get list of split SPAs for this package id and extract suffixes (letters)
+    const ids = existingSplitSPAs.hits.hits.map((hit: any) => hit._source.id);
     const suffixes = ids.map((id: string) => id.split("-").at(-1));
-    const letterCharCodes = suffixes.map((letter: string, idx: number) => letter.charCodeAt(idx));
-    const lastLetterCharCode = Math.max(...letterCharCodes);
+
+    // convert to ASCII to find greatest letter
+    const suffixCharCodes = suffixes.map((letter: string, idx: number) => letter.charCodeAt(idx));
+    const lastLetterCharCode = Math.max(...suffixCharCodes);
+
+    // increment letter but not past "Z"
     if (lastLetterCharCode >= 90) {
       // edit message
       throw new Error("This package can't be further split.");
     }
     const nextLetter = String.fromCharCode(lastLetterCharCode + 1);
     newSplitSPAId = `${spaId}-${nextLetter}`;
-    console.log(letterCharCodes, "LETTERS");
-    console.log(newSplitSPAId, "NEW SPLIT SPA ID");
-    // console.log(lastLetter, "last letter");
-    return newSplitSPAId;
   }
+  return newSplitSPAId;
 };
