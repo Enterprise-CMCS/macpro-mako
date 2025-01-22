1c61dc955f4a910dc00c7deed8c143aa43849b67
tiffanyvu <tiffany.v330@gmail.com>
2025-01-22 11:45:56 -0800

    logic

--- lib/lambda/submit/splitSPAId.ts
+++ lib/lambda/submit/splitSPAId.ts
@@ -7,10 +7,11 @@ export const getNextSplitSPAId = async (spaId: string) => {
   const query = {
     query: {
       regexp: {
-        "id.keyword": `${spaId}-[A-Z]`, // only allow capital letters
+        "id.keyword": `${spaId}-[A-Z]`,
       },
     },
   };
+  // get existing split SPAs for this package id
   const matchingSplitSPAs = await search(domain, index, query);
 
   let newSplitSPAId: string;
@@ -22,17 +23,15 @@ export const getNextSplitSPAId = async (spaId: string) => {
       newSplitSPAId = `${spaId}-A`;
       return newSplitSPAId;
     }
-    // } else {
-    //   newSplitSPAId = "";
-    //   console.log(matchingSplitSPAs, "HELLOOO");
-    //   return newSplitSPAId;
-    // }
   } else {
     newSplitSPAId = "";
     const ids = matchingSplitSPAs.hits.hits.map((hit: any) => hit._source.id);
     console.log(ids);
     const suffixes = ids.map((id: string) => id.split("-").at(-1));
-    console.log(suffixes, "LETTERS");
+    const letterCharCodes = suffixes.map((letter: string, idx: number) => letter.charCodeAt(idx));
+    const lastLetter = Math.max(letterCharCodes);
+    console.log(letterCharCodes, "LETTERS");
+    console.log(lastLetter, "last letter");
     return newSplitSPAId;
   }
 };
