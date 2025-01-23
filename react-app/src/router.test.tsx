df38c549d61b32c158a97b04df6d21cbf4a3a53c
Ty Bolt <tybolt@gmail.com>
2025-01-23 11:35:41 -0500

    fix import issue

--- react-app/src/router.test.tsx
+++ react-app/src/router.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { RouterProvider } from "react-router";
-import { createMemoryRouter } from "react-router-dom";
+import { createMemoryRouter, RouterProvider } from "react-router";
 import { describe, expect, it, vi } from "vitest";
 import { router } from "./router";
 
