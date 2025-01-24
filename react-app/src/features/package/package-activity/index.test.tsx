c25194d25d65ea12752120d57b3857803d646c16
Rafiq Muntaqim <rmuntaqim@fearless.tech>
2025-01-24 13:17:00 -0500

    updating test

--- react-app/src/features/package/package-activity/index.test.tsx
+++ react-app/src/features/package/package-activity/index.test.tsx
@@ -25,10 +25,10 @@ describe("Package Activity", () => {
     expect(toBeEmptyDOMElement);
   });
 
-  it("displays the correct title and description if changelog length is 0", async () => {
+  it("displays the correct title and description if changelog length is 1", async () => {
     await renderFormWithPackageSectionAsync(<PackageActivities />, MISSING_CHANGELOG_ITEM_ID);
 
-    expect(screen.getByText("Package Activity (0)"));
+    expect(screen.getByText("Package Activity (1)"));
     expect(screen.getByText("No package activity recorded"));
     expect(screen.queryByText("Download all documents")).not.toBeInTheDocument();
   });
