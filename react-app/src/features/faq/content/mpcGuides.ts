0f93a6007f2ea0b7c95ad06c80e9e410ceec4a65
Gavin St. Ours <gst-ours@fearless.tech>
2025-01-24 11:28:29 -0500

    Update MPC IG filenames per feedback

--- react-app/src/features/faq/content/mpcGuides.ts
+++ react-app/src/features/faq/content/mpcGuides.ts
@@ -4,26 +4,26 @@ export const MPC_GUIDES: Template[] = [
   {
     title: "G 1",
     text: "Cost-Sharing Requirements Implementation Guide",
-    href: "/mpc/IG_G1_CostSharingRequirements_Final_20241213.pdf",
+    href: "/mpc/IG_G1_CostSharingRequirements.pdf",
   },
   {
     title: "G 2a",
     text: "Cost-Sharing Amounts - Categorically Needy Implementation Guide",
-    href: "/mpc/IG_G2a_CostSharingAmountsCN_Final_20241213.pdf",
+    href: "/mpc/IG_G2a_CostSharingAmountsCN.pdf",
   },
   {
     title: "G 2b",
     text: "Cost-Sharing Amounts - Medically Needy Implementation Guide",
-    href: "/mpc/IG_G2b_CostSharingAmountsMN_FINAL_20241213.pdf",
+    href: "/mpc/IG_G2b_CostSharingAmountsMN.pdf",
   },
   {
     title: "G 2c",
     text: "Cost-Sharing Amounts - Targeting Implementation Guide",
-    href: "/mpc/IG_G2c_CostSharingAmountsTargeting _Final_20241213.pdf",
+    href: "/mpc/IG_G2c_CostSharingAmountsTargeting.pdf",
   },
   {
     title: "G 3",
     text: "Cost-Sharing Limitations Implementation Guide",
-    href: "/mpc/IG_G3_CostSharingLimitations_Final_20241213.pdf",
+    href: "/mpc/IG_G3_CostSharingLimitations.pdf",
   },
 ];
