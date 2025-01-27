d67faca7200a4892e62c32271b28ba62f7018723
Ty Bolt <tybolt@gmail.com>
2025-01-27 13:49:35 -0500

    update urls

--- react-app/src/features/faq/content/chpTemplates.ts
+++ react-app/src/features/faq/content/chpTemplates.ts
@@ -5,114 +5,114 @@ export const CHP_TEMPLATES: Template[] = [
   {
     title: "CS 7",
     text: "Eligibility - Targeted Low-Income Children",
-    href: "/CS7.pdf",
+    href: "/chp/CS7.pdf",
   },
   {
     title: "CS 8",
     text: "Eligibility - Targeted Low-Income Pregnant Women",
-    href: "/CS8.pdf",
+    href: "/chp/CS8.pdf",
   },
   {
     title: "CS 9",
     text: "Eligibility - Coverage From Conception to Birth",
-    href: "/CS9.pdf",
+    href: "/chp/CS9.pdf",
   },
   {
     title: "CS 10",
     text: "Eligibility - Children Who Have Access to Public Employee Coverage",
-    href: "/CS10.pdf",
+    href: "/chp/CS10.pdf",
   },
   {
     title: "CS 11",
     text: "Eligibility - Pregnant Women Who Have Access to Public Employee Coverage",
-    href: "/CS11.pdf",
+    href: "/chp/CS11.pdf",
   },
   {
     title: "CS 12",
     text: "Eligibility - Dental Only Supplemental Coverage",
-    href: "/CS12.pdf",
+    href: "/chp/CS12.pdf",
   },
   {
     title: "CS 13",
     text: "Eligibility - Deemed Newborns",
-    href: "/CS13.pdf",
+    href: "/chp/CS13.pdf",
   },
   {
     title: "CS 15",
     text: "MAGI-Based Income Methodologies",
-    href: "/CS15.pdf",
+    href: "/chp/CS15.pdf",
   },
   {
     title: "CS 16",
     text: "Other Eligibility Criteria - Spenddowns",
-    href: "/CS16.pdf",
+    href: "/chp/CS16.pdf",
   },
 
   // XXI Medicaid Expansion
   {
     title: "CS 3",
     text: "Eligibility for Medicaid Expansion Program",
-    href: "/CS3.pdf",
+    href: "/chp/CS3.pdf",
   },
 
   // Establish 2101(f) Group
   {
     title: "CS 14",
     text: "Eligibility - Children Ineligible for Medicaid as a Result of the Elimination of Income Disregards",
-    href: "/CS14.pdf",
+    href: "/chp/CS14.pdf",
   },
 
   // Eligibility Processing
   {
     title: "CS 24",
     text: "General Eligibility - Eligibility Processing",
-    href: "/CS24.pdf",
+    href: "/chp/CS24.pdf",
   },
 
   // Non-Financial Eligibility
   {
     title: "CS 17",
     text: "Non-Financial Eligibility - Residency",
-    href: "/CS17.pdf",
+    href: "/chp/CS17.pdf",
   },
   {
     title: "CS 18",
     text: "Non-Financial Eligibility - Citizenship",
-    href: "/CS18.pdf",
+    href: "/chp/CS18.pdf",
   },
   {
     title: "CS 19",
     text: "Non-Financial Eligibility - Social Security Number",
-    href: "/CS19.pdf",
+    href: "/chp/CS19.pdf",
   },
   {
     title: "CS 20",
     text: "Non-Financial Eligibility - Substitution of Coverage",
-    href: "/CS20.pdf",
+    href: "/chp/CS20.pdf",
   },
   {
     title: "CS 21",
     text: "Non-Financial Eligibility - Non-Payment of Premiums",
-    href: "/CS21.pdf",
+    href: "/chp/CS21.pdf",
   },
   {
     title: "CS 23",
     text: "Non-Financial Requirements - Other Eligibility Standards",
-    href: "/CS23.pdf",
+    href: "/chp/CS23.pdf",
   },
   {
     title: "CS 27",
     text: "General Eligibility - Continuous Eligibility",
-    href: "/CS27.pdf",
+    href: "/chp/CS27.pdf",
   },
   {
     title: "CS 28",
     text: "General Eligibility - Presumptive Eligibility for Children",
-    href: "/CS28.pdf",
+    href: "/chp/CS28.pdf",
   },
   {
     title: "CS 29",
     text: "General Eligibility - Presumptive Eligibility for Pregnant Women",
-    href: "/CS29.pdf",
+    href: "/chp/CS29.pdf",
   },
 ];
