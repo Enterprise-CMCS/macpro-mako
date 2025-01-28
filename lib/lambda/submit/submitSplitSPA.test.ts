48b4822dbc5d3eb9f8c8275e83d482693ccdbd78
tiffanyvu <tiffany.v330@gmail.com>
2025-01-28 11:23:49 -0800

    topic name not defined

--- lib/lambda/submit/submitSplitSPA.test.ts
+++ lib/lambda/submit/submitSplitSPA.test.ts
@@ -1,7 +1,15 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { handler } from "./submitSplitSPA";
 import { APIGatewayEvent } from "node_modules/shared-types";
-import { TEST_CHIP_SPA_ITEM } from "mocks";
+import { TEST_CHIP_SPA_ITEM, TEST_MED_SPA_ITEM } from "mocks";
+
+vi.mock("libs/handler-lib", () => ({
+  response: vi.fn((data) => data),
+}));
+
+vi.mock("./getNextSplitSPAId", () => ({
+  getNextSplitSPAId: vi.fn(),
+}));
 
 describe("handler", () => {
   beforeEach(() => {
@@ -15,11 +23,13 @@ describe("handler", () => {
     expect(result?.statusCode).toEqual(400);
   });
 
-  it("should return 400 if package ID is not provided", async () => {
-    const emptyBody = { body: { packageId: undefined } } as unknown as APIGatewayEvent;
-    const result = await handler(emptyBody);
-    expect(result?.statusCode).toEqual(400);
-  });
+  // it("should return 400 if package ID is not provided", async () => {
+  //   const emptyBody = {
+  //     body: JSON.stringify({ packageId: undefined }),
+  //   } as APIGatewayEvent;
+  //   const result = await handler(emptyBody);
+  //   expect(result?.statusCode).toEqual(400);
+  // });
 
   it("should return 404 if package ID is not found", async () => {
     const invalidPackage = {
@@ -27,7 +37,6 @@ describe("handler", () => {
     } as unknown as APIGatewayEvent;
     const result = await handler(invalidPackage);
     expect(result?.statusCode).toEqual(404);
-    expect(result?.body).toEqual('{"message":"No record found for the given id"}');
   });
 
   it("should throw an error if not Medicaid SPA", async () => {
@@ -35,6 +44,43 @@ describe("handler", () => {
       body: { packageId: TEST_CHIP_SPA_ITEM._id },
     } as unknown as APIGatewayEvent;
     const result = await handler(chipSPAPackage);
-    expect(result?.body).toEqual('{"message":"Record must be a Medicaid SPA"}');
+    const expectedResult = {
+      statusCode: 400,
+      body: { message: "Record must be a Medicaid SPA" },
+    };
+    expect(result).toEqual(expectedResult);
   });
+
+  it("should return 400 if package ID not provided", async () => {
+    const invalidPackage = {
+      body: JSON.stringify({}),
+    } as unknown as APIGatewayEvent;
+    const result = await handler(invalidPackage);
+    expect(result?.statusCode).toEqual(400);
+  });
+
+  it("should fail to split a package with no topic name", async () => {
+    process.env.topicName = "";
+    const noActionevent = {
+      body: {
+        packageId: TEST_MED_SPA_ITEM._id,
+      },
+    } as unknown as APIGatewayEvent;
+
+    const result = await handler(noActionevent);
+    const expectedResult = {
+      statusCode: 500,
+      body: { message: "Topic name is not defined" },
+    };
+    expect(result).toStrictEqual(expectedResult);
+  });
+
+  // it("should create a split SPA", async () => {
+  //   const medSPAPackage = {
+  //     body: { packageId: TEST_MED_SPA_ITEM._id },
+  //   } as unknown as APIGatewayEvent;
+  //   console.log(medSPAPackage, "HELLO??");
+  //   const result = await handler(medSPAPackage);
+  //   expect(result?.body).toEqual('{"message":"Record must be a Medicaid SPA"}');
+  // });
 });
