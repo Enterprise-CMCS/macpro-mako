6b1fd128b2d0c47a30e65f3643b7f1c5a074c35e
Rafiq Muntaqim <rmuntaqim@fearless.tech>
2025-02-03 21:51:57 -0500

    testing values

--- react-app/src/components/Opensearch/main/Filtering/Drawer/index.tsx
+++ react-app/src/components/Opensearch/main/Filtering/Drawer/index.tsx
@@ -64,7 +64,10 @@ export const OsFilterDrawer = () => {
                 {PK.component === "multiCheck" && (
                   <F.FilterableMultiCheck
                     value={hook.filters[PK.field]?.value as string[]}
-                    onChange={hook.onFilterChange(PK.field)}
+                    onChange={(value) => {
+                      console.log(`Filter changed for ${PK.field}:`, value);
+                      hook.onFilterChange(PK.field)(value);
+                    }}
                     options={hook.aggs?.[PK.field]}
                   />
                 )}
