eed5017b3a2430232d7c7118627d7ab241172a27
Rafiq Muntaqim <rmuntaqim@fearless.tech>
2025-02-05 12:04:31 -0500

    testing select filter

--- react-app/src/components/Opensearch/main/Filtering/Drawer/index.tsx
+++ react-app/src/components/Opensearch/main/Filtering/Drawer/index.tsx
@@ -62,13 +62,12 @@ export const OsFilterDrawer = () => {
                   />
                 )}
                 {PK.component === "multiCheck" && (
-                  <F.FilterableMultiCheck
+                  <F.FilterableSelect
                     value={hook.filters[PK.field]?.value as string[]}
-                    onChange={(value) => {
-                      console.log(`Filter changed for ${PK.field}:`, value);
-                      hook.onFilterChange(PK.field)(value);
-                    }}
-                    options={hook.aggs?.[PK.field]}
+                    onChange={hook.onFilterChange(PK.field)}
+                    options={(hook.aggs?.[PK.field] || []).filter(
+                      (option) => option.value !== "Initial",
+                    )}
                   />
                 )}
                 {PK.component === "dateRange" && (
