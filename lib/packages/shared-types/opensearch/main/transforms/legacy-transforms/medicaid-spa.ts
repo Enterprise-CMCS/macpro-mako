12ea491505ad9cecb33a9fbe9ea4602a1aa56ad1
Brandon Flynn <bflynn@luckyrabbit.tech>
2025-02-08 15:44:48 -0600

    enhance date conversion logic

--- lib/packages/shared-types/opensearch/main/transforms/legacy-transforms/medicaid-spa.ts
+++ lib/packages/shared-types/opensearch/main/transforms/legacy-transforms/medicaid-spa.ts
@@ -23,7 +23,7 @@ export const transform = () => {
       state: data.pk?.split("-")?.[0], // Extract state from pk
       stateStatus,
       statusDate: new Date(data.eventTimestamp).toISOString(),
-      proposedDate: data.proposedEffectiveDate === "none" ? null : data.proposedEffectiveDate, // Handle "none" as null
+      proposedDate: isNaN(new Date(data.proposedEffectiveDate).getTime()) ? null : data.proposedEffectiveDate, // Handle non date convertible data as null
       subject: null,
       submissionDate: new Date(data.eventTimestamp).toISOString(),
       submitterEmail: data.submitterEmail,
