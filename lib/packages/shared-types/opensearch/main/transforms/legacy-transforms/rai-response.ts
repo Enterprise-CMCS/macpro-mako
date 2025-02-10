858ff6dc3c1899a26fe8c2603dc72ca9b4d47700
Brandon Flynn <bflynn@luckyrabbit.tech>
2025-02-10 00:31:58 -0600

    rai response legacy sync

--- lib/packages/shared-types/opensearch/main/transforms/legacy-transforms/rai-response.ts
+++ lib/packages/shared-types/opensearch/main/transforms/legacy-transforms/rai-response.ts
@@ -5,14 +5,11 @@ import { LegacyEvent } from "../../../../events";
 
 export const transform = () => {
   return events["legacy-event"].legacyEventSchema.transform((data) => {
-    const { stateStatus, cmsStatus } = getStatus(SEATOOL_STATUS.SUBMITTED);
-    const timestampDate = new Date(data.eventTimestamp);
-    const todayEpoch = seaToolFriendlyTimestamp(timestampDate);
+      const { stateStatus, cmsStatus } = getStatus(SEATOOL_STATUS.SUBMITTED);
+      let actionType
 
       return {
         id: data.pk,
-        authority: data.authority,
-        ...(data.actionType && { actionType: data.actionType }),
         raiWithdrawEnabled: false,
         makoChangedDate: data.eventTimestamp ? new Date(data.eventTimestamp).toISOString() : null,
         cmsStatus,
