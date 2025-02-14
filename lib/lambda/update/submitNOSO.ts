41b96f40eae256a18dcf4a6717080bed9f0f2004
Andie Swift <andie.swift5@gmail.com>
2025-02-14 15:18:45 -0600

    timestamp is not camelcase

--- lib/lambda/update/submitNOSO.ts
+++ lib/lambda/update/submitNOSO.ts
@@ -61,7 +61,7 @@ const sendSubmitMessage = async (item: submitMessageType) => {
       makoChangedDate: currentTime,
       changedDate: currentTime,
       statusDate: currentTime,
-      timeStamp: currentTime,
+      timestamp: currentTime,
     }),
   );
 
