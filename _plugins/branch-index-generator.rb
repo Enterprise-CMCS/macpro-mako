2f6a2b95c9d7d364cde4f338ac4d2aecf30c15bf
Tiffany Forkner <tforkner@fearless.tech>
2025-02-19 13:32:12 -0500

    adding generator for branch index

---
+++ _plugins/branch-index-generator.rb
@@ -0,0 +1,31 @@
+module Jekyll
+
+  class BranchIndex < Page
+    def initialize(site, base, dir, branch)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      puts "adding to directory #{@dir}"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'playwright-branch.html')
+      self.data['branch'] = branch
+    end
+  end
+
+  class BranchIndexGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'playwright-branch'
+        dir = "/playwright-reports/branches"
+        site.data["playwright-reports"]["branches"].each_key do |branch|
+          Jekyll.logger.info "Branch Index: Generating index for #{branch}"
+          site.pages << BranchIndex.new(site, site.source, File.join(dir, branch), branch)
+        end
+      end
+    end
+  end
+
+end
