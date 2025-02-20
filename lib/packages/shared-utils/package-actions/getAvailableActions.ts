6a70017e14ec36f4d931f39c2954627e65b2fba2
Thomas Walker <thomas.e.walker88@gmail.com>
2025-02-20 10:58:42 -0500

    feat(ui)Update action order (#1174)
    * feat(ui)Update action order
    
    * Update item to be properly removed

--- lib/packages/shared-utils/package-actions/getAvailableActions.ts
+++ lib/packages/shared-utils/package-actions/getAvailableActions.ts
@@ -36,5 +36,12 @@ export const getAvailableActions = (
     const actionsToRemove = [Action.RESPOND_TO_RAI, Action.WITHDRAW_RAI];
     commonActions = commonActions.filter((action: any) => !actionsToRemove.includes(action));
   }
-  return commonActions;
+
+  // We are sorting the actions based on the value they are in the order of the enum.
+  // If we want to update this order just move the values around.
+  const actionOrder = Object.values(Action);
+  const sortedActions = commonActions.sort(
+    (a, b) => actionOrder.indexOf(a) - actionOrder.indexOf(b),
+  );
+  return sortedActions;
 };
