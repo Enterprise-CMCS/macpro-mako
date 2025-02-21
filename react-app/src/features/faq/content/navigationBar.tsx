0c0a7516158997226e93d6b2a2958682fa02e18e
Brandon Flynn <bflynn@luckyrabbit.tech>
2025-02-20 22:48:39 -0600

    Update navigationBar.tsx

--- react-app/src/features/faq/content/navigationBar.tsx
+++ react-app/src/features/faq/content/navigationBar.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {
   Accordion,
   AccordionItem,
+  AccordionContent,
   AccordionTrigger,
-  AccordionContent
-} from '@/components/ui/accordion';
+} from "@/components";
 
 const LeftNavigation: React.FC = () => {
   return (
