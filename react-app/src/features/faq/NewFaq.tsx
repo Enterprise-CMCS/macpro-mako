72c9dca4aefb79b3e634a6fe3a963ee9ecf2bef7
Brandon Flynn <bflynn@luckyrabbit.tech>
2025-02-24 20:37:11 -0600

    dont allow left nav to flex so that it wont grow with screen space

--- react-app/src/features/faq/NewFaq.tsx
+++ react-app/src/features/faq/NewFaq.tsx
@@ -6,25 +6,25 @@ import LeftNavigation from "./content/navigationBar";
 
 export const  NewFaq = (props : any) => {
   return (
-    <div className="min-h-screen flex flex-col">
-    <SubNavHeader>
-      <h1 className="text-xl font-medium">Frequently Asked Questions</h1>
-    </SubNavHeader>
-
-    {/* Main Layout Wrapper - Contains FAQ and Left Navigation */}
-    <section className="relative flex max-w-screen-xl m-auto px-4 lg:px-8 pt-8 gap-6 flex-grow">
-      {/* Left Navigation */}
-      <div className="w-60 shrink-0 sticky top-0 max-h-screen pt-4">
-        <LeftNavigation />
-      </div>
-
-      {/* FAQ Content */}
-      <div className="flex-1">
-        <article className="mb-8">
-          FAQ Content
-        </article>
-      </div>
-    </section>
-  </div>
+    <div className="min-h-screen flex flex-col">     
+      <SubNavHeader>       
+        <h1 className="text-xl font-medium">Frequently Asked Questions</h1>     
+      </SubNavHeader>      
+      
+      {/* Main Layout Wrapper - Contains FAQ and Left Navigation */}     
+      <section className="relative flex max-w-screen-xl m-auto px-4 lg:px-8 pt-8 gap-6 flex-grow">       
+        {/* Left Navigation - Fixed width */}       
+        <div className="w-64 flex-none shrink-0 sticky top-0 max-h-screen pt-4">         
+          <LeftNavigation />       
+        </div>        
+        
+        {/* FAQ Content - Force to take remaining space */}       
+        <div className="flex-1 min-w-0">         
+          <article className="mb-8">           
+            FAQ Content         
+          </article>       
+        </div>     
+      </section>   
+    </div>
   );
 };
