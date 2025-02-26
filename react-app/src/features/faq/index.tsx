7945efaebefdcf53786125b7069c77a47fe8e1cb
Alex Youssefinia <ayoussefinia@fearless.tech>
2025-02-26 11:23:36 -0600

    remove intervals clarify code

--- react-app/src/features/faq/index.tsx
+++ react-app/src/features/faq/index.tsx
@@ -6,24 +6,12 @@ import {NewFaq} from "./NewFaq"
 export const Faq = () => {
   const ldClient = useLDClient();
   const [flagValue, setFlagValue] = useState(false);
+
   useEffect(() => {
-    const updateFlagValue = () => {
-      const updatedFlagValue = ldClient.allFlags()['toggleFaq'] || false;
-      setFlagValue(updatedFlagValue);
-    };
-
-    updateFlagValue();
-
-    // Set up a periodic interval to check for flag changes every 5 seconds
-    const intervalId = setInterval(() => {
-      updateFlagValue();
-    }, 7000); // 5000 ms = 5 seconds
-
-    // Cleanup the interval when the component is unmounted
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [ldClient]);
+    const isFlagOn = ldClient?.variation("toggleFaq");
+    console.log("isFlagOn:  " + isFlagOn);
+    setFlagValue(isFlagOn)
+  }, [ldClient.allFlags()]);
 
   return (
     <>
