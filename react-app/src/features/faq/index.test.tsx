4046e2f5ffc681468b97c66d88c1dcefdac52933
anyoussefinia <ayoussefinia@fearless.tech>
2025-03-02 17:45:30 -0600

    feat(ui): add launch darkly flag to toggle between existing and updated FAQ pages (#1214)
    * faq side nav bar
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * another approach
    
    * style changes for better mock data
    
    * Update navigationBar.tsx
    
    * Update navigationBar.tsx
    
    * Update contactHelpDesk.tsx
    
    * added feature flag toggleFaq to switche between legacy and new FAQ
    
    * fix casing
    
    * add left nav to NewFaq
    
    * dont allow left nav to flex so that it wont grow with screen space
    
    * new layout
    
    * fix
    
    * add fake text
    
    * longer
    
    * even longer
    
    * fix spacing
    
    * last care
    
    * remove extra whitespace
    
    * make false flag rendeer legacy faq
    
    * added unit tests for conditional faq rendering
    
    * added unit tests for conditional faq rendering
    
    * removed feature-flag enum
    
    * remove intervals clarify code
    
    * remove console log
    
    * unit test fix
    
    * lint fixes 1
    
    * lint fixes 2
    
    * lint fix
    
    ---------
    
    Co-authored-by: Brandon Flynn <bflynn@luckyrabbit.tech>

---
+++ react-app/src/features/faq/index.test.tsx
@@ -0,0 +1,39 @@
+// my-test-file.js
+
+import { render, screen } from "@testing-library/react";
+import { useLDClient } from "launchdarkly-react-client-sdk";
+import { expect, vi, test } from "vitest";
+import { Faq } from "./index";
+
+// Mock the `useLDClient` hook from LaunchDarkly
+vi.mock("launchdarkly-react-client-sdk", () => ({
+  useLDClient: vi.fn(),
+}));
+
+test("should render legacy faq when flag is false", () => {
+  // mock return false for toggleFaq flag
+  (useLDClient as jest.Mock).mockReturnValue({
+    variation: vi.fn().mockReturnValue(false),
+    allFlags: vi.fn().mockReturnValue({
+      toggleFaq: false,
+    }),
+  });
+
+  render(<Faq />);
+
+  expect(screen.getByText("OneMAC Help Desk Contact Info")).toBeInTheDocument();
+});
+
+test("should render new faq when flag is true", () => {
+  // mock return true for toggleFaq flag
+  (useLDClient as jest.Mock).mockReturnValue({
+    variation: vi.fn().mockReturnValue(true),
+    allFlags: vi.fn().mockReturnValue({
+      toggleFaq: true,
+    }),
+  });
+
+  render(<Faq />);
+
+  expect(screen.getByText("FAQ Content Goes Here")).toBeInTheDocument();
+});
