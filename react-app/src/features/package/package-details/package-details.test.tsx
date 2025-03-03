c23398de7ff48b4cde85fe02cff0a874bbed57ee
asharonbaltazar <58940073+asharonbaltazar@users.noreply.github.com>
2025-03-03 13:41:37 -0500

    fix(ui): update submission details to include timezone and remove seatool updating `submissionDate` (#1237)
    * chore: simplify and clean up package details
    
    * chore: some renaming
    
    * feat: create `formatDateToEST` and some tests
    
    * chore: use `formatDateToEST` in Package Details/Activity
    
    * fix: remove `submissionDate` from seatool transform
    
    * chore: fix tests
    
    * chore: adjust logic

--- react-app/src/features/package/package-details/package-details.test.tsx
+++ react-app/src/features/package/package-details/package-details.test.tsx
@@ -4,8 +4,7 @@ import { UseQueryResult } from "@tanstack/react-query";
 import * as api from "@/api/useGetUser";
 import * as gi from "@/api/useGetItem";
 
-import { PackageDetails } from ".";
-import { AppK } from "./appk";
+import { SubmissionDetails } from ".";
 
 import { mockUseGetUser, WITHDRAW_APPK_ITEM, WITHDRAW_APPK_ITEM_ID } from "mocks";
 import { renderWithQueryClient } from "@/utils/test-helpers";
@@ -25,19 +24,7 @@ describe("package details", () => {
     });
     const renderComponent = async () => {
       const item = await getItem(WITHDRAW_APPK_ITEM_ID);
-      return renderWithQueryClient(<PackageDetails itemResult={item} />);
-    };
-    const compo = await renderComponent();
-    expect(compo).toMatchSnapshot();
-  });
-  it("makes a an appk element", async () => {
-    vi.spyOn(api, "useGetUser").mockImplementation(() => {
-      const response = mockUseGetUser();
-      response.data.isCms = false;
-      return response as UseQueryResult<OneMacUser, unknown>;
-    });
-    const renderComponent = async () => {
-      return renderWithQueryClient(<AppK />);
+      return renderWithQueryClient(<SubmissionDetails itemResult={item} />);
     };
     const compo = await renderComponent();
     expect(compo).toMatchSnapshot();
