eed64ff4a30f2e89e2ab37407e816da7db56fec4
Thomas Walker <thomas.e.walker88@gmail.com>
2025-03-04 11:31:19 -0500

    Update lib/libs/email/preview/InitialSubmissions/CMS/SnapshotTest/Medicaid_SPA.test.tsx
    Co-authored-by: asharonbaltazar <58940073+asharonbaltazar@users.noreply.github.com>

--- lib/libs/email/preview/InitialSubmissions/CMS/SnapshotTest/Medicaid_SPA.test.tsx
+++ lib/libs/email/preview/InitialSubmissions/CMS/SnapshotTest/Medicaid_SPA.test.tsx
@@ -4,7 +4,7 @@ import { render } from "@testing-library/react";
 import Medicaid_SPA from "../Medicaid_SPA";
 
 describe("Initial Submission CMS Email Snapshot Test", () => {
-  it("renders a Medicaid Spa Preview Template", () => {
+  it("renders a Medicaid SPA Preview Template", () => {
     const { asFragment } = render(<Medicaid_SPA />);
 
     expect(asFragment()).toMatchSnapshot();
