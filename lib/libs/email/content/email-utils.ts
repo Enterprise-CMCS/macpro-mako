e1917298aa0c69accf4dbaff0324e98ec095e231
Andie Swift <aswift@gswell.com>
2025-03-05 14:35:35 -0600

    Delete lib/libs/email/content/email-utils.ts

--- lib/libs/email/content/email-utils.ts
+++
@@ -1,6 +0,0 @@
-import { formatActionType } from "node_modules/shared-utils";
-
-export const actionTypeWithWaiver = (actionType: string): string => {
-  if (actionType === "Initial" || actionType === "New") return "Initial Waiver";
-  else return `Waiver ${formatActionType(actionType)}`;
-};
