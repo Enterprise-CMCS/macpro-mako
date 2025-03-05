8dd688c656c13843f0135a41d3e06c718d3b2d68
Asharon Baltazar <asharonbaltazar@outlook.com>
2025-03-05 14:16:50 -0500

    chore: adjust tests for new changes

--- react-app/src/features/package/index.test.tsx
+++ react-app/src/features/package/index.test.tsx
@@ -3,16 +3,11 @@ import { UseQueryResult } from "@tanstack/react-query";
 import * as api from "@/api/useGetUser";
 
 import { DetailsContent } from ".";
-import { mockUseGetUser, ADMIN_CHANGE_ITEM, ADMIN_ITEM_ID } from "mocks";
+import { mockUseGetUser, ADMIN_ITEM_ID } from "mocks";
 import { OneMacUser } from "@/api/useGetUser";
 import { renderFormAsync } from "@/utils/test-helpers";
-import * as gi from "@/api/useGetItem";
-describe("package details", () => {
-  vi.spyOn(gi, "useGetItemCache").mockReturnValue({
-    data: ADMIN_CHANGE_ITEM._source,
-    refetch: vi.fn(),
-  });
 
+describe("package details", () => {
   it("makes a package", async () => {
     vi.spyOn(api, "useGetUser").mockImplementation(() => {
       const response = mockUseGetUser();
