95f46eca5358a600c7278dab4b4aae23f209388c
tiffanyvu <tiffany.v330@gmail.com>
2025-03-06 07:48:29 -0800

    Update lib/packages/shared-utils/action-type-with-waiver.ts
    Co-authored-by: asharonbaltazar <58940073+asharonbaltazar@users.noreply.github.com>

--- lib/packages/shared-utils/action-type-with-waiver.ts
+++ lib/packages/shared-utils/action-type-with-waiver.ts
@@ -4,5 +4,5 @@ export function formatActionTypeWithWaiver(actionType: string): string {
   //   this allows the function to work if the action type already ran through the other format function (like for emails)
   //   or not - since formatActionType will return the input string if it doesn't match the switch
   if (actionType === "Initial" || actionType === "New") return "Initial Waiver";
-  else return `Waiver ${formatActionType(actionType)}`;
+  return `Waiver ${formatActionType(actionType)}`;
 }
