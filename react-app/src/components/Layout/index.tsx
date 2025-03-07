7a68694ad3eca917e80542324172d7532c416d4a
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2025-03-07 16:30:18 -0500

    feat(change-auth-provider): Change the IDM application to point to OneMAC Legacy's (#1166)
    * adjust app-users to use new role system
    
    * move something
    
    * add custom attribute
    
    * fix login?
    
    * fix type error
    
    * Change custom frontend logic to work (this needs rethought)
    
    * how bout now
    
    * fix role comma issue
    
    * fix all the unit tests
    
    * trying something
    
    * fix state displaying for non-state user in profile page
    
    * fix issue when multiple roles exist

--- react-app/src/components/Layout/index.tsx
+++ react-app/src/components/Layout/index.tsx
@@ -39,9 +39,11 @@ const useGetLinks = () => {
             link: "/dashboard",
             condition:
               userObj.user &&
-              userObj.user["custom:cms-roles"] &&
-              Object.values(UserRoles).some((role) =>
-                userObj.user["custom:cms-roles"].includes(role),
+              (userObj.user["custom:cms-roles"] || userObj.user["custom:ismemberof"]) &&
+              Object.values(UserRoles).some(
+                (role) =>
+                  userObj.user["custom:cms-roles"].includes(role) ||
+                  userObj.user["custom:ismemberof"] === role,
               ),
           },
           {
