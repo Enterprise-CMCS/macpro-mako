ba5d258d211138363c3e5d1a4310022ee6e22b57
Andie Swift <andie.swift5@gmail.com>
2025-03-17 19:14:25 -0500

    styled toggle buttons, added functionality to switch faqs based on chosen opt

--- react-app/src/features/faq/SupportPage.tsx
+++ react-app/src/features/faq/SupportPage.tsx
@@ -1,12 +1,23 @@
-import { useEffect, useState } from "react";
+import { ToggleGroup, ToggleGroupItem } from "@radix-ui/react-toggle-group";
+import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router";
 
-import { SupportSubNavHeader } from "@/components";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  SupportSubNavHeader,
+} from "@/components";
+import { cn } from "@/utils";
 
 import ExpandCollapseBtn from "../../components/SupportPage/expandCollapseBtn";
 import LeftNavigation from "../../components/SupportPage/navigationBar";
-import { oneMACFAQContent, QuestionAnswer } from "./content/SupportMockContent";
+import {
+  oneMACCMSContent,
+  oneMACStateFAQContent,
+  QuestionAnswer,
+} from "./content/SupportMockContent";
 
 const FaqAccordion = ({ question }: { question: QuestionAnswer[] }) => {
   return (
@@ -29,14 +40,23 @@ const FaqAccordion = ({ question }: { question: QuestionAnswer[] }) => {
   );
 };
 
+const toggleGroupItemStyle =
+  "inline-flex items-center justify-center px-3 py-1.5 text-2xl bg-neutral-100 rounded-xs ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus:outline-none disabled:pointer-events-none disabled:opacity-50 border-b-4 border-b-transparent ";
+
+const toggleGroupItemActiveStyle =
+  "data-[state=on]:border-b-primary-dark data-[state=on]:bg-blue-50 data-[state=on]:text-primary-dark data-[state=on]:font-bold";
 export const SupportPage = () => {
   const { id } = useParams<{ id: string }>();
   const [openAccordions, setOpenAccordions] = useState<string[]>([]);
+  const [tgValue, setTGValue] = useState<"cms" | "state">("cms");
+
+  const supportContent = useMemo(() => {
+    if (tgValue === "cms") return oneMACCMSContent;
+    return oneMACStateFAQContent;
+  }, [tgValue]);
 
   const expandAll = () => {
-    const allIds = oneMACFAQContent.flatMap(({ qanda }) =>
-      qanda.map(({ anchorText }) => anchorText),
-    );
+    const allIds = supportContent.flatMap(({ qanda }) => qanda.map(({ anchorText }) => anchorText));
     setOpenAccordions(allIds);
   };
 
@@ -45,7 +65,7 @@ export const SupportPage = () => {
   };
 
   const areAllAccordionsOpen = (function () {
-    const totalQandas = oneMACFAQContent.reduce((total, section) => {
+    const totalQandas = supportContent.reduce((total, section) => {
       return total + section.qanda.length;
     }, 0);
     if (openAccordions.length >= totalQandas) return true;
@@ -71,6 +91,35 @@ export const SupportPage = () => {
         <h1 className="text-4xl font-bold">OneMAC Support</h1>
       </SupportSubNavHeader>
 
+      <div className="flex max-w-screen-xl m-auto px-4 lg:px-8 pt-8 w-full border-b-2 border-b-slate-100 justify-end">
+        <div className="w-2/3 px-4 lg:px-8">
+          <ToggleGroup
+            className="ToggleGroup"
+            type="single"
+            aria-label="Text alignment"
+            value={tgValue}
+            onValueChange={(value: "cms" | "state") => {
+              if (value) setTGValue(value);
+            }}
+          >
+            <ToggleGroupItem
+              className={cn(toggleGroupItemStyle, toggleGroupItemActiveStyle)}
+              value="cms"
+              aria-label="cms"
+            >
+              CMS
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              className={cn(toggleGroupItemStyle, toggleGroupItemActiveStyle)}
+              value="state"
+              aria-label="state"
+            >
+              States
+            </ToggleGroupItem>
+          </ToggleGroup>
+        </div>
+      </div>
+
       {/* Main Layout Wrapper with explicit widths */}
       <div className="max-w-screen-xl m-auto px-4 lg:px-8 pt-8 w-full">
         <div className="flex">
@@ -83,7 +132,7 @@ export const SupportPage = () => {
           <section className="w-2/3 block md:flex md:flex-row max-w-screen-xl m-auto px-4 lg:px-8 pt-8 gap-10">
             <div className="flex-1">
               <Accordion type="multiple" value={openAccordions} onValueChange={setOpenAccordions}>
-                {oneMACFAQContent.map(({ sectionTitle, qanda }, index) => (
+                {supportContent.map(({ sectionTitle, qanda }, index) => (
                   <article key={sectionTitle} className="mb-8">
                     {/* The first article has the expand button on the right */}
                     {index === 0 ? (
