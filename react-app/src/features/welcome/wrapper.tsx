5b1c28fafa1bea8aac00c286121a622a4c61e957
Gavin St. Ours <gst-ours@fearless.tech>
2025-03-20 12:23:36 -0400

    Enables CMS welcome if CMS user

--- react-app/src/features/welcome/wrapper.tsx
+++ react-app/src/features/welcome/wrapper.tsx
@@ -1,15 +1,14 @@
-import React from "react";
+import { isCmsUser } from "shared-utils";
 
-// import { isCmsUser } from "shared-utils";
-// import { useGetUser } from "@/api";
+import { useGetUser } from "@/api";
 import * as F from "@/features";
 
 export const WelcomeWrapper = () => {
-  // const { data: user } = useGetUser();
+  const { data: user } = useGetUser();
 
   // Check if the user exists and has a CMS role
-  // return user && isCmsUser(user.user) ? <F.CMSWelcome /> : <F.Welcome />;
+  return user && isCmsUser(user.user) ? <F.CMSWelcome /> : <F.Welcome />;
 
   // Uncomment to test the CMSWelcome component:
-  return <F.CMSWelcome />;
+  // return <F.CMSWelcome />;
 };
