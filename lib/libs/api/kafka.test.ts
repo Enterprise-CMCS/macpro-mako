d949ae0f764aa62cf0fb7ab5c8e864fdda05d4a0
James Dinh <jdinh@collabralink.com>
2025-03-24 10:07:51 -0700

    Update kafka.test.ts

--- lib/libs/api/kafka.test.ts
+++ lib/libs/api/kafka.test.ts
@@ -72,19 +72,6 @@ describe("Kafka producer functions", () => {
     expect(mockedProducer.send).toHaveBeenCalled();
   });
 
-  it("should throw an error if Kafka send fails and error not an instance of an error", async () => {
-    const topic = "test-topic";
-    const key = "test-key";
-    const value = JSON.stringify({ foo: "bar" });
-
-    const error = { message: "Failed to send message" };
-    mockedProducer.send.mockRejectedValueOnce(error);
-
-    await expect(produceMessage(topic, key, value)).rejects.toThrow(
-      "Failed to send message to Kafka",
-    );
-  });
-
   it("should throw an error if brokerString is not defined", () => {
     delete process.env.brokerString;
     expect(() => getProducer()).toThrowError();
