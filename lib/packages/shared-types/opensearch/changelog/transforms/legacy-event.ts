cbc4f320eabc28083deca7235d1fcb8da986addb
tiffanyvu <tiffany.v330@gmail.com>
2025-03-24 14:01:22 -0700

    add GSI1pk type

--- lib/packages/shared-types/opensearch/changelog/transforms/legacy-event.ts
+++ lib/packages/shared-types/opensearch/changelog/transforms/legacy-event.ts
@@ -4,54 +4,66 @@ import { ONEMAC_LEGACY_ORIGIN } from "../../main/transforms/legacy-transforms";
 export const transform = (id: string) => {
   return legacyEventSchema.transform((data) => {
     // Resolve the action type based on the GSI1pk
-    const eventType = data?.GSI1pk?.split("OneMAC#submit")?.[1] || "";
-
+    const eventTypeMatch = data?.GSI1pk?.match(/OneMAC#(submit|spa|waiver)(.*)/i);
+    const eventType = eventTypeMatch?.[1]?.toLowerCase();
+    let submitType: string = "";
     let event;
 
-    switch (eventType) {
-      case "chipspa":
-      case "medicaidspa":
-      case "waiveramendment":
-      case "waiverappk":
-      case "waiverextension":
-      case "waiverextensionb":
-      case "waiverextensionc":
-      case "waivernew":
-      case "waiverrenewal":
-        event = "new-legacy-submission";
-        break;
-      case "chipsparai":
-      case "medicaidsparai":
-      case "waiveramendmentrai":
-      case "waiverappkrai":
-      case "waiverrai":
-        event = Action.RESPOND_TO_RAI;
-        break;
-      case "chipspawithdraw":
-      case "medicaidspawithdraw":
-      case "waiveramendmentwithdraw":
-      case "waiverappkwithdraw":
-      case "waivernewwithdraw":
-      case "waiverrenewalwithdraw":
-        event = Action.WITHDRAW_PACKAGE;
-        break;
-      case "rairesponsewithdraw":
-        event = Action.LEGACY_WITHDRAW_RAI_REQUEST;
-        break;
-      case "medicaidspasubsequent":
-      case "chipspasubsequent":
-      case "waiverappksubsequent":
-      case "waivernewsubsequent":
-      case "waiverrenewalsubsequent":
-      case "waiveramendmentsubsequent":
-        event = Action.UPLOAD_SUBSEQUENT_DOCUMENTS;
-        break;
-      default:
-        console.log(
-          `Unhandled event type for ${id}:  ${eventType}.  Doing nothing and continuing.`,
-        );
-        event = undefined;
-        break;
+    // const eventType = data?.GSI1pk?.split("OneMAC#submit")?.[1] || "";
+    if (eventType === "spa" || eventType === "waiver") {
+      console.log("IN THIS ONE?");
+      event = "new-legacy-submission";
+    }
+
+    if (eventType === "submit") {
+      submitType = eventTypeMatch?.[2] || "";
+      console.log("IN HERE???");
+
+      switch (submitType) {
+        case "chipspa":
+        case "medicaidspa":
+        case "waiveramendment":
+        case "waiverappk":
+        case "waiverextension":
+        case "waiverextensionb":
+        case "waiverextensionc":
+        case "waivernew":
+        case "waiverrenewal":
+          event = "new-legacy-submission";
+          break;
+        case "chipsparai":
+        case "medicaidsparai":
+        case "waiveramendmentrai":
+        case "waiverappkrai":
+        case "waiverrai":
+          event = Action.RESPOND_TO_RAI;
+          break;
+        case "chipspawithdraw":
+        case "medicaidspawithdraw":
+        case "waiveramendmentwithdraw":
+        case "waiverappkwithdraw":
+        case "waivernewwithdraw":
+        case "waiverrenewalwithdraw":
+          event = Action.WITHDRAW_PACKAGE;
+          break;
+        case "rairesponsewithdraw":
+          event = Action.LEGACY_WITHDRAW_RAI_REQUEST;
+          break;
+        case "medicaidspasubsequent":
+        case "chipspasubsequent":
+        case "waiverappksubsequent":
+        case "waivernewsubsequent":
+        case "waiverrenewalsubsequent":
+        case "waiveramendmentsubsequent":
+          event = Action.UPLOAD_SUBSEQUENT_DOCUMENTS;
+          break;
+        default:
+          console.log(
+            `Unhandled event type for ${id}:  ${eventType}.  Doing nothing and continuing.`,
+          );
+          event = undefined;
+          break;
+      }
     }
 
     // Return if the actionType is unhandled
