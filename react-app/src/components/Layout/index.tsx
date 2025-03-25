0d53d706b0d5128f9a6e88fe2e3479ae26ef5580
Alex Youssefinia <ayoussefinia@fearless.tech>
2025-03-25 17:17:27 -0500

    correct event capture

--- react-app/src/components/Layout/index.tsx
+++ react-app/src/components/Layout/index.tsx
@@ -174,21 +174,21 @@ export const Layout = () => {
 
   useEffect(()=>{
     if(customUserRoles.length > 0) {
-      if(customisMemberOf.includes("onemac-state-user")){
+      if(customUserRoles.includes("onemac-state-user")){
         console.log("user login send state user event");
         ReactGA.set({user_roles: "State Submitter"});
         console.log("user roles" +  customUserRoles);
         ReactGA.event('User Login' + ' - ' + "State Submitter", {
           user_roles: "State Submitter"
         });
-      } else if (customisMemberOf.includes("onemac-state-user")){
+      } else if (customUserRoles.includes("onemac-helpdesk")){
         console.log("user login helpdesk event");
         console.log("user roles" +  customUserRoles);
         ReactGA.set({user_roles: "Helpdesk"});
         ReactGA.event('User Login' + ' - ' + "Helpdesk", {
           user_roles: "Helpdesk"
         });
-      } else if (customisMemberOf.includes("onemac-micro-readonly")){
+      } else if (customUserRoles.includes("onemac-micro-readonly")){
         console.log("user login read only event");
         console.log("user roles" +  customUserRoles);
         ReactGA.set({user_roles: "CMS Read Only"});
