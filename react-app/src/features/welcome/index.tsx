67beecc7431abfc49dbc5cb8a9aab8c48ba37104
Daniel Belcher <daniel.belcher.cs@gmail.com>
2025-03-25 17:33:02 -0700

    test new branch

--- react-app/src/features/welcome/index.tsx
+++ react-app/src/features/welcome/index.tsx
@@ -1,6 +1,6 @@
 import * as Heroicons from "@heroicons/react/24/outline";
 import { QueryClient } from "@tanstack/react-query";
-import { Link } from "react-router";
+import { Link, Navigate } from "react-router";
 
 import { getUser } from "@/api";
 import * as C from "@/components";
@@ -12,23 +12,33 @@ import { useFeatureFlag } from "@/hooks/useFeatureFlag";
 
 export const loader = (queryClient: QueryClient) => {
   return async () => {
+    // Check user query
+    const userFetch = await queryClient.fetchQuery({
+      queryKey: ["user"],
+      queryFn: () => getUser(),
+    });
+    // Verify location point for login redirect
+    const loginRedirect =
+      !["/login", "/faq"].includes(window.location.pathname) && !userFetch?.user;
+
+    if (loginRedirect) {
+      return <Navigate to="/login" />;
+    }
+
+    // Grab query string for possible errors
     const queryString = window.location.search;
     // Parse the query string to get URL parameters
     const queryParams = new URLSearchParams(queryString);
     // Access specific parameters
     const errorDescription = queryParams.get("error_description");
     const error = queryParams.get("error");
+
     if (errorDescription || error) {
       console.error("Authentication Error:", { errorDescription, error });
       return { error };
     }
-    if (!queryClient.getQueryData(["user"])) {
-      return await queryClient.fetchQuery({
-        queryKey: ["user"],
-        queryFn: () => getUser(),
-      });
-    }
-    return queryClient.getQueryData(["user"]);
+
+    return userFetch;
   };
 };
 
