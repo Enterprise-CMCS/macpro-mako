921c516baabb6567fa481304978a1d09fa6a45b8
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2025-03-28 15:22:06 -0400

    😱

--- lib/lambda/user-management/getUserProfile.ts
+++ lib/lambda/user-management/getUserProfile.ts
@@ -7,7 +7,9 @@ export const getUserProfile = async (event: APIGatewayEvent) => {
   try {
     console.log("this is the event: ", event);
     const authDetails = getAuthDetails(event);
+    console.log("what are the auth details", authDetails);
     const userAttributes = await lookupUserAttributes(authDetails.userId, authDetails.poolId);
+    console.log("what are the user attributes", userAttributes);
 
     return await getAllUserRolesByEmail(userAttributes.email);
   } catch (err: unknown) {
