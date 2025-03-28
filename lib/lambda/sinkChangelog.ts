b93448c39c0fd03f5d3fd2b15cdae6ee34a63c49
tiffanyvu <tiffany.v330@gmail.com>
2025-03-28 11:01:24 -0700

    test legacy records with reverseChrono

--- lib/lambda/sinkChangelog.ts
+++ lib/lambda/sinkChangelog.ts
@@ -141,28 +141,34 @@ const processAndIndex = async ({
         record.origin = "onemac";
         console.log("WHAT DOES THE RECORD LOOK LIKE", record);
       }
+      // flatten legacy data record with multiple events
+      const recordsToProcess = record.reverseChrono?.length
+        ? record.reverseChrono.map((activity: any) => ({ ...record, ...activity }))
+        : [record];
 
-      // If the event is a supported event, transform and push to docs array for indexing
-      if (record.event in transforms) {
-        console.log("ARE WE IN HERE LEGACY DATA");
-        const transformForEvent = transforms[record.event as keyof typeof transforms];
-        console.log(transformForEvent, "TRANSFORM FOR EVENT");
-        const result = transformForEvent.transform(offset).safeParse(record);
-        console.log(result, "WHAT IS THE RESULT");
+      for (const currentRecord of recordsToProcess) {
+        // If the event is a supported event, transform and push to docs array for indexing
+        if (currentRecord.event in transforms) {
+          console.log("ARE WE IN HERE LEGACY DATA");
+          const transformForEvent = transforms[currentRecord.event as keyof typeof transforms];
+          console.log(transformForEvent, "TRANSFORM FOR EVENT");
+          const result = transformForEvent.transform(offset).safeParse(record);
+          console.log(result, "WHAT IS THE RESULT");
 
-        if (result.success && result.data === undefined) continue;
-        if (!result.success) {
-          logError({
-            type: ErrorType.VALIDATION,
-            error: result?.error,
-            metadata: { topicPartition, kafkaRecord, record },
-          });
-          continue;
+          if (result.success && result.data === undefined) continue;
+          if (!result.success) {
+            logError({
+              type: ErrorType.VALIDATION,
+              error: result?.error,
+              metadata: { topicPartition, kafkaRecord, record },
+            });
+            continue;
+          }
+          console.log("WHAT IS RESULT DATA AT THIS POINT", result.data);
+          docs.push(result.data);
+        } else {
+          console.log(`No transform found for event: ${record.event}`);
         }
-        console.log("WHAT IS RESULT DATA AT THIS POINT", result.data);
-        docs.push(result.data);
-      } else {
-        console.log(`No transform found for event: ${record.event}`);
       }
     } catch (error) {
       logError({
