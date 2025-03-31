9e75c89ccf4ddcdb35a15da4b4b80c23df03f374
tiffanyvu <tiffany.v330@gmail.com>
2025-03-31 11:22:33 -0700

    questionable things

--- lib/packages/shared-types/events/legacy-event.ts
+++ lib/packages/shared-types/events/legacy-event.ts
@@ -28,8 +28,8 @@ export const legacyEventSchema = legacySharedSchema
   .transform((data) => {
     const seatoolStatus = getSeaToolStatusFromLegacyStatus(data.currentStatus);
     const { stateStatus, cmsStatus } = getStatus(seatoolStatus);
-    const lastEventTimestampDate = new Date(data.lastEventTimestamp);
-    const submissionTimestampDate = new Date(data.submissionTimestamp);
+    const lastEventTimestampDate = new Date(data.lastEventTimestamp ?? data.eventTimestamp);
+    const submissionTimestampDate = new Date(data.submissionTimestamp ?? data.eventTimestamp);
     const isRaiResponseWithdrawEnabled = data.subStatus === "Withdraw Formal RAI Response Enabled";
     console.log(data, "THIS IS THE LGEACY DATA???");
     return {
