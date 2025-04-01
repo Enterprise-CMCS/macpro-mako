e7119b64b5bd5e3f6ac893a13cee70aaaff0c153
tiffanyvu <tiffany.v330@gmail.com>
2025-04-01 13:10:59 -0700

    test fix id for admin legacy change

--- lib/packages/shared-types/events/legacy-admin-change.ts
+++ lib/packages/shared-types/events/legacy-admin-change.ts
@@ -1,12 +1,17 @@
 import { z } from "zod";
 
 // A schema for legacy admin changes
-export const legacyAdminChangeSchema = z.object({
-  changeTimestamp: z.number(),
-  changeType: z.string().nullish().default("legacy-admin-change"),
-  changeMade: z.string(),
-  changeReason: z.string().optional().default(""),
-  // isAdminChange: z.boolean(),
-});
+export const legacyAdminChangeSchema = z
+  .object({
+    pk: z.string(),
+    changeTimestamp: z.number(),
+    changeType: z.string().nullish(),
+    changeMade: z.string(),
+    changeReason: z.string().optional().default(""),
+  })
+  .transform((data) => ({
+    ...data,
+    id: data.pk,
+  }));
 
 export type LegacyAdminChange = z.infer<typeof legacyAdminChangeSchema>;
