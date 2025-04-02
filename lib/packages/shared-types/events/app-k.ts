6c18e607aef95babc9cffa0d01cea9396e965b6e
Thomas Walker <thomas.e.walker88@gmail.com>
2025-04-02 10:26:53 -0400

    tech-debt(cleanup)  Combining shared-types with shared schemas (#1358)
    * Simplify the schemas to a sharedbase
    
    * update schema from incorrect
    
    * remove console.log

--- lib/packages/shared-types/events/app-k.ts
+++ lib/packages/shared-types/events/app-k.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { attachmentArraySchema, attachmentArraySchemaOptional } from "../attachments";
+import { ammendSchema } from "./base-schema";
 
 export const baseSchema = z.object({
   id: z
@@ -27,12 +28,6 @@ export const baseSchema = z.object({
   additionalInformation: z.string().max(4000).nullable().default(null).optional(),
 });
 
-export const schema = baseSchema.extend({
-  actionType: z.string().default("Amend"),
-  origin: z.literal("mako").default("mako"),
-  submitterName: z.string(),
-  submitterEmail: z.string().email(),
-  timestamp: z.number(),
-});
+export const schema = baseSchema.merge(ammendSchema);
 
 export type AppKSubmission = z.infer<typeof schema>;
