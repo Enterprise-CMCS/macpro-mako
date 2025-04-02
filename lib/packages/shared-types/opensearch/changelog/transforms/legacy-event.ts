c80988562dc217ae4d05e2a4d1401ce82d7a9b22
tiffanyvu <tiffany.v330@gmail.com>
2025-04-02 13:09:31 -0700

    test move to separate func

--- lib/packages/shared-types/opensearch/changelog/transforms/legacy-event.ts
+++ lib/packages/shared-types/opensearch/changelog/transforms/legacy-event.ts
@@ -1,70 +1,73 @@
-import { Action, handleLegacyAttachment, legacyEventSchema } from "../../..";
+import { getLegacyEventType } from "shared-utils";
+
+import { handleLegacyAttachment, legacyEventSchema } from "../../..";
 import { ONEMAC_LEGACY_ORIGIN } from "../../main/transforms/legacy-transforms";
 
-export const transform = (id: string) => {
+export const transform = () => {
   return legacyEventSchema.transform((data) => {
-    // Resolve the action type based on the GSI1pk
-    const eventTypeMatch = data?.GSI1pk?.match(/OneMAC#(submit|spa|waiver)(.*)/i);
-    const eventType = eventTypeMatch?.[1]?.toLowerCase();
-    let submitType: string = "";
-    let event;
+    const eventType = getLegacyEventType(data?.GSI1pk);
+    // // Resolve the action type based on the GSI1pk
+    // const eventTypeMatch = data?.GSI1pk?.match(/OneMAC#(submit|spa|waiver)(.*)/i);
+    // const eventType = eventTypeMatch?.[1]?.toLowerCase();
+    // let submitType: string = "";
+    // let event;
 
-    if (eventType === "spa" || eventType === "waiver") {
-      event = "new-legacy-submission";
-    }
+    // if (eventType === "spa" || eventType === "waiver") {
+    //   event = "new-legacy-submission";
+    // }
 
-    if (eventType === "submit") {
-      submitType = eventTypeMatch?.[2] || "";
+    // if (eventType === "submit") {
+    //   submitType = eventTypeMatch?.[2] || "";
 
-      switch (submitType) {
-        case "chipspa":
-        case "medicaidspa":
-        case "waiveramendment":
-        case "waiverappk":
-        case "waiverextension":
-        case "waiverextensionb":
-        case "waiverextensionc":
-        case "waivernew":
-        case "waiverrenewal":
-          event = "new-legacy-submission";
-          break;
-        case "chipsparai":
-        case "medicaidsparai":
-        case "waiveramendmentrai":
-        case "waiverappkrai":
-        case "waiverrai":
-          event = Action.RESPOND_TO_RAI;
-          break;
-        case "chipspawithdraw":
-        case "medicaidspawithdraw":
-        case "waiveramendmentwithdraw":
-        case "waiverappkwithdraw":
-        case "waivernewwithdraw":
-        case "waiverrenewalwithdraw":
-          event = Action.WITHDRAW_PACKAGE;
-          break;
-        case "rairesponsewithdraw":
-          event = Action.LEGACY_WITHDRAW_RAI_REQUEST;
-          break;
-        case "medicaidspasubsequent":
-        case "chipspasubsequent":
-        case "waiverappksubsequent":
-        case "waivernewsubsequent":
-        case "waiverrenewalsubsequent":
-        case "waiveramendmentsubsequent":
-          event = Action.UPLOAD_SUBSEQUENT_DOCUMENTS;
-          break;
-        default:
-          console.log(
-            `Unhandled event type for ${id}:  ${eventType}.  Doing nothing and continuing.`,
-          );
-          event = undefined;
-          break;
-      }
-    }
+    //   switch (submitType) {
+    //     case "chipspa":
+    //     case "medicaidspa":
+    //     case "waiveramendment":
+    //     case "waiverappk":
+    //     case "waiverextension":
+    //     case "waiverextensionb":
+    //     case "waiverextensionc":
+    //     case "waivernew":
+    //     case "waiverrenewal":
+    //       event = "new-legacy-submission";
+    //       break;
+    //     case "chipsparai":
+    //     case "medicaidsparai":
+    //     case "waiveramendmentrai":
+    //     case "waiverappkrai":
+    //     case "waiverrai":
+    //       event = Action.RESPOND_TO_RAI;
+    //       break;
+    //     case "chipspawithdraw":
+    //     case "medicaidspawithdraw":
+    //     case "waiveramendmentwithdraw":
+    //     case "waiverappkwithdraw":
+    //     case "waivernewwithdraw":
+    //     case "waiverrenewalwithdraw":
+    //       event = Action.WITHDRAW_PACKAGE;
+    //       break;
+    //     case "rairesponsewithdraw":
+    //       event = Action.LEGACY_WITHDRAW_RAI_REQUEST;
+    //       break;
+    //     case "medicaidspasubsequent":
+    //     case "chipspasubsequent":
+    //     case "waiverappksubsequent":
+    //     case "waivernewsubsequent":
+    //     case "waiverrenewalsubsequent":
+    //     case "waiveramendmentsubsequent":
+    //       event = Action.UPLOAD_SUBSEQUENT_DOCUMENTS;
+    //       break;
+    //     default:
+    //       console.log(
+    //         `Unhandled event type for ${id}:  ${eventType}.  Doing nothing and continuing.`,
+    //       );
+    //       event = undefined;
+    //       break;
+    //   }
+    // }
 
     // Return if the actionType is unhandled
-    if (event === undefined) return undefined;
+    if (eventType === undefined) return undefined;
 
     // If we're still here, go ahead and transform the data
     const transformedData = {
@@ -73,7 +76,7 @@ export const transform = (id: string) => {
       id: `${data.componentId}-legacy-${data.eventTimestamp ? data.eventTimestamp : data.lastEventTimestamp}`,
       packageId: data.componentId,
       timestamp: data.eventTimestamp,
-      event: event,
+      event: eventType,
       attachments:
         data.attachments?.map((attachment) => handleLegacyAttachment(attachment)) ?? null,
       additionalInformation: data.additionalInformation,
