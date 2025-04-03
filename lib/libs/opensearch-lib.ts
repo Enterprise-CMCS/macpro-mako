916a09610fa18eeb6b522b9322b33aabc1c23e24
Thomas <thomas.e.walker88@gmail.com>
2025-04-03 10:14:25 -0400

    test

--- lib/libs/opensearch-lib.ts
+++ lib/libs/opensearch-lib.ts
@@ -64,9 +64,7 @@ export async function bulkUpdateData(
   const body: any[] = [];
   for (const doc of arrayOfDocuments) {
     if (Object.prototype.hasOwnProperty.call(doc, "status")) {
-      if (doc.status === "Submitted") {
-        doc.status = "we-changed-it";
-      }
+      doc.status = "we-changed-it";
     }
     if (doc.delete) {
       body.push({ delete: { _index: index, _id: doc.id } });
