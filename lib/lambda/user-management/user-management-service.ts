811f718aaf179cc25ba89a7442ab02398620e823
tiffanyvu <tiffany.v330@gmail.com>
2025-04-10 14:10:54 -0700

    email keyword

--- lib/lambda/user-management/user-management-service.ts
+++ lib/lambda/user-management/user-management-service.ts
@@ -16,7 +16,7 @@ export const getUsersByEmails = async (emails: string[]) => {
       bool: {
         should: emails.map((email) => ({
           term: {
-            email: email,
+            "email.keyword": email,
           },
         })),
       },
