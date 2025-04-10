4a602b8057a1858602e3185d6534ca0879933446
Andie Swift <andie.swift5@gmail.com>
2025-04-10 13:53:44 -0500

    andie - added the user table, did some startign styling WIP

--- react-app/src/api/useGetRoleRequests.ts
+++ react-app/src/api/useGetRoleRequests.ts
@@ -4,7 +4,7 @@ import { API } from "aws-amplify";
 export const getRoleRequests = async (): Promise<string> => {
   try {
     const role = await API.get("os", "/getRoleRequests", {});
-    return role;
+    return JSON.stringify(role);
   } catch (e) {
     console.log({ e });
     return "";
