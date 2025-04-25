41f49c758c2d8c97b8aced31e937032de27c496f
Jon Schaffer <jschaffer@gswell.com>
2025-04-25 09:03:22 -0400

    feat: home page smoke tests (#1403)
    feat: remaining PW tests  of landing page
    
    feat: add smoke coverage
    
    fix: remove url from config

--- test/e2e/home/user-profile.spec.ts
+++ test/e2e/home/user-profile.spec.ts
@@ -1,9 +1,28 @@
-import { test } from "@/fixtures/mocked";
+import { expect, test } from "@/fixtures/mocked";
 
-test.describe.skip("User Profile", () => {
+test.describe("User Profile", { tag: ["@profile", "@smoke"] }, () => {
   test.skip("Page header", async () => {});
 
   test.skip("Goto IDM", async () => {});
 
-  test.skip("My Information", async () => {});
+  test.describe("My Information", async () => {
+    test("state user", async ({ page }) => {
+      await page.goto("/profile");
+
+      await expect(page.getByText("State Submitter")).toBeVisible();
+      await expect(page.getByText("State Submitter")).toHaveText("State Submitter");
+    });
+
+    test("reviewer user", async ({ browser }) => {
+      const reviewerContext = await browser.newContext({
+        storageState: "playwright/.auth/reviewer-user.json",
+      });
+      const page = await reviewerContext.newPage();
+
+      await page.goto("/profile");
+
+      await expect(page.getByText("Reviewer", { exact: true })).toBeVisible();
+      await expect(page.getByText("Reviewer", { exact: true })).toHaveText("Reviewer");
+    });
+  });
 });
