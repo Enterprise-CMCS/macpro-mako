59e7ff81cfb8d7002ba88c889a8c4cc516379e6c
Brandon Flynn <bflynn@luckyrabbit.tech>
2025-04-30 07:21:33 -0500

    add waiver id to heading

--- lib/libs/email/content/uploadSubsequentDocuments/emailTemplates/Waiver1915BCMS.tsx
+++ lib/libs/email/content/uploadSubsequentDocuments/emailTemplates/Waiver1915BCMS.tsx
@@ -14,8 +14,8 @@ export const WaiversEmailCMS = ({
   variables: Events["UploadSubsequentDocuments"] & CommonEmailVariables;
 }) => (
   <BaseEmailTemplate
-    previewText={`Action required: review new documents for 1915(b) ${variables.actionType} Waiver in OneMAC`}
-    heading={`New documents have been submitted for 1915(b) ${variables.actionType} Waiver in OneMAC.`}
+    previewText={`Action required: review new documents for 1915(b) ${variables.actionType} Waiver ${variables.id} in OneMAC`}
+    heading={`New documents have been submitted for 1915(b) ${variables.actionType} Waiver ${variables.id} in OneMAC.`}
     applicationEndpointUrl={variables.applicationEndpointUrl}
     footerContent={<BasicFooter />}
   >
