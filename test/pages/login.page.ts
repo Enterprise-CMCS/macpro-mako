f3cc5a0a0b280fde95ca8954edf7321384dab528
James Dinh <jdinh@collabralink.com>
2025-04-30 18:31:01 -0700

    Update login.page.ts

--- test/pages/login.page.ts
+++ test/pages/login.page.ts
@@ -6,10 +6,10 @@ export class LoginPage {
   async goto() {
     await this.page.goto("/");
     const loginButton = this.page.getByRole("button", { name: "Log in" });
-    const signInButton = this.page.getByRole("button", { name: "Sign In" });
+    // const signInButton = this.page.getByRole("button", { name: "Sign In" });
 
     if (await loginButton.isVisible()) await loginButton.click();
-    if (await signInButton.isVisible()) await signInButton.click();
+    // if (await signInButton.isVisible()) await signInButton.click();
   }
 
   async login(email: string, password: string) {
