f1e4d36cda14fb172e4420cf5c7678ba25aa0421
Brandon Flynn <bflynn@luckyrabbit.tech>
2025-05-01 20:45:45 -0500

    update transcript

--- react-app/src/features/faq/content/oneMACFAQContent.tsx
+++ react-app/src/features/faq/content/oneMACFAQContent.tsx
@@ -411,47 +411,58 @@ export const oneMACFAQContent: FAQContent[] = [
                   <AccordionContent>
                     <div className="space-y-2">
                       <p>
-                        OneMac has been updated with a new package action called upload subsequent
-                        documentation that allows states to submit revised or additional
-                        documentation to an under-review spa or waiver submission package.
+                        OneMac has been updated with a new package action called Upload Subsequent
+                        Documentation that allows states to submit revised or additional
+                        documentation to an under review SPA or waiver submission package.
                       </p>
                       <p>
                         Now, instead of having to email these documents to CMS, you can upload them
-                        directly to your submission package in OneMac. The subsequent documentation
-                        feature is available for submission packages that are in the under-review
-                        status. The functionality is accessed in the same way as all other package
-                        actions in OneMac, either directly from the package dashboard as an option
-                        in the actions column drop-down menu, or from within the submission package
-                        itself under the package actions section.
+                        directly to your submission package in OneMac. The Subsequent Documentation
+                        feature is available for submission packages that are in the under review
+                        status.
                       </p>
                       <p>
-                        After selecting the upload subsequent documents package action, you are
-                        taken to the subsequent documentation details page. Select the add file
-                        button for the appropriate document type to add any additional documents
-                        that need to be submitted to CMS. You can submit multiple documents at once,
-                        however, at least one attachment is required. Next, fill out the reason for
-                        subsequent documentation section, explaining why additional documents are
-                        being submitted.
+                        The functionality is accessed in the same way as all other package actions
+                        in OneMac, either directly from the package dashboard as an option in the
+                        Actions column drop-down menu, or from within the submission package itself
+                        under the Package Actions section.
                       </p>
                       <p>
-                        After all information has been added, select submit. You will receive a
-                        confirmation message indicating that these documents will be added to the
-                        submission package and reviewed by CMS. If you are certain you wish to add
-                        these documents to the submission package, select yes, submit. A green
-                        banner is shown indicating that the documents have been submitted and CMS
-                        reviewers will follow up by email if additional information is needed.
-                        Additionally, an email notification will be sent indicating that the action
-                        was taken and CMS will be notified. A new latest package activity field was
-                        also added to the package dashboard and submission package details section.
-                        This field will automatically update to reflect when the most recent package
-                        action occurred, including when subsequent documentation is submitted.
+                        After selecting the Upload Subsequent Documents package action, you are
+                        taken to the Subsequent Documentation Details page. Select the Choose From
+                        Folder link for the appropriate document type, or drag and drop a file from
+                        your computer to add any additional documents that need to be submitted to
+                        CMS. You can submit multiple documents at once, however, at least one
+                        attachment is required.
                       </p>
                       <p>
-                        Thank you for watching this overview of the latest OneMAC updates. If you
-                        have any questions, please free to contact the OneMAC Help Desk at{" "}
-                        <a className="text-primary" href="mailto: OneMAC_Helpdesk@cms.hhs.gov">
+                        Next, fill out the Reason for Subsequent Documentation section explaining
+                        why additional documents are being submitted. After all information has been
+                        added, select Submit. You will receive a confirmation message indicating
+                        that these documents will be added to the submission package and reviewed by
+                        CMS. If you are certain you wish to add these documents to the submission
+                        package, select Yes Submit.
+                      </p>
+                      <p>
+                        A green banner is shown indicating that the documents have been submitted
+                        and CMS reviewers will follow up by email if additional information is
+                        needed. Additionally, an email notification will be sent indicating that the
+                        action was taken and CMS will be notified.
+                      </p>
+                      <p>
+                        A new Latest Package Activity field was also added to the Package Dashboard
+                        and Submission Package Details section. This field will automatically update
+                        to reflect when the most recent package action occurred, including when
+                        subsequent documentation is submitted.
+                      </p>
+                      <p>
+                        Thank you for watching this overview of the new subsequent documentation
+                        package action in OneMac. If you have any questions, please feel free to
+                        contact the OneMac Help Desk at{" "}
+                        <a className="text-primary" href="mailto:OneMAC_Helpdesk@cms.hhs.gov">
                           OneMAC_Helpdesk@cms.hhs.gov
                         </a>
+                        .
                       </p>
                     </div>
                   </AccordionContent>
