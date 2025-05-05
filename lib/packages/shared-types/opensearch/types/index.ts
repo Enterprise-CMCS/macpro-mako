bd20035ba8066c9fa2e2d4d5249e91c6c1de30ac
tiffanyvu <tiffany.v330@gmail.com>
2025-05-05 10:12:32 -0700

    add user and role os type

--- lib/packages/shared-types/opensearch/types/index.ts
+++ lib/packages/shared-types/opensearch/types/index.ts
@@ -1,13 +1,36 @@
 import { z } from "zod";
 
+import {
+  onemacLegacyUserRoleRequest,
+  userInformation,
+  UserRole,
+  userRoleRequest,
+} from "../../events/legacy-user";
 import { AggQuery, Filterable as FIL, Hit, QueryState, Response as Res } from "./../_";
 import { SPA_Type } from "./transforms";
 
 export type Document = z.infer<SPA_Type.Schema>;
+export type UserDocument = z.infer<typeof userInformation>;
+export type RoleDocument =
+  | z.infer<typeof userRoleRequest>
+  | z.infer<typeof onemacLegacyUserRoleRequest>;
 
 export type Response = Res<Document>;
 export type ItemResult = Hit<Document> & {
   found: boolean;
+};
+export type UserResult = {
+  _id: string;
+  found: boolean;
+  _source: UserDocument & {
+    role: UserRole;
+  };
+};
+
+export type RoleResult = {
+  _id: string;
+  found: boolean;
+  _source: RoleDocument;
 };
 
 export type Field = keyof Document | `${keyof Document}.keyword`;
