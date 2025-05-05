bdb3776d3aac1444ffeb2a1e4ebdf84b5e7296ad
Brandon Flynn <bflynn@luckyrabbit.tech>
2025-05-05 09:17:21 -0500

    use package id not event id for changelog id prefix

--- lib/packages/shared-types/opensearch/changelog/transforms/legacy-admin-change.ts
+++ lib/packages/shared-types/opensearch/changelog/transforms/legacy-admin-change.ts
@@ -4,7 +4,7 @@ export const transform = (id: string) => {
   return legacyAdminChangeSchema.transform((data) => {
     const transformedData = {
       // Timestamp is what makes admin changes unique; we add it to the id here, instead of the offset
-      id: `${id}-legacy-admin-change-${data.changeTimestamp}`,
+      id: `${data.id}-legacy-admin-change-${data.changeTimestamp}`,
       packageId: data.id,
       timestamp: data.changeTimestamp,
       actionType: Action.LEGACY_ADMIN_CHANGE,
