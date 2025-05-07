56b355feb202e408d35101177d563f66a5a15afd
Alex Youssefinia <ayoussefinia@fearless.tech>
2025-05-07 13:37:51 -0500

    commit before switch branch

--- lib/packages/shared-types/events/legacy-event.ts
+++ lib/packages/shared-types/events/legacy-event.ts
@@ -28,11 +28,15 @@ export const legacyEventSchema = legacySharedSchema
     }),
   )
   .transform((data) => {
+
     console.log("legacy event .ts transform: ");
+    console.log("component Id", data.componentId)
     console.log("data.currentStatus: ", data.currentStatus);
-    console.log("seatoolStatus: ", getSeaToolStatusFromLegacyStatus(data.currentStatus));
     const seatoolStatus = getSeaToolStatusFromLegacyStatus(data.currentStatus);
+    console.log("seatoolStatus: ", getSeaToolStatusFromLegacyStatus(data.currentStatus));
     const { stateStatus, cmsStatus } = getStatus(seatoolStatus);
+    console.log("stateStatus:", stateStatus);
+    console.log("cmsStatus: ", cmsStatus);
     const lastEventIsoDate = getIsoDateFromTimestamp(data.lastActivityTimestamp);
     const submissionIsoDate = getIsoDateFromTimestamp(data.submissionTimestamp);
     const isRaiResponseWithdrawEnabled = data.subStatus === "Withdraw Formal RAI Response Enabled";
