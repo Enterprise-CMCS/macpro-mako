93eb349a0e425ccf04cf4689935eac9afda249ca
Jonathan Schaffer <jschaffer@gswell.com>
2025-05-09 15:05:33 -0400

    fix: resolve output and config issue

--- test/lib/ci.setup.ts
+++ test/lib/ci.setup.ts
@@ -1,14 +1,14 @@
 import { test as setup } from "@playwright/test";
 
-import { getDeploymentConfig, getSecret } from "./auth.secrets";
+import { getDeploymentConfig, getDeploymentOutput, getSecret } from "./auth.secrets";
 import { generateAuthFile } from "./auth.setup";
 
 const stage = process.env.STAGE_NAME || "main";
 const project = process.env.PROJECT;
 const deploymentConfig = await getDeploymentConfig(stage, project);
-const baseURL = deploymentConfig.applicationEndpointUrl;
-// const password = await getSecret(deploymentConfig.devPasswordArn);
-const password = "";
+const deploymentOutput = await getDeploymentOutput(stage, project);
+const baseURL = deploymentOutput.applicationEndpointUrl;
+const password = await getSecret(deploymentConfig.devPasswordArn);
 
 const stateSubmitterAuthFile = "./playwright/.auth/state-user.json";
 const reviewerAuthFile = "./playwright/.auth/reviewer-user.json";
@@ -17,8 +17,6 @@ const testUsers = {
   state: "george@example.com",
   reviewer: "reviewer@example.com",
 };
-
-console.log("dp config", deploymentConfig);
 
 console.log(`[Setup] Stage: ${stage} | Project: ${project} | Base URL: ${baseURL}`);
 
