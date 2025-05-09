5aca62c931950f6fa64c0fcc590b28a8df22b840
Jonathan Schaffer <jschaffer@gswell.com>
2025-05-09 13:43:17 -0400

    fix: how project is used

--- bin/cli/src/commands/e2e.ts
+++ bin/cli/src/commands/e2e.ts
@@ -16,10 +16,6 @@ export const e2e = {
     console.log(ui);
     await runCommand("bun", ["playwright", "install", "--with-deps", "chromium"], ".");
 
-    await runCommand(
-      "bun",
-      [ui ? "e2e:ui" : "e2e", "--", "--", "--grep", "@CI", "--project", "ci"],
-      ".",
-    );
+    await runCommand("bun", [ui ? "e2e:ui" : "e2e", "--", "--", "--grep", "@CI"], ".");
   },
 };
