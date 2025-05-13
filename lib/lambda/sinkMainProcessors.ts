62c2b6614b7eaca828ca1234d948d6cbfffb7d35
Thomas <thomas.e.walker88@gmail.com>
2025-05-13 11:39:45 -0400

    see what hpapens in main

--- lib/lambda/sinkMainProcessors.ts
+++ lib/lambda/sinkMainProcessors.ts
@@ -79,6 +79,7 @@ const getOneMacRecordWithAllProperties = (
   kafkaRecord: KafkaRecord,
 ): OneMacRecord | undefined => {
   const record = JSON.parse(decodeBase64WithUtf8(value));
+  console.log(`kafkaRecord: ${JSON.stringify(kafkaRecord, null, 2)}`);
   const kafkaSource = String.fromCharCode(...(kafkaRecord.headers[0]?.source || []));
 
   if (isRecordAnAdminOneMacRecord(record)) {
