453f4566d20a6a000c9c81b1d2e70a8a27f486a3
tiffanyvu <tiffany.v330@gmail.com>
2025-05-21 13:54:34 -0700

    i dont wanna talk about it need sleep

--- react-app/src/features/sign-up/stateSignup.test.tsx
+++ react-app/src/features/sign-up/stateSignup.test.tsx
@@ -12,6 +12,7 @@ import { describe, expect, it } from "vitest";
 
 import { renderWithQueryClientAndMemoryRouter } from "@/utils/test-helpers";
 
+import { SignUp } from "./sign-up";
 import { StateSignup } from "./stateSignup";
 
 describe("StateSignup", () => {
@@ -34,7 +35,7 @@ describe("StateSignup", () => {
         },
         {
           path: "/signup",
-          element: <div>Signup</div>,
+          element: <SignUp />,
         },
         {
           path: "/signup/state",
@@ -111,21 +112,17 @@ describe("StateSignup", () => {
     setMockUsername(osStateSubmitter);
     const { user } = await setup();
 
-    const submitButton = screen.getByRole("button", { name: "Submit" });
-    expect(submitButton).toBeDisabled();
-
-    await user.click(screen.getByRole("combobox", { name: /Select state/ }));
-    expect(submitButton).toBeDisabled();
-
-    await waitFor(() => expect(screen.getByText("Maine")).toBeInTheDocument());
-    await user.click(screen.getByText("Maine"));
-    expect(submitButton).toBeEnabled();
-
     const cancelButton = screen.getByRole("button", { name: "Cancel" });
 
     await user.click(cancelButton);
+    await waitFor(() =>
+      expect(screen.getByText("Changes you made will not be saved.")).toBeInTheDocument(),
+    );
 
-    await waitFor(() => expect(screen.getByText("Registration: State Access")).toBeInTheDocument());
+    const confirmButton = screen.getByRole("button", { name: "Confirm" });
+    await user.click(confirmButton);
+
+    expect(screen.getByText(/Registration: User Role/)).toBeInTheDocument();
   });
 
   it("should show an error if there was an error submitting the request", async () => {
