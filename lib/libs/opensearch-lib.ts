953e60c91c346cd0750cd90c3972e91f6f223efc
Thomas <thomas.e.walker88@gmail.com>
2025-05-22 11:47:28 -0400

    additional logging

--- lib/libs/opensearch-lib.ts
+++ lib/libs/opensearch-lib.ts
@@ -63,6 +63,7 @@ export async function bulkUpdateData(
 
   const body: any[] = [];
   for (const doc of arrayOfDocuments) {
+    console.log("doc: " + JSON.stringify(doc));
     if (doc.delete) {
       body.push({ delete: { _index: index, _id: doc.id } });
     } else {
