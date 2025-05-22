cf6bc689611d7f06c038951477f5ad7a50230e1a
Andie Swift <aswift@gswell.com>
2025-05-22 09:42:24 -0500

    feat(ui): add icons to menu to match legacy (#1479)
    * updated lucide react// added icons to the profile
    
    * addressed test issues
    
    * fixed one more language thing
    
    * fixed test errors

--- react-app/src/components/Layout/index.tsx
+++ react-app/src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import { AwsCognitoOAuthOpts } from "@aws-amplify/auth/lib-esm/types";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { Auth } from "aws-amplify";
+import { LogOutIcon, UserPenIcon, UserPlusIcon } from "lucide-react";
 import { useState } from "react";
 import { Link, NavLink, NavLinkProps, Outlet, useNavigate, useRouteError } from "react-router";
 import { UserRoles } from "shared-types";
@@ -136,7 +137,6 @@ const UserDropdownMenu = () => {
 
     setTimeout(() => {
       localStorage.clear();
-
       Object.entries(preserved).forEach(([key, value]) => {
         localStorage.setItem(key, value);
       });
@@ -175,7 +175,10 @@ const UserDropdownMenu = () => {
               asChild
               onSelect={handleViewProfile}
             >
-              <li>View Profile</li>
+              <li>
+                <UserPenIcon className="inline mr-2" />
+                Manage Profile
+              </li>
             </DropdownMenu.Item>
             {/* TODO: conditionally show this if the user IS NOT HELPDESK */}
             {/* // helpdesk, system admins, and cms reviewer users don't even see request role as an option */}
@@ -185,7 +188,10 @@ const UserDropdownMenu = () => {
                 asChild
                 onSelect={() => navigate("/signup")}
               >
-                <li>Request a Role Change</li>
+                <li>
+                  <UserPlusIcon className="inline mr-2" />
+                  Request a Role Change
+                </li>
               </DropdownMenu.Item>
             )}
             <DropdownMenu.Item
@@ -193,7 +199,10 @@ const UserDropdownMenu = () => {
               asChild
               onSelect={handleLogout}
             >
-              <li>Sign Out</li>
+              <li>
+                <LogOutIcon className="inline mr-2" />
+                Log Out
+              </li>
             </DropdownMenu.Item>
           </ul>
         </DropdownMenu.Content>
