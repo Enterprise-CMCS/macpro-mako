b38e536ba7c456e8a7e2dc327d83813d2988f5d5
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2025-05-27 15:41:02 -0400

    fix array bug

--- lib/lambda/processUserRoleEmails.ts
+++ lib/lambda/processUserRoleEmails.ts
@@ -88,7 +88,13 @@ export async function sendUserRoleEmails(
     try {
       const filledTemplate = await template(record);
       const currentCcEmails = filledTemplate?.cc ?? [];
-      filledTemplate.cc = [...currentCcEmails, ...(emails?.userRoleCc ?? [])];
+      // This is used for injecting a test email for higher environments. If this
+      // secret exists it will inject that secrets cc email into the user roles
+      // email templates that get sent out (useful for testing email sending in dev, and val)
+      const userRoleCc = emails.userRoleCc ? [emails.userRoleCc] : [];
+
+      filledTemplate.cc = [...currentCcEmails, ...userRoleCc];
+
       validateEmailTemplate(filledTemplate);
       const params = createEmailParams(
         filledTemplate,
