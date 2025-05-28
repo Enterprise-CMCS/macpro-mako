2e035c5ada9fade4bc693412ac42da1227782d2f
Alex Youssefinia <ayoussefinia@fearless.tech>
2025-05-28 12:24:31 -0500

    try console logging file upload

--- react-app/src/utils/test-helpers/uploadFiles.ts
+++ react-app/src/utils/test-helpers/uploadFiles.ts
@@ -6,20 +6,21 @@ import { SchemaWithEnforcableProps } from "@/components";
 
 type ExtractAttachmentKeys<TSchema extends SchemaWithEnforcableProps> =
   TSchema extends z.ZodEffects<infer InnerSchema> // Handle ZodEffects
-    ? InnerSchema extends z.ZodObject<infer Shape> // Check if inner schema is a ZodObject
-      ? Shape["attachments"] extends z.ZodObject<infer AttachmentsShape> // Ensure attachments is a ZodObject
-        ? keyof AttachmentsShape // Extract the keys from attachments' shape
-        : never
-      : never
-    : TSchema extends z.ZodObject<infer Shape> // Handle direct ZodObject case
-      ? Shape["attachments"] extends z.ZodObject<infer AttachmentsShape> // Ensure attachments is a ZodObject
-        ? keyof AttachmentsShape // Extract the keys from attachments' shape
-        : never
-      : never;
+  ? InnerSchema extends z.ZodObject<infer Shape> // Check if inner schema is a ZodObject
+  ? Shape["attachments"] extends z.ZodObject<infer AttachmentsShape> // Ensure attachments is a ZodObject
+  ? keyof AttachmentsShape // Extract the keys from attachments' shape
+  : never
+  : never
+  : TSchema extends z.ZodObject<infer Shape> // Handle direct ZodObject case
+  ? Shape["attachments"] extends z.ZodObject<infer AttachmentsShape> // Ensure attachments is a ZodObject
+  ? keyof AttachmentsShape // Extract the keys from attachments' shape
+  : never
+  : never;
 
 export const uploadFiles = <TSchema extends SchemaWithEnforcableProps>() => {
   type AttachmentKey = ExtractAttachmentKeys<TSchema>;
 
+  console.log("file uploaded!")
   return async (attachmentKey: AttachmentKey) => {
     const EXAMPLE_FILE = new File(["¯\\_(ツ)_/¯"], "proposal.png", {
       type: "image/png",
