30766aa3e2539fc64020ec7fed224be16b14b2aa
Alex Youssefinia <ayoussefinia@fearless.tech>
2025-06-03 14:28:51 -0500

    try to resolve dependencies

--- lib/packages/shared-types/globals.d.ts
+++ lib/packages/shared-types/globals.d.ts
@@ -1,9 +1,10 @@
-declare const __IS_FRONTEND__: string;
-
 declare global {
     interface Window {
         dataLayer: any[];
         gtag: (...args: any[]) => void;
     }
+
+    const __IS_FRONTEND__: string;
 }
+
 export { };
