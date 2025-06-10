d5466316605c3d72ed5c9cc2368f327efc5ffefa
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2025-06-10 11:14:29 -0400

    fix(remove-email-temp): Remove email change temporaily
    This reverts commit cb3a65641870dc751807c62388a4085aa479159d.

--- lib/libs/email/content/withdrawRai/index.tsx
+++ lib/libs/email/content/withdrawRai/index.tsx
@@ -31,9 +31,11 @@ export const withdrawRai: AuthoritiesWithUserTypesTemplate = {
       variables: Events["WithdrawRai"] & CommonEmailVariables & { emails: EmailAddresses },
     ) => {
       return {
-        to: variables.allStateUsersEmails?.length
-          ? variables.allStateUsersEmails
-          : [`${variables.submitterName} <${variables.submitterEmail}>`],
+        to: [
+          ...variables.emails.cpocEmail,
+          ...variables.emails.srtEmails,
+          `${variables.submitterName} <${variables.submitterEmail}>`,
+        ],
         subject: `Withdraw Formal RAI Response for SPA Package ${variables.id}`,
         body: await render(<MedSpaStateEmail variables={variables} />),
       };
