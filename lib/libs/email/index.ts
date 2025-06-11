65194c54a1781ad75f85dec2eabcc873200fd33d
James Dinh <jdinh@collabralink.com>
2025-06-11 13:05:59 -0700

    Update index.ts

--- lib/libs/email/index.ts
+++ lib/libs/email/index.ts
@@ -98,9 +98,9 @@ export async function getEmailTemplates(
   }
   console.log("b4 event");
   const { event } = record;
-  console.log("b4 email temp", event, emailTemplate);
+  console.log("b4 email temp", event);
   const emailTemplate = emailTemplates[event as keyof EmailTemplates];
-  console.log("after email temp");
+  console.log("after email temp", emailTemplate);
 
   if (event in emailTemplates && hasAuthority(record)) {
     console.log("event in email");
