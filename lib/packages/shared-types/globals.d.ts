a6371db51fbd7dc97cbaf630ff3b8f26214b8ff7
Alex Youssefinia <ayoussefinia@fearless.tech>
2025-06-13 06:22:59 -0500

    remove comment

--- lib/packages/shared-types/globals.d.ts
+++ lib/packages/shared-types/globals.d.ts
@@ -1,11 +1,1 @@
 declare const __IS_FRONTEND__: string;
-// declare global {
-//     interface Window {
-//         dataLayer: any[];
-//         gtag: (...args: any[]) => void;
-//     }
-
-//     const __IS_FRONTEND__: string;
-// }
-
-// export { };
