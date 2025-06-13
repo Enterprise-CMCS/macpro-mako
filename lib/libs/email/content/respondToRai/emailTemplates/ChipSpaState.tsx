723f7ae61513ffb8ba7aad519d738098f70f784a
James Dinh <jdinh8124@gmail.com>
2025-06-13 09:22:52 -0700

    feat(add chip eligibility emails): Add in Chip eligibility emails and prefix Chip SPAs with the word Eligibility when necessary (#1575)
    * add details logic to emails
    
    * add eligibility chip prop for testing
    
    * add email logic
    
    * add email logic
    
    * trying transforms logic
    
    * Update new-chip-details-submission.ts
    
    * Update new-chip-details-submission.ts
    
    * testing
    
    * Update upload-subsequent-documents.ts
    
    * trying legacy response prop
    
    * hardcode test
    
    * Update upload-subsequent-documents.ts
    
    * Update index.tsx
    
    * console log
    
    * see email logic
    
    * hardcode
    
    * add all prefixes
    
    * cleanup
    
    * logs
    
    * add type
    
    * Update base-schema.ts
    
    * Update index.ts
    
    * Update index.ts
    
    * cleanup
    
    * Update processEmails.ts
    
    * Update email.ts
    
    * Update processEmails.ts
    
    * Update processEmails.ts
    
    * Update index.ts
    
    * add chip eligibility logic
    
    * working through logic
    
    * Update processEmails.ts
    
    * Update processEmails.ts
    
    * add logic from OS
    
    * cleanup
    
    * cleanup snapshots
    
    * remove logs
    
    * Update ChipSpaState.tsx
    
    * Update index.tsx
    
    * update tests
    
    * Update processEmails.ts
    
    * add snapshots of new email files
    
    * update snap again

--- lib/libs/email/content/respondToRai/emailTemplates/ChipSpaState.tsx
+++ lib/libs/email/content/respondToRai/emailTemplates/ChipSpaState.tsx
@@ -10,28 +10,32 @@ export const ChipSpaStateEmail = ({
   variables,
 }: {
   variables: Events["RespondToRai"] & CommonEmailVariables;
-}) => (
-  <BaseEmailTemplate
-    previewText={`CHIP SPA ${variables.id} RAI Response Submitted`}
-    heading="This response confirms you submitted a CHIP SPA RAI Response to CMS for review:"
-    applicationEndpointUrl={variables.applicationEndpointUrl}
-    footerContent={<BasicFooter />}
-  >
-    <PackageDetails
-      details={{
-        "State or Territory": variables.territory,
-        Name: variables.submitterName,
-        "Email Address": variables.submitterEmail,
-        "CHIP SPA Package ID": variables.id,
-        "90th Day Deadline": formatNinetyDaysDate(variables.timestamp),
-        Summary: variables.additionalInformation,
-      }}
-    />
-    <Text style={styles.text.description}>
-      {`This response confirms receipt of your CHIP State Plan Amendment (SPA or your response to a
+}) => {
+  const chipPrefix = `CHIP${variables.isChipEligibility ? " Eligibility" : ""}`;
+
+  return (
+    <BaseEmailTemplate
+      previewText={`${chipPrefix} ${variables.id} RAI Response Submitted`}
+      heading={`This response confirms you submitted a ${chipPrefix} SPA RAI Response to CMS for review:`}
+      applicationEndpointUrl={variables.applicationEndpointUrl}
+      footerContent={<BasicFooter />}
+    >
+      <PackageDetails
+        details={{
+          "State or Territory": variables.territory,
+          Name: variables.submitterName,
+          "Email Address": variables.submitterEmail,
+          [`${chipPrefix} SPA Package ID`]: variables.id,
+          "90th Day Deadline": formatNinetyDaysDate(variables.timestamp),
+          Summary: variables.additionalInformation,
+        }}
+      />
+      <Text style={styles.text.description}>
+        {`This response confirms receipt of your CHIP State Plan Amendment (SPA or your response to a
         SPA Request for Additional Information (RAI)). You can expect a formal response to your
         submittal to be issued within 90 days, before ${formatNinetyDaysDate(variables.timestamp)}.`}
-    </Text>
-    <FollowUpNotice isChip withDivider={false} />
-  </BaseEmailTemplate>
-);
+      </Text>
+      <FollowUpNotice isChip withDivider={false} />
+    </BaseEmailTemplate>
+  );
+};
