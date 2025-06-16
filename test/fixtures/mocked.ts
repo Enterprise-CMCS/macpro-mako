76ec138a62afb25377bf0651e3943e5abd86790e
Tiffany Forkner <tforkner@fearless.tech>
2025-06-16 16:35:10 -0400

    added JSDoc

--- test/fixtures/mocked.ts
+++ test/fixtures/mocked.ts
@@ -5,11 +5,25 @@ import {
   toggleGetLDEvalStreamHandler,
   toggleGetLDEvalxHandler,
 } from "mocks";
-import { http } from "msw";
+import { http, RequestHandler } from "msw";
 import type { MockServiceWorker } from "playwright-msw";
 import { createWorkerFixture } from "playwright-msw";
 
-export const overrideFlags = (toggleFlags?: FlagToggles) => [
+/**
+ * Override the default LaunchDarkly flag values.
+ *
+ * @param toggleFlags {FlagToggles} The object of flags to update
+ * @returns {RequestHandler[]} Returns an array of overridden LaunchDarkly handlers
+ * @example
+ * test("a test that is dependant on a flag value", async ({
+ *    page,
+ *    worker // must import worker to use the override
+ * }) => {
+ *    worker.use(...overrideFlags({ "flag-name": "VALUE" })); // Important: must use the spread operator here because `worker.use` takes multiple handlers, not an array of handlers
+ *    // test code
+ * });
+ */
+export const overrideFlags = (toggleFlags?: FlagToggles): RequestHandler[] => [
   toggleGetLDEvalStreamHandler(toggleFlags),
   toggleGetLDEvalxHandler(toggleFlags),
 ];
