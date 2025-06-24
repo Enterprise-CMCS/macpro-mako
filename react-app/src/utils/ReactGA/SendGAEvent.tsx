4217f402745965354ec2113fce0d4d45c51ab941
Alex Youssefinia <ayoussefinia@fearless.tech>
2025-06-23 20:23:25 -0500

    check if window.gtag is function

--- react-app/src/utils/ReactGA/SendGAEvent.tsx
+++ react-app/src/utils/ReactGA/SendGAEvent.tsx
@@ -1,7 +1,7 @@
 export const sendGAEvent = (eventName, eventObject) => { 
   if (eventObject && typeof window.gtag == "function" ) {
     window.gtag("event", eventName, eventObject)
-  } else {
+  } else if (typeof window.gtag == "function") {
     window.gtag("event", eventName);
   }
 }
