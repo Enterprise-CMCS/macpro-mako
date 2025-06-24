57853d32c2f87ea2d04235a0a8e9d61a209f65ba
ranyem <rmclendon@fearless.tech>
2025-06-24 10:40:16 -0400

    feat(ui): User Management" tab missing on the grey bottom footer

--- react-app/src/components/Footer/index.tsx
+++ react-app/src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router";
 import { isStateUser } from "shared-utils";
 
-import { useGetUser } from "@/api";
+import { useGetUser, useGetUserDetails } from "@/api";
 import { Alert, Button } from "@/components";
 import { useFeatureFlag } from "@/hooks/useFeatureFlag";
 
@@ -25,7 +25,12 @@ type MedSpaFooterProps = {
 
 export const Footer = ({ email, address, showNavLinks }: Props) => {
   const shouldShowNavLinks = showNavLinks ?? true;
-  const { data: user } = useGetUser();
+  const { isLoading: userLoading, data: user } = useGetUser();
+  const { data: userDetailsData, isLoading: userDetailsLoading } = useGetUserDetails();
+  const showUserManagement = useMemo(() => {
+    const role = userDetailsData?.role;
+    return ["systemadmin", "statesystemadmin", "cmsroleapprover", "helpdesk"].includes(role);
+  }, [userDetailsData]);
   const isStateHomepage = useFeatureFlag("STATE_HOMEPAGE_FLAG");
 
   return (
@@ -40,6 +45,11 @@ export const Footer = ({ email, address, showNavLinks }: Props) => {
               <a href="/dashboard" className="underline font-bold">
                 <p>Dashboard</p>
               </a>
+              {showUserManagement && (
+                <a href="/usermanagement" className="underline font-bold">
+                  <p>User Management</p>
+                </a>
+              )}
               {isStateHomepage && isStateUser(user.user) && (
                 <a href="/latestupdates" className="underline font-bold">
                   <p>Latest Updates</p>
