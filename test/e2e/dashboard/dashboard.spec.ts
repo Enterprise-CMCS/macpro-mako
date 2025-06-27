3f125e75d8511fa9271c27886c65a1f80ea351e7
Jonathan Schaffer <jschaffer@gswell.com>
2025-06-27 13:18:50 -0400

    fix: revert back to mocked

--- test/e2e/dashboard/dashboard.spec.ts
+++ test/e2e/dashboard/dashboard.spec.ts
@@ -1,6 +1,6 @@
 // import { expect, test } from "@/fixtures/mocked"; // add expect when writing assertions
-// import { expect, test } from "@/fixtures/mocked";
-import { expect, test } from "@playwright/test";
+import { expect, test } from "@/fixtures/mocked";
+// import { expect, test } from "@playwright/test";
 
 // TODO
 test.describe("Dashboard page", { tag: ["@dashboard"] }, () => {
