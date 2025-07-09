7fda8fba91552460f2fa745cd3acb1097fbd9f65
Tiffany Forkner <tforkner@fearless.tech>
2025-07-09 14:51:03 -0400

    WIP

--- react-app/src/components/Opensearch/main/Filtering/Drawer/Filterable/Select.tsx
+++ react-app/src/components/Opensearch/main/Filtering/Drawer/Filterable/Select.tsx
@@ -1,5 +1,18 @@
+import * as SelectPrimitive from "@radix-ui/react-select";
 import { type FC } from "react";
-import Select from "react-select";
+import SelectRS from "react-select";
+
+const Select = SelectPrimitive.Root;
+
+const SelectTrigger = SelectPrimitive.Trigger;
+
+const SelectPortal = SelectPrimitive.Portal;
+
+const SelectContent = SelectPrimitive.Content;
+
+const SelectViewport = SelectPrimitive.Viewport;
+
+const SelectItem = SelectPrimitive.Item;
 
 export interface Option {
   readonly value: string;
@@ -22,13 +35,30 @@ export const FilterableSelect: FC<{
     return selected[0].label;
   };
   return (
-    <Select<any, any>
-      isMulti
-      value={value.map((selected) => ({ value: selected, label: getLabel(selected) }))}
-      onChange={(value) => onChange(value.map((selected: any) => selected.value))}
-      options={options}
-      closeMenuOnSelect={false}
-      placeholder={placeholder}
-    />
+    // <SelectRS<any, any>
+    //   isMulti
+    //   value={value.map((selected) => ({ value: selected, label: getLabel(selected) }))}
+    //   onChange={(value) => onChange(value.map((selected: any) => selected.value))}
+    //   options={options}
+    //   closeMenuOnSelect={false}
+    //   placeholder={placeholder}
+    //   autoFocus
+    //   tabSelectsValue={false}
+    // />
+
+    <Select.Root>
+      <Select.Trigger>…</Select.Trigger>
+      <Select.Portal>
+        <Select.Content>
+          <Select.Viewport>
+            <Select.Item className="SelectItem" disabled>
+              …
+            </Select.Item>
+            <Select.Item>…</Select.Item>
+            <Select.Item>…</Select.Item>
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
   );
 };
