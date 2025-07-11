b4756aa5163bc40216eb9bef327baef9e209cb7e
Tiffany Forkner <tforkner@fearless.tech>
2025-07-11 08:59:21 -0400

    updated FilterableSelect to turn on autoFocus, turn off tabSelectsValue, and add an aria-label

--- react-app/src/components/Opensearch/main/Filtering/Drawer/Filterable/Select.tsx
+++ react-app/src/components/Opensearch/main/Filtering/Drawer/Filterable/Select.tsx
@@ -1,18 +1,5 @@
-import * as SelectPrimitive from "@radix-ui/react-select";
 import { type FC } from "react";
-import SelectRS from "react-select";
-
-const Select = SelectPrimitive.Root;
-
-const SelectTrigger = SelectPrimitive.Trigger;
-
-const SelectPortal = SelectPrimitive.Portal;
-
-const SelectContent = SelectPrimitive.Content;
-
-const SelectViewport = SelectPrimitive.Viewport;
-
-const SelectItem = SelectPrimitive.Item;
+import Select from "react-select";
 
 export interface Option {
   readonly value: string;
@@ -28,37 +15,24 @@ export const FilterableSelect: FC<{
   placeholder?: string;
   onChange: (values: string[]) => void;
   selectedDisplay?: keyof Option;
-}> = ({ options, value, placeholder, onChange, selectedDisplay = "value" }) => {
+  ariaLabel?: string;
+}> = ({ options, value, placeholder, onChange, selectedDisplay = "value", ariaLabel }) => {
   const getLabel = (value) => {
     if (selectedDisplay !== "label") return value;
     const selected = options.filter((option) => option.value === value);
     return selected[0].label;
   };
   return (
-    // <SelectRS<any, any>
-    //   isMulti
-    //   value={value.map((selected) => ({ value: selected, label: getLabel(selected) }))}
-    //   onChange={(value) => onChange(value.map((selected: any) => selected.value))}
-    //   options={options}
-    //   closeMenuOnSelect={false}
-    //   placeholder={placeholder}
-    //   autoFocus
-    //   tabSelectsValue={false}
-    // />
-
-    <Select.Root>
-      <Select.Trigger>…</Select.Trigger>
-      <Select.Portal>
-        <Select.Content>
-          <Select.Viewport>
-            <Select.Item className="SelectItem" disabled>
-              …
-            </Select.Item>
-            <Select.Item>…</Select.Item>
-            <Select.Item>…</Select.Item>
-          </Select.Viewport>
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
+    <Select<any, any>
+      isMulti
+      value={value.map((selected) => ({ value: selected, label: getLabel(selected) }))}
+      onChange={(value) => onChange(value.map((selected: any) => selected.value))}
+      options={options}
+      closeMenuOnSelect={false}
+      placeholder={placeholder}
+      autoFocus
+      tabSelectsValue={false}
+      aria-label={ariaLabel}
+    />
   );
 };
