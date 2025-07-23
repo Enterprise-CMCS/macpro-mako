d5bd22e022e99864003f551fa8b600576f137e90
Tiffany Forkner <tforkner@fearless.tech>
2025-07-23 09:17:58 -0400

    fixed tabbing

--- react-app/src/components/Opensearch/main/Filtering/Drawer/Filterable/Select.tsx
+++ react-app/src/components/Opensearch/main/Filtering/Drawer/Filterable/Select.tsx
@@ -1,5 +1,4 @@
-import { type FC, useEffect, useRef } from "react";
-import Select, { components } from "react-select";
+import Select from "react-select";
 
 export interface Option {
   readonly value: string;
@@ -9,46 +8,45 @@ export interface Option {
   readonly isDisabled?: boolean;
 }
 
-export const FilterableSelect: FC<{
+export type FilterableSelectProps = {
   options: Option[];
   value: string[];
   placeholder?: string;
   onChange: (values: string[]) => void;
   selectedDisplay?: keyof Option;
   ariaLabel?: string;
-}> = ({ options, value, placeholder, onChange, selectedDisplay = "value", ariaLabel }) => {
-  const ref = useRef();
+};
 
-  useEffect(() => {
-    console.log({ ref });
-  }, [ref]);
-
+export const FilterableSelect = ({
+  options,
+  value,
+  placeholder,
+  onChange,
+  selectedDisplay,
+  ariaLabel,
+}: FilterableSelectProps) => {
   const getLabel = (value) => {
     if (selectedDisplay !== "label") return value;
     const selected = options.filter((option: Option) => option.value === value) as Option[];
     return selected[0].label;
   };
 
-  const Input = (props) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { tabIndex, ...rest } = props;
-    return <components.Input {...rest} />;
-  };
-
   return (
-    <div ref={ref}>
-      <Select<any, any>
-        isMulti
-        value={value.map((selected) => ({ value: selected, label: getLabel(selected) }))}
-        onChange={(value) => onChange(value.map((selected: any) => selected.value))}
-        options={options}
-        closeMenuOnSelect={false}
-        placeholder={placeholder}
-        // autoFocus
-        tabSelectsValue={false}
-        aria-label={ariaLabel}
-        components={{ Input }}
-      />
-    </div>
+    <Select<any, any>
+      isMulti
+      value={value.map((selected) => ({ value: selected, label: getLabel(selected) }))}
+      onChange={(value) => onChange(value.map((selected: any) => selected.value))}
+      options={options}
+      closeMenuOnSelect={false}
+      blurInputOnSelect={false}
+      placeholder={placeholder}
+      autoFocus
+      tabSelectsValue={false}
+      aria-label={ariaLabel}
+      onBlur={(event) => {
+        const header = document.getElementById(event.relatedTarget.id);
+        header?.focus();
+      }}
+    />
   );
 };
