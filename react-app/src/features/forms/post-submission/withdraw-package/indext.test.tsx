f14e8d34612461f215d06b403011c223d944633e
Asharon Baltazar <asharonbaltazar@outlook.com>
2025-08-06 10:59:27 -0400

    chore: fix snapshot test

--- react-app/src/features/forms/post-submission/withdraw-package/indext.test.tsx
+++ react-app/src/features/forms/post-submission/withdraw-package/indext.test.tsx
@@ -9,10 +9,11 @@ vi.mock("react-router", async () => ({
   ...(await vi.importActual<Record<string, unknown>>("react-router")),
   useParams: vi.fn().mockReturnValue({ authority: "1915(c)", id: CAPITATED_AMEND_ITEM_ID }),
 }));
+
 describe("WithdrawPackageAction components", () => {
   it("renders WithdrawPackageActionWaiver correctly", async () => {
-    const container = renderFormWithPackageSectionAsync(<WithdrawPackageActionWaiver />);
+    const { asFragment } = await renderFormWithPackageSectionAsync(<WithdrawPackageActionWaiver />);
 
-    expect(await container).toMatchSnapshot();
+    expect(asFragment).toMatchSnapshot();
   });
 });
