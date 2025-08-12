947c80d65e06e5216194dc20de11dead5f2ae659
Tiffany Forkner <tforkner@fearless.tech>
2025-08-12 09:57:45 -0400

    tests running in runner

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-// import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import { cpus } from "os";
 import { join } from "path";
 import { configDefaults, defineConfig } from "vitest/config";
@@ -37,30 +37,30 @@ export default defineConfig({
           environment: "jsdom",
         },
       },
-      // {
-      //   extends: "./react-app/vite.config.ts",
-      //   plugins: [
-      //     storybookTest({
-      //       configDir: "./react-app/.storybook",
-      //       // This should match your package.json script to run Storybook
-      //       // The --ci flag will skip prompts and not open a browser
-      //       storybookScript: "run storybook --ci",
-      //     }),
-      //   ],
-      //   test: {
-      //     name: "storybook",
-      //     root: "./react-app",
-      //     exclude: ["**/*.test.{ts,tsx}", "**/*.mdx"],
-      //     browser: {
-      //       enabled: true,
-      //       provider: "playwright",
-      //       headless: true,
-      //       // https://vitest.dev/guide/browser/playwright
-      //       instances: [{ browser: "chromium" }],
-      //     },
-      //     setupFiles: "./.storybook/vitest.setup.ts",
-      //   },
-      // },
+      {
+        extends: "./react-app/vite.config.ts",
+        plugins: [
+          storybookTest({
+            configDir: "./.storybook",
+            // This should match your package.json script to run Storybook
+            // The --ci flag will skip prompts and not open a browser
+            storybookScript: "run storybook --ci",
+          }),
+        ],
+        test: {
+          name: "storybook",
+          root: "./react-app",
+          exclude: ["**/*.test.{ts,tsx}", "**/*.mdx"],
+          browser: {
+            enabled: true,
+            provider: "playwright",
+            headless: true,
+            // https://vitest.dev/guide/browser/playwright
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: "./.storybook/vitest.setup.ts",
+        },
+      },
     ],
     server: {
       deps: {
