8b8911654370c933d64c476c073ba1803365141a
Tiffany Forkner <tforkner@fearless.tech>
2025-08-15 12:38:10 -0400

    added note about updating the firefox file download behavior to the FAQ

--- react-app/src/features/faq/content/oneMACFAQContent.tsx
+++ react-app/src/features/faq/content/oneMACFAQContent.tsx
@@ -82,8 +82,18 @@ export const oneMACFAQContent: FAQContent[] = [
         question: "What browsers can I use to access the system?",
         answerJSX: (
           <p>
-            The submission portal works best on Google Chrome (Version 91.0.4472.77 or later),
-            Firefox (Version 89.0 or later).
+            The submission portal works best on Google Chrome (Version 91.0.4472.77 or later) or
+            Firefox (Version 89.0 or later). Firefox users may need to adjust their browser settings
+            to enable PDFs to download automatically —{" "}
+            <a
+              href="https://support.mozilla.org/en-US/kb/change-firefox-behavior-when-open-file"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="text-blue-800 underline hover:no-underline"
+            >
+              learn how
+            </a>
+            .
           </p>
         ),
       },
