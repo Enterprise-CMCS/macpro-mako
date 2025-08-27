077d6b4caba0d90765ec45c71e431b62d8a0c6b0
Tiffany Forkner <tforkner@fearless.tech>
2025-08-27 08:58:41 -0400

    fixed issue with user profile stories

--- react-app/.storybook/decorators.tsx
+++ react-app/.storybook/decorators.tsx
@@ -1,5 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { setMockUsername, TEST_REVIEWER_USERNAME, TEST_STATE_SUBMITTER_USERNAME } from "mocks";
+import {
+  CMS_ROLE_APPROVER_USERNAME,
+  DEFAULT_CMS_USER_EMAIL,
+  HELP_DESK_USER_USERNAME,
+  OS_STATE_SYSTEM_ADMIN_USERNAME,
+  setMockUsername,
+  SYSTEM_ADMIN_USERNAME,
+  TEST_REVIEWER_USERNAME,
+  TEST_STATE_SUBMITTER_USERNAME,
+} from "mocks";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,13 +22,6 @@ export const withQueryClient = (Story) => (
   <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
 );
 
-export const asUser = (Story, { parameters }) => {
-  if (parameters.username === null || parameters.username) {
-    setMockUsername(parameters.username);
-  }
-  return Story();
-};
-
 export const asLoggedOut = (Story) => {
   setMockUsername(null);
   return Story();
@@ -30,7 +32,32 @@ export const asStateSubmitter = (Story) => {
   return Story();
 };
 
+export const asStateSystemAdmin = (Story) => {
+  setMockUsername(OS_STATE_SYSTEM_ADMIN_USERNAME);
+  return Story();
+};
+
+export const asDefaultCmsUser = (Story) => {
+  setMockUsername(DEFAULT_CMS_USER_EMAIL);
+  return Story();
+};
+
 export const asCmsReviewer = (Story) => {
   setMockUsername(TEST_REVIEWER_USERNAME);
+  return Story();
+};
+
+export const asCmsRoleApprover = (Story) => {
+  setMockUsername(CMS_ROLE_APPROVER_USERNAME);
+  return Story();
+};
+
+export const asHelpDesk = (Story) => {
+  setMockUsername(HELP_DESK_USER_USERNAME);
+  return Story();
+};
+
+export const asSystemAdmin = (Story) => {
+  setMockUsername(SYSTEM_ADMIN_USERNAME);
   return Story();
 };
--- react-app/src/features/profile/user/UserProfile.stories.tsx
+++ react-app/src/features/profile/user/UserProfile.stories.tsx
@@ -1,14 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import LZ from "lz-string";
-import {
-  CMS_ROLE_APPROVER_USERNAME,
-  DEFAULT_CMS_USER_EMAIL,
-  OS_STATE_SYSTEM_ADMIN_USERNAME,
-  TEST_STATE_SUBMITTER_EMAIL,
-} from "mocks";
+import { DEFAULT_CMS_USER_EMAIL, TEST_STATE_SUBMITTER_EMAIL } from "mocks";
 import { reactRouterParameters, withRouter } from "storybook-addon-remix-react-router";
 
-import { asUser } from "../../../../.storybook/decorators";
+import { asCmsRoleApprover, asStateSystemAdmin } from "../../../../.storybook/decorators";
 import { UserProfile, userProfileLoader } from "./index";
 
 const getProfileId = (userEmail: string) =>
@@ -32,25 +27,23 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const StateUser: Story = {
-  decorators: [asUser],
+  decorators: [asStateSystemAdmin],
   parameters: {
     reactRouter: reactRouterParameters({
       location: {
         pathParams: { profileId: getProfileId(TEST_STATE_SUBMITTER_EMAIL) },
       },
     }),
-    username: OS_STATE_SYSTEM_ADMIN_USERNAME,
   },
 };
 
 export const CmsUser: Story = {
-  decorators: [asUser],
+  decorators: [asCmsRoleApprover],
   parameters: {
     reactRouter: reactRouterParameters({
       location: {
         pathParams: { profileId: getProfileId(DEFAULT_CMS_USER_EMAIL) },
       },
     }),
-    username: CMS_ROLE_APPROVER_USERNAME,
   },
 };
