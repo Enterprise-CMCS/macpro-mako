024af2856ae7189163fa57e21fb18d1b01e97dfa
Tiffany Forkner <thetif@gmail.com>
2025-09-02 12:07:11 -0400

    updating processEmail to not always check for item, moved adjusting timestamp to the processAndSendEmails where it's already getting the item and using the retry (#1734)

--- lib/lambda/utils.ts
+++ lib/lambda/utils.ts
@@ -11,9 +11,9 @@ export type ErrorResponse = {
 };
 
 interface ParsedKafkaEvent {
-  id: string;
   event?: string;
   authority?: string;
+  timestamp: number;
 }
 
 export const handleOpensearchError = (error: unknown): ErrorResponse => {
@@ -44,18 +44,14 @@ export const handleOpensearchError = (error: unknown): ErrorResponse => {
  * @param timestamp the timestamp of the call to the handler
  * @returns the original timestamp, unless the record is a Respond to RAI event for a CHIP SPA.
  */
-export const adjustTimestamp = (
-  parsedRecord: ParsedKafkaEvent,
-  item: ItemResult,
-  timestamp: number,
-): number => {
+export const adjustTimestamp = (parsedRecord: ParsedKafkaEvent, item: ItemResult): number => {
   if (
     !(
       parsedRecord?.event === "respond-to-rai" &&
       parsedRecord?.authority?.toUpperCase() === "CHIP SPA"
     )
   ) {
-    return timestamp;
+    return parsedRecord.timestamp;
   }
 
   const submissionDate = item?._source.submissionDate || "";
@@ -63,7 +59,7 @@ export const adjustTimestamp = (
 
   if (!submissionDate || !raiRequestedDate) {
     console.error("error parsing os record");
-    return timestamp;
+    return parsedRecord.timestamp;
   }
 
   // length of time from when the RAI was requested until now
