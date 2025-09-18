3b650a2213fece01591eb1ea9da3d11c35d16bb4
Tiffany Forkner <thetif@gmail.com>
2025-09-18 13:07:00 -0400

    feat(ui): Help Desk accessibility (#1758)
    * added aria-labels to help desk email and phone number

--- react-app/src/features/profile/me/index.tsx
+++ react-app/src/features/profile/me/index.tsx
@@ -295,15 +295,17 @@ export const MyProfile = () => {
                     </li>
                   )}
                   {isNewUserRoleDisplay && !hideAddRoleButton ? (
-                    <Button
-                      className="w-full border-dashed p-10 text-black font-normal"
-                      variant="outline"
-                      onClick={() =>
-                        isStateUser(user.user) ? navigate("/signup/state") : navigate("/signup")
-                      }
-                    >
-                      Add another user role <PlusIcon className="ml-3" />
-                    </Button>
+                    <li>
+                      <Button
+                        className="w-full border-dashed p-10 text-black font-normal"
+                        variant="outline"
+                        onClick={() =>
+                          isStateUser(user.user) ? navigate("/signup/state") : navigate("/signup")
+                        }
+                      >
+                        Add another user role <PlusIcon className="ml-3" />
+                      </Button>
+                    </li>
                   ) : (
                     <StateAccessControls />
                   )}
