391677288c70322693a387092f34afce0c14c67a
James Dinh <jdinh8124@gmail.com>
2025-12-08 09:13:06 -0800

    Update upload.tsx

--- react-app/src/components/Inputs/upload.tsx
+++ react-app/src/components/Inputs/upload.tsx
@@ -215,7 +215,7 @@ export const Upload = ({ maxFiles, files, setFiles, dataTestId, type }: UploadPr
             id={`upload-${uniqueId}`}
             {...getInputProps()}
             data-testid={`${dataTestId}-upload`}
-            className="hidden"
+            aria-hidden="true"
           />
         </div>
       )}
